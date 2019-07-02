f9d5f887cfe05b5168adeb2808ef395d70259ffe
Yaron Haviv <yaronh@iguaz.io>
2019-07-02 08:42:29 +0300

    add license

--- mlrun/__main__.py
+++ mlrun/__main__.py
@@ -1,17 +1,24 @@
-from os import path
-from pprint import pprint
+# Copyright 2018 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+from os import path
 import click
-import json
-import os
 from ast import literal_eval
 import getpass
-from tempfile import mktemp
-
 import yaml
 
 from .runtimes import run_start
-from .secrets import SecretsStore
 from .utils import run_keys
 
 @click.group()

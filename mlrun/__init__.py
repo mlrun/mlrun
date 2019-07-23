218dd97604460e14c0ef4793516e1c4302614742
Yaron Haviv <yaronh@iguaz.io>
2019-07-24 01:37:27 +0300

    update ver tag

--- mlrun/__init__.py
+++ mlrun/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.0.3'
+__version__ = '0.1.0'
 
 from .run import get_or_create_ctx, run_start, mlrun_op
 from .rundb import get_run_db

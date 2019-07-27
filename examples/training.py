b16fb84c4bfd8cd67fe6b2c8b89c4f64136b95a1
Yaron Haviv <yaronh@iguaz.io>
2019-07-27 03:58:30 +0300

    edit examples

--- examples/training.py
+++ examples/training.py
@@ -1,10 +1,11 @@
 from mlrun import get_or_create_ctx
 from mlrun.artifacts import ChartArtifact, TableArtifact
 
-def my_job():
+
+def training_job():
     # load MLRUN runtime context (will be set by the runtime framework e.g. KubeFlow)
-    context = get_or_create_ctx('mytask')
-    
+    context = get_or_create_ctx('train')
+
     # get parameters from the runtime context (or use defaults)
     p1 = context.get_param('p1', 1)
     p2 = context.get_param('p2', 'a-string')
@@ -14,7 +15,7 @@ def my_job():
     print(f'Params: p1={p1}, p2={p2}')
     print('accesskey = {}'.format(context.get_secret('ACCESS_KEY')))
     print('file\n{}\n'.format(context.get_object('infile.txt').get()))
-    
+
     # RUN some useful code e.g. ML training, data prep, etc.
 
     # log scalar result values (job result metrics)
@@ -22,18 +23,19 @@ def my_job():
     context.log_output('loss', p1 * 3)
 
     # log various types of artifacts (file, web page, table), will be versioned and visible in the UI
-    context.log_artifact('model.txt', body=b'abc is 123')
+    context.log_artifact('model.txt', body=b'abc is 123', labels={'framework': 'xgboost'})
     context.log_artifact('results.html', body=b'<b> Some HTML <b>', viewer='web-app')
     context.log_artifact(TableArtifact('dataset.csv', '1,2,3\n4,5,6\n',
-                                        viewer='table', header=['A', 'B', 'C']))
+                                       viewer='table', header=['A', 'B', 'C']))
 
     # create a chart output (will show in the pipelines UI)
     chart = ChartArtifact('chart.html')
-    chart.header = ['Epoch','Accuracy', 'Loss']
-    for i in range(1,8):
-        chart.add_row([i, i/20+0.75, 0.30-i/20])
+    chart.labels = {'type': 'roc'}
+    chart.header = ['Epoch', 'Accuracy', 'Loss']
+    for i in range(1, 8):
+        chart.add_row([i, i / 20 + 0.75, 0.30 - i / 20])
     context.log_artifact(chart)
 
 
 if __name__ == "__main__":
-    my_job()
+    training_job()

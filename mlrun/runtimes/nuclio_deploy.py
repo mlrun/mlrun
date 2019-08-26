516b869f3f8f710b1b063b6cfa8ab5f8b598844f
Yaron Haviv <yaronh@iguaz.io>
2019-08-27 02:01:07 +0300

    align nuclio runner

--- mlrun/runtimes/nuclio_deploy.py
+++ mlrun/runtimes/nuclio_deploy.py
@@ -14,41 +14,46 @@
 
 from copy import deepcopy
 from ..model import RunObject
-from ..utils import update_in, logger
+from ..utils import update_in, logger, get_in
 from .base import MLRuntime, RunError
 
 
 class NuclioDeployRuntime(MLRuntime):
     kind = 'nuclio'
+    def __init__(self, run: RunObject):
+        super().__init__(run)
+        self.dashboard = ''
+
+    def set_runtime(self, runtime):
+        self.dashboard = runtime.pop("command", '')
+        self.runtime = runtime
 
     def _run(self, runobj: RunObject):
 
         from nuclio.deploy import deploy_config
 
-        runtime = runobj.spec.runtime
-        shortspec = deepcopy(runobj)
-        shortspec.spec.runtime = None
-        extra_env = [{'name': 'MLRUN_EXEC_CONFIG', 'value': shortspec.to_json()}]
+        runtime = self.runtime
+        extra_env = [{'name': 'MLRUN_EXEC_CONFIG', 'value': runobj.to_json()}]
         if self.rundb:
             extra_env.append({'name': 'MLRUN_META_DBPATH', 'value': self.rundb})
 
-        update_in(runtime.spec, 'env', extra_env, append=True)
+        update_in(runtime, 'spec.env', extra_env, append=True)
 
         uid = runobj.metadata.uid
-        update_in(runtime.metadata.labels, 'mlrun/class', self.kind)
-        update_in(runtime.metadata.labels, 'mlrun/uid', uid)
+        update_in(runtime, 'metadata.labels.mlrun/class', self.kind)
+        update_in(runtime, 'metadata.labels.mlrun/uid', uid)
         if runobj.metadata.name:
+            update_in(runtime, 'metadata.name', runobj.metadata.name)
             runtime.metadata.name = runobj.metadata.name
-        name = runtime.metadata.name
+        name = get_in(runtime, 'metadata.name', '')
         project = runobj.metadata.project
 
         if not project or not name:
             raise RunError('name and a project must be specified in the run to deploy this function')
 
-        config = runtime.to_dict(['kind', 'apiVersion', 'spec', 'metadata'])
-        print(config)
+        print(runtime)
 
-        addr = deploy_config(config, runtime.command, name, project, create_new=True)
+        addr = deploy_config(runtime, self.dashboard, name, project, create_new=True)
         logger.info('function address is {}'.format(addr))
         return None
 

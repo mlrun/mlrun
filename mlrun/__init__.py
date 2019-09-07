6ecdeb5131850010a7d6b81c346c4f5026fc3e20
Yaron Haviv <yaronh@iguaz.io>
2019-09-08 02:50:03 +0300

    edit examples

--- mlrun/__init__.py
+++ mlrun/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.1.9'
+__version__ = '0.2.0'
 
 from .run import get_or_create_ctx, new_runner, mlrun_op
 from .db import get_run_db

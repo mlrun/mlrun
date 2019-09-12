49fb9a87720e545411269a26cda58a0bf4555267
Miki Tebeka <miki.tebeka@gmail.com>
2019-09-12 11:03:08 +0300

    Fix tests - check for dask and right iteration result file

--- tests/test_dask.py
+++ tests/test_dask.py
@@ -1,7 +1,17 @@
 from pprint import pprint
 
-from conftest import has_secrets, out_path, rundb_path, tag_test
-from mlrun import get_or_create_ctx, new_function, RunObject, NewRun
+from conftest import rundb_path, tag_test
+from mlrun import new_function, NewRun
+import pytest
+
+
+has_dask = False
+try:
+    import dask  # noqa
+    has_dask = True
+except ImportError:
+    pass
+
 
 def my_func(context, p1=1, p2='a-string'):
     print(f'Run: {context.name} (uid={context.uid})')
@@ -13,16 +23,17 @@ def my_func(context, p1=1, p2='a-string'):
     return 'tst-me-{}'.format(context.iteration)
 
 
-
+@pytest.mark.skipif(not has_dask, reason='missing dask')
 def test_dask_local():
     spec = tag_test(NewRun(params={'p1': 3, 'p2': 'vv'}), 'test_dask_local')
-    run = new_function(command='dask://', rundb=rundb_path).run(spec, handler=my_func)
+    run = new_function(command='dask://', rundb=rundb_path).run(
+        spec, handler=my_func)
     pprint(run.to_dict())
 
 
+@pytest.mark.skipif(not has_dask, reason='missing dask')
 def test_dask_local_hyper():
     task = NewRun().with_hyper_params({'p1': [5, 2, 3]}, 'max.accuracy')
     spec = tag_test(task, 'test_dask_local_hyper')
     run = new_function(command='dask://').run(spec, handler=my_func)
     pprint(run.to_dict())
-

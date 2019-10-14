a43a2b2724428f6f9a1ae035466efa4e09fd0ffc
Yaron Haviv <yaronh@iguaz.io>
2019-10-15 01:25:37 +0300

    function import/export

--- mlrun/__init__.py
+++ mlrun/__init__.py
@@ -14,7 +14,7 @@
 
 __version__ = '0.2.7'
 
-from .run import get_or_create_ctx, new_function, code_to_function
+from .run import get_or_create_ctx, new_function, code_to_function, import_function
 from .db import get_run_db
 from .model import RunTemplate, NewRun, NewTask, RunObject
 from .kfpops import mlrun_op

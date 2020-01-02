df8705c0f0c7e3b75bbb1e1d68842a919e6ba89e
Miki Tebeka <miki@353solutions.com>
2020-01-02 16:12:14 +0200

    relative

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
 import sys
-sys.path.insert(0, '/home/miki/work/iguazio/mlrun')
+sys.path.insert(0, '../mlrun')
 
 
 # -- Project information -----------------------------------------------------

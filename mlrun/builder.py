6757f257846b8171187aa881280bbd8ec401bca7
Yaron Haviv <yaronh@iguaz.io>
2020-01-10 00:46:54 +0200

    fixes to dask and builder

--- mlrun/builder.py
+++ mlrun/builder.py
@@ -26,17 +26,20 @@ from .config import config
 
 def make_dockerfile(base_image,
                     commands=None, src_dir=None,
-                    requirements=None):
+                    requirements=None,
+                    workdir='/run',
+                    extra=''):
     dock = 'FROM {}\n'.format(base_image)
-    dock += 'WORKDIR /run\n'
     if src_dir:
-        dock += 'ADD {} /run\n'.format(src_dir)
+        dock += 'WORKDIR {}\n'.format(workdir)
+        dock += 'ADD {} {}\n'.format(src_dir, workdir)
+        dock += 'ENV PYTHONPATH {}'.format(workdir)
     if requirements:
         dock += 'RUN pip install -r {}\n'.format(requirements)
     if commands:
         dock += ''.join(['RUN {}\n'.format(b) for b in commands])
-    dock += 'ENV PYTHONPATH /run'
-
+    if extra:
+        dock += extra
     print(dock)
     return dock
 
@@ -118,14 +121,22 @@ def build_image(dest,
                 registry=None,
                 interactive=True,
                 name='',
+                extra=None,
                 verbose=False):
 
     if registry:
         dest = '{}/{}'.format(registry, dest)
-    elif not secret_name and 'DOCKER_REGISTRY_SERVICE_HOST' in environ:
-        if dest.startswith('.'):
-            dest = dest[1:]
-        dest = '{}:5000/{}'.format(environ.get('DOCKER_REGISTRY_SERVICE_HOST'), dest)
+    elif dest.startswith('.'):
+        dest = dest[1:]
+        if 'DOCKER_REGISTRY_PORT' in environ:
+            registry = urlparse(environ.get('DOCKER_REGISTRY_PORT')).netloc
+        else:
+            registry = environ.get('DEFAULT_DOCKER_REGISTRY')
+            secret_name = secret_name or environ.get('DEFAULT_DOCKER_SECRET')
+        if not registry:
+            raise ValueError('local docker registry is not defined, set '
+                             'DEFAULT_DOCKER_REGISTRY/SECRET env vars')
+        dest = '{}/{}'.format(registry, dest)
 
     if isinstance(requirements, list):
         requirements_list = requirements
@@ -166,7 +177,8 @@ def build_image(dest,
 
     dock = make_dockerfile(base_image, commands,
                            src_dir=src_dir,
-                           requirements=requirements_path)
+                           requirements=requirements_path,
+                           extra=extra)
 
     kpod = make_kaniko_pod(context, dest, dockertext=dock,
                            inline_code=inline_code, inline_path=inline_path,
@@ -198,6 +210,12 @@ def build_runtime(runtime, with_mlrun, interactive=False):
     if not build.image:
         raise ValueError('build spec must have a target image, set build.image = <target image>')
     logger.info(f'building image ({build.image})')
+
+    if runtime.kind == 'dask':
+        extra = 'ENTRYPOINT ["tini", "-g", "--", "/usr/bin/prepare.sh"]'
+    else:
+        extra = None
+
     name = normalize_name('mlrun-build-{}'.format(runtime.metadata.name))
     status = build_image(build.image,
                          base_image=build.base_image or 'python:3.6-jessie',
@@ -208,6 +226,7 @@ def build_runtime(runtime, with_mlrun, interactive=False):
                          secret_name=build.secret,
                          interactive=interactive,
                          name=name,
+                         extra=extra,
                          with_mlrun=with_mlrun)
     runtime.status.build_pod = None
     if status == 'skipped':

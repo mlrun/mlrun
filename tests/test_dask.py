3519b41d43f6b04cdfcd305da7ea2994b4ac4872
Yaron Haviv <yaronh@iguaz.io>
2020-01-10 02:28:36 +0200

    fixes to dask test

--- tests/test_dask.py
+++ tests/test_dask.py
@@ -26,12 +26,16 @@ try:
 except ImportError:
     pass
 
+def inc(x):
+    return x+2
 
 def my_func(context, p1=1, p2='a-string'):
     print(f'Run: {context.name} (uid={context.uid})')
     print(f'Params: p1={p1}, p2={p2}\n')
 
-    context.log_result('accuracy', p1 * 2)
+    x = context.dask_client.submit(inc, p1)
+
+    context.log_result('accuracy', x.result())
     context.log_metric('loss', 7)
     context.log_artifact('chart', body='abc')
     return 'tst-me-{}'.format(context.iteration)
@@ -45,11 +49,3 @@ def test_dask_local():
     verify_state(run)
 
 
-@pytest.mark.skipif(not has_dask, reason='missing dask')
-def test_dask_local_hyper():
-    task = NewTask().with_hyper_params({'p1': [5, 2, 3]}, 'max.accuracy')
-    spec = tag_test(task, 'test_dask_local_hyper')
-    run = new_function(kind='dask').run(spec, handler=my_func)
-    verify_state(run)
-    assert len(run.status.iterations) == 3+1, 'hyper parameters test failed'
-    pprint(run.to_dict())

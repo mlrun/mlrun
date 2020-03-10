923b7640b360f4aec38de8664b6f111b5dfc07e7
Miki Tebeka <miki@353solutions.com>
2020-03-10 13:36:22 +0200

    Use sphinx_rtd_theme theme in docs

--- docs/conf.py
+++ docs/conf.py
@@ -77,6 +77,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+try:
+    import sphinx_rtd_theme  # noqa
+    html_theme = 'sphinx_rtd_theme'
+except ImportError:
+    pass
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

cbca2a42246db5505c2d9ef7b266ddfb4a0b8034
yasha <58791645+yjb-ds@users.noreply.github.com>
2020-04-21 15:01:04 +0300

    fixed CI (#248)
    * init
    
    * refactored dockerfiles
    
    * pytorch
    
    * added intel-tensorflow to ml-models (cpu)
    
    * remove dask version mismatch wanrings
    
    * resolved dask warnings
    
    * fixed serving base image
    
    * pinned pytorch cpu version
    
    * adjust dev-requirements
    
    * added new_tag arg to build for interdependency
    
    * add back dev-requirements for merge
    
    * fixed build-imgs script for CI

--- build-imgs.sh
+++ build-imgs.sh
@@ -3,11 +3,11 @@ if [ $# -eq 0 ]
     echo "No argument (ver tag) supplied"
 fi
 
-sudo docker build . -f Dockerfile.httpd -t mlrun/mlrun-api:$1
+sudo docker build . -f ./dockerfiles/httpd/Dockerfile -t mlrun/mlrun-api:$1
 sudo docker push mlrun/mlrun-api:$1
 
-sudo docker build . -t mlrun/mlrun:$1
+sudo docker build . -t ./mlrun/mlrun:$1
 sudo docker push mlrun/mlrun:$1
 
-sudo docker build . -f Dockerfile.dask -t mlrun/dask:$1
+sudo docker build . -f ./dockerfiles/dask/Dockerfile -t mlrun/dask:$1
 sudo docker push mlrun/dask:$1

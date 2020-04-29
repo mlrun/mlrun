8c446c21e19bc4715826e8605bda0ddf219c9409
Hedi Ingber <hedingber@gmail.com>
2020-04-29 14:28:46 +0300

    fix app tests

--- tests/app/conftest.py
+++ tests/app/conftest.py
@@ -5,10 +5,12 @@ from fastapi.testclient import TestClient
 
 from mlrun.app.db.sqldb.session import SessionLocal
 from mlrun.app.main import app
+from mlrun.app.initial_data import main
 
 
 @pytest.fixture(scope="session")
 def db() -> Generator:
+    main()
     yield SessionLocal()
 
 

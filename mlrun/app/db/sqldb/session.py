438e5302b9050b10d57eff0d83da18e5a41c162d
Hedi Ingber <hedingber@gmail.com>
2020-04-29 13:46:33 +0300

    Create db table once

--- mlrun/app/db/sqldb/session.py
+++ mlrun/app/db/sqldb/session.py
@@ -5,5 +5,4 @@ from mlrun.app.db.sqldb.models import Base
 from mlrun.config import config
 
 engine = create_engine(config.httpdb.dsn)
-Base.metadata.create_all(bind=engine)
 SessionLocal = sessionmaker(bind=engine)
---
+++ prestart.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+# Create initial data in DB
+python /app/mlrun/app/initial_data.py

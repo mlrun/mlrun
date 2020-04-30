e89cd293eb3d589621e2a9f04e0ce5655dd2b99f
Hedi Ingber <hedingber@gmail.com>
2020-04-30 07:22:39 +0300

    forgot returns

--- mlrun/app/db/filedb/db.py
+++ mlrun/app/db/filedb/db.py
@@ -10,66 +10,66 @@ class FileDB(DBInterface):
         self.db.connect()
 
     def store_log(self, session, uid, project="", body=None, append=False):
-        self.db.store_log(uid, project, body, append)
+        return self.db.store_log(uid, project, body, append)
 
     def get_log(self, session, uid, project="", offset=0, size=0):
-        self.db.get_log(uid, project, offset, size)
+        return self.db.get_log(uid, project, offset, size)
 
     def store_run(self, session, struct, uid, project="", iter=0):
-        self.db.store_run(struct, uid, project, iter)
+        return self.db.store_run(struct, uid, project, iter)
 
     def update_run(self, session, updates: dict, uid, project="", iter=0):
-        self.db.update_run(updates, uid, project, iter)
+        return self.db.update_run(updates, uid, project, iter)
 
     def read_run(self, session, uid, project="", iter=0):
-        self.db.read_run(uid, project, iter)
+        return self.db.read_run(uid, project, iter)
 
     def list_runs(
             self, session, name="", uid=None, project="", labels=None,
             state="", sort=True, last=0, iter=False):
-        self.db.list_runs(name, uid, project, labels, state, sort, last, iter)
+        return self.db.list_runs(name, uid, project, labels, state, sort, last, iter)
 
     def del_run(self, session, uid, project="", iter=0):
-        self.db.del_run(uid, project, iter)
+        return self.db.del_run(uid, project, iter)
 
     def del_runs(self, session, name="", project="", labels=None, state="", days_ago=0):
-        self.db.del_runs(name, project, labels, state, days_ago)
+        return self.db.del_runs(name, project, labels, state, days_ago)
 
     def store_artifact(self, session, key, artifact, uid, iter=None, tag="", project=""):
-        self.db.store_artifact(key, artifact, uid, iter, tag, project)
+        return self.db.store_artifact(key, artifact, uid, iter, tag, project)
 
     def read_artifact(self, session, key, tag="", iter=None, project=""):
-        self.db.read_artifact(key, tag, iter, project)
+        return self.db.read_artifact(key, tag, iter, project)
 
     def list_artifacts(
             self, session, name="", project="", tag="", labels=None,
             since=None, until=None):
-        self.db.list_artifacts(name, project, tag, labels, since, until)
+        return self.db.list_artifacts(name, project, tag, labels, since, until)
 
     def del_artifact(self, session, key, tag="", project=""):
-        self.db.del_artifact(key, tag, project)
+        return self.db.del_artifact(key, tag, project)
 
     def del_artifacts(
             self, session, name="", project="", tag="", labels=None):
-        self.db.del_artifacts(name, project, tag, labels)
+        return self.db.del_artifacts(name, project, tag, labels)
 
     def store_function(self, session, func, name, project="", tag=""):
-        self.db.store_function(func, name, project, tag)
+        return self.db.store_function(func, name, project, tag)
 
     def get_function(self, session, name, project="", tag=""):
-        self.db.get_function(name, project, tag)
+        return self.db.get_function(name, project, tag)
 
     def list_functions(self, session, name, project="", tag="", labels=None):
-        self.db.list_functions(name, project, tag, labels)
+        return self.db.list_functions(name, project, tag, labels)
 
     def store_schedule(self, session, data):
-        self.db.store_schedule(data)
+        return self.db.store_schedule(data)
 
     def list_schedules(self, session):
-        self.db.list_schedules()
+        return self.db.list_schedules()
 
     def list_projects(self, session):
-        self.db.list_projects()
+        return self.db.list_projects()
 
     def list_artifact_tags(self, session, project):
-        self.db.list_artifact_tags(project)
+        return self.db.list_artifact_tags(project)
--- mlrun/db/sqldb.py
+++ mlrun/db/sqldb.py
@@ -39,91 +39,91 @@ class SQLDB(RunDBInterface):
         self.db = SQLAPIDB(self.dsn, self.projects)
 
     def store_log(self, uid, project='', body=b'', append=False):
-        self.db.store_log(self.session, uid, project, body, append)
+        return self.db.store_log(self.session, uid, project, body, append)
 
     def get_log(self, uid, project='', offset=0, size=0):
-        self.db.get_log(self.session, uid, project, offset, size)
+        return self.db.get_log(self.session, uid, project, offset, size)
 
     def store_run(self, struct, uid, project='', iter=0):
-        self.db.store_run(self.session, struct, uid, project, iter)
+        return self.db.store_run(self.session, struct, uid, project, iter)
 
     def update_run(self, updates: dict, uid, project='', iter=0):
-        self.db.update_run(self.session, updates, uid, project, iter)
+        return self.db.update_run(self.session, updates, uid, project, iter)
 
     def read_run(self, uid, project=None, iter=None):
-        self.db.read_run(self.session, uid, project, iter)
+        return self.db.read_run(self.session, uid, project, iter)
 
     def list_runs(
             self, name=None, uid=None, project=None, labels=None,
             state=None, sort=True, last=0, iter=None):
-        self.db.list_runs(self.session, name, uid, project, labels, state, sort, last, iter)
+        return self.db.list_runs(self.session, name, uid, project, labels, state, sort, last, iter)
 
     def del_run(self, uid, project=None, iter=None):
-        self.db.del_run(self.session, uid, project, iter)
+        return self.db.del_run(self.session, uid, project, iter)
 
     def del_runs(
             self, name=None, project=None, labels=None,
             state=None, days_ago=0):
-        self.db.del_runs(self.session, name, project, labels, state, days_ago)
+        return self.db.del_runs(self.session, name, project, labels, state, days_ago)
 
     def store_artifact(
             self, key, artifact, uid, iter=None, tag='', project=''):
-        self.db.store_artifact(self.session, key, artifact, uid, iter, tag, project)
+        return self.db.store_artifact(self.session, key, artifact, uid, iter, tag, project)
 
     def read_artifact(self, key, tag='', iter=None, project=''):
-        self.db.read_artifact(self.session, key, tag, iter, project)
+        return self.db.read_artifact(self.session, key, tag, iter, project)
 
     def list_artifacts(
             self, name=None, project=None, tag=None, labels=None,
             since=None, until=None):
-        self.db.list_artifacts(self.session, name, project, tag, labels, since, until)
+        return self.db.list_artifacts(self.session, name, project, tag, labels, since, until)
 
     def del_artifact(self, key, tag='', project=''):
-        self.db.del_artifact(self.session, key, tag, project)
+        return self.db.del_artifact(self.session, key, tag, project)
 
     def del_artifacts(
             self, name='', project='', tag='', labels=None):
-        self.db.del_artifacts(self.session, name, project, tag, labels)
+        return self.db.del_artifacts(self.session, name, project, tag, labels)
 
     def store_function(self, func, name, project='', tag=''):
-        self.db.store_function(self.session, func, name, project, tag)
+        return self.db.store_function(self.session, func, name, project, tag)
 
     def get_function(self, name, project='', tag=''):
-        self.db.get_function(self.session, name, project, tag)
+        return self.db.get_function(self.session, name, project, tag)
 
     def list_functions(
             self, name, project=None, tag=None, labels=None):
-        self.db.list_functions(self.session, name, project, tag, labels)
+        return self.db.list_functions(self.session, name, project, tag, labels)
 
     def list_artifact_tags(self, project):
-        self.db.list_artifact_tags(self.session, project)
+        return self.db.list_artifact_tags(self.session, project)
 
     def store_schedule(self, data):
-        self.db.store_schedule(self.session, data)
+        return self.db.store_schedule(self.session, data)
 
     def list_schedules(self):
-        self.db.list_schedules(self.session)
+        return self.db.list_schedules(self.session)
 
     def tag_objects(self, objs, project: str, name: str):
-        self.db.tag_objects(self.session, objs, project, name)
+        return self.db.tag_objects(self.session, objs, project, name)
 
     def del_tag(self, project: str, name: str):
-        self.db.del_tag(self.session, project, name)
+        return self.db.del_tag(self.session, project, name)
 
     def find_tagged(self, project: str, name: str):
-        self.db.find_tagged(self.session, project, name)
+        return self.db.find_tagged(self.session, project, name)
 
     def list_tags(self, project: str):
-        self.db.list_tags(self.session, project)
+        return self.db.list_tags(self.session, project)
 
     def add_project(self, project: dict):
-        self.db.add_project(self.session, project)
+        return self.db.add_project(self.session, project)
 
     def update_project(self, name, data: dict):
-        self.db.update_project(self.session, name, data)
+        return self.db.update_project(self.session, name, data)
 
     def get_project(self, name=None, project_id=None):
-        self.db.get_project(self.session, name, project_id)
+        return self.db.get_project(self.session, name, project_id)
 
     def list_projects(self, owner=None):
-        self.db.list_projects(self.session, owner)
+        return self.db.list_projects(self.session, owner)

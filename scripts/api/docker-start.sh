05a06273a23553b5c36054640f448ab45de66135
Hedi Ingber <hedingber@gmail.com>
2020-05-08 09:00:44 +0300

    docker file fixes

--- scripts/api/docker-start.sh
+++ scripts/api/docker-start.sh
@@ -1,11 +1,10 @@
 #! /usr/bin/env bash
 
 # Execute docker prestart
-./docker-prestart.sh
+/mlrun/scripts/api/docker-prestart.sh
 
-cd /mlrun || exit
-
-uvicorn mlrun.api.main:app --port "$PORT" --host 0.0.0.0
+# shellcheck disable=SC2154
+uvicorn mlrun.api.main:app --port "$MLRUN_httpdb__port" --host 0.0.0.0
 
 
 

8b6f3a0abbecf48a7dcc03c42163d799297e4a4a
Hedi Ingber <hedingber@gmail.com>
2020-05-12 23:06:39 +0300

    extend healthz response

--- mlrun/api/api/endpoints/healthz.py
+++ mlrun/api/api/endpoints/healthz.py
@@ -1,3 +1,5 @@
+from os import environ
+
 from fastapi import APIRouter
 
 from mlrun.config import config
@@ -9,4 +11,9 @@ router = APIRouter()
 def health():
     return {
         "version": config.version,
+        "namespace": config.namespace,
+        "docker_registry": environ.get('DEFAULT_DOCKER_REGISTRY', ''),
+        "remote_host": config.remote_host,
+        "ui_url": config.ui_url,
+        "artifact_path": config.artifact_path,
     }

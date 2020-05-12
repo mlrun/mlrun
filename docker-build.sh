137997ee0cd2a81bef20f2e0bd721ad9f7efe1bf
Yasha Borstein <yashab@iguazio.com>
2020-05-13 02:42:32 +0300

    dockeerfile changes

--- docker-build.sh
+++ docker-build.sh
@@ -4,17 +4,18 @@ set -e
 
 args=("$@")
 
-export REPO=${args[0]}
-export PREFIX=${args[1]}
-export MLRUN_COMMIT=${args[2]}
-export NEW_TAG=${args[3]}
-export PYTHON_VER_ML=${args[4]}
-export PYTHON_VER_CORE=${args[5]}
-export SOURCE=${args[6]}
+export REPO=mlrun
+export PREFIX=ml
+export MLRUN_COMMIT=development
+export NEW_TAG=0.4.7
+export PYTHON_VER_ML=3.7
+export PYTHON_VER_CORE=3.6
 
-for IMAGE in 'base' 'models' 'models-gpu' 'serving'
+# export DOCKER_BUILDKIT=1
+
+for IMAGE in 'base' 'models' 'models-gpu'
 do
-    docker build \
+    docker build  \
         -f ./dockerfiles/$IMAGE/Dockerfile \
         --build-arg MLRUN_TAG=$MLRUN_COMMIT \
         --build-arg REPO=$REPO \
@@ -26,9 +27,24 @@ do
     docker push $REPO/$PREFIX-$IMAGE:$NEW_TAG
 done
 
-for IMAGE in 'dask' 'mlrun-api' 'test'
+# need some python 3.6 for (legacy) package consistency 
+for IMAGE in 'base' 'models' 'models-gpu'
 do
     docker build \
+        -f ./dockerfiles/$IMAGE/py"${PYTHON_VER_CORE//.}"/Dockerfile \
+        --build-arg MLRUN_TAG=$MLRUN_COMMIT \
+        --build-arg REPO=$REPO \
+        --build-arg PREFIX=$PREFIX \
+        --build-arg NEW_TAG=$NEW_TAG \
+        --build-arg PYTHON_VER=$PYTHON_VER_CORE \
+        -t yjbds/$PREFIX-$IMAGE:$NEW_TAG-py"${PYTHON_VER_CORE//.}" .
+
+    docker push yjbds/$PREFIX-$IMAGE:$NEW_TAG-py"${PYTHON_VER_CORE//.}"
+done
+
+for IMAGE in 'dask' 'mlrun-api' 'test'
+do
+    docker build \ 
         -f ./dockerfiles/$IMAGE/Dockerfile \
         --build-arg MLRUN_TAG=$MLRUN_COMMIT \
         --build-arg REPO=$REPO \

b7318e0847536db737508484297bd5aa4079bf9e
Hedi Ingber <hedingber@gmail.com>
2020-05-23 22:49:21 +0300

    don't forget to close session

--- mlrun/api/initial_data.py
+++ mlrun/api/initial_data.py
@@ -1,7 +1,7 @@
 import logging
 
 from mlrun.api.db.init_db import init_db
-from mlrun.api.db.session import create_session
+from mlrun.api.db.session import create_session, close_session
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -10,7 +10,10 @@ logger = logging.getLogger(__name__)
 def init_data() -> None:
     logger.info("Creating initial data")
     db_session = create_session()
-    init_db(db_session)
+    try:
+        init_db(db_session)
+    finally:
+        close_session(db_session)
     logger.info("Initial data created")
 
 

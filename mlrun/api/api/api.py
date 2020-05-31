b33616a0a72fc520ce0a4084648ffab84606cca1
Hedi Ingber <hedingber@gmail.com>
2020-05-31 22:40:26 +0300

    Include API

--- mlrun/api/api/api.py
+++ mlrun/api/api/api.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
 
 from mlrun.api.api import deps
-from mlrun.api.api.endpoints import artifacts, files, functions, healthz, logs, pipelines, projects, runs, schedules, \
-    submit, tags, workflows
+from mlrun.api.api.endpoints import artifacts, files, functions, healthz, logs, pipelines, projects, runs, runtimes, \
+    schedules, submit, tags, workflows
 
 api_router = APIRouter()
 api_router.include_router(artifacts.router, tags=["artifacts"], dependencies=[Depends(deps.AuthVerifier)])
@@ -13,6 +13,7 @@ api_router.include_router(logs.router, tags=["logs"], dependencies=[Depends(deps
 api_router.include_router(pipelines.router, tags=["pipelines"], dependencies=[Depends(deps.AuthVerifier)])
 api_router.include_router(projects.router, tags=["projects"], dependencies=[Depends(deps.AuthVerifier)])
 api_router.include_router(runs.router, tags=["runs"], dependencies=[Depends(deps.AuthVerifier)])
+api_router.include_router(runtimes.router, tags=["runtimes"], dependencies=[Depends(deps.AuthVerifier)])
 api_router.include_router(schedules.router, tags=["schedules"], dependencies=[Depends(deps.AuthVerifier)])
 api_router.include_router(submit.router, tags=["submit"], dependencies=[Depends(deps.AuthVerifier)])
 api_router.include_router(tags.router, tags=["tags"], dependencies=[Depends(deps.AuthVerifier)])

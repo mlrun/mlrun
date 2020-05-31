09ca7129e60ebba71f594fd6d1373bf8e9c5a95c
Hedi Ingber <hedingber@gmail.com>
2020-06-01 01:08:25 +0300

    Bad request on invalid kind

--- mlrun/api/api/endpoints/runtimes.py
+++ mlrun/api/api/endpoints/runtimes.py
@@ -1,5 +1,9 @@
+from http import HTTPStatus
+
 from fastapi import APIRouter, status, Response
 
+from mlrun.api.api.utils import log_and_raise
+from mlrun.runtimes import RuntimeKinds
 from mlrun.runtimes import get_runtime_handler
 
 router = APIRouter()
@@ -10,6 +14,8 @@ def get_runtime(
         kind: str,
         namespace: str = None,
         label_selector: str = None):
+    if kind not in RuntimeKinds.all():
+        log_and_raise(HTTPStatus.NOT_FOUND, kind=kind, err='Invalid runtime kind')
     runtime_handler = get_runtime_handler(kind)
     resources = runtime_handler.list_resources(namespace, label_selector)
     return {

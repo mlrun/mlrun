f1acfa5b418341fa0e0182e46cc1cc9160d83e47
Hedi Ingber <hedingber@gmail.com>
2020-06-01 01:24:00 +0300

    trying

--- mlrun/api/api/endpoints/runtimes.py
+++ mlrun/api/api/endpoints/runtimes.py
@@ -15,7 +15,7 @@ def get_runtime(
         namespace: str = None,
         label_selector: str = None):
     if kind not in RuntimeKinds.all():
-        log_and_raise(HTTPStatus.NOT_FOUND, kind=kind, err='Invalid runtime kind')
+        log_and_raise(HTTPStatus.BAD_REQUEST, kind=kind, err='Invalid runtime kind')
     runtime_handler = get_runtime_handler(kind)
     resources = runtime_handler.list_resources(namespace, label_selector)
     return {
@@ -29,6 +29,8 @@ def delete_runtime(
         namespace: str = None,
         label_selector: str = None,
         running: bool = False):
+    if kind not in RuntimeKinds.all():
+        log_and_raise(status.HTTP_400_BAD_REQUEST, kind=kind, err='Invalid runtime kind')
     runtime_handler = get_runtime_handler(kind)
     runtime_handler.delete_resources(namespace, label_selector, running)
     return Response(status_code=status.HTTP_204_NO_CONTENT)

89b7808974658c3ece3f7295908f297d0697b5b5
LiranBG <liranbg@gmail.com>
2020-06-08 18:37:49 +0300

    config refactor

--- mlrun/config.py
+++ mlrun/config.py
@@ -11,11 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """
 Configuration system.
 
 Configuration can be in either a configuration file specified by
-MLRUN_CONFIG_FILE environment variable or by environmenet variables.
+MLRUN_CONFIG_FILE environment variable or by environment variables.
 
 Environment variables are in the format "MLRUN_httpdb__port=8080". This will be
 mapped to config.httpdb.port. Values should be in JSON format.
@@ -24,42 +25,36 @@ mapped to config.httpdb.port. Values should be in JSON format.
 import json
 import os
 from collections.abc import Mapping
-from distutils.util import strtobool
 from os.path import expanduser
-from threading import Lock
-from urllib.parse import urlparse
 from . import __version__
 
 import yaml
 
 env_prefix = 'MLRUN_'
-env_file_key = '{}CONIFG_FILE'.format(env_prefix)
-_load_lock = Lock()
-_none_type = type(None)
-
+env_file_key = f'{env_prefix}CONIFG_FILE'
 
 default_config = {
-    'namespace': 'default-tenant',   # default kubernetes namespace
-    'dbpath': '',                    # db/api url
+    'namespace': 'default-tenant',  # default kubernetes namespace
+    'dbpath': '',  # db/api url
     # url to nuclio dashboard api (can be with user & token, e.g. https://username:password@dashboard-url.com)
     'nuclio_dashboard_url': '',
-    'ui_url': '',                    # remote/external mlrun UI url (for hyperlinks)
+    'ui_url': '',  # remote/external mlrun UI url (for hyperlinks)
     'remote_host': '',
-    'version': '',                   # will be set to current version
-    'images_tag': '',                # tag to use with mlrun images e.g. mlrun/mlrun (defaults to version)
-    'kfp_ttl': '86400',              # KFP ttl in sec, after that completed PODs will be deleted
-    'kfp_image': '',                 # image to use for KFP runner (defaults to mlrun/mlrun)
-    'kaniko_version': 'v0.19.0',     # kaniko builder version
-    'package_path': 'mlrun',         # mlrun pip package
+    'version': '',  # will be set to current version
+    'images_tag': '',  # tag to use with mlrun images e.g. mlrun/mlrun (defaults to version)
+    'kfp_ttl': '86400',  # KFP ttl in sec, after that completed PODs will be deleted
+    'kfp_image': '',  # image to use for KFP runner (defaults to mlrun/mlrun)
+    'kaniko_version': 'v0.19.0',  # kaniko builder version
+    'package_path': 'mlrun',  # mlrun pip package
     'default_image': 'python:3.6-jessie',
-    'default_project': 'default',    # default project name
-    'default_archive': '',           # default remote archive URL (for build tar.gz)
-    'mpijob_crd_version': '',        # mpijob crd version (e.g: "v1alpha1". must be in: mlrun.runtime.MPIJobCRDVersions)
+    'default_project': 'default',  # default project name
+    'default_archive': '',  # default remote archive URL (for build tar.gz)
+    'mpijob_crd_version': '',  # mpijob crd version (e.g: "v1alpha1". must be in: mlrun.runtime.MPIJobCRDVersions)
     'hub_url': 'https://raw.githubusercontent.com/mlrun/functions/{tag}/{name}/function.yaml',
     'ipython_widget': True,
     'log_level': 'ERROR',
-    'submit_timeout': '180',         # timeout when submitting a new k8s resource
-    'artifact_path': '',             # default artifacts path/url
+    'submit_timeout': '180',  # timeout when submitting a new k8s resource
+    'artifact_path': '',  # default artifacts path/url
     'httpdb': {
         'port': 8080,
         'dirpath': expanduser('~/.mlrun/db'),
@@ -76,19 +71,18 @@ default_config = {
 }
 
 
-class Config:
-    _missing = object()
+class Config(object):
 
     def __init__(self, cfg=None):
         cfg = {} if cfg is None else cfg
+
         # Can't use self._cfg = cfg → infinite recursion
         object.__setattr__(self, '_cfg', cfg)
 
     def __getattr__(self, attr):
-        val = self._cfg.get(attr, self._missing)
-        if val is self._missing:
+        if attr not in self._cfg:
             raise AttributeError(attr)
-
+        val = self._cfg[attr]
         if isinstance(val, Mapping):
             return self.__class__(val)
         return val
@@ -103,117 +97,101 @@ class Config:
         name = self.__class__.__name__
         return f'{name}({self._cfg!r})'
 
-    def update(self, cfg):
-        for key, value in cfg.items():
-            if hasattr(self, key):
-                if isinstance(value, dict):
-                    getattr(self, key).update(value)
-                else:
-                    setattr(self, key, value)
+    def update(self, cfg=None, **cfg_kwargs):
+        """
+        Update self from dict, followed by update by given kwargs
+        """
+        if isinstance(cfg, dict):
+            self._update(cfg)
+        self._update(cfg_kwargs)
 
     def dump_yaml(self, stream=None):
         return yaml.dump(self._cfg, stream, default_flow_style=False)
 
-    @staticmethod
-    def reload():
-        _populate()
+    @classmethod
+    def load(cls, env=None):
+        """
+        reload configuration from config file (if exists in environment) and from environment variables.
+        """
+        config_instance = cls(default_config)
+        config_path = os.environ.get(env_file_key)
+        if config_path:
+            with open(config_path) as fp:
+                data = yaml.safe_load(fp)
+
+            if not isinstance(data, dict):
+                raise TypeError(f'configuration in {config_path} not a dict')
+
+            config_instance.update(data)
+
+        config_instance._enrich_from_env(env)
+        return config_instance
+
+    def _has_attr(self, attr):
+        return attr in self._cfg
+
+    def _update(self, d: dict):
+        for key, value in d.items():
+            if hasattr(self, key):
+                if isinstance(value, dict):
+                    getattr(self, key)._update(value)
+                else:
+                    setattr(self, key, value)
+
+    def _enrich_from_env(self, env=None, prefix=env_prefix):
+        """Read configuration from environment"""
+
+        env = os.environ if env is None else env
+
+        for key, value in env.items():
+            if not key.startswith(prefix) or key == env_file_key:
+                continue
+            try:
+
+                # values can be JSON encoded
+                value = json.loads(value)
+            except ValueError:
+
+                # Leave as string
+                pass
+
+            # trim prefix
+            key = key[len(prefix):]
+
+            # 'A__B' → ['a', 'b']
+            path = key.lower().split('__')
+
+            while len(path) > 1:
+                name, *path = path
+                setattr(self, name, {})
+            setattr(self, path[0], value)
+
+        # check for mlrun-api or db kubernetes service
+        api_port = env.get('MLRUN_API_PORT')
+        if api_port and not self._has_attr('dbpath'):
+            self.dbpath = f'http://mlrun-api:{self.httpdb.port or 8080}'
+
+        # workaround to try and detect IGZ domain in 2.8
+        igz_domain = env.get('IGZ_NAMESPACE_DOMAIN')
+        if not igz_domain and 'DEFAULT_DOCKER_REGISTRY' in env:
+            registry = env['DEFAULT_DOCKER_REGISTRY']
+            if registry.startswith('docker-registry.default-tenant'):
+                igz_domain = registry[len('docker-registry.'):]
+                if ':' in igz_domain:
+                    igz_domain = igz_domain[:igz_domain.rfind(':')]
+                env['IGZ_NAMESPACE_DOMAIN'] = igz_domain
+
+        # set ui service url
+        uisvc = env.get('MLRUN_UI_SERVICE_HOST')
+        if igz_domain and uisvc and not self._has_attr('ui_url'):
+            self.ui_url = f'https://mlrun-ui.{igz_domain}'
+
+        # set image
+        if not self._has_attr('kfp_image'):
+            self.kfp_image = f'mlrun/mlrun:{__version__ or "latest"}'
+
+        # set version
+        self.version = __version__
 
 
-# Global configuration
 config = Config(default_config)
-
-
-def _populate():
-    """Populate configuration from config file (if exists in environment) and
-    from environment variables.
-
-    populate will run only once, after first call it does nothing.
-    """
-    global _loaded
-
-    with _load_lock:
-        _do_populate()
-
-
-def _do_populate(env=None):
-    global config
-
-    config = Config(default_config)
-    config_path = os.environ.get(env_file_key)
-    if config_path:
-        with open(config_path) as fp:
-            data = yaml.safe_load(fp)
-
-        if not isinstance(data, dict):
-            raise TypeError(f'configuration in {config_path} not a dict')
-
-        config.update(data)
-
-    data = read_env(env)
-    if data:
-        config.update(data)
-
-
-def _convert_str(value, typ):
-    if typ in (str, _none_type):
-        return value
-
-    if typ is bool:
-        return strtobool(value)
-
-    # e.g. int('8080') → 8080
-    return typ(value)
-
-
-def read_env(env=None, prefix=env_prefix):
-    """Read configuration from environment"""
-    env = os.environ if env is None else env
-
-    config = {}
-    for key, value in env.items():
-        if not key.startswith(env_prefix) or key == env_file_key:
-            continue
-        try:
-            value = json.loads(value)  # values can be JSON encoded
-        except ValueError:
-            pass  # Leave as string
-        key = key[len(env_prefix):]  # Trim MLRUN_
-        path = key.lower().split('__')  # 'A__B' → ['a', 'b']
-        cfg = config
-        while len(path) > 1:
-            name, *path = path
-            cfg = cfg.setdefault(name, {})
-        cfg[path[0]] = value
-
-    # check for mlrun-api or db kubernetes service
-    svc = env.get('MLRUN_API_PORT')
-    if svc and not config.get('dbpath'):
-        config['dbpath'] = 'http://mlrun-api:{}'.format(
-            default_config['httpdb']['port'] or 8080)
-
-    uisvc = env.get('MLRUN_UI_SERVICE_HOST')
-    igz_domain = env.get('IGZ_NAMESPACE_DOMAIN')
-
-    # workaround to try and detect IGZ domain in 2.8
-    if not igz_domain and 'DEFAULT_DOCKER_REGISTRY' in env:
-        registry = env['DEFAULT_DOCKER_REGISTRY']
-        if registry.startswith('docker-registry.default-tenant'):
-            igz_domain = registry[len('docker-registry.'):]
-            if ':' in igz_domain:
-                igz_domain = igz_domain[:igz_domain.rfind(':')]
-            env['IGZ_NAMESPACE_DOMAIN'] = igz_domain
-
-    if uisvc and not config.get('ui_url'):
-        if igz_domain:
-            config['ui_url'] = 'https://mlrun-ui.{}'.format(igz_domain)
-
-    if not config.get('kfp_image'):
-        tag = __version__ or 'latest'
-        config['kfp_image'] = 'mlrun/mlrun:{}'.format(tag)
-
-    config['version'] = __version__
-
-    return config
-
-
-_populate()
--- tests/test_config.py
+++ tests/test_config.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from copy import deepcopy
+from typing import IO
 from mlrun import config as mlconf
 from contextlib import contextmanager
 from os import environ
@@ -25,33 +27,29 @@ ns_env_key = f'{mlconf.env_prefix}NAMESPACE'
 
 @pytest.fixture
 def config():
-    old = mlconf.config
-    mlconf.config = mlconf.Config(mlconf.default_config)
-    mlconf._loaded = False
-
-    yield mlconf.config
-
-    mlconf.config = old
-    mlconf._loaded = False
+    yield mlconf.Config(deepcopy(mlconf.default_config))
 
 
-@contextmanager
-def patch_env(kw):
-    old, new = [], []
-    for key, value in kw.items():
-        old_val = environ.get(key)
-        if old_val:
-            old.append((key, old_val))
-        else:
-            new.append(key)
-        environ[key] = value
+@pytest.fixture
+def create_yaml_config():
+    temp_file: IO = None
 
-    yield
+    def _create_file(**kw):
+        nonlocal temp_file
+        temp_file = NamedTemporaryFile(mode='wt', suffix='.yml', delete=False)
+        yaml.dump(kw, temp_file, default_flow_style=False)
+        temp_file.flush()
+        return temp_file.name
 
-    for key, value in old:
-        environ[key] = value
-    for key in new:
-        del environ[key]
+    try:
+        yield _create_file
+    finally:
+        if temp_file:
+            temp_file.close()
+
+
+def test_sanity(config):
+    assert config._cfg is not mlconf.config._cfg
 
 
 def test_nothing(config):
@@ -59,32 +57,24 @@ def test_nothing(config):
     assert config.namespace == expected, 'namespace changed'
 
 
-def create_yaml_config(**kw):
-    tmp = NamedTemporaryFile(mode='wt', suffix='.yml', delete=False)
-    yaml.dump(kw, tmp, default_flow_style=False)
-    tmp.flush()
-    return tmp.name
-
-
-def test_file(config):
+def test_file(config, create_yaml_config):
     ns = 'banana'
     config_path = create_yaml_config(namespace=ns)
-
-    with patch_env({mlconf.env_file_key: config_path}):
-        mlconf.config.reload()
+    with _patch_env({mlconf.env_file_key: config_path}):
+        config = config.load()
 
     assert config.namespace == ns, 'not populated from file'
 
 
 def test_env(config):
     ns = 'orange'
-    with patch_env({ns_env_key: ns}):
-        mlconf.config.reload()
+    with _patch_env({ns_env_key: ns}):
+        config = config.load()
 
     assert config.namespace == ns, 'not populated from env'
 
 
-def test_env_override(config):
+def test_env_override(config, create_yaml_config):
     env_ns = 'daffy'
     config_ns = 'bugs'
 
@@ -94,8 +84,8 @@ def test_env_override(config):
         ns_env_key: env_ns,
     }
 
-    with patch_env(env):
-        mlconf.config.reload()
+    with _patch_env(env):
+        config = config.load()
 
     assert config.namespace == env_ns, 'env did not override'
 
@@ -105,3 +95,33 @@ def test_can_set(config):
     val = 90
     config.x.y = val
     assert config.x.y == val, 'bad config update'
+
+
+def test_update(config):
+    config._cfg['x'] = None
+    config._cfg['y'] = None
+
+    config.update(x=1)
+    assert config.x == 1, 'failed to update'
+    config.update({'y': 2})
+    assert config.y == 2, 'failed to update'
+
+
+@contextmanager
+def _patch_env(kw):
+
+    # create a backup
+    os_environ_copy = environ.copy()
+    try:
+
+        # update environ with given kw
+        environ.update(kw)
+        yield
+    finally:
+
+        # removes given kw from environ
+        for key in kw.keys():
+            del environ[key]
+
+        # redo environ from backup
+        environ.update(os_environ_copy)

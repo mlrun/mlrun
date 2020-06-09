e5b7d6dd8fef516f2bd7f5710f957e616c00cb5e
Hedi Ingber <hedingber@gmail.com>
2020-06-10 01:06:43 +0300

    Fix mpijob resolution

--- mlrun/runtimes/utils.py
+++ mlrun/runtimes/utils.py
@@ -63,13 +63,11 @@ cached_mpijob_crd_version = None
 def resolve_mpijob_crd_version(api_context=False):
     global cached_mpijob_crd_version
     if not cached_mpijob_crd_version:
-        # try getting mpijob crd version from config
+
+        # config override everything
         mpijob_crd_version = config.mpijob_crd_version
 
         if not mpijob_crd_version:
-            # set default mpijob crd version
-            mpijob_crd_version = MPIJobCRDVersions.default()
-
             in_k8s_cluster = get_k8s_helper(init=False).is_running_inside_kubernetes_cluster()
             if in_k8s_cluster:
                 k8s_helper = get_k8s_helper()
@@ -79,18 +77,20 @@ def resolve_mpijob_crd_version(api_context=False):
                 res = k8s_helper.list_pods(namespace=namespace, selector='component=mpi-operator')
                 if len(res) > 0:
                     mpi_operator_pod = res[0]
-                    mpijob_crd_version = mpi_operator_pod.metadata.labels.get('crd-version', mpijob_crd_version)
+                    mpijob_crd_version = mpi_operator_pod.metadata.labels.get('crd-version')
             elif not in_k8s_cluster and not api_context:
                 # connect will populate the config from the server config
                 # TODO: something nicer
                 get_run_db().connect()
-                if not config.mpijob_crd_version:
-                    raise Exception('Server does not have configured mpijob crd version')
                 mpijob_crd_version = config.mpijob_crd_version
 
+            # If resolution failed simply use default
+            if not mpijob_crd_version:
+                mpijob_crd_version = MPIJobCRDVersions.default()
+
         if mpijob_crd_version not in MPIJobCRDVersions.all():
-            raise ValueError('unsupported mpijob crd version: {}. '
-                             'supported versions: {}'.format(mpijob_crd_version, MPIJobCRDVersions.all()))
+            raise ValueError(f'unsupported mpijob crd version: {mpijob_crd_version}. '
+                             f'supported versions: {MPIJobCRDVersions.all()}')
         cached_mpijob_crd_version = mpijob_crd_version
 
     return cached_mpijob_crd_version

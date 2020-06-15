c0b67ba3b7e930807b864fa65c154a4c7c3bad11
Hedi Ingber <hedingber@gmail.com>
2020-06-15 05:19:52 +0300

    need the default project

--- mlrun/runtimes/base.py
+++ mlrun/runtimes/base.py
@@ -876,7 +876,7 @@ class BaseRuntimeHandler(ABC):
 
     @staticmethod
     def _resolve_runtime_resource_run(runtime_resource: Dict) -> Tuple[str, str]:
-        project = runtime_resource.get('metadata', {}).get('labels', {}).get('mlrun/project')
+        project = runtime_resource.get('metadata', {}).get('labels', {}).get('mlrun/project', config.default_project)
         uid = runtime_resource.get('metadata', {}).get('labels', {}).get('mlrun/uid')
         return project, uid
 

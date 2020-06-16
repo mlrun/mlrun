c7f454031a49cf232c0872cd9077497dada8951d
aviaIguazio <avia@iguazio.com>
2020-06-16 14:08:20 +0300

    updating k8s utils to support executing shell command

--- tests/test_k8s_utils.py
+++ tests/test_k8s_utils.py
@@ -1,14 +1,22 @@
 import pytest
 from mlrun.k8s_utils import get_k8s_helper
 
+v3io_access_key = '<v3io_access_key>'
 
 def test_k8s_get_pod():
     cli = get_k8s_helper()
-    pod = cli.get_pod('avi-shell-6d9c467597-89lss', namespace='default-tenant')
+    pod = cli.get_pod(v3io_access_key, namespace='default-tenant')
     print(pod)
 
 
 def test_k8s_get_pod():
     cli = get_k8s_helper()
-    pod = cli.exec_shell_cmd("echo HELLO", 'v3io_pass')
+    cmd = "spark-submit"
+    pod = cli.exec_shell_cmd(cmd, v3io_access_key)
     print(pod)
+
+
+def test_validate_access_key():
+    cli = get_k8s_helper()
+    resp = cli._validate_access_key(v3io_access_key)
+    print(resp)

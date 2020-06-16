a58e98bd4366b7d583c3133c1354c421cec28a59
Hedingber <hedingber@gmail.com>
2020-06-16 13:10:07 +0300

    Improve integration tests (#316)
    * Fail automatically when subshell command fail
    
    * Install mlrun as a package in integration tests image
    
    * Fix wait for pipeline completion and add to mlrun_jobs
    
    * wait for pipeline completion in mlrun_dask
    
    * minimize integration test required env vars
    
    * stick to the real pipelines statuses values
    
    * install mlrun as package in unit tests as well

--- mlrun/run.py
+++ mlrun/run.py
@@ -46,11 +46,11 @@ from .utils import (get_in, logger, parse_function_uri, update_in,
 
 
 class RunStatuses(object):
-    succeeded = 'succeeded'
-    failed = 'failed'
-    skipped = 'skipped'
-    error = 'error'
-    running = 'running'
+    succeeded = 'Succeeded'
+    failed = 'Failed'
+    skipped = 'Skipped'
+    error = 'Error'
+    running = 'Running'
 
     @staticmethod
     def all():
@@ -694,18 +694,20 @@ def run_pipeline(pipeline, arguments=None, project=None, experiment=None,
 
 def wait_for_pipeline_completion(run_id,
                                  timeout=60 * 60,
-                                 expected_statuses: typing.List[str]=None,
-                                 namespace=None):
+                                 expected_statuses: typing.List[str] = None,
+                                 namespace = None):
     """Wait for Pipeline status, timeout in sec
 
     :param run_id:     id of pipelines run
     :param timeout:    wait timeout in sec
-    :param expected_statuses:  list of expected statuses, otherwise will raise
-                               succeeded | failed | skipped | error | running
+    :param expected_statuses:  list of expected statuses, one of [ Succeeded | Failed | Skipped | Error ], by default
+                               [ Succeeded ]
     :param namespace:  k8s namespace if not default
 
     :return kfp run dict
     """
+    if expected_statuses is None:
+        expected_statuses = [RunStatuses.succeeded]
     namespace = namespace or mlconf.namespace
     remote = not get_k8s_helper(init=False).is_running_inside_kubernetes_cluster()
     logger.debug(f"Waiting for run completion."
@@ -721,7 +723,7 @@ def wait_for_pipeline_completion(run_id,
         def get_pipeline_if_completed(run_id, namespace=namespace):
             resp = mldb.get_pipeline(run_id, namespace=namespace)
             status = resp['run']['status']
-            if status.lower() not in RunStatuses.stable_statuses():
+            if status not in RunStatuses.stable_statuses():
 
                 # TODO: think of nicer liveness indication and make it re-usable
                 # log '.' each retry as a liveness indication

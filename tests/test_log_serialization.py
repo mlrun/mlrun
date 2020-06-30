17138da72e07beb561a36282646f5f071bc1fb63
yaron haviv <yaronh@iguazio.com>
2020-07-01 00:58:20 +0300

    lint

--- tests/test_log_serialization.py
+++ tests/test_log_serialization.py
@@ -11,10 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from os import path
 
 from tests.conftest import (
-    examples_path,
     out_path,
     tag_test,
     verify_state,

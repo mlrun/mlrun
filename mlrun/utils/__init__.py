8641b79efaa334388db7619c0fa559db1dbd4d44
Hedi Ingber <hedingber@gmail.com>
2020-07-06 17:34:20 +0300

    fix tests

--- mlrun/utils/__init__.py
+++ mlrun/utils/__init__.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import mlrun.utils.helpers  # noqa
-import mlrun.utils.logger  # noqa
+from .helpers import *  # noqa
+from .logger import *  # noqa

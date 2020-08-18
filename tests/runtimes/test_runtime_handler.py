d4732c69ef378b9e42188f9e0f923bccc26468df
Hedi Ingber <hedingber@gmail.com>
2020-08-18 04:18:39 +0300

    add dask services mock and assertions

--- tests/runtimes/test_runtime_handler.py
+++ tests/runtimes/test_runtime_handler.py
@@ -4,26 +4,38 @@ from mlrun.runtimes import get_runtime_handler, RuntimeKinds
 
 
 def test_list_kubejob_resources(k8s_helper_mock):
-    pods = _mock_kubejob_pods()
+    pods = _create_kubejob_pod_mocks()
     k8s_helper_mock.list_pods.return_value = pods
     runtime_handler = get_runtime_handler(RuntimeKinds.job)
     resources = runtime_handler.list_resources()
-    k8s_helper_mock.list_pods.assert_called_once_with(k8s_helper_mock.resolve_namespace(),
-                                                      selector=runtime_handler._get_default_label_selector())
+    k8s_helper_mock.list_pods.assert_called_once_with(
+        k8s_helper_mock.resolve_namespace(),
+        selector=runtime_handler._get_default_label_selector(),
+    )
     _assert_resources(resources, expected_pods=pods)
 
 
 def test_list_daskjob_resources(k8s_helper_mock):
-    pods = _mock_daskjob_pods()
+    pods = _create_daskjob_pod_mocks()
+    services = _create_daskjob_service_mocks()
     k8s_helper_mock.list_pods.return_value = pods
-    runtime_handler = get_runtime_handler(RuntimeKinds.job)
+    k8s_helper_mock.v1api.list_namespaced_service.return_value = services
+    runtime_handler = get_runtime_handler(RuntimeKinds.dask)
     resources = runtime_handler.list_resources()
-    k8s_helper_mock.list_pods.assert_called_once_with(k8s_helper_mock.resolve_namespace(),
-                                                      selector=runtime_handler._get_default_label_selector())
-    _assert_resources(resources, expected_pods=pods)
+    k8s_helper_mock.list_pods.assert_called_once_with(
+        k8s_helper_mock.resolve_namespace(),
+        selector=runtime_handler._get_default_label_selector(),
+    )
+    k8s_helper_mock.v1api.list_namespaced_service.assert_called_once_with(
+        k8s_helper_mock.resolve_namespace(),
+        label_selector=runtime_handler._get_default_label_selector(),
+    )
+    _assert_resources(resources, expected_pods=pods, expected_services=services)
 
 
-def _assert_resources(resources, expected_crds=None, expected_pods=None, expected_services=None):
+def _assert_resources(
+    resources, expected_crds=None, expected_pods=None, expected_services=None
+):
     if expected_crds is None:
         expected_crds = []
     if expected_pods is None:
@@ -35,11 +47,39 @@ def _assert_resources(resources, expected_crds=None, expected_pods=None, expecte
     for index, pod in enumerate(expected_pods):
         pod_dict = pod.to_dict()
         assert resources['pod_resources'][index]['name'] == pod_dict['metadata']['name']
-        assert resources['pod_resources'][index]['labels'] == pod_dict['metadata']['labels']
+        assert (
+            resources['pod_resources'][index]['labels']
+            == pod_dict['metadata']['labels']
+        )
         assert resources['pod_resources'][index]['status'] == pod_dict['status']
+    assert len(resources['service_resources']) == len(expected_services.items)
+    for index, service in enumerate(expected_services.items):
+        assert resources['service_resources'][index]['name'] == service.metadata.name
+        assert (
+            resources['service_resources'][index]['labels'] == service.metadata.labels
+        )
 
 
-def _mock_kubejob_pods():
+def _create_daskjob_service_mocks():
+    service_mock = unittest.mock.Mock()
+    service_mock.metadata.name.return_value = 'mlrun-mydask-d7656bc1-0'
+    service_mock.metadata.labels.return_value = {
+        'app': 'dask',
+        'dask.org/cluster-name': 'mlrun-mydask-d7656bc1-0',
+        'dask.org/component': 'scheduler',
+        'mlrun/class': 'dask',
+        'mlrun/function': 'mydask',
+        'mlrun/project': 'default',
+        'mlrun/scrape_metrics': 'False',
+        'mlrun/tag': 'latest',
+        'user': 'root',
+    }
+    services_mock = unittest.mock.Mock()
+    services_mock.items = [service_mock]
+    return services_mock
+
+
+def _create_kubejob_pod_mocks():
     pod_dict = {
         'metadata': {
             'name': 'my-training-j7dtf',
@@ -92,10 +132,10 @@ def _mock_kubejob_pods():
                 {
                     'container_id': 'docker://c00c36dc9a702508c76b6074f2c2fa3e569daaf13f5a72931804da04a6e96987',
                     'image': 'docker-registry.default-tenant.app.hedingber-210-1.iguazio-cd0.com:80/mlrun/func-defaul'
-                             't-my-trainer-latest:latest',
+                    't-my-trainer-latest:latest',
                     'image_id': 'docker-pullable://docker-registry.default-tenant.app.hedingber-210-1.iguazio-cd0.com'
-                                ':80/mlrun/func-default-my-trainer-latest@sha256:d23c93a997fa5ab89d899bf1bf1cb97fa506'
-                                '97a74c61927c1df3266340076efc',
+                    ':80/mlrun/func-default-my-trainer-latest@sha256:d23c93a997fa5ab89d899bf1bf1cb97fa506'
+                    '97a74c61927c1df3266340076efc',
                     'last_state': {
                         'running': None,
                         'terminated': None,
@@ -130,12 +170,12 @@ def _mock_kubejob_pods():
             'start_time': '2020-08-17T18:08:23+00:00',
         },
     }
-    pod = unittest.mock.Mock()
-    pod.to_dict.return_value = pod_dict
-    return [pod]
+    pod_mock = unittest.mock.Mock()
+    pod_mock.to_dict.return_value = pod_dict
+    return [pod_mock]
 
 
-def _mock_daskjob_pods():
+def _create_daskjob_pod_mocks():
     scheduler_pod_dict = {
         'metadata': {
             'name': 'mlrun-mydask-d7656bc1-0n4z9z',
@@ -159,7 +199,7 @@ def _mock_daskjob_pods():
                     'message': None,
                     'reason': None,
                     'status': 'True',
-                    'type': 'Initialized'
+                    'type': 'Initialized',
                 },
                 {
                     'last_probe_time': None,
@@ -167,7 +207,7 @@ def _mock_daskjob_pods():
                     'message': None,
                     'reason': None,
                     'status': 'True',
-                    'type': 'Ready'
+                    'type': 'Ready',
                 },
                 {
                     'last_probe_time': None,
@@ -175,7 +215,7 @@ def _mock_daskjob_pods():
                     'message': None,
                     'reason': None,
                     'status': 'True',
-                    'type': 'ContainersReady'
+                    'type': 'ContainersReady',
                 },
                 {
                     'last_probe_time': None,
@@ -183,29 +223,28 @@ def _mock_daskjob_pods():
                     'message': None,
                     'reason': None,
                     'status': 'True',
-                    'type': 'PodScheduled'
-                }
+                    'type': 'PodScheduled',
+                },
             ],
             'container_statuses': [
                 {
                     'container_id': 'docker://c24d68d4c71d8f61bed56aaf424dc7ffb86a9f59d7710afaa1e28f47bd466a5e',
                     'image': 'mlrun/ml-models:0.5.1',
-                    'image_id': 'docker-pullable://mlrun/ml-models@sha256:07cc9e991dc603dbe100f4eae93d20f3ce53af1e6d4af5191dbc66e6dfbce85b',
+                    'image_id': 'docker-pullable://mlrun/ml-models@sha256:07cc9e991dc603dbe100f4eae93d20f3ce53af1e6d4af'
+                    '5191dbc66e6dfbce85b',
                     'last_state': {
                         'running': None,
                         'terminated': None,
-                        'waiting': None
+                        'waiting': None,
                     },
                     'name': 'base',
                     'ready': True,
                     'restart_count': 0,
                     'state': {
-                        'running': {
-                            'started_at': '2020-08-18T00:36:19+00:00'
-                        },
+                        'running': {'started_at': '2020-08-18T00:36:19+00:00'},
                         'terminated': None,
-                        'waiting': None
-                    }
+                        'waiting': None,
+                    },
                 }
             ],
             'host_ip': '172.31.6.138',
@@ -216,8 +255,8 @@ def _mock_daskjob_pods():
             'pod_ip': '10.200.0.48',
             'qos_class': 'BestEffort',
             'reason': None,
-            'start_time': '2020-08-18T00:35:15+00:00'
-        }
+            'start_time': '2020-08-18T00:35:15+00:00',
+        },
     }
     worker_pod_dict = {
         'metadata': {
@@ -231,7 +270,7 @@ def _mock_daskjob_pods():
                 'mlrun/project': 'default',
                 'mlrun/scrape_metrics': 'False',
                 'mlrun/tag': 'latest',
-                'user': 'root'
+                'user': 'root',
             },
         },
         'status': {
@@ -242,7 +281,7 @@ def _mock_daskjob_pods():
                     'message': None,
                     'reason': None,
                     'status': 'True',
-                    'type': 'Initialized'
+                    'type': 'Initialized',
                 },
                 {
                     'last_probe_time': None,
@@ -250,7 +289,7 @@ def _mock_daskjob_pods():
                     'message': None,
                     'reason': None,
                     'status': 'True',
-                    'type': 'Ready'
+                    'type': 'Ready',
                 },
                 {
                     'last_probe_time': None,
@@ -258,7 +297,7 @@ def _mock_daskjob_pods():
                     'message': None,
                     'reason': None,
                     'status': 'True',
-                    'type': 'ContainersReady'
+                    'type': 'ContainersReady',
                 },
                 {
                     'last_probe_time': None,
@@ -266,29 +305,28 @@ def _mock_daskjob_pods():
                     'message': None,
                     'reason': None,
                     'status': 'True',
-                    'type': 'PodScheduled'
-                }
+                    'type': 'PodScheduled',
+                },
             ],
             'container_statuses': [
                 {
                     'container_id': 'docker://18f75b15b9fbf0ed9136d9ec7f14cf1d62dbfa078877f89847fa346fe09ff574',
                     'image': 'mlrun/ml-models:0.5.1',
-                    'image_id': 'docker-pullable://mlrun/ml-models@sha256:07cc9e991dc603dbe100f4eae93d20f3ce53af1e6d4af5191dbc66e6dfbce85b',
+                    'image_id': 'docker-pullable://mlrun/ml-models@sha256:07cc9e991dc603dbe100f4eae93d20f3ce53af1e6d4af'
+                    '5191dbc66e6dfbce85b',
                     'last_state': {
                         'running': None,
                         'terminated': None,
-                        'waiting': None
+                        'waiting': None,
                     },
                     'name': 'base',
                     'ready': True,
                     'restart_count': 0,
                     'state': {
-                        'running': {
-                            'started_at': '2020-08-18T00:36:23+00:00'
-                        },
+                        'running': {'started_at': '2020-08-18T00:36:23+00:00'},
                         'terminated': None,
-                        'waiting': None
-                    }
+                        'waiting': None,
+                    },
                 }
             ],
             'host_ip': '172.31.6.138',
@@ -299,11 +337,11 @@ def _mock_daskjob_pods():
             'pod_ip': '10.200.0.51',
             'qos_class': 'BestEffort',
             'reason': None,
-            'start_time': '2020-08-18T00:36:21+00:00'
-        }
+            'start_time': '2020-08-18T00:36:21+00:00',
+        },
     }
-    scheduler_pod = unittest.mock.Mock()
-    scheduler_pod.to_dict.return_value = scheduler_pod_dict
-    worker_pod = unittest.mock.Mock()
-    worker_pod.to_dict.return_value = worker_pod_dict
-    return [scheduler_pod, worker_pod]
+    scheduler_pod_mock = unittest.mock.Mock()
+    scheduler_pod_mock.to_dict.return_value = scheduler_pod_dict
+    worker_pod_mock = unittest.mock.Mock()
+    worker_pod_mock.to_dict.return_value = worker_pod_dict
+    return [scheduler_pod_mock, worker_pod_mock]

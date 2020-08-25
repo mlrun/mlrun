12515e9516a8a37779889d48929f64129938d83c
Hedi Ingber <hedingber@gmail.com>
2020-08-25 04:12:58 +0300

    Add test that reproduce the bug

---
+++ tests/api/api/test_runs.py
@@ -0,0 +1,25 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+
+from mlrun.api.utils.singletons.db import get_db
+
+
+def test_run_with_nan_in_body(db: Session, client: TestClient, monkeypatch) -> None:
+    run_with_nan_float = {
+        "status": {
+            "artifacts": [
+                {
+                    "preview": [
+                        [0.0, float("Nan"), 1.3],
+                    ],
+                },
+            ],
+        },
+    }
+    uid = "some-uid"
+    project = "some-project"
+    get_db().store_run(db, run_with_nan_float, uid, project)
+    resp = client.get(f"/api/run/{project}/{uid}")
+    assert resp.status_code == HTTPStatus.OK.value

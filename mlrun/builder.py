ef31022e95c1b651e77be4cc1c23a700787d8bd1
hedingber <hedingber@gmail.com>
2020-09-11 01:44:50 +0300

    tag_image -> enrich_image one more

--- mlrun/builder.py
+++ mlrun/builder.py
@@ -20,7 +20,7 @@ from urllib.parse import urlparse
 
 from .datastore import store_manager
 from .k8s_utils import BasePod, get_k8s_helper
-from .utils import logger, normalize_name, tag_image
+from .utils import logger, normalize_name, enrich_image
 from .config import config
 
 
@@ -251,7 +251,7 @@ def build_runtime(runtime, with_mlrun, interactive=False):
         extra = None
 
     name = normalize_name("mlrun-build-{}".format(runtime.metadata.name))
-    base_image = tag_image(build.base_image or "mlrun/mlrun")
+    base_image = enrich_image(build.base_image or "mlrun/mlrun")
     if not build.base_image:
         with_mlrun = False
 

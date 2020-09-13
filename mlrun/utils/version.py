a09ad23c8901f7b5467c60360a4e125db8a2f1cc
hedingber <hedingber@gmail.com>
2020-09-13 18:01:50 +0300

    Fix path

--- mlrun/utils/version.py
+++ mlrun/utils/version.py
@@ -1,9 +1,12 @@
 import json
 import typing
+from pathlib import Path
 
 
 def _resolve_version_info() -> typing.Dict[str, str]:
-    with open("../../version.json") as version_file:
+    project_root_path = Path(__file__).absolute().parent.parent.parent
+    version_file_path = project_root_path / "version.json"
+    with open(version_file_path) as version_file:
         return json.load(version_file)
 
 

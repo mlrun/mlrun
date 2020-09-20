80614f189aced767f81a003c03db5668aeb08448
hedingber <hedingber@gmail.com>
2020-09-20 21:58:02 +0300

    linting

--- mlrun/utils/version/__init__.py
+++ mlrun/utils/version/__init__.py
@@ -1,1 +1,1 @@
-from .version import Version
+from .version import Version  # noqa: F401
--- mlrun/utils/version/version.py
+++ mlrun/utils/version/version.py
@@ -9,16 +9,15 @@ class Version(metaclass=mlrun.utils.singleton.Singleton):
     def __init__(self):
         # When installing un-released version (e.g. by doing pip install git+https://github.com/mlrun/mlrun@development)
         # it won't have a version file, so adding some sane defaults
-        self.version_info = {
-            "git_commit": "unknown",
-            "version": "unstable"
-        }
+        self.version_info = {"git_commit": "unknown", "version": "unstable"}
         try:
             self.version_info = json.loads(
                 importlib.resources.read_text("mlrun.utils.version", "version.json")
             )
-        except:
-            mlrun.utils.logger.warning("Failed resolving version info. Ignoring and using defaults")
+        except Exception:
+            mlrun.utils.logger.warning(
+                "Failed resolving version info. Ignoring and using defaults"
+            )
 
     def get(self):
         return self.version_info

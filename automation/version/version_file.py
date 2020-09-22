0eb214dd7fc1c6e9be99b9fc41f1be1d9835bd1e
hedingber <hedingber@gmail.com>
2020-09-22 03:36:07 +0300

    Fixing logger stuff in version_file.py

--- automation/version/version_file.py
+++ automation/version/version_file.py
@@ -7,7 +7,8 @@ import click
 import logging
 
 # not using mlrun.utils.logger to not require mlrun package
-logger = logging.Logger(name="version", level="DEBUG")
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("version_file")
 
 
 @click.group()
@@ -22,6 +23,7 @@ def create(mlrun_version: str):
     try:
         out, _, _ = _run_command("git", args=["rev-parse", "HEAD"])
         git_commit = out.strip()
+        logger.debug(f"Found git commit: {git_commit}")
 
     except Exception as exc:
         logger.warning("Failed to get version", exc_info=exc)
@@ -39,7 +41,7 @@ def create(mlrun_version: str):
 
 
 def _run_command(
-    command: str, args: list = None, suppress_errors: bool = False,
+    command: str, args: list = None,
 ) -> (str, str, int):
     if args:
         command += " " + " ".join(args)

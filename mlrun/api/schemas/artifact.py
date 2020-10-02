8e20947ba2b1993e7adb3ffe87f70f99b8bfbf31
hedingber <hedingber@gmail.com>
2020-10-02 04:14:08 +0300

    Add test

--- mlrun/api/schemas/artifact.py
+++ mlrun/api/schemas/artifact.py
@@ -1,9 +1,6 @@
 import enum
 import typing
 
-import mlrun.artifacts.dataset
-import mlrun.artifacts.model
-
 
 class ArtifactCategories(str, enum.Enum):
     model = "model"
@@ -11,6 +8,10 @@ class ArtifactCategories(str, enum.Enum):
     other = "other"
 
     def to_kinds_filter(self) -> typing.Tuple[typing.List[str], bool]:
+        # FIXME: these artifact definitions (or at least the kinds enum) should sit in a dedicated module
+        # import here to prevent import cycle
+        import mlrun.artifacts.dataset
+        import mlrun.artifacts.model
         if self.value == ArtifactCategories.model.value:
             return [mlrun.artifacts.model.ModelArtifact.kind], False
         if self.value == ArtifactCategories.dataset.value:

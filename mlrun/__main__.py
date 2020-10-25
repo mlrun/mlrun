b45eaa480a62f30f84d339987378e66b36f7e280
Hedingber <hedingber@gmail.com>
2020-10-25 18:13:33 +0200

    Enable to set scrape metrics label on piplines runs (#491)

--- mlrun/__main__.py
+++ mlrun/__main__.py
@@ -121,6 +121,11 @@ def main():
 @click.option("--label", multiple=True, help="run labels (key=val)")
 @click.option("--watch", "-w", is_flag=True, help="watch/tail run log")
 @click.option("--verbose", is_flag=True, help="verbose log")
+@click.option(
+    "--scrape-metrics",
+    is_flag=True,
+    help="whether to add the `mlrun/scrape-metrics` label to this run's resources",
+)
 @click.argument("run_args", nargs=-1, type=click.UNPROCESSED)
 def run(
     url,
@@ -153,6 +158,7 @@ def run(
     label,
     watch,
     verbose,
+    scrape_metrics,
     run_args,
 ):
     """Execute a task and inject parameters."""
@@ -237,6 +243,8 @@ def run(
     set_item(
         runobj.spec, secrets, run_keys.secrets, line2keylist(secrets, "kind", "source")
     )
+    set_item(runobj.spec, verbose, "verbose")
+    set_item(runobj.spec, scrape_metrics, "scrape_metrics")
 
     if kfp or runobj.spec.verbose or verbose:
         print("MLRun version: {}".format(str(Version().get())))

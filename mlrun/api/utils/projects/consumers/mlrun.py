1102794e8509668e6e4217847582cca124125eff
hedingber <hedingber@gmail.com>
2020-11-18 04:56:42 +0200

    implement mlrun consumer

--- mlrun/api/utils/projects/consumers/mlrun.py
+++ mlrun/api/utils/projects/consumers/mlrun.py
@@ -2,13 +2,14 @@ import sqlalchemy.orm
 
 import mlrun.api.schemas
 import mlrun.api.utils.projects.consumers.base
+import mlrun.api.utils.singletons.db
 
 
 class Consumer(mlrun.api.utils.projects.consumers.base.Consumer):
     def create_project(
         self, session: sqlalchemy.orm.Session, project: mlrun.api.schemas.ProjectCreate
     ):
-        pass
+        mlrun.api.utils.singletons.db.get_db().create_project(session, project)
 
     def update_project(
         self,
@@ -16,17 +17,19 @@ class Consumer(mlrun.api.utils.projects.consumers.base.Consumer):
         name: str,
         project: mlrun.api.schemas.ProjectUpdate,
     ):
-        pass
+        mlrun.api.utils.singletons.db.get_db().update_project(session, name, project)
 
     def delete_project(self, session: sqlalchemy.orm.Session, name: str):
-        pass
+        mlrun.api.utils.singletons.db.get_db().delete_project(session, name)
 
     def get_project(
         self, session: sqlalchemy.orm.Session, name: str
     ) -> mlrun.api.schemas.ProjectOutput:
-        pass
+        return mlrun.api.utils.singletons.db.get_db().get_project(session, name)
 
     def list_projects(
         self, session: sqlalchemy.orm.Session, owner: str = None, full: bool = True,
     ) -> mlrun.api.schemas.ProjectsOutput:
-        pass
+        return mlrun.api.utils.singletons.db.get_db().list_projects(
+            session, owner, full
+        )

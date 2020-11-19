cc6f57ea8b9077637283b2306c277a738bf14ab9
hedingber <hedingber@gmail.com>
2020-11-19 23:18:49 +0200

    loop on projects instead of consumers

--- mlrun/api/utils/projects/manager.py
+++ mlrun/api/utils/projects/manager.py
@@ -1,3 +1,4 @@
+import collections
 import typing
 
 import humanfriendly
@@ -85,87 +86,113 @@ class ProjectsManager(metaclass=mlrun.utils.singleton.Singleton):
     def _sync_projects(self):
         session = mlrun.api.db.session.create_session()
         try:
-            # search for projects created only in consumers
-            for consumer_name in self._consumers.keys():
-                # re-generating all of the maps every time since _ensure_consumer_projects_synced might cause changes
-                # preparations - build maps for easier searches
-                master_projects: mlrun.api.schemas.ProjectsOutput
-                consumer_projects_map: typing.Dict[str, mlrun.api.schemas.ProjectsOutput]
-                master_projects, consumer_projects_map = self._run_on_all_consumers(
-                    "list_projects", session
-                )
-                master_project_names = {
-                    project.name for project in master_projects.projects
-                }
-                consumer_project_names_map = {}
-                for _consumer_name, consumer_projects in consumer_projects_map.items():
-                    consumer_project_names_map[_consumer_name] = set()
-                    for project in consumer_projects.projects:
-                        consumer_project_names_map[_consumer_name].add(project.name)
+            # re-generating all of the maps every time since _ensure_consumer_projects_synced might cause changes
+            master_projects: mlrun.api.schemas.ProjectsOutput
+            consumer_projects_map: typing.Dict[str, mlrun.api.schemas.ProjectsOutput]
+            master_projects, consumer_projects_map = self._run_on_all_consumers(
+                "list_projects", session
+            )
+            master_project_names = {
+                project.name for project in master_projects.projects
+            }
+            # create reverse map project -> consumer names
+            project_consumer_names_map = collections.defaultdict(set)
+            for _consumer_name, consumer_projects in consumer_projects_map.items():
+                for project in consumer_projects.projects:
+                    project_consumer_names_map[project.name].add(_consumer_name)
 
-                # use helper function to decrease nesting
-                self._ensure_consumer_projects_synced(
+            # create map - consumer name -> project name -> project for easier searches
+            consumers_projects_map = collections.defaultdict(dict)
+            for _consumer_name, consumer_projects in consumer_projects_map.items():
+                for project in consumer_projects.projects:
+                    consumers_projects_map[_consumer_name][project.name] = project
+
+            # create map - master project name -> master project for easier searches
+            master_projects_map = {}
+            for master_project in master_projects.projects:
+                master_projects_map[master_project.name] = master_project
+
+            all_project = master_project_names.copy()
+            all_project.update(project_consumer_names_map.keys())
+
+            for project in all_project:
+                self._ensure_project_synced(
                     session,
                     master_project_names,
-                    consumer_project_names_map,
-                    consumer_name,
-                    consumer_projects_map[consumer_name],
+                    project_consumer_names_map[project],
+                    project,
+                    consumers_projects_map,
+                    master_projects_map
                 )
-
         finally:
             mlrun.api.db.session.close_session(session)
 
-    def _ensure_consumer_projects_synced(
+    def _ensure_project_synced(
         self,
         session: sqlalchemy.orm.Session,
         master_project_names: typing.Set[str],
-        consumer_project_names_map: typing.Dict[str, set],
-        consumer_name: str,
-        consumer_projects: mlrun.api.schemas.ProjectsOutput,
+        consumer_names: typing.Set[str],
+        project_name: str,
+        consumers_projects_map: typing.Dict[str, typing.Dict[str, mlrun.api.schemas.Project]],
+        master_projects_map: typing.Dict[str, mlrun.api.schemas.Project],
     ):
-        for project in consumer_projects.projects:
-            if project.name not in master_project_names:
-                logger.debug(
-                    "Found project in consumer that is not in master. Creating",
-                    consumer_name=consumer_name,
-                    project=project.name,
+        project = None
+        project_consumer_name = None
+        # first verify that the master have this project
+        if project_name not in master_project_names:
+            project_in_master = False
+            logger.debug(
+                "Found project in some of the consumers that is not in master. Creating",
+                consumer_names=consumer_names,
+                project=project_name,
+            )
+            try:
+                # Heuristically pick the first consumer
+                project_consumer_name = list(consumer_names)[0]
+                project = consumers_projects_map[project_consumer_name][project_name]
+                self._master_consumer.create_project(
+                    session, mlrun.api.schemas.ProjectCreate(**project.dict())
                 )
-                try:
-                    self._master_consumer.create_project(
-                        session, mlrun.api.schemas.ProjectCreate(**project.dict())
+            except Exception as exc:
+                logger.warning(
+                    "Failed creating missing project in master",
+                    project_consumer_name=project_consumer_name,
+                    project=project,
+                    project_name=project_name,
+                    exc=str(exc),
+                )
+            else:
+                project_in_master = True
+        else:
+            project_in_master = True
+            project = master_projects_map[project_name]
+
+        # only if project in master - align the rest of consumers
+        if project_in_master:
+            missing_consumers = set(consumer_names).symmetric_difference(self._consumers.keys())
+            if missing_consumers:
+                for missing_consumer in missing_consumers:
+                    logger.debug(
+                        "Project is missing from consumer. Creating",
+                        missing_consumer_name=missing_consumer,
+                        project_consumer_name=project_consumer_name,
+                        project_name=project_name,
+                        project=project,
                     )
-                except Exception as exc:
-                    logger.warning(
-                        "Failed creating missing project in master",
-                        consumer_name=consumer_name,
-                        project=project.name,
-                        exc=str(exc),
-                    )
-                else:
-                    # only if we successfully created in master - align the rest of consumers
-                    for (
-                        _consumer_name,
-                        consumer_project_names,
-                    ) in consumer_project_names_map.items():
-                        if project.name not in consumer_project_names:
-                            logger.debug(
-                                "Project is missing from consumer as well. Creating",
-                                proejct_consumer_name=consumer_name,
-                                missing_consumer_name=_consumer_name,
-                                project=project.name,
-                            )
-                            try:
-                                self._consumers[_consumer_name].create_project(
-                                    session,
-                                    mlrun.api.schemas.ProjectCreate(**project.dict()),
-                                )
-                            except Exception as exc:
-                                logger.warning(
-                                    "Failed creating missing project in consumer",
-                                    consumer_name=_consumer_name,
-                                    project=project.name,
-                                    exc=str(exc),
-                                )
+                    try:
+                        self._consumers[missing_consumer].create_project(
+                            session,
+                            mlrun.api.schemas.ProjectCreate(**project.dict()),
+                        )
+                    except Exception as exc:
+                        logger.warning(
+                            "Failed creating missing project in consumer",
+                            missing_consumer_name=missing_consumer,
+                            project_consumer_name=project_consumer_name,
+                            project_name=project_name,
+                            project=project,
+                            exc=str(exc),
+                        )
 
     def _run_on_all_consumers(
         self, method: str, *args, **kwargs

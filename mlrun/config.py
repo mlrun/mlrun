6d3fbf153493776674ed4a8326c356c108cf6b1e
Hedi Ingber <hedingber@gmail.com>
2020-12-10 00:39:57 +0200

    bump kaniko version

--- mlrun/config.py
+++ mlrun/config.py
@@ -53,7 +53,7 @@ default_config = {
     "igz_version": "",  # the version of the iguazio system the API is running on
     "spark_app_image": "",  # image to use for spark operator app runtime
     "spark_app_image_tag": "",  # image tag to use for spark opeartor app runtime
-    "kaniko_version": "v0.19.0",  # kaniko builder version
+    "kaniko_version": "v0.24.0",  # kaniko builder version
     "package_path": "mlrun",  # mlrun pip package
     "default_image": "python:3.6-jessie",
     "default_project": "default",  # default project name

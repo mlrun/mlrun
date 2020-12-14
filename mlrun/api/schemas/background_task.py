bc5f8d7303c3dc9c559a6c011023e6d897956e7d
Hedi Ingber <hedingber@gmail.com>
2020-12-15 00:24:42 +0200

    Add background tasks util to the API

---
+++ mlrun/api/schemas/background_task.py
@@ -0,0 +1,34 @@
+import datetime
+import enum
+import typing
+
+import pydantic
+
+import mlrun.api.schemas.object
+
+
+class BackgroundTaskState(str, enum.Enum):
+    succeeded = "succeeded"
+    failed = "failed"
+    running = "running"
+
+
+class BackgroundTaskMetadata(pydantic.BaseModel):
+    name: str
+    created: typing.Optional[datetime.datetime]
+    updated: typing.Optional[datetime.datetime]
+
+
+class BackgroundTaskSpec(pydantic.BaseModel):
+    pass
+
+
+class BackgroundTaskStatus(pydantic.BaseModel):
+    state: BackgroundTaskState
+
+
+class BackgroundTask(pydantic.BaseModel):
+    kind: mlrun.api.schemas.object.ObjectKind = pydantic.Field(mlrun.api.schemas.object.ObjectKind.feature_set, const=True)
+    metadata: BackgroundTaskMetadata
+    spec: BackgroundTaskSpec
+    status: BackgroundTaskStatus

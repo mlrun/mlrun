eeecd88ed3e7cab4a6b5447b4a8975192d83a672
Saar Cohen <saarc@iguazio.com>
2020-12-15 17:39:19 +0200

    small fix in tests

--- tests/api/api/feature_store/base.py
+++ tests/api/api/feature_store/base.py
@@ -17,10 +17,10 @@ def _list_and_assert_objects(
     assert response.status_code == HTTPStatus.OK.value
     response_body = response.json()
     assert entity_name in response_body
-    actual_result = len(response_body[entity_name])
+    number_of_entities = len(response_body[entity_name])
     assert (
-        actual_result == expected_number_of_entities
-    ), f"wrong number of {entity_name} in response - {actual_result} instead of {expected_number_of_entities}"
+        number_of_entities == expected_number_of_entities
+    ), f"wrong number of {entity_name} in response - {number_of_entities} instead of {expected_number_of_entities}"
     return response_body
 
 

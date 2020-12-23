d05643575fc2a7b10424fd032cde21f362bf946a
Hedi Ingber <hedingber@gmail.com>
2020-12-23 03:40:44 +0200

    Add nuclio client tests

--- tests/api/utils/clients/test_nuclio.py
+++ tests/api/utils/clients/test_nuclio.py
@@ -34,8 +34,11 @@ def test_get_project(
     project_labels = {
         "some-label": "some-label-value",
     }
+    project_annotations = {
+        "some-annotation": "some-annotation-value",
+    }
     response_body = _generate_project_body(
-        project_name, project_description, project_labels, with_spec=True,
+        project_name, project_description, project_labels, project_annotations, with_spec=True,
     )
     requests_mock.get(f"{api_url}/api/projects/{project_name}", json=response_body)
     project = nuclio_client.get_project(None, project_name)
@@ -45,14 +48,19 @@ def test_get_project(
         deepdiff.DeepDiff(project_labels, project.metadata.labels, ignore_order=True,)
         == {}
     )
+    assert (
+            deepdiff.DeepDiff(project_annotations, project.metadata.annotations, ignore_order=True, )
+            == {}
+    )
 
-    # now without description and labels
+    # now without description, labels and annotations
     response_body = _generate_project_body(project_name, with_spec=True)
     requests_mock.get(f"{api_url}/api/projects/{project_name}", json=response_body)
     project = nuclio_client.get_project(None, project_name)
     assert project.metadata.name == project_name
     assert project.spec.description is None
     assert project.metadata.labels is None
+    assert project.metadata.annotations is None
 
 
 def test_list_project(
@@ -64,10 +72,12 @@ def test_list_project(
         {"name": "project-name-1"},
         {"name": "project-name-2", "description": "project-description-2"},
         {"name": "project-name-3", "labels": {"key": "value"}},
+        {"name": "project-name-4", "annotations": {"annotation-key": "annotation-value"}},
         {
-            "name": "project-name-4",
+            "name": "project-name-5",
             "description": "project-description-4",
             "labels": {"key2": "value2"},
+            "annotations": {"annotation-key2": "annotation-value2"},
         },
     ]
     response_body = {
@@ -75,6 +85,7 @@ def test_list_project(
             mock_project["name"],
             mock_project.get("description"),
             mock_project.get("labels"),
+            mock_project.get("annotations"),
             with_spec=True,
         )
         for mock_project in mock_projects
@@ -92,6 +103,14 @@ def test_list_project(
             )
             == {}
         )
+        assert (
+            deepdiff.DeepDiff(
+                mock_projects[index].get("annotations"),
+                project.metadata.annotations,
+                ignore_order=True,
+            )
+            == {}
+        )
 
 
 def test_create_project(
@@ -104,6 +123,9 @@ def test_create_project(
     project_labels = {
         "some-label": "some-label-value",
     }
+    project_annotations = {
+        "some-annotation": "some-annotation-value",
+    }
 
     def verify_creation(request, context):
         assert (
@@ -112,6 +134,7 @@ def test_create_project(
                     project_name,
                     project_description,
                     project_labels,
+                    project_annotations,
                     with_namespace=False,
                 ),
                 request.json(),
@@ -126,7 +149,7 @@ def test_create_project(
         None,
         mlrun.api.schemas.Project(
             metadata=mlrun.api.schemas.ProjectMetadata(
-                name=project_name, labels=project_labels
+                name=project_name, labels=project_labels, annotations=project_annotations,
             ),
             spec=mlrun.api.schemas.ProjectSpec(description=project_description),
         ),
@@ -143,6 +166,9 @@ def test_store_project_creation(
     project_labels = {
         "some-label": "some-label-value",
     }
+    project_annotations = {
+        "some-annotation": "some-annotation-value",
+    }
 
     def verify_store_creation(request, context):
         assert (
@@ -151,6 +177,7 @@ def test_store_project_creation(
                     project_name,
                     project_description,
                     project_labels,
+                    project_annotations,
                     with_namespace=False,
                 ),
                 request.json(),
@@ -171,7 +198,7 @@ def test_store_project_creation(
         project_name,
         mlrun.api.schemas.Project(
             metadata=mlrun.api.schemas.ProjectMetadata(
-                name=project_name, labels=project_labels
+                name=project_name, labels=project_labels, annotations=project_annotations
             ),
             spec=mlrun.api.schemas.ProjectSpec(description=project_description),
         ),
@@ -188,8 +215,11 @@ def test_store_project_update(
     project_labels = {
         "some-label": "some-label-value",
     }
+    project_annotations = {
+        "some-annotation": "some-annotation-value",
+    }
     mocked_project_body = _generate_project_body(
-        project_name, labels={"label-key": "label-value"}, with_spec=True
+        project_name, with_spec=True
     )
 
     def verify_store_update(request, context):
@@ -199,6 +229,7 @@ def test_store_project_update(
                     project_name,
                     project_description,
                     project_labels,
+                    project_annotations,
                     with_namespace=False,
                 ),
                 request.json(),
@@ -218,7 +249,7 @@ def test_store_project_update(
         project_name,
         mlrun.api.schemas.Project(
             metadata=mlrun.api.schemas.ProjectMetadata(
-                name=project_name, labels=project_labels
+                name=project_name, labels=project_labels, annotations=project_annotations,
             ),
             spec=mlrun.api.schemas.ProjectSpec(description=project_description),
         ),
@@ -235,8 +266,11 @@ def test_patch_project(
     project_labels = {
         "some-label": "some-label-value",
     }
+    project_annotations = {
+        "some-annotation": "some-annotation-value",
+    }
     mocked_project_body = _generate_project_body(
-        project_name, labels={"label-key": "label-value"}, with_spec=True
+        project_name, labels={"label-key": "label-value"}, annotations={"annotation-key": "annotation-value"}, with_spec=True
     )
 
     def verify_patch(request, context):
@@ -244,6 +278,7 @@ def test_patch_project(
         expected_body = mocked_project_body
         expected_body["spec"]["description"] = project_description
         expected_body["metadata"]["labels"].update(project_labels)
+        expected_body["metadata"]["annotations"].update(project_annotations)
         assert (
             deepdiff.DeepDiff(expected_body, request.json(), ignore_order=True,) == {}
         )
@@ -257,7 +292,7 @@ def test_patch_project(
         None,
         project_name,
         {
-            "metadata": {"labels": project_labels},
+            "metadata": {"labels": project_labels, "annotations": project_annotations},
             "spec": {"description": project_description},
         },
     )
@@ -286,7 +321,7 @@ def test_delete_project(
 
 
 def _generate_project_body(
-    name=None, description=None, labels=None, with_namespace=True, with_spec=False
+    name=None, description=None, labels=None, annotations=None, with_namespace=True, with_spec=False
 ):
     body = {
         "metadata": {"name": name},
@@ -299,4 +334,6 @@ def _generate_project_body(
         body["metadata"]["namespace"] = "default-tenant"
     if labels:
         body["metadata"]["labels"] = labels
+    if annotations:
+        body["metadata"]["annotations"] = annotations
     return body

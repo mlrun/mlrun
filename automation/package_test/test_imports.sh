1465e321cef60c3cdd606a6e09ce37e3945a6d24
Hedi Ingber <hedingber@gmail.com>
2020-12-28 05:20:25 +0200

    Fix package tests

--- automation/package_test/test_imports.sh
+++ automation/package_test/test_imports.sh
@@ -7,15 +7,15 @@ test_import () {
     # Create an empty environment
     virtualenv venv
     source venv/bin/activate
-    pip install .["$1"]
+    pip install ."$1"
     python -c "$2"
     deactivate
     rm -rf venv
 }
 
-test_import ""                    "import mlrun"
-test_import "api"                 "import mlrun.api.main"
-test_import "dask"                "from dask.distributed import Client"
-test_import "v3io"                "import mlrun.datastore.v3io"
-test_import "s3"                  "import mlrun.datastore.s3"
-test_import "azure-blob-storage"  "import mlrun.datastore.azure_blob"
+test_import ""                      "import mlrun"
+test_import "[api]"                 "import mlrun.api.main"
+test_import "[dask]"                "from dask.distributed import Client"
+test_import "[v3io]"                "import mlrun.datastore.v3io"
+test_import "[s3]"                  "import mlrun.datastore.s3"
+test_import "[azure-blob-storage]"  "import mlrun.datastore.azure_blob"

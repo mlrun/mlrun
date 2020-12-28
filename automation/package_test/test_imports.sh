17d17b34924378a353f432a05ce2049977b7e859
Hedi Ingber <hedingber@gmail.com>
2020-12-28 05:43:21 +0200

    skip API test for python version 3.6

--- automation/package_test/test_imports.sh
+++ automation/package_test/test_imports.sh
@@ -15,7 +15,11 @@ test_import () {
 }
 
 test_import ""                      "import mlrun"
-test_import "[api]"                 "import mlrun.api.main"
+# API works only with python 3.7 and above
+if [ "$PYTHON_VERSION" != "3.6" ]
+  then
+    test_import "[api]"                 "import mlrun.api.main"
+fi
 test_import "[dask]"                "import dask"
 test_import "[v3io]"                "import mlrun.datastore.v3io"
 test_import "[s3]"                  "import mlrun.datastore.s3"

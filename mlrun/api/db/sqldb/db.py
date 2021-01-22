6a30e69b50cd3aa1b4c36914be97c325dfd94b84
Hedi Ingber <hedingber@gmail.com>
2021-01-22 22:18:05 +0200

    Adding data migration with tests

--- mlrun/api/db/sqldb/db.py
+++ mlrun/api/db/sqldb/db.py
@@ -206,6 +206,11 @@ class SQLDB(mlrun.api.utils.projects.remotes.member.Member, DBInterface):
     def store_artifact(
         self, session, key, artifact, uid, iter=None, tag="", project=""
     ):
+        self._store_artifact(session, key, artifact, uid, iter, tag, project)
+
+    def _store_artifact(
+            self, session, key, artifact, uid, iter=None, tag="", project="", tag_artifact=True,
+    ):
         project = project or config.default_project
         get_project_member().ensure_project(session, project)
         artifact = artifact.copy()

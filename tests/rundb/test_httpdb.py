cbc37849e018c832a4550b0facb5ffb8ee17a41d
Michael Livshits <livsmichael@gmail.com>
2021-01-26 12:41:41 +0200

    Added test, incomplete

--- tests/rundb/test_httpdb.py
+++ tests/rundb/test_httpdb.py
@@ -13,10 +13,14 @@
 # limitations under the License.
 
 import codecs
-import deepdiff
+import json
+import os
+import string
 from collections import namedtuple
+from datetime import datetime, timedelta
 from os import environ
 from pathlib import Path
+from random import randint, choice
 from shutil import rmtree
 from socket import socket
 from subprocess import Popen, run, PIPE, DEVNULL
@@ -24,21 +28,45 @@ from sys import executable
 from tempfile import mkdtemp
 from uuid import uuid4
 
+import deepdiff
+import pandas as pd
 import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+from v3io.dataplane import RaiseForStatus
+from v3io_frames import frames_pb2 as fpb2
+from v3io_frames.errors import CreateError
 
-from mlrun.api import schemas
 import mlrun.errors
+import mlrun.projects.project
 from mlrun import RunObject
+from mlrun.api import schemas
+from mlrun.api.api.endpoints.model_endpoints import (
+    get_endpoint_kv_record_by_id,
+    ENDPOINTS_TABLE_PATH,
+    ENDPOINT_EVENTS_TABLE_PATH,
+    V3IO_ACCESS_KEY,
+    V3IO_FRAMESD,
+    V3IO_API,
+)
+from mlrun.api.schemas import (
+    ModelEndpointMetadata,
+    ModelEndpointSpec,
+    ModelEndpoint,
+    ObjectStatus,
+)
 from mlrun.artifacts import Artifact
 from mlrun.db import RunDBError
 from mlrun.db.httpdb import HTTPRunDB
+from mlrun.utils.v3io_clients import get_v3io_client, get_frames_client
 from tests.conftest import wait_for_server, tests_root_directory
-import mlrun.projects.project
 
 project_dir_path = Path(__file__).absolute().parent.parent.parent
 Server = namedtuple("Server", "url conn workdir")
 
 docker_tag = "mlrun/test-api"
+
+ENV_PARAMS = {"V3IO_ACCESS_KEY", "V3IO_WEBAPI_PORT_8081_TCP", "FRAMESD_PORT_8081_TCP"}
 
 
 def free_port():
@@ -600,3 +628,259 @@ def _assert_projects(expected_project, project):
     )
     assert expected_project.spec.desired_state == project.spec.desired_state
     assert expected_project.spec.desired_state == project.status.state
+
+
+def _build_skip_message():
+    return f"One of the required environment params is not initialized ({', '.join(ENV_PARAMS)})"
+
+
+def _is_env_params_dont_exist() -> bool:
+    return not all((os.environ.get(r, False) for r in ENV_PARAMS))
+
+
+@pytest.mark.skipif(
+    _is_env_params_dont_exist(), reason=_build_skip_message(),
+)
+def test_clear_endpoint(create_server):
+    server: Server = create_server()
+    db: HTTPRunDB = server.conn
+
+    secrets = _get_model_endpoint_test_secrets()
+    endpoint = _mock_random_endpoint()
+
+    _write_endpoint_to_kv(endpoint)
+
+    kv_record = get_endpoint_kv_record_by_id(
+        secrets, endpoint.metadata.project, endpoint.id
+    )
+
+    assert kv_record
+
+    db.clear_endpoint_record(endpoint.metadata.project, endpoint.id)
+
+    kv_record = get_endpoint_kv_record_by_id(
+        secrets, endpoint.metadata.project, endpoint.id
+    )
+    assert not kv_record
+
+
+@pytest.mark.skipif(
+    _is_env_params_dont_exist(), reason=_build_skip_message(),
+)
+def test_list_endpoints(create_server):
+    server: Server = create_server()
+    db: HTTPRunDB = server.conn
+
+    secrets = _get_model_endpoint_test_secrets()
+    endpoints_in = [_mock_random_endpoint("active") for _ in range(5)]
+
+    for endpoint in endpoints_in:
+        _write_endpoint_to_kv(endpoint)
+
+    endpoints_out = db.list_model_endpoints(
+        "test", token=secrets[V3IO_ACCESS_KEY]
+    ).endpoints
+
+    endpoints_in_set = {json.dumps(e.dict(), sort_keys=True) for e in endpoints_in}
+    endpoints_out_set = {json.dumps(e.dict(), sort_keys=True) for e in endpoints_out}
+    endpoints_intersect = endpoints_in_set.intersection(endpoints_out_set)
+
+    assert len(endpoints_intersect) == 5
+
+
+@pytest.mark.skipif(
+    _is_env_params_dont_exist(), reason=_build_skip_message(),
+)
+def test_list_endpoints_filter(create_server):
+    server: Server = create_server()
+    db: HTTPRunDB = server.conn
+
+    secrets = _get_model_endpoint_test_secrets()
+    for i in range(5):
+        endpoint_details = _mock_random_endpoint()
+
+        if i < 1:
+            endpoint_details.spec.model = "filterme"
+
+        if i < 2:
+            endpoint_details.spec.function = "filterme"
+
+        if i < 3:
+            endpoint_details.metadata.tag = "filterme"
+
+        if i < 4:
+            endpoint_details.metadata.labels = {"filtermex": "1", "filtermey": "2"}
+
+        _write_endpoint_to_kv(endpoint_details)
+
+    filter_model = db.list_model_endpoints(
+        "test", model="filterme", token=secrets[V3IO_ACCESS_KEY]
+    ).endpoints
+    assert len(filter_model) == 1
+
+    filter_function = db.list_model_endpoints(
+        "test", function="filterme", token=secrets[V3IO_ACCESS_KEY]
+    ).endpoints
+    assert len(filter_function) == 2
+
+    filter_tag = db.list_model_endpoints(
+        "test", tag="filterme", token=secrets[V3IO_ACCESS_KEY]
+    ).endpoints
+    assert len(filter_tag) == 3
+
+    filter_labels = db.list_model_endpoints(
+        "test", labels=["filterme=1"], token=secrets[V3IO_ACCESS_KEY]
+    ).endpoints
+    assert len(filter_labels) == 4
+
+    filter_labels = db.list_model_endpoints(
+        "test", labels=["filterme=1", "filtermey=2"], token=secrets[V3IO_ACCESS_KEY]
+    ).endpoints
+    assert len(filter_labels) == 4
+
+    filter_labels = db.list_model_endpoints(
+        "test", labels=["filtermey=2"], token=secrets[V3IO_ACCESS_KEY]
+    ).endpoints
+    assert len(filter_labels) == 4
+
+
+@pytest.mark.skipif(
+    _is_env_params_dont_exist(), reason=_build_skip_message(),
+)
+def test_get_endpoint_metrics(create_server):
+    server: Server = create_server()
+    db: HTTPRunDB = server.conn
+
+    secrets = _get_model_endpoint_test_secrets()
+    start = datetime.utcnow()
+    frames = get_frames_client(container="projects", address=secrets[V3IO_FRAMESD])
+
+    for i in range(5):
+        endpoint = _mock_random_endpoint()
+        _write_endpoint_to_kv(endpoint)
+
+        frames.create(
+            backend="tsdb",
+            table=f"test/{ENDPOINT_EVENTS_TABLE_PATH}",
+            rate="10/m",
+            if_exists=1,
+        )
+
+        total = 0
+
+        dfs = []
+
+        for i in range(10):
+            count = randint(1, 10)
+            total += count
+            data = {
+                "predictions_per_second_count_1s": count,
+                "endpoint_id": endpoint.id,
+                "timestamp": start - timedelta(minutes=10 - i),
+            }
+            df = pd.DataFrame(data=[data])
+            dfs.append(df)
+
+        frames.write(
+            backend="tsdb",
+            table=f"test/{ENDPOINT_EVENTS_TABLE_PATH}",
+            dfs=dfs,
+            index_cols=["timestamp", "endpoint_id"],
+        )
+
+        response = db.get_model_endpoint(
+            "test", endpoint.id, metrics=True, token=secrets[V3IO_ACCESS_KEY]
+        )
+        assert len(response.metrics) != 0
+        assert response.metrics["predictions_per_second"] == "predictions_per_second"
+
+        response_total = sum((m[1] for m in first_metric["values"]))
+
+        assert total == response_total
+
+
+def _mock_random_endpoint(state: str = "") -> ModelEndpoint:
+    return ModelEndpoint(
+        metadata=ModelEndpointMetadata(
+            project="test",
+            tag=f"v{randint(0, 100)}",
+            labels={
+                f"{choice(string.ascii_letters)}": randint(0, 100) for _ in range(1, 5)
+            },
+        ),
+        spec=ModelEndpointSpec(
+            model=f"model_{randint(0, 100)}",
+            function=f"function_{randint(0, 100)}",
+            model_class="classifier",
+        ),
+        status=ObjectStatus(state=state),
+    )
+
+
+def _write_endpoint_to_kv(endpoint: ModelEndpoint):
+    secrets = _get_model_endpoint_test_secrets()
+    client = get_v3io_client(
+        endpoint=secrets[V3IO_API], access_key=secrets[V3IO_ACCESS_KEY]
+    )
+    client.kv.put(
+        container="projects",
+        table_path=f"{endpoint.metadata.project}/{ENDPOINTS_TABLE_PATH}/",
+        key=endpoint.id,
+        attributes={
+            "project": endpoint.metadata.project,
+            "function": endpoint.spec.function,
+            "model": endpoint.spec.model,
+            "tag": endpoint.metadata.tag,
+            "model_class": endpoint.spec.model_class,
+            "labels": json.dumps(endpoint.metadata.labels),
+            **{f"_{k}": v for k, v in endpoint.metadata.labels.items()},
+        },
+    )
+
+
+@pytest.fixture(autouse=True)
+def cleanup_endpoints(db: Session, client: TestClient):
+    secrets = _get_model_endpoint_test_secrets()
+
+    v3io = get_v3io_client(
+        endpoint=secrets[V3IO_API], access_key=secrets[V3IO_ACCESS_KEY]
+    )
+
+    frames = get_frames_client(container="projects", address=secrets[V3IO_FRAMESD])
+    try:
+        all_records = v3io.kv.new_cursor(
+            container="projects",
+            table_path=f"test/{ENDPOINTS_TABLE_PATH}",
+            raise_for_status=RaiseForStatus.never,
+        ).all()
+
+        all_records = [r["__name"] for r in all_records]
+
+        # Cleanup KV
+        for record in all_records:
+            v3io.kv.delete(
+                container="projects",
+                table_path=f"test/{ENDPOINTS_TABLE_PATH}",
+                key=record,
+                raise_for_status=RaiseForStatus.never,
+            )
+    except RuntimeError:
+        pass
+
+    try:
+        # Cleanup TSDB
+        frames.delete(
+            backend="tsdb",
+            table=f"test/{ENDPOINT_EVENTS_TABLE_PATH}",
+            if_missing=fpb2.IGNORE,
+        )
+    except CreateError:
+        pass
+
+
+def _get_model_endpoint_test_secrets():
+    return {
+        V3IO_ACCESS_KEY: os.environ.get("V3IO_ACCESS_KEY"),
+        V3IO_FRAMESD: os.environ.get("FRAMESD_PORT_8081_TCP").replace("tcp", "https"),
+        V3IO_API: os.environ.get("V3IO_WEBAPI_PORT_8081_TCP").replace("tcp", "https"),
+    }

4a26c600b230611b0e5d4ebc358ad452a4be4db1
yaron haviv <yaronh@iguazio.com>
2021-02-14 00:24:41 +0200

    add storey to requirements

--- mlrun/feature_store/sources.py
+++ mlrun/feature_store/sources.py
@@ -78,7 +78,7 @@ class DFSourceDriver:
         import storey
 
         return storey.DataframeSource(
-            dfs=self._df, key_column=key_column, time_column=time_column,
+            dfs=self._df, key_field=key_column, time_field=time_column,
         )
 
 

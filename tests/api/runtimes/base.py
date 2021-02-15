69ff8f8fd493da6fa4da3ca5d09f185a9aaaf4ae
Saar Cohen <saarc@iguazio.com>
2021-02-15 16:29:27 +0200

    Fixed according to review + added more tests

--- tests/api/runtimes/base.py
+++ tests/api/runtimes/base.py
@@ -8,6 +8,7 @@ from copy import deepcopy
 from mlrun.config import config as mlconf
 import json
 from mlrun.model import new_task
+import deepdiff
 
 logger = create_logger(level="debug", name="test-runtime")
 
@@ -15,6 +16,7 @@ logger = create_logger(level="debug", name="test-runtime")
 class TestRuntimeBase:
     def setup_method(self, method):
         self.namespace = mlconf.namespace = "test-namespace"
+        get_k8s().namespace = self.namespace
         self._logger = logger
         self.project = "test-project"
         self.name = "test-function"
@@ -52,7 +54,7 @@ class TestRuntimeBase:
                 state=container_state,
                 image=self.image_name,
                 image_id="must-provide-image-id",
-                name="must-provide-name",
+                name=self.name,
                 ready=True,
                 restart_count=0,
             )
@@ -81,9 +83,9 @@ class TestRuntimeBase:
             **kwargs,
         )
 
-    def _assert_labels_exist_in_pod_creation(self, labels: dict):
+    def _assert_labels(self, labels: dict, expected_class_name):
         expected_labels = {
-            "mlrun/class": "job",
+            "mlrun/class": expected_class_name,
             "mlrun/name": self.name,
             "mlrun/project": self.project,
             "mlrun/tag": "latest",
@@ -92,7 +94,7 @@ class TestRuntimeBase:
         for key in expected_labels:
             assert labels[key] == expected_labels[key]
 
-    def _assert_function_config_as_expected(
+    def _assert_function_config(
         self,
         config,
         expected_params,
@@ -107,16 +109,40 @@ class TestRuntimeBase:
         function_spec = config["spec"]
         assert function_spec["output_path"] == self.artifact_path
         if expected_params:
-            assert function_spec["parameters"] == expected_params
+            assert (
+                deepdiff.DeepDiff(
+                    function_spec["parameters"], expected_params, ignore_order=True
+                )
+                == {}
+            )
         if expected_inputs:
-            assert function_spec["inputs"] == expected_inputs
+            assert (
+                deepdiff.DeepDiff(
+                    function_spec["inputs"], expected_inputs, ignore_order=True
+                )
+                == {}
+            )
         if expected_hyper_params:
-            assert function_spec["hyperparams"] == expected_hyper_params
+            assert (
+                deepdiff.DeepDiff(
+                    function_spec["hyperparams"],
+                    expected_hyper_params,
+                    ignore_order=True,
+                )
+                == {}
+            )
         if expected_secrets:
-            assert function_spec["secret_sources"] == [expected_secrets]
+            assert (
+                deepdiff.DeepDiff(
+                    function_spec["secret_sources"],
+                    [expected_secrets],
+                    ignore_order=True,
+                )
+                == {}
+            )
 
     @staticmethod
-    def _assert_env_variables_set_correctly_in_pod_env(pod_env, expected_variables):
+    def _assert_pod_env(pod_env, expected_variables):
         for env_variable in pod_env:
             name = env_variable["name"]
             if name in expected_variables:
@@ -133,7 +159,7 @@ class TestRuntimeBase:
         container_spec = pod_spec.containers[0]
 
         pod_env = container_spec.env
-        self._assert_env_variables_set_correctly_in_pod_env(
+        self._assert_pod_env(
             pod_env,
             {
                 "V3IO_API": None,
@@ -149,42 +175,119 @@ class TestRuntimeBase:
             },
             "name": "v3io",
         }
-        assert pod_spec.volumes[0] == expected_volume
+        assert (
+            deepdiff.DeepDiff(pod_spec.volumes[0], expected_volume, ignore_order=True)
+            == {}
+        )
 
         expected_volume_mounts = [
             {"mountPath": "/v3io", "name": "v3io", "subPath": ""},
             {"mountPath": "/User", "name": "v3io", "subPath": f"users/{v3io_user}"},
         ]
-        assert container_spec.volume_mounts == expected_volume_mounts
+        assert (
+            deepdiff.DeepDiff(
+                container_spec.volume_mounts, expected_volume_mounts, ignore_order=True
+            )
+            == {}
+        )
+
+    def _assert_pvc_mount_configured(self, pvc_name, pvc_mount_path, volume_name):
+        args, _ = get_k8s().v1api.create_namespaced_pod.call_args
+        pod_spec = args[1].spec
+
+        expected_volume = {
+            "name": volume_name,
+            "persistentVolumeClaim": {"claimName": pvc_name},
+        }
+        assert (
+            deepdiff.DeepDiff(pod_spec.volumes[0], expected_volume, ignore_order=True)
+            == {}
+        )
+
+        expected_volume_mounts = [
+            {"mountPath": pvc_mount_path, "name": volume_name},
+        ]
+
+        container_spec = pod_spec.containers[0]
+        assert (
+            deepdiff.DeepDiff(
+                container_spec.volume_mounts, expected_volume_mounts, ignore_order=True
+            )
+            == {}
+        )
+
+    def _assert_secret_mount(self, volume_name, secret_name, default_mode, mount_path):
+        args, _ = get_k8s().v1api.create_namespaced_pod.call_args
+        pod_spec = args[1].spec
+
+        expected_volume = {
+            "name": volume_name,
+            "secret": {"defaultMode": default_mode, "secretName": secret_name},
+        }
+        assert (
+            deepdiff.DeepDiff(pod_spec.volumes[0], expected_volume, ignore_order=True)
+            == {}
+        )
+
+        expected_volume_mounts = [
+            {"mountPath": mount_path, "name": volume_name},
+        ]
+
+        container_spec = pod_spec.containers[0]
+        assert (
+            deepdiff.DeepDiff(
+                container_spec.volume_mounts, expected_volume_mounts, ignore_order=True
+            )
+            == {}
+        )
 
     def _assert_pod_create_called(
         self,
-        expected_params={},
-        expected_inputs={},
-        expected_hyper_params={},
-        expected_secrets={},
-        expected_limits={},
+        expected_runtime_class_name="job",
+        expected_params=None,
+        expected_inputs=None,
+        expected_hyper_params=None,
+        expected_secrets=None,
+        expected_limits=None,
+        expected_requests=None,
+        expected_env={},
     ):
         create_pod_mock = get_k8s().v1api.create_namespaced_pod
         create_pod_mock.assert_called_once()
         args, _ = create_pod_mock.call_args
-        # assert args[0] == self.namespace
+        assert args[0] == self.namespace
         pod_spec = args[1]
-        self._assert_labels_exist_in_pod_creation(pod_spec.metadata.labels)
+        self._assert_labels(pod_spec.metadata.labels, expected_runtime_class_name)
 
         container_spec = pod_spec.spec.containers[0]
+
         if expected_limits:
-            assert container_spec.resources["limits"] == expected_limits
+            assert (
+                deepdiff.DeepDiff(
+                    container_spec.resources["limits"],
+                    expected_limits,
+                    ignore_order=True,
+                )
+                == {}
+            )
+        if expected_requests:
+            assert (
+                deepdiff.DeepDiff(
+                    container_spec.resources["requests"],
+                    expected_requests,
+                    ignore_order=True,
+                )
+                == {}
+            )
 
         pod_env = container_spec.env
 
-        self._assert_env_variables_set_correctly_in_pod_env(
-            pod_env, {"MLRUN_NAMESPACE": self.namespace}
-        )
+        expected_env["MLRUN_NAMESPACE"] = self.namespace
+        self._assert_pod_env(pod_env, expected_env)
         for env_variable in pod_env:
             if env_variable["name"] == "MLRUN_EXEC_CONFIG":
                 function_config = json.loads(env_variable["value"])
-                self._assert_function_config_as_expected(
+                self._assert_function_config(
                     function_config,
                     expected_params,
                     expected_inputs,
@@ -193,15 +296,3 @@ class TestRuntimeBase:
                 )
 
         assert pod_spec.spec.containers[0].image == self.image_name
-
-        # print(f"POD spec:\n {args[1]}")
-
-    # Needed for tracking pod status/logs
-    @staticmethod
-    def _mock_get_pod():
-        get_k8s().v1api.read_namespaced_pod = unittest.mock.Mock()
-
-    # Needed for Vault functionality
-    @staticmethod
-    def _mock_read_namespaced_service_account():
-        get_k8s().v1api.read_namespaced_service_account = unittest.mock.Mock()
--- tests/api/runtimes/test_kubejob.py
+++ tests/api/runtimes/test_kubejob.py
@@ -1,23 +1,30 @@
+import os
 from tests.api.runtimes.base import TestRuntimeBase
 from mlrun.runtimes.kubejob import KubejobRuntime
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 from mlrun.runtimes.utils import generate_resources
 from mlrun.platforms import auto_mount
-import os
+import unittest.mock
+from mlrun.api.utils.singletons.k8s import get_k8s
+from mlrun.utils.vault import VaultStore
+from kubernetes import client
+from mlrun.config import config as mlconf
 
 
 class TestKubejobRuntime(TestRuntimeBase):
     def custom_setup(self):
         self._mock_create_namespaced_pod()
         self.image_name = "mlrun/mlrun:latest"
+        self.vault_secrets = ["secret1", "secret2", "AWS_KEY"]
+        self.vault_secret_name = "test-secret"
 
     def _generate_runtime(self):
         runtime = KubejobRuntime()
         runtime.spec.image = self.image_name
         return runtime
 
-    def test_kubejob_runtime_run_without_runspec(self, db: Session, client: TestClient):
+    def test_run_without_runspec(self, db: Session, client: TestClient):
         runtime = self._generate_runtime()
         self._execute_run(runtime)
         self._assert_pod_create_called()
@@ -28,7 +35,7 @@ class TestKubejobRuntime(TestRuntimeBase):
         self._execute_run(runtime, params=params, inputs=inputs)
         self._assert_pod_create_called(expected_params=params, expected_inputs=inputs)
 
-    def test_kubejob_runtime_run_with_runspec(self, db: Session, client: TestClient):
+    def test_run_with_runspec(self, db: Session, client: TestClient):
         task = self._generate_task()
         params = {"p1": "v1", "p2": 20}
         task.with_params(**params)
@@ -55,25 +62,100 @@ class TestKubejobRuntime(TestRuntimeBase):
             expected_secrets=secret_source,
         )
 
-    def test_kubejob_runtime_run_with_pod_modifications(
+    def test_run_with_resource_limits_and_requests(
         self, db: Session, client: TestClient
     ):
         runtime = self._generate_runtime()
 
-        expected_limits = generate_resources(2, 4, 4, "test/gpu")
+        gpu_type = "test/gpu"
+        expected_limits = generate_resources(2, 4, 4, gpu_type)
         runtime.with_limits(
             mem=expected_limits["memory"],
             cpu=expected_limits["cpu"],
-            gpus=expected_limits["test/gpu"],
-            gpu_type="test/gpu",
+            gpus=expected_limits[gpu_type],
+            gpu_type=gpu_type,
         )
 
-        # Set the env variable, so auto_mount() will pick it up and mount v3io
+        expected_requests = generate_resources(mem=2, cpu=3)
+        runtime.with_requests(
+            mem=expected_requests["memory"], cpu=expected_requests["cpu"]
+        )
+
+        self._execute_run(runtime)
+        self._assert_pod_create_called(
+            expected_limits=expected_limits, expected_requests=expected_requests
+        )
+
+    def test_run_with_mounts(self, db: Session, client: TestClient):
+        runtime = self._generate_runtime()
+
+        # Mount v3io - Set the env variable, so auto_mount() will pick it up and mount v3io
         v3io_access_key = "1111-2222-3333-4444"
+        v3io_user = "test-user"
         os.environ["V3IO_ACCESS_KEY"] = v3io_access_key
-        os.environ["V3IO_USERNAME"] = "test_user"
+        os.environ["V3IO_USERNAME"] = v3io_user
         runtime.apply(auto_mount())
 
         self._execute_run(runtime)
-        self._assert_pod_create_called(expected_limits=expected_limits)
-        self._assert_v3io_mount_configured("test_user", v3io_access_key)
+        self._assert_pod_create_called()
+        self._assert_v3io_mount_configured(v3io_user, v3io_access_key)
+
+        # Mount a PVC. Create a new runtime so we don't have both v3io and the PVC mounted
+        runtime = self._generate_runtime()
+        pvc_name = "test-pvc"
+        pvc_mount_path = "/volume/mount/path"
+        volume_name = "test-volume-name"
+        runtime.apply(auto_mount(pvc_name, pvc_mount_path, volume_name))
+
+        self._execute_run(runtime)
+        self._assert_pod_create_called()
+        self._assert_pvc_mount_configured(pvc_name, pvc_mount_path, volume_name)
+
+    # For now Vault is only supported in KubeJob, so it's here. Once it's relevant to other runtimes, this can
+    # move to the base.
+    def _mock_vault_functionality(self):
+        secret_dict = {key: "secret" for key in self.vault_secrets}
+        VaultStore.get_secrets = unittest.mock.Mock(return_value=secret_dict)
+
+        object_meta = client.V1ObjectMeta(
+            name="test-service-account", namespace=self.namespace
+        )
+        secret = client.V1ObjectReference(
+            name=self.vault_secret_name, namespace=self.namespace
+        )
+        service_account = client.V1ServiceAccount(
+            metadata=object_meta, secrets=[secret]
+        )
+        get_k8s().v1api.read_namespaced_service_account = unittest.mock.Mock(
+            return_value=service_account
+        )
+
+    def test_run_with_vault_secrets(self, db: Session, client: TestClient):
+        self._mock_vault_functionality()
+        runtime = self._generate_runtime()
+
+        task = self._generate_task()
+
+        task.metadata.project = self.project
+        secret_source = {
+            "kind": "vault",
+            "source": {"project": self.project, "secrets": self.vault_secrets},
+        }
+        task.with_secrets(secret_source["kind"], self.vault_secrets)
+        vault_url = "/url/for/vault"
+        mlconf.secret_stores.vault.remote_url = vault_url
+        mlconf.secret_stores.vault.token_path = vault_url
+
+        self._execute_run(runtime, runspec=task)
+
+        self._assert_pod_create_called(
+            expected_secrets=secret_source,
+            expected_env={
+                "MLRUN_SECRET_STORES__VAULT__ROLE": f"project:{self.project}",
+                "MLRUN_SECRET_STORES__VAULT__URL": vault_url,
+            },
+        )
+
+        self._assert_secret_mount(
+            "vault-secret", self.vault_secret_name, 420, vault_url
+        )

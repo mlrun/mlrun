4fcfcb81481c82779b13b533778a6d0f6e9ae778
katyak <katyak@iguazio.com>
2021-03-07 16:26:28 +0200

    minor fixes

--- tests/feature-store/test_flow.py
+++ tests/feature-store/test_flow.py
@@ -1,25 +1,15 @@
-import os
-from http import HTTPStatus
-
-from storey.dtypes import V3ioError
-
-import mlrun
 import pytest
 import pandas as pd
-import asyncio
 import random
 import string
-import aiohttp
 import os
 from tests.conftest import results, tests_root_directory
-
-from storey import MapClass, ReadCSV, build_flow, ReduceToDataFrame
+from storey import ReadCSV, build_flow, ReduceToDataFrame
 
 from mlrun.datastore.targets import CSVTarget
-from mlrun.utils import logger
 import mlrun.feature_store as fs
 from mlrun.config import config as mlconf
-from mlrun.feature_store import FeatureSet, Entity, run_ingestion_job
+from mlrun.feature_store import Entity
 from mlrun.data_types.data_types import ValueType
 
 local_dir = f"{tests_root_directory}/feature-store/"
@@ -41,7 +31,6 @@ def _generate_random_name():
 
 @pytest.mark.skipif(not has_db(), reason="no db access")
 def test_read_csv():
-
     csv_path = results_dir + _generate_random_name() + ".csv"
 
     init_store()
@@ -65,10 +54,10 @@ def test_read_csv():
     ]).run()
     termination_result = controller.await_termination()
 
-    expected = pd.DataFrame({0: ['ticker', 'MSFT', 'GOOG', 'AAPL'], 1: ['name', "Microsoft Corporation", "Alphabet Inc", "Apple Inc"],
+    expected = pd.DataFrame({0: ['ticker', 'MSFT', 'GOOG', 'AAPL'],
+                             1: ['name', "Microsoft Corporation", "Alphabet Inc", "Apple Inc"],
                              2: ['exchange', "NASDAQ", "NASDAQ", "NASDAQ"]})
 
     assert termination_result.equals(expected), f"{termination_result}\n!=\n{expected}"
 
     os.remove(csv_path)
-

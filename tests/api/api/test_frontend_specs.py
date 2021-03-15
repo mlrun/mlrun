7b11ae00b679e19d94e6ee71aec1945bfeae458e
Hedi Ingber <hedingber@gmail.com>
2021-03-15 04:51:38 +0200

    imports

--- tests/api/api/test_frontend_specs.py
+++ tests/api/api/test_frontend_specs.py
@@ -1,8 +1,8 @@
+import http
 import unittest.mock
-from http import HTTPStatus
 
-from fastapi.testclient import TestClient
-from sqlalchemy.orm import Session
+import fastapi.testclient
+import sqlalchemy.orm
 
 import mlrun.api.crud
 import mlrun.api.schemas
@@ -10,7 +10,9 @@ import mlrun.api.utils.clients.iguazio
 import mlrun.errors
 
 
-def test_get_frontend_specs(db: Session, client: TestClient) -> None:
+def test_get_frontend_specs(
+    db: sqlalchemy.orm.Session, client: fastapi.testclient.TestClient
+) -> None:
     grafana_url = "some-url.com"
     mlrun.api.utils.clients.iguazio.Client().get_grafana_service_url_if_exists = unittest.mock.Mock(
         return_value=grafana_url
@@ -18,7 +20,7 @@ def test_get_frontend_specs(db: Session, client: TestClient) -> None:
 
     # no cookie so no url
     response = client.get("/api/frontend-specs")
-    assert response.status_code == HTTPStatus.OK.value
+    assert response.status_code == http.HTTPStatus.OK.value
     frontend_spec = mlrun.api.schemas.FrontendSpec(**response.json())
     assert frontend_spec.jobs_dashboard_url is None
     assert (
@@ -29,7 +31,7 @@ def test_get_frontend_specs(db: Session, client: TestClient) -> None:
     response = client.get(
         "/api/frontend-specs", cookies={"session": "some-session-cookie"}
     )
-    assert response.status_code == HTTPStatus.OK.value
+    assert response.status_code == http.HTTPStatus.OK.value
     frontend_spec = mlrun.api.schemas.FrontendSpec(**response.json())
     assert (
         frontend_spec.jobs_dashboard_url

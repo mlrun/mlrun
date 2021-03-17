08e19a9456b7f29edb5f3116141cec098f1d565f
urihoenig <urih@iguaz.io>
2021-03-17 23:59:34 +0200

    [Feature store] Add delete methods (#809)

--- mlrun/feature_store/__init__.py
+++ mlrun/feature_store/__init__.py
@@ -21,6 +21,8 @@ __all__ = [
     "infer_metadata",
     "run_ingestion_job",
     "deploy_ingestion_service",
+    "delete_feature_set",
+    "delete_feature_vector",
     "Feature",
     "Entity",
     "FeatureSet",
@@ -31,6 +33,8 @@ __all__ = [
 from ..data_types import InferOptions, ValueType
 from ..features import Entity, Feature
 from .api import (
+    delete_feature_set,
+    delete_feature_vector,
     deploy_ingestion_service,
     get_offline_features,
     get_online_feature_service,

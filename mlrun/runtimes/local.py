8cb9501bdc4b57c803f52b8ceb2784471bcc147d
yaron haviv <yaronh@iguazio.com>
2021-03-30 01:48:56 +0300

    collect logs from pod when needed

--- mlrun/runtimes/local.py
+++ mlrun/runtimes/local.py
@@ -72,7 +72,7 @@ class ParallelRunner:
             resp, sout, serr = future.result()
             runobj = RunObject.from_dict(resp)
             try:
-                log_std(self._db_conn, runobj, sout, serr)
+                log_std(self._db_conn, runobj, sout, serr, skip=self.is_child)
                 resp = self._post_run(resp)
             except RunError as err:
                 resp = self._post_run(resp, err=str(err))

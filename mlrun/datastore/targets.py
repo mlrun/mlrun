6a68840ffcdc0a5e2e3787118b57527840f20826
yaron haviv <yaronh@iguazio.com>
2021-04-25 17:07:44 +0300

    use framesd from config

--- mlrun/datastore/targets.py
+++ mlrun/datastore/targets.py
@@ -432,7 +432,7 @@ class NoSqlTarget(BaseStoreTarget):
             container, path = split_path(path_with_container)
 
             frames_client = get_frames_client(
-                token=access_key, address=resolve_framesd(), container=container
+                token=access_key, address=config.v3io_framesd, container=container
             )
 
             frames_client.write("kv", path, df, index_cols=key_column, **kwargs)
@@ -520,7 +520,7 @@ class TSDBTarget(BaseStoreTarget):
         container, path = split_path(path_with_container)
 
         frames_client = get_frames_client(
-            token=access_key, address=resolve_framesd(), container=container,
+            token=access_key, address=config.v3io_framesd, container=container,
         )
 
         frames_client.write(
@@ -618,7 +618,3 @@ def _get_target_path(driver, resource):
     # todo: handle ver tag changes, may need to copy files?
     name = f"{name}-{version or 'latest'}"
     return f"{data_prefix}/{kind_prefix}/{name}{suffix}"
-
-
-def resolve_framesd():
-    return os.environ.get("V3IO_FRAMESD") or config.v3io_framesd

eaa7d1e6e55dc93a91d92a9a9037ad28ea3a4076
katyak <katyak@iguazio.com>
2021-04-28 13:49:45 +0300

    minor fix

--- mlrun/datastore/sources.py
+++ mlrun/datastore/sources.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from copy import copy
-from typing import Dict, Optional, Union
 from datetime import datetime
+from typing import Dict, Optional, Union
 
 import mlrun
 

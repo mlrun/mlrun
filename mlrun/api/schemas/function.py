aa7543342c85b8c17dce7a39bee82c877d936495
Hedi Ingber <hedingber@gmail.com>
2021-06-17 14:32:34 +0300

    build as well

--- mlrun/api/schemas/function.py
+++ mlrun/api/schemas/function.py
@@ -7,7 +7,9 @@ class FunctionState(str, enum.Enum):
     error = "error"  # represents deployment error
 
     deploying = "deploying"
-    # there is currently an  usage of the builder (lower) pod state as the function state, ideally these two would map
-    # to deploying but for backwards compatibility reasons we have to keep them
+    # there is currently an abuse usage of the builder (lower) pod state as the function state, ideally these two would
+    # map to deploying but for backwards compatibility reasons we have to keep them
     running = "running"
     pending = "pending"
+    # same goes for the build which is not coming from the pod, but is used and we can't just omit it for BC reasons
+    build = "build"

c1c6b06fce92d43b11db5f385c9e50b4940090ab
Hedi Ingber <hedingber@gmail.com>
2021-06-29 20:39:55 +0300

    Fixing and adding test

--- mlrun/api/utils/scheduler.py
+++ mlrun/api/utils/scheduler.py
@@ -23,7 +23,7 @@ from mlrun.utils import logger
 class Scheduler:
     def __init__(self):
         scheduler_config = json.loads(config.httpdb.scheduling.scheduler_config)
-        self._scheduler = AsyncIOScheduler(gconfig=scheduler_config)
+        self._scheduler = AsyncIOScheduler(gconfig=scheduler_config, prefix=None)
         # this should be something that does not make any sense to be inside project name or job name
         self._job_id_separator = "-_-"
         # we don't allow to schedule a job to run more then one time per X

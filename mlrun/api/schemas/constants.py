c63c1cf0b5f318cad7f02d65717e85590737d4fa
Hedi Ingber <hedingber@gmail.com>
2021-07-19 01:16:39 +0300

    Support check deletion strategy

--- mlrun/api/schemas/constants.py
+++ mlrun/api/schemas/constants.py
@@ -32,6 +32,7 @@ class DeletionStrategy(str, Enum):
     restricted = "restricted"
     cascade = "cascade"
     cascading = "cascading"
+    check = "check"
 
     @staticmethod
     def default():
@@ -52,6 +53,8 @@ class DeletionStrategy(str, Enum):
             return "restricted"
         elif self.is_cascading():
             return "cascading"
+        elif self.value == DeletionStrategy.check.value:
+            return "check"
         else:
             raise mlrun.errors.MLRunInvalidArgumentError(
                 f"Unknown deletion strategy: {self.value}"
@@ -62,6 +65,8 @@ class DeletionStrategy(str, Enum):
             return "restricted"
         elif self.is_cascading():
             return "cascading"
+        elif self.value == DeletionStrategy.check.value:
+            raise NotImplementedError("Iguazio does not support the check deletion strategy")
         else:
             raise mlrun.errors.MLRunInvalidArgumentError(
                 f"Unknown deletion strategy: {self.value}"

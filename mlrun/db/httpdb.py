6d9151e9fc124921e31b087caf34f4d13b7b76e0
Hedi Ingber <hedingber@gmail.com>
2021-07-21 01:15:42 +0300

    More "correct" import for urllib3.util.retry.Retry

--- mlrun/db/httpdb.py
+++ mlrun/db/httpdb.py
@@ -24,7 +24,7 @@ import kfp
 import requests
 import semver
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
+from urllib3.util.retry import Retry
 
 import mlrun
 import mlrun.projects

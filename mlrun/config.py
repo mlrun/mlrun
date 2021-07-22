bcc0bab47654f0ca93408c141cacabb4738b48a2
Michael Livshits <livsmichael@gmail.com>
2021-07-22 10:58:30 +0300

    fixed user space path

--- mlrun/config.py
+++ mlrun/config.py
@@ -188,7 +188,7 @@ default_config = {
         "drift_thresholds": {"default": {"possible_drift": 0.5, "drift_detected": 0.7}},
         "store_prefixes": {
             "default": "v3io:///users/pipelines/{project}/model-endpoints/{kind}",
-            "user_space": "v3io:///project/{project}/model-endpoints/{kind}"
+            "user_space": "v3io:///projects/{project}/model-endpoints/{kind}"
         },
     },
     "secret_stores": {

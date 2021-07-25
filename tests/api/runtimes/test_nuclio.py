f250dbf84984a93636797a66ffdac483ec3f52e8
AlonMaor14 <48641682+AlonMaor14@users.noreply.github.com>
2021-07-25 13:19:34 +0300

    [Nuclio] Support node selection (#1138)

--- tests/api/runtimes/test_nuclio.py
+++ tests/api/runtimes/test_nuclio.py
@@ -1,4 +1,5 @@
 import base64
+import json
 import os
 import unittest.mock
 
@@ -7,7 +8,7 @@ import nuclio
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
-from mlrun import code_to_function
+from mlrun import code_to_function, mlconf
 from mlrun.platforms.iguazio import split_path
 from mlrun.runtimes.constants import NuclioIngressAddTemplatedIngressModes
 from mlrun.runtimes.function import (
@@ -16,6 +17,7 @@ from mlrun.runtimes.function import (
     enrich_function_with_ingress,
     resolve_function_ingresses,
 )
+from mlrun.runtimes.pod import KubeResourceSpec
 from tests.api.runtimes.base import TestRuntimeBase
 
 
@@ -194,6 +196,42 @@ class TestNuclioRuntime(TestRuntimeBase):
         }
         self._assert_pod_env(env_config, expected_env)
 
+    def _assert_node_selections(
+        self,
+        kube_resource_spec: KubeResourceSpec,
+        expected_node_name=None,
+        expected_node_selector=None,
+        expected_affinity=None,
+    ):
+        args, _ = nuclio.deploy.deploy_config.call_args
+        deploy_spec = args[0]["spec"]
+
+        if expected_node_name:
+            assert deploy_spec["nodeName"] == expected_node_name
+
+        if expected_node_selector:
+            assert (
+                deepdiff.DeepDiff(
+                    deploy_spec["nodeSelector"],
+                    expected_node_selector,
+                    ignore_order=True,
+                )
+                == {}
+            )
+        if expected_affinity:
+
+            # deploy_spec returns affinity in CamelCase, V1Affinity is in snake_case
+            assert (
+                deepdiff.DeepDiff(
+                    kube_resource_spec._transform_affinity_to_k8s_class_instance(
+                        deploy_spec["affinity"]
+                    ),
+                    expected_affinity,
+                    ignore_order=True,
+                )
+                == {}
+            )
+
     def test_enrich_with_ingress_no_overriding(self, db: Session, client: TestClient):
         """
         Expect no ingress template to be created, thought its mode is "always",
@@ -297,6 +335,64 @@ class TestNuclioRuntime(TestRuntimeBase):
         deploy_nuclio_function(function)
         self._assert_deploy_called_basic_config()
         self._assert_nuclio_v3io_mount(local_path, remote_path)
+
+    def test_deploy_with_node_selection(self, db: Session, client: TestClient):
+        function = self._generate_runtime("nuclio")
+
+        node_name = "some-node-name"
+        function.with_node_selection(node_name=node_name)
+
+        deploy_nuclio_function(function)
+        self._assert_deploy_called_basic_config()
+        self._assert_node_selections(function.spec, expected_node_name=node_name)
+
+        function = self._generate_runtime("nuclio")
+
+        node_selector = {
+            "label-1": "val1",
+            "label-2": "val2",
+        }
+        mlconf.default_function_node_selector = base64.b64encode(
+            json.dumps(node_selector).encode("utf-8")
+        )
+        function.with_node_selection(node_selector=node_selector)
+        deploy_nuclio_function(function)
+        self._assert_deploy_called_basic_config(call_count=2)
+        self._assert_node_selections(
+            function.spec, expected_node_selector=node_selector
+        )
+
+        function = self._generate_runtime("nuclio")
+
+        node_selector = {
+            "label-3": "val3",
+            "label-4": "val4",
+        }
+        function.with_node_selection(node_selector=node_selector)
+        deploy_nuclio_function(function)
+        self._assert_deploy_called_basic_config(call_count=3)
+        self._assert_node_selections(
+            function.spec, expected_node_selector=node_selector
+        )
+
+        function = self._generate_runtime("nuclio")
+        affinity = self._generate_affinity()
+
+        function.with_node_selection(affinity=affinity)
+        deploy_nuclio_function(function)
+        self._assert_deploy_called_basic_config(call_count=4)
+        self._assert_node_selections(function.spec, expected_affinity=affinity)
+
+        function = self._generate_runtime("nuclio")
+        function.with_node_selection(node_name, node_selector, affinity)
+        deploy_nuclio_function(function)
+        self._assert_deploy_called_basic_config(call_count=5)
+        self._assert_node_selections(
+            function.spec,
+            expected_node_name=node_name,
+            expected_node_selector=node_selector,
+            expected_affinity=affinity,
+        )
 
     def test_load_function_with_source_archive_git(self):
         fn = self._generate_runtime("nuclio")

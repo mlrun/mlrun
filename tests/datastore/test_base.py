bf4ed17c9a59a0121ef8be3348e007d3c4af459c
Greg <gregory.hayes@ecolab.com>
2021-07-25 20:45:33 +0000

    Added test to load mlrun store object into dask dataframe

--- tests/datastore/test_base.py
+++ tests/datastore/test_base.py
@@ -1,3 +1,4 @@
+import dask.dataframe as dd
 import mlrun.datastore
 
 
@@ -6,3 +7,10 @@ def test_http_fs_parquet_as_df():
         "https://s3.wasabisys.com/iguazio/data/market-palce/aggregate/metrics.pq"
     )
     data_item.as_df()
+
+def test_load_object_into_dask_dataframe():
+    data_item = mlrun.datastore.store_manager.object(
+        "s3://iguazio/data/market-palce/aggregate/metrics.pq"
+    )
+    ddf = data_item.as_df(df_module = dd)
+    assert hasattr(ddf, "dask")

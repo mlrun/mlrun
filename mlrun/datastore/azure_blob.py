3636c2295440279be2e04cb0a1469dc7a4d8d036
Greg Hayes <hayesgb@gmail.com>
2021-07-28 18:09:31 -0500

    Linting

--- mlrun/datastore/azure_blob.py
+++ mlrun/datastore/azure_blob.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pathlib import Path
 import time
+from pathlib import Path
 
 import fsspec
 from azure.storage.blob import BlobServiceClient

9d208419bcfa7025bc57ea3fce2e2284126a8bad
katyak <katyak@iguazio.com>
2021-07-29 13:14:28 +0300

    cleanup

--- mlrun/feature_store/api.py
+++ mlrun/feature_store/api.py
@@ -246,7 +246,6 @@ def ingest(
             overwrite = False
         else:
             overwrite = True
-    from time import sleep
 
     if run_config:
         # remote job execution
@@ -254,11 +253,6 @@ def ingest(
         source, run_config.parameters = set_task_params(
             featureset, source, targets, run_config.parameters, infer_options
         )
-        print("going to sleep2")
-
-        sleep(90)
-
-        print("waking up2")
         name = f"{featureset.metadata.name}_ingest"
         return run_ingestion_job(
             name, featureset, run_config, source.schedule, spark_context
@@ -277,12 +271,6 @@ def ingest(
             raise mlrun.errors.MLRunInvalidArgumentError(
                 "data source was not specified"
             )
-
-        print("going to sleep")
-
-        sleep(90)
-
-        print("waking up")
 
         if source.schedule and featureset.status.targets and featureset.status.targets[0].last_written:
             min_time = datetime.fromisoformat(featureset.status.targets[0].last_written)

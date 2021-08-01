c44ed49202efe994ec7effc0b1b2427f9e12c35f
Dina Nimrodi <dinan@iguazio.com>
2021-08-01 16:26:16 +0300

    lint

--- mlrun/feature_store/feature_set.py
+++ mlrun/feature_store/feature_set.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import warnings
-from typing import TYPE_CHECKING, List, Optional
+from typing import List
 
 import pandas as pd
 

a9305a3bbbed3ab94f462e7efd9d4c785d2b866f
Hedi Ingber <hedingber@gmail.com>
2021-08-01 20:30:58 +0300

    Run things in threadpool

--- mlrun/api/api/endpoints/artifacts.py
+++ mlrun/api/api/endpoints/artifacts.py
@@ -11,6 +11,7 @@ from mlrun.api import schemas
 from mlrun.api.api import deps
 from mlrun.api.api.utils import log_and_raise
 from mlrun.api.utils.singletons.db import get_db
+import mlrun.api.utils.singletons.project_member
 from mlrun.config import config
 from mlrun.utils import logger
 
@@ -29,10 +30,10 @@ async def store_artifact(
     auth_verifier: deps.AuthVerifier = Depends(deps.AuthVerifier),
     db_session: Session = Depends(deps.get_db_session),
 ):
-    mlrun.api.utils.singletons.project_member.get_project_member().ensure_project(
+    await run_in_threadpool(mlrun.api.utils.singletons.project_member.get_project_member().ensure_project,
         db_session, project, auth_info=auth_verifier.auth_info
     )
-    mlrun.api.utils.clients.opa.Client().query_resource_permissions(
+    await run_in_threadpool(mlrun.api.utils.clients.opa.Client().query_resource_permissions,
         mlrun.api.schemas.AuthorizationResourceTypes.artifact,
         project,
         key,

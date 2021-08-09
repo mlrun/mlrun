9872d8c5d2c82b371b02e70ce89ff4f3bcb3077b
Greg Hayes <hayesgb@gmail.com>
2021-08-09 06:08:10 -0500

    Reduced size of test dfs in test_dask_to_dataset.py so tests run in docker images

--- tests/artifacts/test_dask_to_dataset.py
+++ tests/artifacts/test_dask_to_dataset.py
@@ -13,15 +13,14 @@ def test_dataset_preview_size_limit_from_large_dask_dataframe(monkeypatch):
     convert any Dask DataFrames of size <1GB to Pandas, else use Dask to create the artifact.
     """
     # Set a MAX_DDF_SIZE param to simplify testing
-    monkeypatch.setattr(mlrun, "artifacts.dataset.max_ddf_size", 0.3)
-    # mlrun.artifacts.dataset.max_ddf_size = 0.3
+    monkeypatch.setattr(mlrun.artifacts.dataset, "max_ddf_size", 0.001)
 
     print("Creating dataframe and setting memory limit")
-    A = numpy.random.random_sample(size=(9000000, 6))
+    A = numpy.random.random_sample(size=(50000, 6))
     df = pandas.DataFrame(data=A, columns=list("ABCDEF"))
     print("Verify the memory size of the dataframe is >400MB")
-    assert (df.memory_usage().sum() // 1e6) > 400
-    ddf = dd.from_pandas(df, npartitions=10)
+    assert (df.memory_usage().sum() // 1e3) > 200
+    ddf = dd.from_pandas(df, npartitions=4)
     artifact = mlrun.artifacts.dataset.DatasetArtifact(df=ddf)
     assert len(artifact.preview) == mlrun.artifacts.dataset.default_preview_rows_length
 
@@ -37,15 +36,16 @@ def test_dataset_preview_size_limit_from_large_dask_dataframe(monkeypatch):
     # For a large DDF, sample 20% of the rows for
     # creating the preview
     assert len(artifact.preview) == len(ddf.sample(frac=0.2).compute().values.tolist())
+    print("Passed assertion on preview")
 
     # more than allowed columns
     number_of_columns = mlrun.artifacts.dataset.max_preview_columns * 3
     data_frame = pandas.DataFrame(
-        numpy.random.randint(0, 10, size=(200000, number_of_columns)),
+        numpy.random.randint(0, 10, size=(2000, number_of_columns)),
         columns=list(range(number_of_columns)),
     )
     ddf = dd.from_pandas(data_frame, npartitions=4)
-    ddf = ddf.repartition(partition_size="100MB")
+    ddf = ddf.repartition(partition_size="1MB")
     artifact = mlrun.artifacts.dataset.DatasetArtifact(df=ddf)
     assert len(artifact.preview[0]) == mlrun.artifacts.dataset.max_preview_columns
     assert artifact.stats is None
@@ -67,20 +67,18 @@ def test_dataset_preview_size_limit_from_large_dask_dataframe(monkeypatch):
     assert artifact.stats is None
 
 
-def test_dataset_preview_size_limit_from_small_dask_dataframe(make_client):
-    A = numpy.random.random_sample(size=(10000, 6))
+def test_dataset_preview_size_limit_from_small_dask_dataframe():
+    print("Starting preview for small dask dataframe")
+    A = numpy.random.random_sample(size=(100, 6))
     df = pandas.DataFrame(data=A, columns=list("ABCDEF"))
     ddf = dd.from_pandas(df, npartitions=4).persist()
-    print("Done making dataframe")
     artifact = mlrun.artifacts.dataset.DatasetArtifact(df=ddf)
     assert len(artifact.preview) == mlrun.artifacts.dataset.default_preview_rows_length
-    print("passed length assertion on preview")
 
     # override limit
     limit = 25
     artifact = mlrun.artifacts.dataset.DatasetArtifact(df=ddf, preview=limit)
     assert len(artifact.preview) == limit
-    print("passed limit override")
 
     # ignore limits
     artifact = mlrun.artifacts.dataset.DatasetArtifact(
@@ -88,6 +86,7 @@ def test_dataset_preview_size_limit_from_small_dask_dataframe(make_client):
     )
     # For a small DDF (<1GB), convert to Pandas
     assert len(artifact.preview) == len(ddf)
+    print("passed length assertion on preview")
 
     # more than allowed columns
     number_of_columns = mlrun.artifacts.dataset.max_preview_columns * 3

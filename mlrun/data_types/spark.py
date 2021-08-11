3e05b95831c771adcb379072b796072e9afd7d24
katyak <katyak@iguazio.com>
2021-08-11 14:18:36 +0300

    wip

--- mlrun/data_types/spark.py
+++ mlrun/data_types/spark.py
@@ -132,11 +132,15 @@ def get_df_stats_spark(df, options, num_bins=20):
     if InferOptions.get_common_options(options, InferOptions.Index):
         df = df.select("*").withColumn("id", funcs.monotonically_increasing_id())
 
-    describe_df = df.toPandas().set_index("summary")
+#    describe_df = df.describe().toPandas()
+#    describe_df = describe_df.set_index("summary")
+
+    describe_df = df.toPandas().describe(include="all", percentiles=[], datetime_is_numeric=True)
+    describe_df = describe_df.set_index("summary")
+
+#    describe_df = df.toPandas().set_index("summary")
     results_dict = {}
-    for col, values in describe_df.describe(
-        include="all", percentiles=[], datetime_is_numeric=True
-    ).items():
+    for col, values in describe_df.items():
         stats_dict = {}
         for stat, val in values.dropna().items():
             if isinstance(val, (float, np.floating, np.float64)):

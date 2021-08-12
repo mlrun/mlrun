0a98c085bc22908e21c341282d948e01ea49a82e
AlonMaor14 <maoralo@post.bgu.ac.il>
2021-08-12 11:16:31 +0300

    comment move

--- mlrun/api/api/endpoints/healthz.py
+++ mlrun/api/api/endpoints/healthz.py
@@ -6,10 +6,11 @@ import mlrun.api.schemas
 router = APIRouter()
 
 
-# TODO: From 0.7.0 client uses the /client-spec endpoint,
-#  when this is the oldest relevant client, remove this logic from the healthz endpoint
 @router.get(
     "/healthz", response_model=mlrun.api.schemas.ClientSpec,
 )
 def health():
+
+    # TODO: From 0.7.0 client uses the /client-spec endpoint,
+    #  when this is the oldest relevant client, remove this logic from the healthz endpoint
     return mlrun.api.crud.client_spec.ClientSpec().get_client_spec()

4f0fb48e5f4d5d890cba3cb4aab9bb03dd183b36
Hedi Ingber <hedingber@gmail.com>
2021-08-15 05:36:16 +0300

    linting

--- mlrun/__main__.py
+++ mlrun/__main__.py
@@ -597,9 +597,7 @@ def get(kind, name, selector, namespace, uid, project, tag, db, extra_args):
         run_db = get_run_db(db or mlconf.dbpath)
         if name:
             # the runtime identifier is its kind
-            runtime = run_db.list_runtime_resources(
-                kind=name, label_selector=selector
-            )
+            runtime = run_db.list_runtime_resources(kind=name, label_selector=selector)
             print(dict_to_yaml(runtime.dict()))
             return
         runtimes = run_db.list_runtime_resources(label_selector=selector)

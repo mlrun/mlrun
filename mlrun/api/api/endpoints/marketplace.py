c5e13bff25003731c0b77bded5cbdde6aed67df6
Saar Cohen <saarc@iguazio.com>
2021-08-18 10:32:26 +0300

    add missing import

--- mlrun/api/api/endpoints/marketplace.py
+++ mlrun/api/api/endpoints/marketplace.py
@@ -6,6 +6,7 @@ from fastapi import APIRouter, Depends, Query, Response
 from sqlalchemy.orm import Session
 
 import mlrun
+import mlrun.api.utils.clients.opa
 from mlrun.api.api.deps import AuthVerifierDep
 from mlrun.api.crud.marketplace import Marketplace
 from mlrun.api.schemas import AuthorizationAction

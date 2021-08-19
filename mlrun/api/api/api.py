ba992b3fe0768c905dc0b837c5e215364ea4b095
Saar Cohen <saarc@iguazio.com>
2021-08-19 16:16:51 +0300

    adding router dependency

--- mlrun/api/api/api.py
+++ mlrun/api/api/api.py
@@ -86,4 +86,8 @@ api_router.include_router(
 )
 api_router.include_router(grafana_proxy.router, tags=["grafana", "model-endpoints"])
 api_router.include_router(model_endpoints.router, tags=["model-endpoints"])
-api_router.include_router(marketplace.router, tags=["marketplace"])
+api_router.include_router(
+    marketplace.router,
+    tags=["marketplace"],
+    dependencies=[Depends(deps.AuthVerifierDep)],
+)

24b093d754d546e9f59fcbee5aa68d3f94580de5
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-08-22 18:46:19 +0300

    Update mlrun_interface.py

--- mlrun/frameworks/sklearn/mlrun_interface.py
+++ mlrun/frameworks/sklearn/mlrun_interface.py
@@ -1,58 +1,63 @@
+from mlrun.frameworks._common import MLRunInterface
+import pandas as pd
+from mlrun.mlutils.plots import eval_model_v2
+import pickle
+import joblib
+
 class SklearnMLRunInterface(MLRunInterface):
     """
     MLRun model is for enabling additional features supported by MLRun in keras. With MLRun model one can apply horovod
     and use auto logging with ease.
     """
-    
+
     # MLRun's context default name:
     DEFAULT_CONTEXT_NAME = "mlrun-sklearn"
-    
+
     @classmethod
-    def add_interface(cls, model, context, *args, **kwargs):
+    def add_interface(cls, model, context, data, *args, **kwargs):
         """
         Wrap the given model with MLRun model features, providing it with MLRun model attributes including its
         parameters and methods.
         :param model: The model to wrap.
+        :param data:
         :return: The wrapped model.
         """
 
         # Wrap the fit method:
-        def fit_wrapper(fit_method):
+        def fit_wrapper(fit_method, **kwargs):
             def wrapper(*args, **kwargs):
-                print('X_train',args[0])
-                print('X_test',args[1])
-            
-                context.log_dataset('train_set', 
-                                    df=pd.concat([args[0], args[1]], axis=1),
-                                    format='csv', index=False, 
+                context.log_dataset('train_set',
+                                    df=pd.concat([data['X_train'], data['y_train']], axis=1),
+                                    format='csv', index=False,
                                     artifact_path=context.artifact_subpath('data'))
-                
+
                 # Call the original fit method
                 fit_method(*args, **kwargs)
-                
+
                 # Original fit method
                 setattr(model, "fit", fit_method)
-                
+
                 # Post fit
-                post_fit(*args, **kwargs)
+                if data.get("X_test") is not None:
+                    post_fit(*args, **kwargs)
+
             return wrapper
-        setattr(model, "fit", fit_wrapper(model.fit))
-        
+
+        setattr(model, "fit", fit_wrapper(model.fit, **kwargs))
+
         def post_fit(*args, **kwargs):
-            
             # Evaluate model results and get the evaluation metrics
-            eval_metrics = eval_model_v2(context, X_test, y_test, model)
-            
+            eval_metrics = eval_model_v2(context, data['X_test'], data['y_test'], model)
 
             # Model Parameters
-            model_parameters = {key: str(item) for key,item in model.get_params().items()}      
-                        
+            model_parameters = {key: str(item) for key, item in model.get_params().items()}
+
             # Log model
-            context.log_model("model",
-                              body=dumps(model),
-                              parameters = model_parameters,
-                              artifact_path=context.artifact_subpath("models"),
-                              extra_data = eval_metrics,
-                              model_file="model.pkl",
-                              metrics=context.results,
-                              labels={"class": str(model.__class__)})
+            # context.log_model("model",
+            #                   # body=dumps(model),
+            #                   parameters=model_parameters,
+            #                   artifact_path=context.artifact_subpath("models"),
+            #                   # extra_data=eval_metrics,
+            #                   model_file="model.pkl",
+            #                   metrics=context.results,
+            #                   labels={"class": str(model.__class__)})

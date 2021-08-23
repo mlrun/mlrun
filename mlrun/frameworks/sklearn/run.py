027b0e14ef1df70e6937f2d0e3b2fa75acf84344
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-08-23 14:29:20 +0300

    Adding regression

--- mlrun/frameworks/sklearn/run.py
+++ mlrun/frameworks/sklearn/run.py
@@ -7,6 +7,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import pandas as pd
+from sklearn import linear_model
+from sklearn.datasets import load_boston
 
 def classification():
     classification_models = [
@@ -29,11 +31,38 @@ def classification():
     for m in classification_models:
         model = apply_mlrun(m, X_train=X_train, X_test=X_test,
                             y_train=y_train, y_test=y_test)
-        model.fit(X_train, y_train.values.reshape(-1, ))
+        model.save()
+        # model.fit(X_train, y_train.values.reshape(-1, ))
+        # pred = model.predict(X_test)
+        # print(model,pred)
+
+def regression():
+    # Import Regression Models
+    regression_models = [linear_model.LinearRegression(),
+                         linear_model.Ridge(),
+                         linear_model.Lasso(),
+                         linear_model.TweedieRegressor()]
+
+    # Import
+    boston = load_boston()
+    bos = pd.DataFrame(boston['data'], columns=boston['feature_names'])
+    bos['PRICE'] = boston['target']
+
+    X = bos.drop(['PRICE'], axis=1)
+    y = bos['PRICE']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    for m in regression_models:
+        model = apply_mlrun(m, X_train=X_train, X_test=X_test,
+                            y_train=y_train, y_test=y_test)
+
+        #     model = apply_mlrun(m, X_train=X_train,
+        #                         y_train=y_train)
+        model.fit(X_train, y_train)
         pred = model.predict(X_test)
-        print(model,pred)
+        print(pred)
+
+# classification()
 
 
-classification()
-
-
+regression()

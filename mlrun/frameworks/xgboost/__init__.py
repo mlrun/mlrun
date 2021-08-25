fea138c25ff48c40595b2178c2e277e26633b878
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-08-25 20:33:48 +0300

    Update __init__.py

--- mlrun/frameworks/xgboost/__init__.py
+++ mlrun/frameworks/xgboost/__init__.py
@@ -16,7 +16,7 @@ def apply_mlrun(
     :return: The model with MLRun's interface.
     """
     if context is None:
-        context = mlrun.get_or_create_ctx(XGBMLRunInterface.DEFAULT_CONTEXT_NAME)
+        context = mlrun.get_or_create_ctx('mlrun_xgb')
         
     # Add MLRun's interface to the model:
     XGBMLRunInterface.add_interface(model, context, kwargs)

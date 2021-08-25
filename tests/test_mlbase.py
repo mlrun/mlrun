19d0cae43576bbbf4bb6d672c5c833de78394d92
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-08-25 19:52:17 +0300

    Update test_mlbase.py

--- tests/test_mlbase.py
+++ tests/test_mlbase.py
@@ -28,8 +28,7 @@ def run_mlbase_sklearn_classification(context):
 
 
 def run_mlbase_xgboost_regression(context):
-    model = xgb.XGBRegressor(objective='reg:linear', colsample_bytree=0.3, learning_rate=0.1,
-                             max_depth=5, alpha=10, n_estimators=10)
+    model = xgb.XGBRegressor()
     X_train, X_test, y_train, y_test = get_dataset(classification=False)
     model = apply_mlrun(model, context, X_train=X_train,
                         y_train=y_train, X_test=X_test, y_test=y_test)

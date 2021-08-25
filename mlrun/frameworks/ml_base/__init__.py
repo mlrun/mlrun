4fe0093b9abcd533cdb832df177850370a7c6d5a
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-08-25 20:06:11 +0300

    Create __init__.py

---
+++ mlrun/frameworks/ml_base/__init__.py
@@ -0,0 +1,24 @@
+import mlrun
+from mlrun.frameworks.mlbase.mlrun_interface import MLBaseMLRunInterface
+from mlrun.frameworks.sklearn.model_handler import SklearnModelHandler
+
+def apply_mlrun(
+    model,
+    context: mlrun.MLClientCtx = None,
+    **kwargs):
+    
+    """
+    Wrap the given model with MLRun model, saving the model's attributes and methods while giving it mlrun's additional
+    features.
+    :param model:       The model to wrap.
+    :param context:     MLRun context to work with. If no context is given it will be retrieved via
+                        'mlrun.get_or_create_ctx(None)'
+    :return: The model with MLRun's interface.
+    """
+    if context is None:
+        context = mlrun.get_or_create_ctx(MLBaseMLRunInterface.DEFAULT_CONTEXT_NAME)
+        
+    # Add MLRun's interface to the model:
+    data = kwargs
+    MLBaseMLRunInterface.add_interface(model, context, data)
+    return model

bf2cfb223b9647e8b1616a7afc0d3ff96b681075
Ben <benbd86@gmail.com>
2021-08-26 13:12:34 +0300

    fmt and lint

--- mlrun/datastore/v3io.py
+++ mlrun/datastore/v3io.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import time
 import re
+import time
 from copy import deepcopy
 from datetime import datetime
 

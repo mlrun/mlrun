49b1acc18a6a331ae1da8eee36d6b589607d0dd8
Hedi Ingber <hedingber@gmail.com>
2021-08-29 22:24:48 +0300

    Remove stupid check

--- tests/system/examples/basics/test_db.py
+++ tests/system/examples/basics/test_db.py
@@ -62,15 +62,3 @@ class TestDB(TestMLRunSystem):
                     artifact_exists = True
                     break
             assert artifact_exists
-
-        runtimes = self._run_db.list_runtime_resources()
-        assert len(runtimes.dict()) == len(
-            mlrun.runtimes.RuntimeKinds.runtime_with_handlers()
-        )
-        for runtime_kind in mlrun.runtimes.RuntimeKinds.runtime_with_handlers():
-            runtime_exists = False
-            for runtime in runtimes:
-                if runtime.kind == runtime_kind:
-                    runtime_exists = True
-                    break
-            assert runtime_exists

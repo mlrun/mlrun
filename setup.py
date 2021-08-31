6212f46d27e32b9ecd315aa588f0938b201bc774
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-08-31 18:59:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ setup(
         "mlrun.frameworks.pytorch.callbacks",
         "mlrun.frameworks.keras",
         "mlrun.frameworks.keras.callbacks",
-        "mlrun.frameworks.mlbase",
+        "mlrun.frameworks.ml_base",
         "mlrun.frameworks.sklearn",
         "mlrun.frameworks.xgboost",
     ],

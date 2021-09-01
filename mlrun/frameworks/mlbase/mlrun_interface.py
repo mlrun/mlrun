96e05a1eddb0e3b27d880d05230093dc37cce22b
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-09-01 15:29:14 +0300

    Update mlrun_interface.py

--- mlrun/frameworks/mlbase/mlrun_interface.py
+++ mlrun/frameworks/mlbase/mlrun_interface.py
@@ -30,62 +30,70 @@ class MLBaseMLRunInterface(MLRunInterface):
                 y_train = args[1]
                 train_set = pd.concat([X_train, y_train], axis=1)
                 train_set.reset_index(drop=True, inplace=True)
-                
-                context.log_dataset('train_set',
-                                    df=train_set,
-                                    format='csv', index=False,
-                                    artifact_path=context.artifact_subpath('data'),
-                                    labels={"class": str(model.__class__)},
-                                    )
 
                 # Call the original fit method
                 fit_method(*args, **kwargs)
 
                 # Original fit method
                 setattr(model, "fit", fit_method)
+                
                 # Post fit
-                if data.get("X_test") is not None:
-                    _post_fit(*args, **kwargs)
+                _post_fit(*args, **kwargs)
             return wrapper
 
         setattr(model, "fit", fit_wrapper(model.fit, **kwargs))
 
         def _post_fit(*args, **kwargs):
             
-            # Identify splits and build test set
-            X_test = data['X_test']
-            y_test = data['y_test']
-            test_set = pd.concat([X_test, y_test], axis=1)
-            test_set.reset_index(drop=True, inplace=True)
+            if data.get("X_test") is not None:
+                # Identify splits and build test set
+                X_test = data['X_test']
+                y_test = data['y_test']
+                test_set = pd.concat([X_test, y_test], axis=1)
+                test_set.reset_index(drop=True, inplace=True)
             
-            # Model Parameters
-            model_parameters = {key: str(item) for key, item in model.get_params().items()}
-            
-            # Evaluate model results and get the evaluation metrics
-            eval_metrics = eval_model_v2(context, X_test, y_test, model)
+                # Model Parameters
+                model_parameters = {key: str(item) for key, item in model.get_params().items()}
 
-            # Log test dataset
-            context.log_dataset(
-                "test_set",
-                df=test_set,
-                format="parquet",
-                index=False,
-                labels={"data-type": "held-out"},
-                artifact_path=context.artifact_subpath("data"),
-            )
+                # Evaluate model results and get the evaluation metrics
+                eval_metrics = eval_model_v2(context, X_test, y_test, model)
 
-            # Log fitted model
-            context.set_label("class", str(model.__class__.__name__))
-            context.log_model(model_name or "model",
-                              db_key=model_name,
-                              body=dumps(model),
-                              training_set=test_set,
-                              artifact_path=context.artifact_subpath("models"),
-                              extra_data=eval_metrics,
-                              framework=f"{str(model.__module__).split('.')[0]}",
-                              algorithm=f"{str(model.__class__.__name__)}",
-                              model_file=f"{str(model.__class__.__name__)}.pkl",
-                              metrics=context.results,
-                              format='pkl',
-                              label_column = y_test.columns.to_list(),
-                              )
+                # Log test dataset
+                context.log_dataset(
+                    "test_set",
+                    df=test_set,
+                    format="parquet",
+                    index=False,
+                    labels={"data-type": "held-out"},
+                    artifact_path=context.artifact_subpath("data"),
+                )
+                
+                context.set_label("class", str(model.__class__.__name__))
+                
+                # Log fitted model and metrics
+                context.log_model(model_name or "model",
+                                  db_key=model_name,
+                                  body=dumps(model),
+                                  training_set=test_set,
+                                  artifact_path=context.artifact_subpath("models"),
+                                  extra_data=eval_metrics,
+                                  framework=f"{str(model.__module__).split('.')[0]}",
+                                  algorithm=f"{str(model.__class__.__name__)}",
+                                  model_file=f"{str(model.__class__.__name__)}.pkl",
+                                  metrics=context.results,
+                                  format="pkl",
+                                  label_column = y_test.columns.to_list(),
+                                  )
+            else:
+                # Log fitted model
+                context.log_model(model_name or "model",
+                                  db_key=model_name,
+                                  body=dumps(model),
+                                  artifact_path=context.artifact_subpath("models"),
+                                  framework=f"{str(model.__module__).split('.')[0]}",
+                                  algorithm=f"{str(model.__class__.__name__)}",
+                                  model_file=f"{str(model.__class__.__name__)}.pkl",
+                                  metrics=context.results,
+                                  format="pkl",
+                                  label_column = y_test.columns.to_list(),
+                                  )

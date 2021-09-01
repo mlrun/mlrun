9240d40e8dff40ffa7799dc4c97222e8fd3f2458
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-09-01 20:29:16 +0300

    Update test_mlbase.py

--- tests/test_mlbase.py
+++ tests/test_mlbase.py
@@ -11,22 +11,28 @@ def get_dataset(classification=True):
     if classification:
         iris = load_iris()
         X = pd.DataFrame(data=iris.data, columns=iris.feature_names)
-        y = pd.DataFrame(data=iris.target, columns=['species'])
+        y = pd.DataFrame(data=iris.target, columns=["species"])
     else:
         boston = load_boston()
         X = pd.DataFrame(data=boston.data, columns=boston.feature_names)
-        y = pd.DataFrame(data=boston.target, columns=['target'])
+        y = pd.DataFrame(data=boston.target, columns=["target"])
     return train_test_split(X, y, test_size=0.2)
 
 
 def run_mlbase_sklearn_classification(context):
     model = LogisticRegression()
     X_train, X_test, y_train, y_test = get_dataset()
-    model = apply_mlrun_sklearn(model, context, model_name='my_model_name', X_test=X_test, y_test=y_test)
+    model = apply_mlrun_sklearn(
+        model, context, model_name="my_model_name", X_test=X_test, y_test=y_test
+    )
     model.fit(X_train, y_train)
 
 
 def test_run_mlbase_sklearn_classification():
     sklearn_run = new_function().run(handler=run_mlbase_sklearn_classification)
-    assert (sklearn_run.artifact('my_model_name').meta.to_dict()['metrics']['accuracy']) > 0
-    assert (sklearn_run.artifact('my_model_name').meta.to_dict()['model_file']) == 'LogisticRegression.pkl'
+    assert (
+        sklearn_run.artifact("my_model_name").meta.to_dict()["metrics"]["accuracy"]
+    ) > 0
+    assert (
+        sklearn_run.artifact("my_model_name").meta.to_dict()["model_file"]
+    ) == "LogisticRegression.pkl"

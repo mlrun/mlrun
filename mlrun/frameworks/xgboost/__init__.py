6fa0705fa934faf611351ebe7e61a772cef50e42
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-09-01 20:30:18 +0300

    Update __init__.py

--- mlrun/frameworks/xgboost/__init__.py
+++ mlrun/frameworks/xgboost/__init__.py
@@ -11,11 +11,7 @@ from mlrun.frameworks._common.pkl_model_server import PickleModelServer
 XGBModelServer = PickleModelServer
 
 
-def apply_mlrun(
-        model,
-        context: mlrun.MLClientCtx = None,
-        model_name=None,
-        **kwargs):
+def apply_mlrun(model, context: mlrun.MLClientCtx = None, model_name=None, **kwargs):
     """
     Wrap the given model with MLRun model, saving the model's attributes and methods while giving it mlrun's additional
     features.
@@ -37,7 +33,7 @@ def apply_mlrun(
     :return: The model with MLRun's interface.
     """
     if context is None:
-        context = mlrun.get_or_create_ctx('mlrun_xgb')
+        context = mlrun.get_or_create_ctx("mlrun_xgb")
 
     # Add MLRun's interface to the model:
     MLBaseMLRunInterface.add_interface(model, context, model_name, kwargs)

8812a9b5afd297d4c7d9d651a649deb8536c2d63
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-09-01 12:46:26 +0300

    Update mlrun_interface.py

--- mlrun/frameworks/mlbase/mlrun_interface.py
+++ mlrun/frameworks/mlbase/mlrun_interface.py
@@ -75,5 +75,4 @@ class MLBaseMLRunInterface(MLRunInterface):
                               algorithm=f"{str(model.__class__)}",
                               model_file=f"{str(model.__class__.__name__)}.pkl",
                               metrics=context.results,
-                              label_column = data['y_test'].columns.to_list(),
                               )

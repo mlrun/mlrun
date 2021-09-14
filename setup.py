671d7c24f10b784d42ea1c4aceb8c7653f19f82b
Gal Topper <galt@iguazio.com>
2021-09-14 16:59:27 +0300

    make fmt

--- setup.py
+++ setup.py
@@ -86,7 +86,10 @@ extras_require = {
         "s3fs>=0.5.0, <=2021.8.1",
     ],
     # <12.7.0 from adlfs 0.6.3
-    "azure-blob-storage": ["azure-storage-blob~=12.0, <12.7.0", "adlfs~=0.7.1, <=2021.8.1"],
+    "azure-blob-storage": [
+        "azure-storage-blob~=12.0, <12.7.0",
+        "adlfs~=0.7.1, <=2021.8.1",
+    ],
     "azure-key-vault": ["azure-identity~=1.5", "azure-keyvault-secrets~=4.2"],
     # mlrun.frameworks requirements per framework:  # TODO: should be added in a later PR
     "bokeh": ["bokeh~=2.3"],

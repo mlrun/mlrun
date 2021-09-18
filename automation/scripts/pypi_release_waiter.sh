73bddf5216c0e03b503046733168fbc23b73277c
Hedi Ingber <hedingber@gmail.com>
2021-09-18 17:22:45 +0300

    Don't search only latest version

--- automation/scripts/pypi_release_waiter.sh
+++ automation/scripts/pypi_release_waiter.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
 
 version=$1
 version=${version#"v"}
 echo "Waiting for version to be released on Pypi. version:$version"
 while true ; do
-  latest_version="$(curl -sf https://pypi.org/pypi/mlrun/json | jq -r '.info.version')"
-  if [ "$latest_version" = "$version" ]; then
-    echo "Version released: $latest_version"
+  released_versions="$(curl -sf https://pypi.org/pypi/mlrun/json | jq -r '.releases | keys | join(",")')"
+  if [[ "$released_versions" == *"$version"* ]]; then
+    echo "Version released: $version"
     break;
   else
-    echo "Version not released yet. Sleeping and retrying. latest version=$latest_version waiting for version=$version"
+    echo "Version not released yet. Sleeping and retrying. waiting for version=$version released_versions=$released_versions"
   fi;
   sleep 60;
 done;

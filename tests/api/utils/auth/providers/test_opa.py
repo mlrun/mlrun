96912dcebc296d47fb43e7d108b354c5a77f2621
Hedi Ingber <hedingber@gmail.com>
2021-09-27 23:49:44 +0300

    linting

--- tests/api/utils/auth/providers/test_opa.py
+++ tests/api/utils/auth/providers/test_opa.py
@@ -36,7 +36,7 @@ async def permission_filter_path() -> str:
 
 @pytest.fixture()
 async def opa_provider(
-        api_url: str, permission_query_path: str, permission_filter_path: str,
+    api_url: str, permission_query_path: str, permission_filter_path: str,
 ) -> mlrun.api.utils.auth.providers.opa.Provider:
     mlrun.mlconf.httpdb.authorization.opa.log_level = 10
     mlrun.mlconf.httpdb.authorization.mode = "opa"
@@ -47,10 +47,10 @@ async def opa_provider(
 
 
 def test_query_permissions_success(
-        api_url: str,
-        permission_query_path: str,
-        opa_provider: mlrun.api.utils.auth.providers.opa.Provider,
-        requests_mock: requests_mock_package.Mocker,
+    api_url: str,
+    permission_query_path: str,
+    opa_provider: mlrun.api.utils.auth.providers.opa.Provider,
+    requests_mock: requests_mock_package.Mocker,
 ):
     resource = "/projects/project-name/functions/function-name"
     action = mlrun.api.schemas.AuthorizationAction.create
@@ -60,14 +60,14 @@ def test_query_permissions_success(
 
     def mock_permission_query_success(request, context):
         assert (
-                deepdiff.DeepDiff(
-                    opa_provider._generate_permission_request_body(
-                        resource, action.value, auth_info
-                    ),
-                    request.json(),
-                    ignore_order=True,
-                )
-                == {}
+            deepdiff.DeepDiff(
+                opa_provider._generate_permission_request_body(
+                    resource, action.value, auth_info
+                ),
+                request.json(),
+                ignore_order=True,
+            )
+            == {}
         )
         context.status_code = http.HTTPStatus.OK.value
         return {"result": True}
@@ -80,10 +80,10 @@ def test_query_permissions_success(
 
 
 def test_filter_by_permission(
-        api_url: str,
-        permission_filter_path: str,
-        opa_provider: mlrun.api.utils.auth.providers.opa.Provider,
-        requests_mock: requests_mock_package.Mocker,
+    api_url: str,
+    permission_filter_path: str,
+    opa_provider: mlrun.api.utils.auth.providers.opa.Provider,
+    requests_mock: requests_mock_package.Mocker,
 ):
     resources = [
         {"resource_id": 1, "opa_resource": "/some-resource", "allowed": True},
@@ -113,14 +113,14 @@ def test_filter_by_permission(
     def mock_filter_query_success(request, context):
         opa_resources = [resource["opa_resource"] for resource in resources]
         assert (
-                deepdiff.DeepDiff(
-                    opa_provider._generate_filter_request_body(
-                        opa_resources, action.value, auth_info
-                    ),
-                    request.json(),
-                    ignore_order=True,
-                )
-                == {}
+            deepdiff.DeepDiff(
+                opa_provider._generate_filter_request_body(
+                    opa_resources, action.value, auth_info
+                ),
+                request.json(),
+                ignore_order=True,
+            )
+            == {}
         )
         context.status_code = http.HTTPStatus.OK.value
         return {"result": allowed_opa_resources}
@@ -132,18 +132,18 @@ def test_filter_by_permission(
         resources, lambda resource: resource["opa_resource"], action, auth_info
     )
     assert (
-            deepdiff.DeepDiff(
-                expected_allowed_resources, allowed_resources, ignore_order=True,
-            )
-            == {}
+        deepdiff.DeepDiff(
+            expected_allowed_resources, allowed_resources, ignore_order=True,
+        )
+        == {}
     )
 
 
 def test_query_permissions_failure(
-        api_url: str,
-        permission_query_path: str,
-        opa_provider: mlrun.api.utils.auth.providers.opa.Provider,
-        requests_mock: requests_mock_package.Mocker,
+    api_url: str,
+    permission_query_path: str,
+    opa_provider: mlrun.api.utils.auth.providers.opa.Provider,
+    requests_mock: requests_mock_package.Mocker,
 ):
     resource = "/projects/project-name/functions/function-name"
     action = mlrun.api.schemas.AuthorizationAction.create
@@ -153,14 +153,14 @@ def test_query_permissions_failure(
 
     def mock_permission_query_failure(request, context):
         assert (
-                deepdiff.DeepDiff(
-                    opa_provider._generate_permission_request_body(
-                        resource, action.value, auth_info
-                    ),
-                    request.json(),
-                    ignore_order=True,
-                )
-                == {}
+            deepdiff.DeepDiff(
+                opa_provider._generate_permission_request_body(
+                    resource, action.value, auth_info
+                ),
+                request.json(),
+                ignore_order=True,
+            )
+            == {}
         )
         context.status_code = http.HTTPStatus.OK.value
         return {"result": False}
@@ -169,37 +169,37 @@ def test_query_permissions_failure(
         f"{api_url}{permission_query_path}", json=mock_permission_query_failure
     )
     with pytest.raises(
-            mlrun.errors.MLRunAccessDeniedError,
-            match=f"Not allowed to {action} resource {resource}",
+        mlrun.errors.MLRunAccessDeniedError,
+        match=f"Not allowed to {action} resource {resource}",
     ):
         opa_provider.query_permissions(resource, action, auth_info)
 
 
 def test_query_permissions_use_cache(
-        api_url: str,
-        permission_query_path: str,
-        opa_provider: mlrun.api.utils.auth.providers.opa.Provider,
-        requests_mock: requests_mock_package.Mocker,
+    api_url: str,
+    permission_query_path: str,
+    opa_provider: mlrun.api.utils.auth.providers.opa.Provider,
+    requests_mock: requests_mock_package.Mocker,
 ):
     auth_info = mlrun.api.schemas.AuthInfo(user_id="user-id")
     project_name = "project-name"
     opa_provider.add_allowed_project_for_owner(project_name, auth_info)
     mocker = requests_mock.post(f"{api_url}{permission_query_path}", json={})
     assert (
-            opa_provider.query_permissions(
-                f"/projects/{project_name}/resource",
-                mlrun.api.schemas.AuthorizationAction.create,
-                auth_info,
-            )
-            is True
+        opa_provider.query_permissions(
+            f"/projects/{project_name}/resource",
+            mlrun.api.schemas.AuthorizationAction.create,
+            auth_info,
+        )
+        is True
     )
     assert mocker.call_count == 0
 
 
 def test_allowed_project_owners_cache(
-        api_url: str,
-        permission_query_path: str,
-        opa_provider: mlrun.api.utils.auth.providers.opa.Provider,
+    api_url: str,
+    permission_query_path: str,
+    opa_provider: mlrun.api.utils.auth.providers.opa.Provider,
 ):
     auth_info = mlrun.api.schemas.AuthInfo(user_id="user-id")
     project_name = "project-name"
@@ -207,30 +207,30 @@ def test_allowed_project_owners_cache(
     # ensure nothing is wrong with adding the same project twice
     opa_provider.add_allowed_project_for_owner(project_name, auth_info)
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name}/resource", auth_info
-            )
-            is True
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name}/resource", auth_info
+        )
+        is True
     )
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                "/some-non-project-resource", auth_info
-            )
-            is False
+        opa_provider._check_allowed_project_owners_cache(
+            "/some-non-project-resource", auth_info
+        )
+        is False
     )
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name}/resource",
-                mlrun.api.schemas.AuthInfo(user_id="other-user-id"),
-            )
-            is False
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name}/resource",
+            mlrun.api.schemas.AuthInfo(user_id="other-user-id"),
+        )
+        is False
     )
 
 
 def test_allowed_project_owners_cache_ttl_refresh(
-        api_url: str,
-        permission_query_path: str,
-        opa_provider: mlrun.api.utils.auth.providers.opa.Provider,
+    api_url: str,
+    permission_query_path: str,
+    opa_provider: mlrun.api.utils.auth.providers.opa.Provider,
 ):
     auth_info = mlrun.api.schemas.AuthInfo(user_id="user-id")
     opa_provider._allowed_project_owners_cache_ttl_seconds = 1
@@ -238,27 +238,27 @@ def test_allowed_project_owners_cache_ttl_refresh(
     opa_provider.add_allowed_project_for_owner(project_name, auth_info)
     time.sleep(0.3)
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name}/resource", auth_info
-            )
-            is True
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name}/resource", auth_info
+        )
+        is True
     )
     # This will refresh the ttl
     opa_provider.add_allowed_project_for_owner(project_name, auth_info)
     time.sleep(0.7)
     # by now, more than the first 1 second surely passed, so if it works, ttl refreshed
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name}/resource", auth_info
-            )
-            is True
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name}/resource", auth_info
+        )
+        is True
     )
 
 
 def test_allowed_project_owners_cache_clean_expired(
-        api_url: str,
-        permission_query_path: str,
-        opa_provider: mlrun.api.utils.auth.providers.opa.Provider,
+    api_url: str,
+    permission_query_path: str,
+    opa_provider: mlrun.api.utils.auth.providers.opa.Provider,
 ):
     auth_info = mlrun.api.schemas.AuthInfo(user_id="user-id")
     auth_info_2 = mlrun.api.schemas.AuthInfo(user_id="user-id-2")
@@ -271,80 +271,80 @@ def test_allowed_project_owners_cache_clean_expired(
     # Note that the _check_allowed_project_owners_cache method calls the clean method so no need to call the clean
     # method explicitly
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name}/resource", auth_info
-            )
-            is True
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name}/resource", auth_info
+        )
+        is True
     )
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name}/resource", auth_info_2
-            )
-            is False
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name}/resource", auth_info_2
+        )
+        is False
     )
     opa_provider.add_allowed_project_for_owner(project_name_2, auth_info)
     opa_provider.add_allowed_project_for_owner(project_name, auth_info_2)
     time.sleep(1)
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name}/resource", auth_info
-            )
-            is False
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name}/resource", auth_info
+        )
+        is False
     )
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name}/resource", auth_info_2
-            )
-            is True
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name}/resource", auth_info_2
+        )
+        is True
     )
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name_2}/resource", auth_info
-            )
-            is True
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name_2}/resource", auth_info
+        )
+        is True
     )
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name_2}/resource", auth_info_2
-            )
-            is False
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name_2}/resource", auth_info_2
+        )
+        is False
     )
     opa_provider.add_allowed_project_for_owner(project_name_3, auth_info)
     opa_provider.add_allowed_project_for_owner(project_name_2, auth_info_2)
     time.sleep(1)
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name}/resource", auth_info
-            )
-            is False
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name}/resource", auth_info
+        )
+        is False
     )
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name}/resource", auth_info_2
-            )
-            is False
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name}/resource", auth_info_2
+        )
+        is False
     )
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name_2}/resource", auth_info
-            )
-            is False
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name_2}/resource", auth_info
+        )
+        is False
     )
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name_2}/resource", auth_info_2
-            )
-            is True
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name_2}/resource", auth_info_2
+        )
+        is True
     )
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name_3}/resource", auth_info
-            )
-            is True
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name_3}/resource", auth_info
+        )
+        is True
     )
     assert (
-            opa_provider._check_allowed_project_owners_cache(
-                f"/projects/{project_name_3}/resource", auth_info_2
-            )
-            is False
+        opa_provider._check_allowed_project_owners_cache(
+            f"/projects/{project_name_3}/resource", auth_info_2
+        )
+        is False
     )

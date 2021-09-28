54844c025f47b550c9d0c875d5f0acddf006b359
Hedi Ingber <hedingber@gmail.com>
2021-09-28 03:21:26 +0300

    Fix convenience tests

--- tests/test_convenince_methods.py
+++ tests/test_convenince_methods.py
@@ -3,9 +3,11 @@ import pytest
 import mlrun
 import mlrun.errors
 import mlrun.projects.project
+import mlrun.api.db.sqldb.db
+import sqlalchemy.orm
 
 
-def test_set_environment_with_invalid_project_name():
+def test_set_environment_with_invalid_project_name(db: mlrun.api.db.sqldb.db.SQLDB, db_session: sqlalchemy.orm.Session):
     invalid_name = "project_name"
     with pytest.raises(mlrun.errors.MLRunInvalidArgumentError):
         mlrun.set_environment(project=invalid_name)

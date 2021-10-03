c788b307d02b5a9ef1e12180d603d1d2cee13033
Hedi Ingber <hedingber@gmail.com>
2021-10-04 02:51:11 +0300

    Add missing package to setup.py

--- setup.py
+++ setup.py
@@ -132,6 +132,7 @@ setup(
         "mlrun.api.utils.auth",
         "mlrun.api.utils.auth.providers",
         "mlrun.api.utils.clients",
+        "mlrun.api.utils.db",
         "mlrun.api.utils.projects",
         "mlrun.api.utils.projects.remotes",
         "mlrun.api.utils.singletons",

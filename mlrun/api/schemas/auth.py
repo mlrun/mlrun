e8b8effd144d23adb68ef0599bc34138ac899bc3
Hedi Ingber <hedingber@gmail.com>
2021-10-10 04:19:13 +0300

    Add access key to auth info and use it in env enrichment

--- mlrun/api/schemas/auth.py
+++ mlrun/api/schemas/auth.py
@@ -83,6 +83,7 @@ class AuthInfo(pydantic.BaseModel):
     # Iguazio auth
     session: typing.Optional[str] = None
     data_session: typing.Optional[str] = None
+    access_key: typing.Optional[str] = None
     user_id: typing.Optional[str] = None
     user_group_ids: typing.List[str] = []
     projects_role: typing.Optional[ProjectsRole] = None

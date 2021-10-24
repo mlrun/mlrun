e815cc767033fb39a1ca4d9a28514f8197004c7d
Yaron Haviv <yaronh@iguaz.io>
2021-10-24 18:13:12 +0300

    [Runtimes] Fix and align use of nuclio:mlrun runtime with hyper-params (#1443)

--- tests/system/runtimes/test_nuclio.py
+++ tests/system/runtimes/test_nuclio.py
@@ -94,3 +94,68 @@ class TestNuclioRuntimeWithStream(tests.system.base.TestMLRunSystem):
 
         self._logger.debug("Deploying nuclio function")
         function.deploy()
+
+
+@tests.system.base.TestMLRunSystem.skip_test_if_env_not_configured
+class TestNuclioMLRunJobs(tests.system.base.TestMLRunSystem):
+    project_name = "nuclio-mlrun-jobs"
+
+    def _deploy_function(self, replicas=1):
+        filename = str(self.assets_path / "handler.py")
+        fn = mlrun.code_to_function(
+            filename=filename,
+            name="nuclio-mlrun",
+            kind="nuclio:mlrun",
+            image="mlrun/mlrun",
+            handler="my_func",
+        )
+        # replicas * workers need to match or exceed parallel_runs
+        fn.spec.replicas = replicas
+        fn.with_http(workers=2)
+        fn.deploy()
+        return fn
+
+    def test_single_run(self):
+        fn = self._deploy_function()
+        run_result = fn.run(params={"p1": 8})
+
+        print(run_result.to_yaml())
+        assert run_result.state() == "completed", "wrong state"
+        # accuracy = p1 * 2
+        assert run_result.output("accuracy") == 16, "unexpected results"
+
+    def test_hyper_run(self):
+        fn = self._deploy_function(2)
+
+        hyper_param_options = mlrun.model.HyperParamOptions(
+            parallel_runs=4, selector="max.accuracy", max_errors=1,
+        )
+
+        p1 = [4, 2, 5, 8, 9, 6, 1, 11, 1, 1, 2, 1, 1]
+        run_result = fn.run(
+            params={"p2": "xx"},
+            hyperparams={"p1": p1},
+            hyper_param_options=hyper_param_options,
+        )
+        print(run_result.to_yaml())
+        assert run_result.state() == "completed", "wrong state"
+        # accuracy = max(p1) * 2
+        assert run_result.output("accuracy") == 22, "unexpected results"
+
+        # test early stop
+        hyper_param_options = mlrun.model.HyperParamOptions(
+            parallel_runs=1,
+            selector="max.accuracy",
+            max_errors=1,
+            stop_condition="accuracy>9",
+        )
+
+        run_result = fn.run(
+            params={"p2": "xx"},
+            hyperparams={"p1": p1},
+            hyper_param_options=hyper_param_options,
+        )
+        print(run_result.to_yaml())
+        assert run_result.state() == "completed", "wrong state"
+        # accuracy = max(p1) * 2, stop where accuracy > 9
+        assert run_result.output("accuracy") == 10, "unexpected results"

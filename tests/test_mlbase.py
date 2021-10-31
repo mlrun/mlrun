c2cee6a008efe4f837b1dda69fc4808597a373e0
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-10-31 14:13:09 +0200

    Update test_mlbase.py

--- tests/test_mlbase.py
+++ tests/test_mlbase.py
@@ -1,13 +1,12 @@
+import lightgbm as lgb
 import pandas as pd
 import xgboost as xgb
-import lightgbm as lgb
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_boston
-from sklearn.linear_model import LogisticRegression
-from mlrun.frameworks.sklearn import apply_mlrun
 from mlrun import new_function
-from lightgbm import LGBMClassifier
+from mlrun.frameworks.sklearn import apply_mlrun
+from sklearn.datasets import load_boston, load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
 
 def get_dataset(classification=True):
     if classification:
@@ -24,37 +23,52 @@ def get_dataset(classification=True):
 def run_mlbase_sklearn_classification(context):
     model = LogisticRegression()
     X_train, X_test, y_train, y_test = get_dataset()
-    model = apply_mlrun(model, context, X_train=X_train,
-                        y_train=y_train, X_test=X_test, y_test=y_test)
+    model = apply_mlrun(
+        model, context, X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test
+    )
     model.fit(X_train, y_train)
 
 
 def run_mlbase_xgboost_regression(context):
     model = xgb.XGBRegressor()
     X_train, X_test, y_train, y_test = get_dataset(classification=False)
-    model = apply_mlrun(model, context, X_train=X_train,
-                        y_train=y_train, X_test=X_test, y_test=y_test)
+    model = apply_mlrun(
+        model, context, X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test
+    )
     model.fit(X_train, y_train)
+
 
 def run_mlbase_lgbm_classification(context):
     model = lgb.LGBMClassifier()
     X_train, X_test, y_train, y_test = get_dataset()
-    model = apply_mlrun(model, context, X_train=X_train,
-                        y_train=y_train, X_test=X_test, y_test=y_test)
+    model = apply_mlrun(
+        model, context, X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test
+    )
     model.fit(X_train, y_train)
-    
+
+
 def test_run_mlbase_sklearn_classification():
     sklearn_run = new_function().run(handler=run_mlbase_sklearn_classification)
-    assert (sklearn_run.artifact('model').meta.to_dict()['metrics']['accuracy']) > 0
-    assert (sklearn_run.artifact('model').meta.to_dict()['model_file']) == 'LogisticRegression.pkl'
+    assert (sklearn_run.artifact("model").meta.to_dict()["metrics"]["accuracy"]) > 0
+    assert (
+        sklearn_run.artifact("model").meta.to_dict()["model_file"]
+    ) == "LogisticRegression.pkl"
+
 
 def test_run_mlbase_xgboost_regression():
     xgb_run = new_function().run(handler=run_mlbase_xgboost_regression)
-    assert (xgb_run.artifact('model').meta.to_dict()['metrics']['accuracy']) > 0
-    assert 'confusion matrix' not in (xgb_run.artifact('model').meta.to_dict()['extra_data'])
-    assert (xgb_run.artifact('model').meta.to_dict()['model_file']) == 'XGBRegressor.pkl'
-    
+    assert (xgb_run.artifact("model").meta.to_dict()["metrics"]["accuracy"]) > 0
+    assert "confusion matrix" not in (
+        xgb_run.artifact("model").meta.to_dict()["extra_data"]
+    )
+    assert (
+        xgb_run.artifact("model").meta.to_dict()["model_file"]
+    ) == "XGBRegressor.pkl"
+
+
 def test_run_mlbase_lgbm_classification():
     lgbm_run = new_function().run(handler=run_mlbase_lgbm_classification)
-    assert (lgbm_run.artifact('model').meta.to_dict()['metrics']['accuracy']) > 0
-    assert (lgbm_run.artifact('model').meta.to_dict()['model_file']) == 'LGBMClassifier.pkl'
+    assert (lgbm_run.artifact("model").meta.to_dict()["metrics"]["accuracy"]) > 0
+    assert (
+        lgbm_run.artifact("model").meta.to_dict()["model_file"]
+    ) == "LGBMClassifier.pkl"

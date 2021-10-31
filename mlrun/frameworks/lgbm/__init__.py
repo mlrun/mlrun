746e5cd1ad8d23bf41f66cd1619e21c05f5ab55c
AlxZed <34654828+AlxZed@users.noreply.github.com>
2021-10-31 12:31:22 +0200

    Update and rename mlrun/frameworks/lightgbm/__init__.py to mlrun/frameworks/lgbm/__init__.py

--- mlrun/frameworks/lgbm/__init__.py
+++ mlrun/frameworks/lgbm/__init__.py
@@ -4,8 +4,12 @@ __init__ function of LightGBM-autologger. Will be extended and contain multiple 
 """
 
 import mlrun
+import numpy as np
+import pandas as pd
+
 from mlrun.frameworks._common.pkl_model_server import PickleModelServer
 from mlrun.frameworks.mlbase.mlrun_interface import MLBaseMLRunInterface
+from typing import Union
 
 # Temporary placeholder, LGBMModelServer may deviate from PklModelServer in upcoming versions.
 LGBMModelServer = PickleModelServer
@@ -14,35 +18,38 @@ LGBMModelServer = PickleModelServer
 def apply_mlrun(
     model,
     context: mlrun.MLClientCtx = None,
-    X_test=None,
-    y_test=None,
-    model_name=None,
-    generate_test_set=True,
+    X_test: Union[numpy.ndarray,pd.core.frame.DataFrame] = None,
+    y_test: Union[numpy.ndarray,pd.core.frame.DataFrame] = None,
+    model_name: str = None,
+    generate_test_set: bool = True,
     feature_vector = None,
     **kwargs
 ):
     """
     Wrap the given model with MLRun model, saving the model's attributes and methods while giving it mlrun's additional
     features.
+    
     examples::
         model = LGBMClassifier()
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
         model = apply_mlrun(model, context, X_test=X_test, y_test=y_test)
         model.fit(X_train, y_train)
+        
     :param model:       The model to wrap.
-    :param context:     MLRun context to work with. If no context is given it will be retrieved via
-                        'mlrun.get_or_create_ctx(None)'
-    :param X_test:      X test data (for accuracy and plots generation)
-    :param y_test:      y test data (for accuracy and plots generation)
-    :param model_name:  model artifact name
-    :param generate_test_set:  will generate a test_set dataset artifact
+    :param context:     MLRun context to work with. If no context is given it will be retrieved via 'mlrun.get_or_create_ctx(None)'
+    :param X_test:      X_test dataset (for accuracy and plots generation)
+    :param y_test:      y_test dataset (for accuracy and plots generation)
+    :param model_name:          model artifact name
+    :param generate_test_set:   generates a test_set dataset artifact
+    :param feature_vector: 
+    
     :return: The model with MLRun's interface.
     """
     if context is None:
         context = mlrun.get_or_create_ctx("mlrun_lgbm")
 
     if feature_vector and hasattr(feature_vector, "uri"): 
-         kwargs["feature_vector"]=feature_vector.uri
+         kwargs["feature_vector"] = feature_vector.uri
         
     kwargs["X_test"] = X_test
     kwargs["y_test"] = y_test

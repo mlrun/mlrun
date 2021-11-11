1a9f34a9af422d283eef2e20262d38ab499c5c1b
Saar Cohen <66667568+theSaarco@users.noreply.github.com>
2021-11-11 13:01:11 +0200

    [Secrets] Avoid auto-adding internal project secrets + model monitoring access-key changes (#1487)

--- mlrun/runtimes/pod.py
+++ mlrun/runtimes/pod.py
@@ -479,6 +479,9 @@ class KubeResource(BaseRuntime):
     def _add_project_k8s_secrets_to_spec(
         self, secrets, runobj=None, project=None, encode_key_names=True
     ):
+        # Needs to happen here to avoid circular dependencies
+        from mlrun.api.crud.secrets import Secrets
+
         # the secrets param may be an empty dictionary (asking for all secrets of that project) -
         # it's a different case than None (not asking for project secrets at all).
         if (
@@ -494,7 +497,11 @@ class KubeResource(BaseRuntime):
 
         secret_name = self._get_k8s().get_project_secret_name(project_name)
         existing_secret_keys = (
-            self._get_k8s().get_project_secret_keys(project_name) or []
+            Secrets()
+            .list_secret_keys(
+                project_name, mlrun.api.schemas.SecretProviderName.kubernetes
+            )
+            .secret_keys
         )
 
         # If no secrets were passed or auto-adding all secrets, we need all existing keys

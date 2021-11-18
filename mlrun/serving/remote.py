001cf8f61b1e8934a870beb7a52294afd4580345
Gal Topper <galt@iguazio.com>
2021-11-18 14:25:16 +0200

    [Serving] Fix RemoteStep
    ML-1394

--- mlrun/serving/remote.py
+++ mlrun/serving/remote.py
@@ -146,7 +146,7 @@ class RemoteStep(StepToDict, storey.SendToHttp):
 
     def _generate_request(self, event, body):
         method = self.method or event.method or "POST"
-        headers = self.headers or event.headers or {}
+        headers = self.headers or {}
 
         if self._url_function_handler:
             url = self._url_function_handler(body)
@@ -280,7 +280,7 @@ class BatchHttpRequests(StepToDict, _ConcurrentJobExecution):
     async def _process_event(self, event):
         # async implementation (with storey)
         method = self.method or event.method or "POST"
-        headers = self.headers or event.headers or {}
+        headers = self.headers or {}
         input_list = self._get_event_or_body(event)
         is_get = method == "GET"
         is_json = False

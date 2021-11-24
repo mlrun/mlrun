39720753c17d5dbc5609447e85d16fbf55f548db
Adam <a.melnick@me.com>
2021-11-24 18:35:41 +0200

    nicer imports

--- mlrun/api/db/init_db.py
+++ mlrun/api/db/init_db.py
@@ -1,18 +1,10 @@
 from sqlalchemy.orm import Session
 
-from mlrun.api.db.sqldb.models import Base as MySQLBase
-from mlrun.api.db.sqldb.models_sqlite import Base as SQLiteBase
+from mlrun.api.db.sqldb.models import Base
 from mlrun.api.db.sqldb.session import get_engine
-from mlrun.api.utils.db.mysql import MySQLUtil
 from mlrun.config import config
 
 
 def init_db(db_session: Session) -> None:
-    mysql_dsn_data = MySQLUtil.get_mysql_dsn_data()
-    if mysql_dsn_data:
-        base = MySQLBase
-    else:
-        base = SQLiteBase
-
     if config.httpdb.db_type != "filedb":
-        base.metadata.create_all(bind=get_engine())
+        Base.metadata.create_all(bind=get_engine())

7a93ef38527b58489b58418bf8ce8f05c6eb0448
Hedi Ingber <hedingber@gmail.com>
2021-11-28 02:11:02 +0200

    Fix test

--- tests/api/test_api_states.py
+++ tests/api/test_api_states.py
@@ -1,4 +1,5 @@
 import http
+import unittest.mock
 
 import fastapi.testclient
 import sqlalchemy.orm
@@ -36,6 +37,9 @@ def test_waiting_for_migrations_state(
 
 
 def test_init_data_migration_required_recognition() -> None:
+    # mock that migration is needed
+    original_is_migration_needed = mlrun.api.initial_data._is_migration_needed
+    mlrun.api.initial_data._is_migration_needed = unittest.mock.Mock(return_value=True)
     mlrun.api.initial_data.init_data()
-    # db doesn't exists - therefore schema migration needed - therefore state should be set
     mlrun.mlconf.httpdb.state = mlrun.api.schemas.APIStates.waiting_for_migrations
+    mlrun.api.initial_data._is_migration_needed = original_is_migration_needed

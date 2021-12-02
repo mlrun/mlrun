4aebb4b5518c21bf557d55b78a8b7d9e1a710a40
Ben <benbd86@gmail.com>
2021-12-02 11:22:40 +0200

    fix inconsistency of s3 paths

--- mlrun/datastore/utils.py
+++ mlrun/datastore/utils.py
@@ -3,4 +3,6 @@ def store_path_to_spark(path):
         path = "v3io:" + path[len("v3io:/") :]
     elif path.startswith("s3:///"):
         path = "s3a:" + path[len("s3:/") :]
+    elif path.startswith("s3://"):
+        path = "s3a:" + path[len("s3:") :]
     return path

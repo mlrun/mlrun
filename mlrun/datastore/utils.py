6df2ffff21f052a37ee389a96f8eee6907ce09ae
Ben <benbd86@gmail.com>
2021-12-02 14:14:59 +0200

    remove support of s3:///

--- mlrun/datastore/utils.py
+++ mlrun/datastore/utils.py
@@ -5,9 +5,10 @@ def store_path_to_spark(path):
         if path.startswith("s3:///"):
             # 's3:///' not supported since mlrun 0.9.0 should use s3:// instead
             from mlrun.errors import MLRunInvalidArgumentError
-            valid_path = path.replace('///', '//')
+
+            valid_path = "s3:" + path[len("s3:/") :]
             raise MLRunInvalidArgumentError(
-                f"'s3:///' is not supported, try using 's3://' instead.\nUse '{valid_path}'"
+                f"'s3:///' is not supported, try using 's3://' instead.\nE.g: '{valid_path}'"
             )
         else:
             path = "s3a:" + path[len("s3:") :]

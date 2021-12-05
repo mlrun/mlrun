8342c6aacece44cc653448d34073addb2f7e001a
yaron haviv <yaronh@iguazio.com>
2021-12-06 01:04:56 +0200

    fix spark

--- mlrun/feature_store/retrieval/spark_merger.py
+++ mlrun/feature_store/retrieval/spark_merger.py
@@ -68,15 +68,11 @@ class SparkFeatureMerger(BaseMerger):
 
             df = source.to_spark_df(self.spark, named_view=self.named_view)
             timestamp_key = feature_set.spec.timestamp_key
-            if (
-                timestamp_key
-                and timestamp_key not in df.columns
-                and timestamp_key not in column_names
-            ):
-                columns.append((timestamp_key, timestamp_key))
+            if timestamp_key and timestamp_key not in column_names:
+                columns.append((timestamp_key, None))
             for entity in feature_set.spec.entities.keys():
-                if entity not in df.columns and entity not in column_names:
-                    columns.append((entity, entity))
+                if entity not in column_names:
+                    columns.append((entity, None))
 
             print("extended cols:", columns)
             df = df.select([col(name).alias(alias or name) for name, alias in columns])

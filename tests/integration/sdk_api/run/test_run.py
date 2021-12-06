505b1a9a68d86b42ff1ccb79c9716ea010e916f8
Hedi Ingber <hedingber@gmail.com>
2021-12-06 12:11:09 +0200

    Add test

--- tests/integration/sdk_api/run/test_run.py
+++ tests/integration/sdk_api/run/test_run.py
@@ -11,3 +11,18 @@ class TestRun(tests.integration.sdk_api.base.TestMLRunIntegration):
         )
         assert len(runs) == 1
         assert runs[0]["metadata"]["project"] == mlrun.mlconf.default_project
+
+    def test_ctx_state_change(self):
+        ctx_name = "some-context"
+        ctx = mlrun.get_or_create_ctx(ctx_name)
+        runs = mlrun.get_run_db().list_runs(
+            name=ctx_name, project=mlrun.mlconf.default_project
+        )
+        assert len(runs) == 1
+        assert runs[0]["status"]["state"] == mlrun.runtimes.constants.RunStates.running
+        ctx.set_state(mlrun.runtimes.constants.RunStates.completed)
+        runs = mlrun.get_run_db().list_runs(
+            name=ctx_name, project=mlrun.mlconf.default_project
+        )
+        assert len(runs) == 1
+        assert runs[0]["status"]["state"] == mlrun.runtimes.constants.RunStates.completed

9f59abf69ae81526880ead7dd0f666f2e09bafb6
tankilevitch <tomtankilevitch@gmail.com>
2021-12-29 18:23:42 +0200

    changed condition skipif

--- tests/api/api/test_docs.py
+++ tests/api/api/test_docs.py
@@ -19,7 +19,7 @@ def does_env_exists(env):
     return False
 
 
-@pytest.mark.skipif(os.getenv('OPENAPI_JSON_TARGET_PATH') is not None,
+@pytest.mark.skipif(os.getenv('OPENAPI_JSON_TARGET_PATH') is None,
                     reason="Supposed to run only for CI backward compatibility checks")
 def test_save_openapi_json(db: sqlalchemy.orm.Session, client: fastapi.testclient.TestClient
                            ) -> None:

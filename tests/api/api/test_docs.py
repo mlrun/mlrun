78fb0986850dfa489fdb4aa686abb4a6edb4e45d
tankilevitch <tomtankilevitch@gmail.com>
2021-12-29 18:18:55 +0200

    a

--- tests/api/api/test_docs.py
+++ tests/api/api/test_docs.py
@@ -15,12 +15,12 @@ def test_docs(
 
 def does_env_exists(env):
     if os.getenv(env):
-        return False
-    return True
+        return True
+    return False
 
 
-@pytest.mark.skipif(does_env_exists('OPENAPI_JSON_TARGET_PATH'),
-                    "Supposed to run only for CI backward compatibility checks")
+@pytest.mark.skipif(os.getenv('OPENAPI_JSON_TARGET_PATH') is not None,
+                    reason="Supposed to run only for CI backward compatibility checks")
 def test_save_openapi_json(db: sqlalchemy.orm.Session, client: fastapi.testclient.TestClient
                            ) -> None:
     response = client.get("/api/openapi.json")

9ad956573fc666601f1a078f492dca7316caadec
Tom Tankilevitch <59158507+Tankilevitch@users.noreply.github.com>
2022-01-02 17:02:37 +0200

    [CI] Add step that runs backward compatibility tests on PRs (#1592)

--- tests/api/api/test_docs.py
+++ tests/api/api/test_docs.py
@@ -1,6 +1,8 @@
 import http
+import os
 
 import fastapi.testclient
+import pytest
 import sqlalchemy.orm
 
 
@@ -9,3 +11,21 @@ def test_docs(
 ) -> None:
     response = client.get("/api/openapi.json")
     assert response.status_code == http.HTTPStatus.OK.value
+
+
+@pytest.mark.skipif(
+    os.getenv("MLRUN_OPENAPI_JSON_NAME") is None,
+    reason="Supposed to run only for CI backward compatibility tests",
+)
+def test_save_openapi_json(
+    db: sqlalchemy.orm.Session, client: fastapi.testclient.TestClient
+) -> None:
+    """"The purpose of the test is to create an openapi.json file that is used to run backward compatibility tests"""
+    response = client.get("/api/openapi.json")
+    path = os.path.abspath(os.getcwd())
+    if os.getenv("MLRUN_BC_TESTS_OPENAPI_OUTPUT_PATH"):
+        path = os.getenv("MLRUN_BC_TESTS_OPENAPI_OUTPUT_PATH")
+    with open(
+        os.path.join(os.path.join(path, os.getenv("MLRUN_OPENAPI_JSON_NAME"))), "w"
+    ) as file:
+        file.write(response.text)

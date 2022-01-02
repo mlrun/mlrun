ff03994746e59781454381a81c0633e394da2e34
tankilevitch <tomtankilevitch@gmail.com>
2022-01-02 09:42:04 +0200

    fixed getenv to search for MLRUN_* environment

--- tests/api/api/test_docs.py
+++ tests/api/api/test_docs.py
@@ -23,6 +23,6 @@ def test_save_openapi_json(
     """"The purpose of the test is to create an openapi.json file that is used to run backward compatibility tests"""
     response = client.get("/api/openapi.json")
     with open(
-        os.path.join(os.getenv("OPENAPI_JSON_TARGET_PATH"), "openapi.json"), "w"
+        os.path.join(os.getenv("MLRUN_OPENAPI_JSON_TARGET_PATH"), "openapi.json"), "w"
     ) as file:
         file.write(response.text)

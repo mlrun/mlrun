f9e0b9cf406a09b1f58404e3449463167149e920
tankilevitch <tomtankilevitch@gmail.com>
2022-01-06 15:26:43 +0200

    renamed test function and removed redundant function declaration.

--- tests/system/backward_compatiblity/test_code_executed_in_runing_job.py
+++ tests/system/backward_compatiblity/test_code_executed_in_runing_job.py
@@ -10,7 +10,7 @@ from tests.system.base import TestMLRunSystem
 class TestCodeExecutedInRunningJob(TestMLRunSystem):
     project_name = "db-system-test-project"
 
-    def test_log_artifact(self):
+    def test_api_backward_compatibility_in_running_runtime(self):
         filename = str(pathlib.Path(__file__).parent / "assets" / "function.py")
         function = mlrun.code_to_function(
             project=self.project_name,
@@ -23,15 +23,8 @@ class TestCodeExecutedInRunningJob(TestMLRunSystem):
             handler="backward_compatibility_test_in_runtime_success",
         )
 
-        failure_function = mlrun.code_to_function(
-            project=self.project_name,
-            filename=filename,
-            kind="job",
-            image="mlrun/mlrun",
-        )
-
         with pytest.raises(mlrun.runtimes.utils.RunError):
-            failure_function.run(
+            function.run(
                 name="test-backward-compatibility-in-running-job-failure",
                 handler="backward_compatibility_test_in_runtime_failure",
             )

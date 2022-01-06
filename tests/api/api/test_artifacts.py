01ea00a0efc9711a9fd7c62c75cd6279616cb47d
tankilevitch <tomtankilevitch@gmail.com>
2022-01-06 20:47:02 +0200

    defined paths as CONSTANTS.

--- tests/api/api/test_artifacts.py
+++ tests/api/api/test_artifacts.py
@@ -9,12 +9,15 @@ PROJECT = "prj"
 KEY = "some-key"
 UID = "some-uid"
 TAG = "some-tag"
+API_PROJECTS_PATH = "/api/projects"
+API_ARTIFACT_PATH = "/api/artifact"
+API_ARTIFACTS_PATH = "/api/artifacts"
 
 
 def test_list_artifact_tags(db: Session, client: TestClient) -> None:
-    resp = client.get(f"/api/projects/{PROJECT}/artifact-tags")
+    resp = client.get(f"{API_PROJECTS_PATH}/{PROJECT}/artifact-tags")
     assert resp.status_code == HTTPStatus.OK, "status"
-    assert resp.json()["PROJECT"] == PROJECT, "PROJECT"
+    assert resp.json()["project"] == PROJECT, "project"
 
 
 def _create_project(client: TestClient, project_name: str = PROJECT):
@@ -24,7 +27,7 @@ def _create_project(client: TestClient, project_name: str = PROJECT):
             description="banana", source="source", goals="some goals"
         ),
     )
-    resp = client.post("/api/projects", json=project.dict())
+    resp = client.post(API_PROJECTS_PATH, json=project.dict())
     assert resp.status_code == HTTPStatus.CREATED
     return resp
 
@@ -32,31 +35,30 @@ def _create_project(client: TestClient, project_name: str = PROJECT):
 def test_store_artifact_with_empty_dict(db: Session, client: TestClient):
     _create_project(client)
 
-    resp = client.post(f"/api/artifact/{PROJECT}/{UID}/{KEY}?tag={TAG}", data="{}")
+    resp = client.post(
+        f"{API_ARTIFACT_PATH}/{PROJECT}/{UID}/{KEY}?tag={TAG}", data="{}"
+    )
     assert resp.status_code == HTTPStatus.OK
 
-    resp = client.get(f"/api/projects/{PROJECT}/artifact/{KEY}?tag={TAG}")
+    resp = client.get(f"{API_PROJECTS_PATH}/{PROJECT}/artifact/{KEY}?tag={TAG}")
     assert resp.status_code == HTTPStatus.OK
 
 
 def test_delete_artifacts_after_storing_empty_dict(db: Session, client: TestClient):
     _create_project(client)
-    artifacts_path = "/api/artifacts"
     empty_artifact = "{}"
 
     resp = client.post(
-        f"/api/artifact/{PROJECT}/{UID}/{KEY}?tag={TAG}", data=empty_artifact
+        f"{API_ARTIFACT_PATH}/{PROJECT}/{UID}/{KEY}?tag={TAG}", data=empty_artifact
     )
     assert resp.status_code == HTTPStatus.OK
 
-    uid2 = "uid2"
-    key2 = "key2"
     resp = client.post(
-        f"/api/artifact/{PROJECT}/{uid2}/{key2}?tag={TAG}", data=empty_artifact
+        f"{API_ARTIFACT_PATH}/{PROJECT}/{UID}2/{KEY}2?tag={TAG}", data=empty_artifact
     )
     assert resp.status_code == HTTPStatus.OK
 
-    project_artifacts_path = f"{artifacts_path}?project={PROJECT}"
+    project_artifacts_path = f"{API_ARTIFACTS_PATH}?project={PROJECT}"
 
     resp = client.get(project_artifacts_path)
     assert len(resp.json()["artifacts"]) == 2
@@ -66,5 +68,3 @@ def test_delete_artifacts_after_storing_empty_dict(db: Session, client: TestClie
 
     resp = client.get(project_artifacts_path)
     assert len(resp.json()["artifacts"]) == 0
-
-

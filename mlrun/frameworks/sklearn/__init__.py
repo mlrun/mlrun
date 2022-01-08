a4fc445d1b9ffb9d2743e221cf31dfe11ea21344
guy1992l <83535508+guy1992l@users.noreply.github.com>
2022-01-08 19:09:04 +0200

    [Frameworks] Add the `tag` attribute to the ml frameworks (#1618)
    (cherry picked from commit 3b5cfe4ed788eb1b69a66b0bf60d6045f471fb9a)

--- mlrun/frameworks/sklearn/__init__.py
+++ mlrun/frameworks/sklearn/__init__.py
@@ -19,29 +19,30 @@ def apply_mlrun(
     X_test=None,
     y_test=None,
     model_name=None,
+    tag: str = "",
     generate_test_set=True,
     **kwargs
-):
+) -> SKLearnModelHandler:
     """
     Wrap the given model with MLRun model, saving the model's attributes and methods while giving it mlrun's additional
     features.
 
-    examples::
-
+    examples:
         model = LogisticRegression()
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
         model = apply_mlrun(model, context, X_test=X_test, y_test=y_test)
         model.fit(X_train, y_train)
 
-    :param model:       The model to wrap.
-    :param context:     MLRun context to work with. If no context is given it will be retrieved via
-                        'mlrun.get_or_create_ctx(None)'
-    :param X_test:      X test data (for accuracy and plots generation)
-    :param y_test:      y test data (for accuracy and plots generation)
-    :param model_name:  model artifact name
-    :param generate_test_set:  will generate a test_set dataset artifact
+    :param model:             The model which will have the fit() function wrapped
+    :param context:           MLRun context to work with. If no context is given it will be retrieved via
+                              'mlrun.get_or_create_ctx(None)'
+    :param X_test:            X_test dataset
+    :param y_test:            y_test dataset
+    :param model_name:        The model artifact name (Optional)
+    :param tag:               Tag of a version to give to the logged model.
+    :param generate_test_set: Generates a test_set dataset artifact
 
-    :return: The model with MLRun's interface.
+    :return: The model in a MLRun model handler.
     """
     if context is None:
         context = mlrun.get_or_create_ctx("mlrun_sklearn")
@@ -55,5 +56,5 @@ def apply_mlrun(
     )
 
     # Add MLRun's interface to the model:
-    MLMLRunInterface.add_interface(mh, context, model_name, kwargs)
+    MLMLRunInterface.add_interface(mh, context, tag, kwargs)
     return mh

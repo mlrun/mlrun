c8755eda3257d9ffa1e8f6aae5221409f214f5e7
Tom Tankilevitch <59158507+Tankilevitch@users.noreply.github.com>
2022-01-12 11:24:49 +0200

    [System Tests] Fix incompatibility with artifact paths (#1624)

--- tests/system/backward_compatiblity/test_api_backward_compatibility.py
+++ tests/system/backward_compatiblity/test_api_backward_compatibility.py
@@ -1,6 +1,5 @@
 import pathlib
 
-import pandas as pd
 import pytest
 
 import mlrun
@@ -16,24 +15,29 @@ class TestAPIBackwardCompatibility(TestMLRunSystem):
         filename = str(pathlib.Path(__file__).parent / "assets" / "function.py")
         success_handler = "api_backward_compatibility_tests_succeeding_function"
         failure_handler = "api_backward_compatibility_tests_failing_function"
+        dataset_name = "test_dataset"
 
-        raw_data = {
-            "first_name": ["Jason", "Molly", "Tina", "Jake", "Amy"],
-        }
-        df = pd.DataFrame(raw_data, columns=["first_name"])
-        dataset_artifact = self.project.log_dataset(
-            "mydf", df=df, stats=True, format="parquet"
-        )
         function = mlrun.code_to_function(
             project=self.project_name,
             filename=filename,
             kind="job",
             image="mlrun/mlrun",
         )
+        artifact_path = ""
+        if TestMLRunSystem.is_enterprise_environment():
+            artifact_path = f"v3io:///projects/{self.project_name}/artifacts"
+
+        run = function.run(
+            name="log_dataset",
+            handler="log_dataset",
+            artifact_path=artifact_path,
+            params={"dataset_name": dataset_name},
+        )
+
         function.run(
             name=f"test_{success_handler}",
             handler=success_handler,
-            inputs={"dataset_src": dataset_artifact.uri},
+            inputs={"dataset_src": run.outputs[dataset_name]},
         )
 
         with pytest.raises(mlrun.runtimes.utils.RunError):

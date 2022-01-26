abe3eab464dde6daf02c45c39e98af325060785a
Hedi Ingber <hedingber@gmail.com>
2022-01-27 01:21:48 +0200

    Nicer way

--- tests/api/crud/test_projects.py
+++ tests/api/crud/test_projects.py
@@ -11,7 +11,7 @@ import mlrun.api.schemas
 
 
 @pytest.mark.asyncio
-async def test_calculate_pipelines_counters(db: sqlalchemy.orm.Session,):
+async def test_calculate_pipelines_counters(db: sqlalchemy.orm.Session, monkeypatch):
     mlrun.mlconf.kfp_url = ""
     mlrun.mlconf.igz_version = ""
     mlrun.mlconf.namespace = "mlrun"
@@ -19,11 +19,11 @@ async def test_calculate_pipelines_counters(db: sqlalchemy.orm.Session,):
     assert response == collections.defaultdict(int)
 
     mlrun.mlconf.kfp_url = "https://somekfp-url.com"
-    original_run_in_threadpool = fastapi.concurrency.run_in_threadpool
-    fastapi.concurrency.run_in_threadpool = unittest.mock.MagicMock(
-        return_value=asyncio.Future()
+    run_in_threadpool_mock = unittest.mock.MagicMock(return_value=asyncio.Future())
+    monkeypatch.setattr(
+        fastapi.concurrency, "run_in_threadpool", run_in_threadpool_mock
     )
-    fastapi.concurrency.run_in_threadpool.return_value.set_result(
+    run_in_threadpool_mock.return_value.set_result(
         (1, 1, [{"project": "kfp", "status": mlrun.run.RunStatuses.running}])
     )
     expected_pipelines_counter = collections.defaultdict(int)
@@ -31,4 +31,3 @@ async def test_calculate_pipelines_counters(db: sqlalchemy.orm.Session,):
     pipelines_counter = await mlrun.api.crud.Projects()._calculate_pipelines_counters()
     assert fastapi.concurrency.run_in_threadpool.call_count == 1
     assert pipelines_counter == expected_pipelines_counter
-    fastapi.concurrency.run_in_threadpool = original_run_in_threadpool

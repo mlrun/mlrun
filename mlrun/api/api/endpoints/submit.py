163343274915957babf4da90b67fdfa6cdec839a
Hedi Ingber <hedingber@gmail.com>
2022-02-03 01:57:34 +0200

    Use constants and enable to override client version from outside

--- mlrun/api/api/endpoints/submit.py
+++ mlrun/api/api/endpoints/submit.py
@@ -25,7 +25,7 @@ async def submit_job(
     username: Optional[str] = Header(None, alias="x-remote-user"),
     auth_info: mlrun.api.schemas.AuthInfo = Depends(deps.authenticate_request),
     db_session: Session = Depends(deps.get_db_session),
-    client_version: Optional[str] = Header(None, alias="x-mlrun-client-version"),
+    client_version: Optional[str] = Header(None, alias=mlrun.api.schemas.HeaderNames.client_version),
 ):
     data = None
     try:

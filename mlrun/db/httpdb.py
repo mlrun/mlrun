85733a3907e0d5696d49e341d8456c2fa9042e3c
tankilevitch <tomtankilevitch@gmail.com>
2022-02-02 19:23:59 +0200

    fix circular imports

--- mlrun/db/httpdb.py
+++ mlrun/db/httpdb.py
@@ -39,6 +39,7 @@ from ..feature_store import FeatureSet, FeatureVector
 from ..lists import ArtifactList, RunList
 from ..runtimes import BaseRuntime
 from ..utils import datetime_to_iso, dict_to_json, logger, new_pipe_meta
+from ..utils.version import Version
 from .base import RunDBError, RunDBInterface
 
 _artifact_keys = [
@@ -114,7 +115,7 @@ class HTTPRunDB(RunDBInterface):
         self.session = None
         self._wait_for_project_terminal_state_retry_interval = 3
         self._wait_for_project_deletion_interval = 3
-        self.client_version = mlrun.Version().get()["version"]
+        self.client_version = Version().get()["version"]
 
     def __repr__(self):
         cls = self.__class__.__name__

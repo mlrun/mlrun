24fe6386d57fb5f2e31f0a9ef9ecec06e24712b4
tankilevitch <tomtankilevitch@gmail.com>
2022-02-07 21:22:43 +0200

    fix

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,6 +1,9 @@
+import unittest.mock
+
 import mlrun
 import mlrun.api.schemas
 import mlrun.builder
+import mlrun.utils.version
 from mlrun.config import config
 
 
@@ -48,22 +51,21 @@ def test_resolve_mlrun_install_command():
             "client_version": "0.9.3",
             "server_mlrun_version_specifier": None,
             "expected_mlrun_install_command": f"{pip_command} "
-                                              f'"mlrun[complete] @ git+https://github.com/mlrun/mlrun@v0.10.0"',
+            f'"mlrun[complete] @ git+https://github.com/mlrun/mlrun@v0.10.0"',
         },
         {
             "test_description": "when mlrun_version_specifier is not configured and the server_mlrun_version_specifier"
-                                " is setup, expected to install the server_mlrun_version_specifier even if"
-                                " the client_version configured",
+            " is setup, expected to install the server_mlrun_version_specifier even if"
+            " the client_version configured",
             "mlrun_version_specifier": None,
             "client_version": "0.9.3",
             "server_mlrun_version_specifier": "mlrun[complete]==0.10.0-server-version",
             "expected_mlrun_install_command": f'{pip_command} "mlrun[complete]==0.10.0-server-version"',
         },
-
         {
             "test_description": "when client_version is specified and stable and mlrun_version_specifier and"
-                                " server_mlrun_version_specifier are not configured,"
-                                " expected to install stable client version",
+            " server_mlrun_version_specifier are not configured,"
+            " expected to install stable client version",
             "mlrun_version_specifier": None,
             "client_version": "0.9.3",
             "server_mlrun_version_specifier": None,
@@ -71,27 +73,27 @@ def test_resolve_mlrun_install_command():
         },
         {
             "test_description": "when client_version is specified and unstable and mlrun_version_specifier and"
-                                " server_mlrun_version_specifier are not configured,"
-                                " expected to install mlrun/mlrun@development",
+            " server_mlrun_version_specifier are not configured,"
+            " expected to install mlrun/mlrun@development",
             "mlrun_version_specifier": None,
             "client_version": "unstable",
             "server_mlrun_version_specifier": None,
             "expected_mlrun_install_command": f'{pip_command} "mlrun[complete] @ git+'
-                                              f'https://github.com/mlrun/mlrun@development"',
+            f'https://github.com/mlrun/mlrun@development"',
         },
         {
             "test_description": "when only the config.version is configured and unstable,"
-                                " expected to install mlrun/mlrun@development",
+            " expected to install mlrun/mlrun@development",
             "mlrun_version_specifier": None,
             "client_version": None,
             "server_mlrun_version_specifier": None,
             "version": "unstable",
             "expected_mlrun_install_command": f'{pip_command} "mlrun[complete] @ git+'
-                                              f'https://github.com/mlrun/mlrun@development"',
+            f'https://github.com/mlrun/mlrun@development"',
         },
         {
-            "test_description": "when only the config.version is configured and unstable,"
-                                " expected to install config.version",
+            "test_description": "when only the config.version is configured and stable,"
+            " expected to install config.version",
             "mlrun_version_specifier": None,
             "client_version": None,
             "server_mlrun_version_specifier": None,
@@ -104,7 +106,10 @@ def test_resolve_mlrun_install_command():
             "server_mlrun_version_specifier"
         )
         config.package_path = case.get("package_path", "mlrun")
-        config.version = case.get("version", "unstable")
+        if case.get("version") is not None:
+            mlrun.utils.version.Version().get = unittest.mock.Mock(
+                return_value={"version": case["version"]}
+            )
 
         mlrun_version_specifier = case.get("mlrun_version_specifier")
         client_version = case.get("client_version")

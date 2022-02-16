4071752273860a3a561c0cd7e47bb1a27e312773
Hedingber <hedingber@gmail.com>
2022-02-16 16:54:40 +0200

    [Builder] Support configuring image name prefix template to enforce (#1755)
    (cherry picked from commit 102429982da8cc9baf8284dc06e422fcc580f86b)

--- tests/api/api/test_frontend_spec.py
+++ tests/api/api/test_frontend_spec.py
@@ -49,6 +49,18 @@ def test_get_frontend_spec(
         bla = f"{{{expected_template_field}}}"
         assert bla in frontend_spec.function_deployment_target_image_template
 
+    assert frontend_spec.default_function_pod_resources, mlrun.api.schemas.Resources(
+        **default_function_pod_resources
+    )
+    assert (
+        frontend_spec.function_deployment_target_image_name_prefix_template
+        == mlrun.mlconf.httpdb.builder.function_target_image_name_prefix_template
+    )
+    assert (
+        frontend_spec.function_deployment_target_image_registries_to_enforce_prefix
+        == mlrun.runtimes.utils.resolve_function_target_image_registries_to_enforce_prefix()
+    )
+
 
 def test_get_frontend_spec_jobs_dashboard_url_resolution(
     db: sqlalchemy.orm.Session, client: fastapi.testclient.TestClient

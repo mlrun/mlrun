4ff52dd7426102e58b95c2ae4a1103692d5bdeba
Ben <benbd86@gmail.com>
2022-02-24 12:30:34 +0200

    fix test test_ingest_partitioned_by_key_and_time

--- tests/system/feature_store/test_feature_store.py
+++ tests/system/feature_store/test_feature_store.py
@@ -606,7 +606,7 @@ class TestFeatureStore(TestMLRunSystem):
 
         file_system = fsspec.filesystem("v3io")
         kind = TargetTypes.parquet
-        path = f"{get_default_prefix_for_target(kind)}/sets/{name}-latest"
+        path = f"{get_default_prefix_for_target(kind)}/sets/{name}"
         path = path.format(
             name=name,
             kind=kind,

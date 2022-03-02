a84681eac9dfbe57ecf14d5f57a3589ed86a7533
Saar Cohen <66667568+theSaarco@users.noreply.github.com>
2022-03-02 13:12:24 +0000

    [Spark] Support auto-add of k8s project secrets (#1790)
    (cherry picked from commit 688756a4827f65dd519669dc7dd4ade203b6a213)

--- mlrun/runtimes/pod.py
+++ mlrun/runtimes/pod.py
@@ -481,6 +481,20 @@ class KubeResource(BaseRuntime):
             new_meta.generate_name = norm_name
         return new_meta
 
+    def _add_secrets_to_spec_before_running(self, runobj):
+        if self._secrets:
+            if self._secrets.has_vault_source():
+                self._add_vault_params_to_spec(runobj)
+            if self._secrets.has_azure_vault_source():
+                self._add_azure_vault_params_to_spec(
+                    self._secrets.get_azure_vault_k8s_secret()
+                )
+            self._add_project_k8s_secrets_to_spec(
+                self._secrets.get_k8s_secrets(), runobj
+            )
+        else:
+            self._add_project_k8s_secrets_to_spec(None, runobj)
+
     def _add_azure_vault_params_to_spec(self, k8s_secret_name=None):
         secret_name = (
             k8s_secret_name or mlconf.secret_stores.azure_vault.default_secret_name

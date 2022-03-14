547e2516e80199a31aab59ed78eda835fa316cd4
Ben <benbd86@gmail.com>
2022-03-14 12:33:12 +0200

    fixing issue with access denied on spark operator.

--- mlrun/platforms/iguazio.py
+++ mlrun/platforms/iguazio.py
@@ -247,7 +247,7 @@ def mount_v3iod(namespace, v3io_config_configmap):
         # this is a legacy path for the daemon shared memory
         host_path = "/dev/shm/" + namespace
 
-        # path of shared memory for daemon was changed from Igauzio 3.2.3
+        # path to shared memory for daemon was changed in Igauzio 3.2.3-b1
         igz_version = mlrun.mlconf.get_parsed_igz_version()
         if igz_version and igz_version >= semver.VersionInfo.parse("3.2.3-b1"):
             host_path = "/var/run/iguazio/dayman-shm/" + namespace

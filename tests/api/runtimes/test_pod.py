eb8a492efa5862fa254ef3fbb65fce99d4dd4c75
tankilevitch <tomtankilevitch@gmail.com>
2022-03-14 10:00:49 +0200

    add test_attribute_serialization

--- tests/api/runtimes/test_pod.py
+++ tests/api/runtimes/test_pod.py
@@ -1,5 +1,6 @@
 import deepdiff
 import pytest
+from kubernetes import client
 
 import mlrun
 import mlrun.errors
@@ -8,22 +9,42 @@ from tests.api.runtimes.base import TestRuntimeBase
 
 
 class TestKubeResource(TestRuntimeBase):
-    def test_affinity_serialization(self):
-        affinity = self._generate_affinity()
-        kube_resource = mlrun.runtimes.pod.KubeResource()
-        # simulating the client - setting from a class instance
-        kube_resource.with_node_selection(affinity=affinity)
-        # simulating sending to API - serialization through dict
-        kube_resource_dict = kube_resource.to_dict()
-        kube_resource = kube_resource.from_dict(kube_resource_dict)
-        assert (
-            deepdiff.DeepDiff(
-                kube_resource.spec.affinity.to_dict(),
-                affinity.to_dict(),
-                ignore_order=True,
+    def test_attribute_serializations(self):
+        def set_with_node_selection(resource, attr_name, attr):
+            if attr_name == "tolerations":
+                resource.with_node_selection(tolerations=attr)
+            if attr_name == "affinity":
+                resource.with_node_selection(affinity=attr)
+
+        api = client.ApiClient()
+        for test_case in [
+            {
+                "attribute_name": "affinity",
+                "attribute": self._generate_affinity(),
+            },
+            {
+                "attribute_name": "tolerations",
+                "attribute": self._generate_tolerations(),
+            },
+        ]:
+            attribute_name = test_case.get("attribute_name")
+            attribute = test_case.get("attribute")
+            kube_resource = mlrun.runtimes.pod.KubeResource()
+
+            # simulating the client - setting from a class instance
+            set_with_node_selection(kube_resource, attribute_name, attribute)
+            # simulating sending to API - serialization through dict
+            kube_resource_dict = kube_resource.to_dict()
+            kube_resource = kube_resource.from_dict(kube_resource_dict)
+
+            assert (
+                deepdiff.DeepDiff(
+                    kube_resource.spec.to_dict()[attribute_name],
+                    api.sanitize_for_serialization(attribute),
+                    ignore_order=True,
+                )
+                == {}
             )
-            == {}
-        )
 
     def test_with_limits_regex_validation(self):
         cases = [

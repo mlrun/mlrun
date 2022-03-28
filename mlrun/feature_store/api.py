1c7820b985dc5301eb0d4a6b3aea4c3aac99bd8a
Gal Topper <galt@iguazio.com>
2022-03-28 23:12:06 +0800

    [Datastore] Add Snowflake source (#1845)

--- mlrun/feature_store/api.py
+++ mlrun/feature_store/api.py
@@ -25,6 +25,7 @@ from ..data_types import InferOptions, get_infer_interface
 from ..datastore.sources import BaseSourceDriver, StreamSource
 from ..datastore.store_resources import parse_store_uri
 from ..datastore.targets import (
+    BaseStoreTarget,
     get_default_prefix_for_source,
     get_default_targets,
     get_target_driver,
@@ -712,8 +713,8 @@ def deploy_ingestion_service(
 def _ingest_with_spark(
     spark=None,
     featureset: Union[FeatureSet, str] = None,
-    source: DataSource = None,
-    targets: List[DataTargetBase] = None,
+    source: BaseSourceDriver = None,
+    targets: List[BaseStoreTarget] = None,
     infer_options: InferOptions = InferOptions.default(),
     mlrun_context=None,
     namespace=None,
@@ -793,10 +794,11 @@ def _ingest_with_spark(
                     if partition not in df.columns and partition in time_unit_to_op:
                         op = time_unit_to_op[partition]
                         df = df.withColumn(partition, op(timestamp_col))
+            df_to_write = target.prepare_spark_df(df)
             if overwrite:
-                df.write.mode("overwrite").save(**spark_options)
+                df_to_write.write.mode("overwrite").save(**spark_options)
             else:
-                df.write.mode("append").save(**spark_options)
+                df_to_write.write.mode("append").save(**spark_options)
             target.set_resource(featureset)
             target.update_resource_status("ready")
 

ac506b1dca18a695251ff673c689486d7a407fdd
tankilevitch <tomtankilevitch@gmail.com>
2022-04-06 15:28:41 +0300

    add spec check between kubeResourceSpec and other runtimes

--- tests/runtimes/test_pod.py
+++ tests/runtimes/test_pod.py
@@ -21,6 +21,17 @@ def test_runtimes_inheritance():
             mlrun.runtimes.sparkjob.spark2job.Spark2JobSpec,
             mlrun.runtimes.sparkjob.spark3job.Spark3JobSpec,
         ],
+        mlrun.runtimes.pod.KubeResourceSpec: [
+            mlrun.runtimes.daskjob.DaskSpec,
+            mlrun.runtimes.function.NuclioSpec,
+            mlrun.runtimes.serving.ServingSpec,
+            mlrun.runtimes.mpijob.abstract.MPIResourceSpec,
+            mlrun.runtimes.mpijob.v1.MPIV1ResourceSpec,
+            mlrun.runtimes.remotesparkjob.RemoteSparkSpec,
+            mlrun.runtimes.sparkjob.abstract.AbstractSparkJobSpec,
+            mlrun.runtimes.sparkjob.spark2job.Spark2JobSpec,
+            mlrun.runtimes.sparkjob.spark3job.Spark3JobSpec,
+        ],
         mlrun.runtimes.base.FunctionStatus: [
             mlrun.runtimes.daskjob.DaskStatus,
             mlrun.runtimes.function.NuclioStatus,

75c71419f5bf67d00de0f5342a46192bb400ffe0
tankilevitch <tomtankilevitch@gmail.com>
2022-04-07 13:26:20 +0300

    add default_preemption_mode to frontend_spec schema

--- mlrun/api/schemas/frontend_spec.py
+++ mlrun/api/schemas/frontend_spec.py
@@ -44,3 +44,4 @@ class FrontendSpec(pydantic.BaseModel):
     auto_mount_params: typing.Dict[str, str] = {}
     default_artifact_path: str
     default_function_pod_resources: Resources = Resources()
+    default_preemption_mode: str

8ff1232bba67f1d49b3ef4b5fa15a961248eb54b
tankilevitch <tomtankilevitch@gmail.com>
2022-04-12 01:24:28 +0300

    add prints of runs to pass linting

--- tests/projects/assets/remote_pipeline.py
+++ tests/projects/assets/remote_pipeline.py
@@ -14,5 +14,6 @@ def func2(context, x=0):
 @kfp.dsl.pipeline(name="remote_pipeline", description="tests remote pipeline")
 def pipeline():
     run1 = mlrun.run_function("func1", handler="func1", params={"p1": 9})
-
+    print(run1)
     run2 = mlrun.run_function("func2", handler="func2", params={"x": 2})
+    print(run2)

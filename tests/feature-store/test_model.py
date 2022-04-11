6ee955e004cbe5a2bb072b7f830cc1599779205c
Ben <benbd86@gmail.com>
2022-04-11 13:41:00 +0300

    moved test

--- tests/feature-store/test_model.py
+++ tests/feature-store/test_model.py
@@ -11,10 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import mlrun
+import pytest
 from mlrun.data_types.data_types import ValueType
 from mlrun.feature_store import Entity, Feature, FeatureSet
 from mlrun.feature_store.common import parse_feature_string
+
+from mlrun.model import TargetPathObject
 
 
 def test_feature_set():
@@ -43,3 +46,33 @@ def test_features_parser():
             assert case.get("error", False), f"got unexpected error {exc}"
             continue
         assert result == case["result"]
+
+
+@pytest.mark.parametrize(
+    "raise_error, pass_run_id, is_single_file, target_path",
+    [
+        (False, True, False, "v3io:///bigdata/{run_id}"),
+        (False, True, True, "v3io:///bigdata/{run_id}/file.parquet"),
+        (False, False, False, "v3io:///bigdata/"),
+        (False, False, True, "v3io:///bigdata/file.parquet"),
+        (False, True, False, "v3io:///bigdata/"),
+        (False, True, True, "v3io:///bigdata/file.parquet"),
+        (True, False, False, "v3io:///bigdata/{run_id}"),
+        (True, False, True, "v3io:///bigdata/{run_id}/file.parquet"),
+    ],
+)
+def test_different_target_path_scenarios_for_run_id(
+    raise_error, pass_run_id, is_single_file, target_path
+):
+    run_id = "run_id_val" if pass_run_id else None
+
+    if raise_error:
+        with pytest.raises(mlrun.errors.MLRunInvalidArgumentError):
+            TargetPathObject(target_path, run_id, is_single_file)
+    else:
+        tp_obj = TargetPathObject(target_path, run_id, is_single_file)
+        assert (mlrun.model.RUN_ID_PLACE_HOLDER in tp_obj.get_templated_path()) == (
+            (pass_run_id and not is_single_file)
+            or mlrun.model.RUN_ID_PLACE_HOLDER in target_path
+        )
+        assert mlrun.model.RUN_ID_PLACE_HOLDER not in tp_obj.get_absolute_path()

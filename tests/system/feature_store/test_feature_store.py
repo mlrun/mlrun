7e6f4f6ee8d90a394e3267131efe06ab90446c45
Ben <benbd86@gmail.com>
2022-04-25 18:34:18 +0300

    add test

--- tests/system/feature_store/test_feature_store.py
+++ tests/system/feature_store/test_feature_store.py
@@ -2466,6 +2466,21 @@ class TestFeatureStore(TestMLRunSystem):
         assert res is not None
         assert res.to_dataframe().to_dict() == expected
 
+    def test_feature_vector_with_all_features_and_label_feature_two_feature_sets(self):
+        feature_set = FeatureSet("fs1", entities=[Entity("ticker")])
+        fs.ingest(feature_set, stocks)
+        feature_set = FeatureSet("fs2", entities=[Entity("ticker")])
+        fs.ingest(feature_set, stocks)
+
+        fv = fs.FeatureVector("fv-label", ["fs1.* as fs1", "fs2.* as fs2"], "fs1.name")
+        res = fs.get_offline_features(fv)
+
+        expected = ["fs1_exchange", "name", "fs2_name", "fs2_exchange"]
+        assert res is not None
+        assert len(expected) == len(res.to_dataframe().to_dict().keys())
+        for key in res.to_dataframe().to_dict().keys():
+            assert key in expected
+
 
 def verify_purge(fset, targets):
     fset.reload(update_spec=False)

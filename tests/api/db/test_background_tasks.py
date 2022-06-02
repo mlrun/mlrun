502a55346f90e725fc5e5be5a34cede3dc8782f1
tankilevitch <tomtankilevitch@gmail.com>
2022-06-02 14:36:27 +0300

    add more tests

--- tests/api/db/test_background_tasks.py
+++ tests/api/db/test_background_tasks.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 import pytest
@@ -77,13 +78,15 @@ def test_store_background_task_after_status_updated(
 @pytest.mark.parametrize(
     "db,db_session", [(dbs[0], dbs[0])], indirect=["db", "db_session"]
 )
-def test_storing_project_background_task(db: DBInterface, db_session: Session):
+def test_storing_project_background_task_and_non_project_background_task_with_same_name(
+    db: DBInterface, db_session: Session
+):
     task_name = "test"
     project = "test-project"
     project_timeout = 40
     no_project_timeout = 1
     db.store_background_task(
-        db_session, "test", timeout=project_timeout, project=project
+        db_session, name=task_name, timeout=project_timeout, project=project
     )
     # expecting to fail because there is no background task named tested without a project
     with pytest.raises(mlrun.errors.MLRunNotFoundError):
@@ -116,3 +119,50 @@ def test_storing_project_background_task(db: DBInterface, db_session: Session):
     )
     assert background_task.metadata.timeout == project_timeout
     assert background_task.metadata.project == project
+
+
+@pytest.mark.parametrize(
+    "db,db_session", [(dbs[0], dbs[0])], indirect=["db", "db_session"]
+)
+def test_get_background_task_with_exceeded_timeout_and_disabled_timeout(
+    db: DBInterface, db_session: Session
+):
+    task_name = "test"
+    project = "test-project"
+    task_timeout = 0
+    mlrun.mlconf.background_tasks_timeout_defaults.mode = "disabled"
+    db.store_background_task(
+        db_session, name=task_name, timeout=task_timeout, project=project
+    )
+    background_task = db.get_background_task(db_session, task_name, project)
+    assert background_task.metadata.timeout == task_timeout
+    # expecting created and updated time to be equal because mode disabled even if timeout exceeded
+    assert background_task.metadata.created == background_task.metadata.updated
+    assert background_task.status.state == mlrun.api.schemas.BackgroundTaskState.running
+    # expecting timeout to exceed
+    assert (
+        datetime.timedelta(seconds=background_task.metadata.timeout)
+        + background_task.metadata.updated
+        < datetime.datetime.utcnow()
+    )
+    task_name = "test1"
+    db.store_background_task(db_session, name=task_name, project=project)
+    # because timeout default mode is disabled, expecting not to enrich the background task timeout
+    background_task = db.get_background_task(db_session, task_name, project)
+    assert background_task.metadata.timeout is None
+    assert background_task.metadata.created == background_task.metadata.updated
+    assert background_task.status.state == mlrun.api.schemas.BackgroundTaskState.running
+
+    db.store_background_task(
+        db_session,
+        name=task_name,
+        project=project,
+        state=mlrun.api.schemas.BackgroundTaskState.succeeded,
+    )
+    background_task_new = db.get_background_task(db_session, task_name, project)
+    assert (
+        background_task_new.status.state
+        == mlrun.api.schemas.BackgroundTaskState.succeeded
+    )
+    assert background_task_new.metadata.updated > background_task.metadata.updated
+    assert background_task_new.metadata.created == background_task.metadata.created

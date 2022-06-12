ccd4991c18e18dd0fda9d110b6788929c8d2f5ae
Hedi Ingber <hedingber@gmail.com>
2022-06-12 15:13:32 +0300

    Add test

---
+++ tests/rundb/test_unit_httpdb.py
@@ -0,0 +1,25 @@
+# test_httpdb.py actually holds integration tests (that should be migrated to tests/integration/sdk_api/httpdb)
+# currently we are running it in the integration tests CI step so adding this file for unit tests for the httpdb
+import enum
+import unittest.mock
+
+import mlrun.db.httpdb
+
+
+class SomeEnumClass(str, enum.Enum):
+    value1 = "value1"
+    value2 = "value2"
+
+
+def test_api_call_enum_conversion():
+    db = mlrun.db.httpdb.HTTPRunDB("fake-url")
+    db.session = unittest.mock.Mock()
+    db.api_call(
+        "GET",
+        "some-path",
+        headers={"enum-value": SomeEnumClass.value1, "string-value": "value"},
+        params={"enum-value": SomeEnumClass.value2, "string-value": "value"},
+    )
+    for dict_key in ["headers", "params"]:
+        for value in db.session.request.call_args_list[0][1][dict_key].values():
+            assert type(value) == str

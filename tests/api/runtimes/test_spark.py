abd04460f04917d354da8c6944675848278166e4
Ben <benbd86@gmail.com>
2022-06-13 17:19:33 +0300

    PR comment

--- tests/api/runtimes/test_spark.py
+++ tests/api/runtimes/test_spark.py
@@ -157,16 +157,12 @@ class TestSpark3Runtime(tests.api.runtimes.base.TestRuntimeBase):
     def test_deploy_default_image_without_limits(
         self, db: sqlalchemy.orm.Session, client: fastapi.testclient.TestClient
     ):
-        default_docker_registry = mlrun.config.config.httpdb.builder.docker_registry
         mlrun.config.config.httpdb.builder.docker_registry = "test_registry"
-        try:
-            runtime: mlrun.runtimes.Spark3Runtime = self._generate_runtime()
-            runtime.spec.image = None
-            runtime.spec.use_default_image = True
-            self.execute_function(runtime)
-            self._assert_custom_object_creation_config()
-        finally:
-            mlrun.config.config.httpdb.builder.docker_registry = default_docker_registry
+        runtime: mlrun.runtimes.Spark3Runtime = self._generate_runtime()
+        runtime.spec.image = None
+        runtime.spec.use_default_image = True
+        self.execute_function(runtime)
+        self._assert_custom_object_creation_config()
 
     def test_run_without_runspec(
         self, db: sqlalchemy.orm.Session, client: fastapi.testclient.TestClient

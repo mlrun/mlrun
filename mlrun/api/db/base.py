46e64162daf090c6c777222e5719ceaca469e467
Tom Tankilevitch <59158507+Tankilevitch@users.noreply.github.com>
2022-06-16 09:29:16 +0300

    [Background Tasks] Support multi-instance API - Phase 1 (#2012)

--- mlrun/api/db/base.py
+++ mlrun/api/db/base.py
@@ -501,3 +501,18 @@ class DBInterface(ABC):
 
     def get_marketplace_source(self, session, name) -> schemas.IndexedMarketplaceSource:
         pass
+
+    def store_background_task(
+        self,
+        session,
+        name: str,
+        project: str,
+        state: str = schemas.BackgroundTaskState.running,
+        timeout: int = None,
+    ):
+        pass
+
+    def get_background_task(
+        self, session, name: str, project: str
+    ) -> schemas.BackgroundTask:
+        pass

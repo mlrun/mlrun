fe96411e2a2e11fb8087ba1b764668998671fbb8
tankilevitch <tomtankilevitch@gmail.com>
2022-06-22 18:46:49 +0300

    .

--- mlrun/runtimes/sparkjob/abstract.py
+++ mlrun/runtimes/sparkjob/abstract.py
@@ -597,6 +597,7 @@ with ctx:
             logger.error(f"Exception when creating SparkJob ({crd}): {exc}")
             raise RunError(f"Exception when creating SparkJob: {exc}")
 
+
     def get_job(self, name, namespace=None):
         k8s = self._get_k8s()
         namespace = k8s.resolve_namespace(namespace)

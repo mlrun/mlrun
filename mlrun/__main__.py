da424ca13f01207f42b1e3ab5af3c118922aa0e0
Tom Tankilevitch <59158507+Tankilevitch@users.noreply.github.com>
2022-06-25 21:28:01 +0300

    [Projects] Add timeout attribute to run method (#2074)

--- mlrun/__main__.py
+++ mlrun/__main__.py
@@ -819,6 +819,12 @@ def logs(uid, project, offset, db, watch):
 @click.option("--engine", default=None, help="workflow engine (kfp/local)")
 @click.option("--local", is_flag=True, help="try to run workflow functions locally")
 @click.option(
+    "--timeout",
+    type=int,
+    default=None,
+    help="timeout in seconds to wait for pipeline completion (used when watch=True)",
+)
+@click.option(
     "--env-file", default="", help="path to .env file to load config/variables from"
 )
 def project(
@@ -843,6 +849,7 @@ def project(
     engine,
     local,
     env_file,
+    timeout,
 ):
     """load and/or run a project"""
     if env_file:
@@ -923,7 +930,7 @@ def project(
             exit(1)
 
         if watch and run_result and run_result.workflow.engine == "kfp":
-            proj.get_run_status(run_result)
+            proj.get_run_status(run_result, timeout=timeout)
 
     elif sync:
         print("saving project functions to db ..")

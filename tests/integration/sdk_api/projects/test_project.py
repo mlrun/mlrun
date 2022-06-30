c74810a69396ef6393e8e33b664733de30ed94c8
tankilevitch <tomtankilevitch@gmail.com>
2022-06-30 17:50:49 +0300

    add integration test

--- tests/integration/sdk_api/projects/test_project.py
+++ tests/integration/sdk_api/projects/test_project.py
@@ -1,3 +1,5 @@
+import pytest
+
 import mlrun
 import tests.integration.sdk_api.base
 
@@ -16,3 +18,36 @@ class TestProject(tests.integration.sdk_api.base.TestMLRunIntegration):
         project = mlrun.new_project(project_name)
         project.save_to_db()
         mlrun.load_project(".", f"db://{project_name}")
+
+    def test_load_project(self):
+        project_name = "some-project"
+        project = mlrun.new_project(project_name)
+        project.save_to_db()
+
+        loaded_project_name = "loaded-project"
+        mlrun.load_project(
+            ".", f"db://{project_name}", name=loaded_project_name, save=False
+        )
+        # loaded project but didn't saved expected to fail
+        with pytest.raises(mlrun.errors.MLRunNotFoundError):
+            mlrun.load_project(".", f"db://{loaded_project_name}", save=False)
+
+        # loaded project and auto saved
+        mlrun.load_project(".", f"db://{project_name}", name=loaded_project_name)
+        # load project expects to succeed because already saved in DB
+        mlrun.load_project(".", f"db://{loaded_project_name}")
+
+    # def test_get_or_create_project(self):
+    #     project_name = "some-project"
+    #     with pytest.raises(mlrun.errors.MLRunNotFoundError):
+    #         project = mlrun.get_or_create_project(project_name, "./",save=False)
+    #
+    #     project = mlrun.new_project(project_name, save=False)
+    #     project.set_function("hub://describe", "describe")
+    #     project_function_object = project.spec._function_objects
+    #     project_file_path = pathlib.Path(tests.conftest.results) / "project.yaml"
+    #     project.export(str(project_file_path))
+    #     imported_project = mlrun.load_project("./", str(project_file_path))
+    #
+    #     project = mlrun.get_or_create_project(project_name, "./", save=False)
+    #

b1294ec3bf7d73b3c3d5986aad5d3d009c49a598
tankilevitch <tomtankilevitch@gmail.com>
2022-07-02 21:29:04 +0300

    fix test

--- tests/integration/sdk_api/projects/test_project.py
+++ tests/integration/sdk_api/projects/test_project.py
@@ -1,6 +1,9 @@
+import pathlib
+
 import pytest
 
 import mlrun
+import tests.conftest
 import tests.integration.sdk_api.base
 
 
@@ -19,36 +22,29 @@ class TestProject(tests.integration.sdk_api.base.TestMLRunIntegration):
         project.save_to_db()
         mlrun.load_project(".", f"db://{project_name}")
 
-    def test_load_project(self):
+    def test_load_project_with_save(self):
         project_name = "some-project"
         project = mlrun.new_project(project_name)
-        project.save_to_db()
+        project_file_path = pathlib.Path(tests.conftest.results) / "project.yaml"
+        project.export(str(project_file_path))
 
-        loaded_project_name = "loaded-project"
+        imported_project_name = "imported-project"
+        # loaded project but didn't saved
         mlrun.load_project(
-            ".", f"db://{project_name}", name=loaded_project_name, save=False
+            "./", str(project_file_path), name=imported_project_name, save=False
         )
-        # loaded project but didn't saved expected to fail
+
+        # loading project from db, but earlier load didn't saved, expected to fail
         with pytest.raises(mlrun.errors.MLRunNotFoundError):
-            mlrun.load_project(".", f"db://{loaded_project_name}", save=False)
+            mlrun.load_project(".", f"db://{imported_project_name}", save=False)
 
-        # loaded project and auto saved
-        loaded_project = mlrun.load_project(".", f"db://{project_name}", name=loaded_project_name)
-        print(loaded_project.name, loaded_project, loaded_project)
-        # load project expects to succeed because already saved in DB
-        mlrun.load_project("./", f"db://{loaded_project_name}")
+        # loading project and saving
+        expected_project = mlrun.load_project(
+            "./", str(project_file_path), name=imported_project_name
+        )
 
-    # def test_get_or_create_project(self):
-    #     project_name = "some-project"
-    #     with pytest.raises(mlrun.errors.MLRunNotFoundError):
-    #         project = mlrun.get_or_create_project(project_name, "./",save=False)
-    #
-    #     project = mlrun.new_project(project_name, save=False)
-    #     project.set_function("hub://describe", "describe")
-    #     project_function_object = project.spec._function_objects
-    #     project_file_path = pathlib.Path(tests.conftest.results) / "project.yaml"
-    #     project.export(str(project_file_path))
-    #     imported_project = mlrun.load_project("./", str(project_file_path))
-    #
-    #     project = mlrun.get_or_create_project(project_name, "./", save=False)
-    #
+        # loading project from db, expected to succeed
+        load_project_from_db = mlrun.load_project(
+            ".", f"db://{imported_project_name}", save=False
+        )
+        assert expected_project == load_project_from_db

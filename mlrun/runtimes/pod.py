66a6e47b7428bdd3f7d08cea5891906fc8e5bdfa
AlonMaor14 <maoralo@post.bgu.ac.il>
2022-07-04 12:06:37 +0300

    fix

--- mlrun/runtimes/pod.py
+++ mlrun/runtimes/pod.py
@@ -148,7 +148,7 @@ class KubeResourceSpec(FunctionSpec):
         self.image_pull_policy = image_pull_policy
         self.service_account = service_account
         self.image_pull_secret = (  # TODO: check which runtimes override this value
-            image_pull_secret or mlrun.mlconf.builder.docker_registry_secret
+            image_pull_secret or mlrun.mlconf.httpdb.builder.docker_registry_secret
         )
         self.node_name = node_name
         self.node_selector = (

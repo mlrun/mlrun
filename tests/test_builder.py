f6af4c2cf1d5d1104a0eac7bb48462d8712f6585
Alon Maor <48641682+AlonMaor14@users.noreply.github.com>
2022-07-21 12:58:14 +0300

    [Builder] create ECR cache repo (#2154)

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -460,14 +460,14 @@ def test_build_runtime_ecr_with_ec2_iam_policy(monkeypatch):
     ]
     assert len(pod_spec.init_containers) == 2
     for init_container in pod_spec.init_containers:
-        if init_container.name == "create-repo":
+        if init_container.name == "create-repos":
             assert (
                 "aws ecr create-repository --region region --repository-name mlrun/func-some-project-some-function"
                 in init_container.args[1]
             )
             break
     else:
-        pytest.fail("no create-repo init container")
+        pytest.fail("no create-repos init container")
 
 
 def test_build_runtime_resolve_ecr_registry(monkeypatch):
@@ -506,14 +506,16 @@ def test_build_runtime_resolve_ecr_registry(monkeypatch):
         )
         pod_spec = _create_pod_mock_pod_spec()
         for init_container in pod_spec.init_containers:
-            if init_container.name == "create-repo":
+            if init_container.name == "create-repos":
                 assert (
                     f"aws ecr create-repository --region us-east-2 --repository-name {case.get('repo')}"
                     in init_container.args[1]
                 ), f"test case: {case.get('name')}"
                 break
         else:
-            pytest.fail(f"no create-repo init container, test case: {case.get('name')}")
+            pytest.fail(
+                f"no create-repos init container, test case: {case.get('name')}"
+            )
 
 
 def test_build_runtime_ecr_with_aws_secret(monkeypatch):
@@ -561,7 +563,7 @@ def test_build_runtime_ecr_with_aws_secret(monkeypatch):
         env.to_dict() for env in pod_spec.containers[0].env
     ]
     for init_container in pod_spec.init_containers:
-        if init_container.name == "create-repo":
+        if init_container.name == "create-repos":
             assert aws_mount in [
                 volume_mount.to_dict() for volume_mount in init_container.volume_mounts
             ]
@@ -570,7 +572,7 @@ def test_build_runtime_ecr_with_aws_secret(monkeypatch):
             ]
             break
     else:
-        pytest.fail("no create-repo init container")
+        pytest.fail("no create-repos init container")
 
 
 def _get_target_image_from_create_pod_mock():

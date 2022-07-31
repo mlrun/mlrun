2187e8a7a85d7c447f743ff6150aa155d6b37aa1
tankilevitch <tomtankilevitch@gmail.com>
2022-07-31 13:40:23 +0300

    add keep alive

--- automation/system_test/prepare.py
+++ automation/system_test/prepare.py
@@ -84,6 +84,7 @@ class SystemTestPreparer:
         self._logger.info("Connecting to data-cluster", data_cluster_ip=data_cluster_ip)
         if not self._debug:
             self._ssh_client = paramiko.SSHClient()
+            self._ssh_client.get_transport().set_keepalive(720)
             self._ssh_client.set_missing_host_key_policy(paramiko.WarningPolicy)
             self._ssh_client.connect(
                 data_cluster_ip,

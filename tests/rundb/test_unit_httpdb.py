ed3232d250e8414dcad62dc752dcae5ddf2fd460
quaark <adamm@iguazio.com>
2022-08-04 10:21:21 +0300

    retry api call on connection reset

--- tests/rundb/test_unit_httpdb.py
+++ tests/rundb/test_unit_httpdb.py
@@ -2,6 +2,7 @@
 # currently we are running it in the integration tests CI step so adding this file for unit tests for the httpdb
 import enum
 import unittest.mock
+import pytest
 
 import mlrun.db.httpdb
 
@@ -27,3 +28,15 @@ def test_api_call_enum_conversion():
     for dict_key in ["headers", "params"]:
         for value in db.session.request.call_args_list[1][1][dict_key].values():
             assert type(value) == str
+
+
+def test_connection_reset_causes_retries():
+    db = mlrun.db.httpdb.HTTPRunDB("fake-url")
+    db.session = unittest.mock.Mock()
+    db.session.request.side_effect = ConnectionResetError
+
+    with unittest.mock.patch("time.sleep") as _:
+        with pytest.raises(ConnectionResetError):
+            db.api_call("GET", "some-path")
+
+    assert db.session.request.call_count == mlrun.db.httpdb.HTTP_RETRY_AMOUNT

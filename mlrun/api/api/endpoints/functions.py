a6e99b7c632949234d3ae24b433b8cf941f360ca
Tom Tankilevitch <59158507+Tankilevitch@users.noreply.github.com>
2022-08-06 16:39:39 +0300

    [Runtimes] Backport Fix security context enrichment for dask and nuclio [1.0.x] (#2230)

--- mlrun/api/api/endpoints/functions.py
+++ mlrun/api/api/endpoints/functions.py
@@ -481,6 +481,7 @@ def _build_function(
             mlrun.api.api.utils.ensure_function_has_auth_set(fn, auth_info)
             mlrun.api.api.utils.process_function_service_account(fn)
             mlrun.api.api.utils.mask_sensitive_data(fn, auth_info)
+            mlrun.api.api.utils.ensure_function_security_context(fn, auth_info)
 
             if fn.kind == RuntimeKinds.serving:
                 # Handle model monitoring
@@ -581,6 +582,7 @@ def _start_function(
             mlrun.api.api.utils.ensure_function_has_auth_set(function, auth_info)
             mlrun.api.api.utils.process_function_service_account(function)
             mlrun.api.api.utils.mask_sensitive_data(function, auth_info)
+            mlrun.api.api.utils.ensure_function_security_context(function, auth_info)
             #  resp = resource["start"](fn)  # TODO: handle resp?
             resource["start"](function, client_version=client_version)
             function.save(versioned=False)

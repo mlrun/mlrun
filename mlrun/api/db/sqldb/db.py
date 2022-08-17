fe23df6917de32eb1664314da92037c2aa08f7b5
tankilevitch <tomtankilevitch@gmail.com>
2022-08-17 11:40:52 +0300

    fix CI

--- mlrun/api/db/sqldb/db.py
+++ mlrun/api/db/sqldb/db.py
@@ -2744,9 +2744,10 @@ class SQLDB(DBInterface):
     ) -> schemas.ScheduleRecord:
         schedule = schemas.ScheduleRecord.from_orm(schedule_record)
         schedule.creation_time = self._add_utc_timezone(schedule.creation_time)
-        schedule.next_run_time = schedule.next_run_time.replace(
-            tzinfo=dateutil.tz.tzlocal()
-        )
+        if schedule.next_run_time:
+            schedule.next_run_time = schedule.next_run_time.replace(
+                tzinfo=dateutil.tz.tzlocal()
+            )
         return schedule
 
     @staticmethod

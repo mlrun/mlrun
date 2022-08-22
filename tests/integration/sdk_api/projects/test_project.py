9d7366082a611c96067a13ba9723a8d8d892a6c8
AlonMaor14 <maoralo@post.bgu.ac.il>
2022-08-22 18:32:42 +0300

    .

--- tests/integration/sdk_api/projects/test_project.py
+++ tests/integration/sdk_api/projects/test_project.py
@@ -1,4 +1,3 @@
-import json
 import pathlib
 
 import deepdiff
@@ -25,7 +24,7 @@ class TestProject(tests.integration.sdk_api.base.TestMLRunIntegration):
         with pytest.raises(mlrun.errors.MLRunConflictError) as exc:
             mlrun.new_project(project_name)
         assert (
-            f"Project with name {project_name} already exists. Use override=True to override the existing project."
+            f"Project with name {project_name} already exists. Use overwrite=True to override the existing project."
             in str(exc.value)
         )
 

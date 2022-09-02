76da9370665cc1b0f94f1bff6d71376973767a30
jilnogold <jillg@iguazio.com>
2022-09-02 07:27:08 +0300

    project.py - add line break

--- mlrun/projects/project.py
+++ mlrun/projects/project.py
@@ -211,7 +211,8 @@ def load_project(
     example::
 
         # Load the project and run the 'main' workflow.
-        # When using git as the url source the context directory must be an empty or non-existent folder as the git repo will be cloned there
+        # When using git as the url source the context directory must be an empty or non-existent 
+        # folder as the git repo will be cloned there
         project = load_project("./demo_proj", "git://github.com/mlrun/project-demo.git")
         project.run("main", arguments={'data': data_url})
 

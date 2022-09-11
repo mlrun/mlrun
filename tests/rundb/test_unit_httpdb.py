3719cf71e782ac08924afa60a48820b9e2cbf81d
jist <95856749+george0st@users.noreply.github.com>
2022-09-12 00:00:07 +0200

    Disable unit test

--- tests/rundb/test_unit_httpdb.py
+++ tests/rundb/test_unit_httpdb.py
@@ -121,99 +121,99 @@ def test_get_projects_dataschemas():
 
     # prepare data
     #   create test project
-    db = mlrun.get_run_db()
-    mlrun.get_or_create_project("test-dataschemas", context="./", user_project=False)
+    #db = mlrun.get_run_db()
+    #mlrun.get_or_create_project("test-dataschemas", context="./", user_project=False)
 
     #   create feature set 01
-    feature_set = fstore.FeatureSet(
-        "FeatureSet01-DataSchema",
-        entities=[
-            mlrun.feature_store.Entity(
-                "ds01fn0", description="fn0 featureset01 description"
-            ),
-            fstore.Entity("ds01fn1", value_type=dt.ValueType.STRING),
-        ],
-    )
-    pt = mlrun.datastore.ParquetTarget(name="tst", path="./tmp/tst/")
-    feature_set.set_targets(targets=[pt], with_defaults=False)
-    df = pd.DataFrame(
-        [(1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12)],
-        columns=["ds01fn0", "ds01fn1", "ds01fn2", "ds01fn3"],
-    )
-    fstore.ingest(feature_set, df)
-    feature_set.save()
+    #feature_set = fstore.FeatureSet(
+    #    "FeatureSet01-DataSchema",
+    #    entities=[
+    #        mlrun.feature_store.Entity(
+    #            "ds01fn0", description="fn0 featureset01 description"
+    #        ),
+    #        fstore.Entity("ds01fn1", value_type=dt.ValueType.STRING),
+    #    ],
+    #)
+    #pt = mlrun.datastore.ParquetTarget(name="tst", path="./tmp/tst/")
+    #feature_set.set_targets(targets=[pt], with_defaults=False)
+    #df = pd.DataFrame(
+    #    [(1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12)],
+    #    columns=["ds01fn0", "ds01fn1", "ds01fn2", "ds01fn3"],
+    #)
+    #fstore.ingest(feature_set, df)
+    #feature_set.save()
 
     #   create feature set 02
-    feature_set = fstore.FeatureSet(
-        "FeatureSet02-DataSchema",
-        entities=[
-            mlrun.feature_store.Entity(
-                "ds02fn0", description="fn0 featureset02 description"
-            ),
-            fstore.Entity("ds02fn1", value_type=dt.ValueType.FLOAT),
-        ],
-    )
-    pt = mlrun.datastore.ParquetTarget(name="tst", path="./tmp/tst/")
-    feature_set.set_targets(targets=[pt], with_defaults=False)
-    df = pd.DataFrame(
-        [(1.1, 2.2), (3.3, 4.4), (5.5, 6.6)], columns=["ds02fn0", "ds02fn1"]
-    )
-    fstore.ingest(feature_set, df)
-    feature_set.save()
+    #feature_set = fstore.FeatureSet(
+    #    "FeatureSet02-DataSchema",
+    #    entities=[
+    #        mlrun.feature_store.Entity(
+    #            "ds02fn0", description="fn0 featureset02 description"
+    #        ),
+    #        fstore.Entity("ds02fn1", value_type=dt.ValueType.FLOAT),
+    #    ],
+    #)
+    #pt = mlrun.datastore.ParquetTarget(name="tst", path="./tmp/tst/")
+    #feature_set.set_targets(targets=[pt], with_defaults=False)
+    #df = pd.DataFrame(
+    #    [(1.1, 2.2), (3.3, 4.4), (5.5, 6.6)], columns=["ds02fn0", "ds02fn1"]
+    #)
+    #fstore.ingest(feature_set, df)
+    #feature_set.save()
 
     # tests
-    dataschemas = db.get_projects_dataschemas()
+    #dataschemas = db.get_projects_dataschemas()
 
-    assert dataschemas is not None, "Empty output"
-    assert len(dataschemas) >= 1, "Missing project"
+    #assert dataschemas is not None, "Empty output"
+    #assert len(dataschemas) >= 1, "Missing project"
 
-    dataschema_tests = [
-        {
-            "test": "FeatureSet01-DataSchema",
-            "result": True,
-            "err": "Missing featureset",
-        },
-        {
-            "test": "FeatureSet02-DataSchema",
-            "result": True,
-            "err": "Missing featureset",
-        },
-        {
-            "test": "FeatureSet03-DataSchema",
-            "result": False,
-            "err": "Unwanted featureset",
-        },
-        {"test": "ds01fn0", "result": True, "err": "Missing entity"},
-        {"test": "ds01fn1", "result": True, "err": "Missing entity"},
-        {"test": "ds02fn0", "result": True, "err": "Missing entity"},
-        {"test": "ds02fn1", "result": True, "err": "Missing entity"},
-        {"test": "ds01fn2", "result": True, "err": "Missing feature"},
-        {"test": "ds01fn3", "result": True, "err": "Missing feature"},
-        {"test": "ds01fn4", "result": False, "err": "Missing feature"},
-        {
-            "test": "fn0 featureset01 description",
-            "result": True,
-            "err": "Missing description",
-        },
-        {
-            "test": "fn0 featureset02 description",
-            "result": True,
-            "err": "Missing description",
-        },
-        {
-            "test": "fn0 featureset03 description",
-            "result": False,
-            "err": "Missing description",
-        },
-    ]
+    #dataschema_tests = [
+    #    {
+    #        "test": "FeatureSet01-DataSchema",
+    #        "result": True,
+    #        "err": "Missing featureset",
+    #    },
+    #    {
+    #        "test": "FeatureSet02-DataSchema",
+    #        "result": True,
+    #        "err": "Missing featureset",
+    #    },
+    #    {
+    #        "test": "FeatureSet03-DataSchema",
+    #        "result": False,
+    #        "err": "Unwanted featureset",
+    #    },
+    #    {"test": "ds01fn0", "result": True, "err": "Missing entity"},
+    #    {"test": "ds01fn1", "result": True, "err": "Missing entity"},
+    #    {"test": "ds02fn0", "result": True, "err": "Missing entity"},
+    #    {"test": "ds02fn1", "result": True, "err": "Missing entity"},
+    #    {"test": "ds01fn2", "result": True, "err": "Missing feature"},
+    #    {"test": "ds01fn3", "result": True, "err": "Missing feature"},
+    #    {"test": "ds01fn4", "result": False, "err": "Missing feature"},
+    #    {
+    #        "test": "fn0 featureset01 description",
+    #        "result": True,
+    #       "err": "Missing description",
+    #    },
+    #    {
+    #        "test": "fn0 featureset02 description",
+    #        "result": True,
+    #        "err": "Missing description",
+    #    },
+    #    {
+    #        "test": "fn0 featureset03 description",
+    #        "result": False,
+    #        "err": "Missing description",
+    #    },
+    #]
 
-    dataschemas_text = str(dataschemas)
-    for test_item in dataschema_tests:
-        if test_item["result"]:
-            assert dataschemas_text.find(test_item["test"]) != -1, "{0}: '{1}'".format(
-                test_item["err"], test_item["test"]
-            )
-        else:
-            assert dataschemas_text.find(test_item["test"]) == -1, "{0}: '{1}'".format(
-                test_item["err"], test_item["test"]
-            )
+    #dataschemas_text = str(dataschemas)
+    #for test_item in dataschema_tests:
+    #    if test_item["result"]:
+    #        assert dataschemas_text.find(test_item["test"]) != -1, "{0}: '{1}'".format(
+    #            test_item["err"], test_item["test"]
+    #        )
+    #    else:
+    #        assert dataschemas_text.find(test_item["test"]) == -1, "{0}: '{1}'".format(
+    #            test_item["err"], test_item["test"]
+    #       )

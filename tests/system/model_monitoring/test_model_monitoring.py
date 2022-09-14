0ae201f904968e15a0bb4757b8e82969ba4748d0
eyald <eyald@iguazio.com>
2022-09-14 12:07:09 +0300

    move monitoring feature set from spec to status

--- tests/system/model_monitoring/test_model_monitoring.py
+++ tests/system/model_monitoring/test_model_monitoring.py
@@ -582,7 +582,7 @@ class TestModelMonitoringAPI(TestMLRunSystem):
         m_fs = mlrun.feature_store.get_feature_set(
             f"store://feature-sets/{self.project}/monitoring-serving-diabetes_model-latest:latest"
         )
-        assert model_endpoint.spec.monitoring_feature_set_uri == m_fs.uri
+        assert model_endpoint.status.monitoring_feature_set_uri == m_fs.uri
 
     @staticmethod
     def _get_auth_info() -> mlrun.api.schemas.AuthInfo:

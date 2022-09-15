3e3b1a1492fc8f2104aa992efaac81b231cba273
eyald <eyald@iguazio.com>
2022-09-15 11:37:35 +0300

    adding None value as default

--- mlrun/api/schemas/model_endpoints.py
+++ mlrun/api/schemas/model_endpoints.py
@@ -119,7 +119,7 @@ class ModelEndpointStatus(ObjectStatus):
     children: Optional[List[str]]
     children_uids: Optional[List[str]]
     endpoint_type: Optional[EndpointType]
-    monitoring_feature_set_uri: Optional[str]
+    monitoring_feature_set_uri: Optional[str] = None
 
     class Config:
         extra = Extra.allow

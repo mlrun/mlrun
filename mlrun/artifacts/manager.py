3408dee9699266bec6cccb896fa61dd7db139d63
Tom Tankilevitch <59158507+Tankilevitch@users.noreply.github.com>
2022-09-15 19:34:35 +0300

    [Artifacts] Support uploading artifact by its hash (#2368)

--- mlrun/artifacts/manager.py
+++ mlrun/artifacts/manager.py
@@ -15,6 +15,8 @@
 import pathlib
 from os.path import isdir
 
+import mlrun.config
+
 from ..db import RunDBInterface
 from ..utils import is_legacy_artifact, is_relative_path, logger
 from .base import (
@@ -83,7 +85,7 @@ class ArtifactProducer:
         self.iteration = 0
         self.inputs = {}
 
-    def get_meta(self):
+    def get_meta(self) -> dict:
         return {"kind": self.kind, "name": self.name, "tag": self.tag}
 
 
@@ -165,11 +167,16 @@ class ArtifactManager:
         item.db_key = db_key if db_key else ""
         item.viewer = viewer or item.viewer
         item.tree = producer.tag
-        item.labels = labels or item.labels
+        item.tag = tag or item.tag
+
         item.producer = producer.get_meta()
+        item.labels = labels or item.labels
+        # if running as part of a workflow, enrich artifact with workflow uid label
+        if item.producer.get("workflow"):
+            item.labels.update({"workflow-id": item.producer.get("workflow")})
+
         item.iter = producer.iteration
         item.project = producer.project
-        item.tag = tag or item.tag
 
         if target_path:
             if is_relative_path(target_path):
@@ -182,7 +189,14 @@ class ArtifactManager:
                 raise ValueError(f"Cannot upload from remote path {src_path}")
             target_path = src_path
             upload = False
-        elif not item.is_inline():
+
+        # if mlrun.mlconf.should_generate_target_path_from_artifact_hash() outputs True and the user
+        # didn't pass target_path explicitly then we won't use `generate_target_path` to calculate the target path,
+        # but rather use the `resolve_<body/file>_target_hash_path` in the `item.upload` method.
+        elif (
+            not item.is_inline()
+            and not mlrun.mlconf.should_generate_target_path_from_artifact_hash()
+        ):
             target_path = item.generate_target_path(artifact_path, producer)
 
         if target_path and item.is_dir and not target_path.endswith("/"):
@@ -194,7 +208,7 @@ class ArtifactManager:
         self.artifacts[key] = item
 
         if ((upload is None and item.kind != "dir") or upload) and not item.is_inline():
-            item.upload()
+            item.upload(artifact_path=artifact_path)
 
         if db_key:
             self._log_to_db(db_key, producer.project, producer.inputs, item)
--- mlrun/projects/project.py
+++ mlrun/projects/project.py
@@ -1495,7 +1495,7 @@ class MlrunProject(ModelObj):
         else:
             raise ValueError("unsupported file suffix, use .yaml, .json, or .zip")
 
-        return self.log_artifact(artifact, artifact_path=artifact_path)
+        return self.log_artifact(artifact, artifact_path=artifact_path, upload=False)
 
     def reload(self, sync=False, context=None) -> "MlrunProject":
         """reload the project and function objects from the project yaml/specs
@@ -1887,10 +1887,17 @@ class MlrunProject(ModelObj):
 
     def _enrich_artifact_path_with_workflow_uid(self):
         artifact_path = self.spec.artifact_path or mlrun.mlconf.artifact_path
-        if not mlrun.mlconf.enrich_artifact_path_with_workflow_id:
-            return artifact_path
+
         workflow_uid_string = "{{workflow.uid}}"
-        if workflow_uid_string in artifact_path:
+        if (
+            not mlrun.mlconf.enrich_artifact_path_with_workflow_id
+            # no need to add workflow.uid to the artifact path for uniqueness,
+            # this is already being handled by generating
+            # the artifact target path from the artifact content hash ( body / file etc...)
+            or mlrun.mlconf.should_generate_target_path_from_artifact_hash()
+            # if the artifact path already contains workflow.uid, no need to add it again
+            or workflow_uid_string in artifact_path
+        ):
             return artifact_path
 
         # join paths and replace "\" with "/" (in case of windows clients)

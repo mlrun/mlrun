0d90b9d8b1258e7b096e7babc34dc9d61af328e4
tankilevitch <tomtankilevitch@gmail.com>
2022-09-18 15:24:19 +0300

    linting....

--- automation/system_test/prepare.py
+++ automation/system_test/prepare.py
@@ -445,9 +445,7 @@ class SystemTestPreparer:
         if self._override_mlrun_images:
             override_image_arg = f"--override-images {self._override_mlrun_images}"
 
-        provctl_create_patch_log = (
-            f"/tmp/provctl-create-patch-{time_string}.log"
-        )
+        provctl_create_patch_log = f"/tmp/provctl-create-patch-{time_string}.log"
         self._run_command(
             f"./{provctl_path}",
             args=[
@@ -474,9 +472,7 @@ class SystemTestPreparer:
         self._run_command(f"cat {provctl_create_patch_log}")
 
         self._logger.info("Patching MLRun version", mlrun_version=self._mlrun_version)
-        provctl_patch_mlrun_log = (
-            f"/tmp/provctl-patch-mlrun-{time_string}.log"
-        )
+        provctl_patch_mlrun_log = f"/tmp/provctl-patch-mlrun-{time_string}.log"
         self._run_command(
             f"./{provctl_path}",
             args=[

e7a90ccd9060b81f9b4718eb838d797958341c0b
eyald <eyald@iguazio.com>
2022-09-22 13:26:36 +0300

    fix monitoring test

--- tests/system/model_monitoring/test_model_monitoring.py
+++ tests/system/model_monitoring/test_model_monitoring.py
@@ -578,10 +578,9 @@ class TestModelMonitoringAPI(TestMLRunSystem):
         # ) + len(model_endpoint.spec.label_names)
 
         # Validate monitoring feature set URI
-        m_fs = mlrun.feature_store.get_feature_set(
-            f"store://feature-sets/{self.project}/monitoring-serving-diabetes_model-latest:latest"
+        assert mlrun.feature_store.get_feature_set(
+            model_endpoint.status.monitoring_feature_set_uri
         )
-        assert model_endpoint.status.monitoring_feature_set_uri == m_fs.uri
 
     @staticmethod
     def _get_auth_info() -> mlrun.api.schemas.AuthInfo:

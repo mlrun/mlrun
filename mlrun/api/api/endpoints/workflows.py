16f1d77493f2de78131f4845b6a67690e96c217e
Yonatan Shelach <yonatanshelach@Yonatans-MacBook-Pro.local>
2022-10-02 09:15:33 +0300

    added debugging messages

--- mlrun/api/api/endpoints/workflows.py
+++ mlrun/api/api/endpoints/workflows.py
@@ -13,6 +13,13 @@ from sqlalchemy.orm import Session
 router = fastapi.APIRouter()
 
 
+def print_debug(key, val):
+    prefix = '<DEBUG YONI>\n'
+    suffix = '\n<END DEBUG YONI>'
+    msg = f'{key}: {val}\n type: {type(val)}'
+    print(prefix + msg + suffix)
+
+
 @router.post(
     "/projects/{project}/workflows/{name}/submit",
     response_model=mlrun.api.schemas.PipelinesOutput,
@@ -20,8 +27,8 @@ router = fastapi.APIRouter()
 def submit_workflow(
     project: str,
     name: str,
-    spec: mlrun.api.schemas.WorkflowSpec,
-    arguments: Dict,
+    spec: Optional[mlrun.api.schemas.WorkflowSpec] = None,
+    arguments: Optional[Dict] = None,
     artifact_path: Optional[str] = None,
     source: Optional[str] = None,
     run_name: Optional[str] = None,
@@ -29,13 +36,15 @@ def submit_workflow(
     auth_info: mlrun.api.schemas.AuthInfo = fastapi.Depends(mlrun.api.api.deps.authenticate_request),
     db_session: Session = fastapi.Depends(mlrun.api.api.deps.get_db_session),
 ):
+    print_debug('input workflow_name', name)  # TODO: Remove!
     # Getting project:
     project = mlrun.api.utils.singletons.project_member.get_project_member().get_project(
         db_session=db_session,
         name=project,
         leader_session=auth_info.session
     )
-
+    # Debugging:
+    print_debug('project_name', project.metadata.name)  # TODO: Remove!
     # Checking CREATE run permission
     mlrun.api.utils.auth.verifier.AuthVerifier().query_project_resource_permissions(
         resource_type=mlrun.api.schemas.AuthorizationResourceTypes.run,
@@ -48,16 +57,20 @@ def submit_workflow(
     mlrun.api.utils.auth.verifier.AuthVerifier().query_project_resource_permissions(
         resource_type=mlrun.api.schemas.AuthorizationResourceTypes.workflow,
         project_name=project.metadata.name,
-        resource_name=name or spec.name,  # todo: seem to be unnecessary
+        resource_name=name,
         action=mlrun.api.schemas.AuthorizationAction.read,
         auth_info=auth_info,
     )
-
+    print("project's workflows", project.spec.workflows)
     # Verifying that project has a workflow name:
     workflow_names = [workflow.name for workflow in project.spec.workflows]
-    workflow_name = name or spec.name  # todo: seem to be unnecessary
-    if workflow_name not in workflow_names:
-        raise mlrun.errors.MLRunInvalidArgumentError(f'{workflow_name} workflow not found in project')
+    if name not in workflow_names:
+        raise mlrun.errors.MLRunInvalidArgumentError(f'{name} workflow not found in project')
+    if not spec:
+        for workflow in project.spec.workflows:
+            if workflow.name == name:
+                spec = workflow
+                break
 
     # Preparing inputs for _RemoteRunner.run():
     if source:
@@ -75,7 +88,7 @@ def submit_workflow(
     run = mlrun.projects.pipelines._RemoteRunner.run(
         project=project,
         workflow_spec=workflow_spec,
-        name=workflow_name,
+        name=name,
         workflow_handler=spec.handler,
         artifact_path=artifact_path,
         namespace=namespace,

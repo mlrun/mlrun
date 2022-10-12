82794c6c6cdf768611725ecdb872e40a264b6462
Gal Topper <galt@iguazio.com>
2022-10-12 16:03:00 +0800

    Add comment

--- mlrun/platforms/iguazio.py
+++ mlrun/platforms/iguazio.py
@@ -367,7 +367,7 @@ class OutputStream:
         endpoint=None,
         access_key=None,
         mock=False,
-        **kwargs,
+        **kwargs,  # to avoid failing on extra parameters
     ):
         v3io_client_kwargs = {}
         if endpoint:

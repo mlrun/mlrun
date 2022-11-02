338ca38e1f7ec340bc7bde9b769b7d227e0190bb
quaark <adamm@iguazio.com>
2022-11-02 11:37:44 +0200

    comments

--- mlrun/api/api/endpoints/runtime_resources.py
+++ mlrun/api/api/endpoints/runtime_resources.py
@@ -223,8 +223,13 @@ def _delete_runtime_resources(
         mlrun.api.schemas.AuthorizationAction.delete,
     )
 
+    # TODO: once we have more granular permissions, we should check if the user is allowed to delete the specific
+    #  runtime resources and not just the project in general
     if not_allowed_projects_exist:
-        # some resources are not allowed, return 403
+
+        # if the user is not allowed to delete at least one of the projects, we return 403 as to:
+        # 1. not leak information about the existence of not allowed projects
+        # 2. not allow the user to do a partial delete action (delete some projects' resources and not others)
         raise mlrun.errors.MLRunAccessDeniedError(
             "Access denied to one or more runtime resources"
         )

mlrun/mlrun

commit 6073bbb157f803019585ef54c15bdb750aee2412

[Datastore] Fix `KafkaSource` (#2549)
[ML-2714](https://jira.iguazeng.com/browse/ML-2714)

This fix is only relevant for ingestion service. Serving does not use `KafkaSource`, but rather creates `KafkaTrigger` directly, and should already work (requires testing).

mlrun/datastore/sources.py
@@ -771,13 +771,14 @@ class KafkaSource(OnlineSource):
 
     def __init__(
         self,
-        brokers="localhost:9092",
-        topics="topic",
+        brokers=None,
+        topics=None,
         group="serving",
         initial_offset="earliest",
         partitions=None,
         sasl_user=None,
         sasl_pass=None,
+        attributes=None,
         **kwargs,
     ):
         """Sets kafka source for the flow
@@ -789,37 +790,40 @@ class KafkaSource(OnlineSource):
         :param partitions: Optional, A list of partitions numbers for which the function receives events.
         :param sasl_user: Optional, user name to use for sasl authentications
         :param sasl_pass: Optional, password to use for sasl authentications
+        :param attributes: Optional, extra attributes to be passed to kafka trigger
         """
         if isinstance(topics, str):
             topics = [topics]
         if isinstance(brokers, str):
             brokers = [brokers]
-        attrs = {
-            "brokers": brokers,
-            "topics": topics,
-            "partitions": partitions,
-            "group": group,
-            "initial_offset": initial_offset,
-        }
+        attributes = copy(attributes)
+        attributes["brokers"] = brokers
+        attributes["topics"] = topics
+        attributes["group"] = group
+        attributes["initial_offset"] = initial_offset
+        if partitions is not None:
+            attributes["partitions"] = partitions
+        sasl = attributes.pop("sasl", {})
         if sasl_user and sasl_pass:
-            attrs["sasl_user"] = sasl_user
-            attrs["sasl_pass"] = sasl_pass
-        super().__init__(attributes=attrs, **kwargs)
+            sasl["enabled"] = True
+            sasl["user"] = sasl_user
+            sasl["password"] = sasl_pass
+        if sasl:
+            attributes["sasl"] = sasl
+        super().__init__(attributes=attributes, **kwargs)
 
     def add_nuclio_trigger(self, function):
-        partitions = self.attributes.get("partitions")
+        extra_attributes = copy(self.attributes)
+        partitions = extra_attributes.pop("partitions", None)
         trigger = KafkaTrigger(
-            brokers=self.attributes["brokers"],
-            topics=self.attributes["topics"],
+            brokers=extra_attributes.pop("brokers"),
+            topics=extra_attributes.pop("topics"),
             partitions=partitions,
-            consumer_group=self.attributes["group"],
-            initial_offset=self.attributes["initial_offset"],
+            consumer_group=extra_attributes.pop("group"),
+            initial_offset=extra_attributes.pop("initial_offset"),
+            extra_attributes=extra_attributes,
         )
         func = function.add_trigger("kafka", trigger)
-        sasl_user = self.attributes.get("sasl_user")
-        sasl_pass = self.attributes.get("sasl_pass")
-        if sasl_user and sasl_pass:
-            trigger.sasl(sasl_user, sasl_pass)
         replicas = 1 if not partitions else len(partitions)
         func.spec.min_replicas = replicas
         func.spec.max_replicas = replicas

tests/datastore/test_base.py
@@ -18,6 +18,8 @@ import dask.dataframe as dd
 
 import mlrun.datastore
 import mlrun.datastore.wasbfs
+from mlrun import new_function
+from mlrun.datastore import KafkaSource
 
 
 def test_http_fs_parquet_as_df():
@@ -52,3 +54,30 @@ def test_load_object_into_dask_dataframe_using_wasbs_url():
     )
     ddf = data_item.as_df(df_module=dd)
     assert isinstance(ddf, dd.DataFrame)
+
+
+def test_kafka_source():
+    source = KafkaSource(
+        brokers="broker_host:9092",
+        topics="mytopic",
+        group="mygroup",
+        sasl_user="myuser",
+        sasl_pass="mypassword",
+        attributes={
+            "sasl": {
+                "handshake": True,
+            },
+        },
+    )
+    function = new_function(kind="remote")
+    source.add_nuclio_trigger(function)
+    attributes = function.spec.config["spec.triggers.kafka"]["attributes"]
+    assert attributes["brokers"] == ["broker_host:9092"]
+    assert attributes["topics"] == ["mytopic"]
+    assert attributes["consumerGroup"] == "mygroup"
+    assert attributes["sasl"] == {
+        "enabled": True,
+        "user": "myuser",
+        "password": "mypassword",
+        "handshake": True,
+    }

commit 0c8d85ee66b2f75183abf46a8bb721db2baa4a5e

[Datastore] Fix `KafkaSource` without attributes
Fix regression introduced by #2549.

[ML-2738](https://jira.iguazeng.com/browse/ML-2738)

mlrun/datastore/sources.py
@@ -796,7 +796,7 @@ class KafkaSource(OnlineSource):
             topics = [topics]
         if isinstance(brokers, str):
             brokers = [brokers]
-        attributes = copy(attributes)
+        attributes = {} if attributes is None else copy(attributes)
         attributes["brokers"] = brokers
         attributes["topics"] = topics
         attributes["group"] = group

tests/datastore/test_base.py
@@ -56,7 +56,7 @@ def test_load_object_into_dask_dataframe_using_wasbs_url():
     assert isinstance(ddf, dd.DataFrame)
 
 
-def test_kafka_source():
+def test_kafka_source_with_attributes():
     source = KafkaSource(
         brokers="broker_host:9092",
         topics="mytopic",
@@ -80,4 +80,25 @@ def test_kafka_source():
         "user": "myuser",
         "password": "mypassword",
         "handshake": True,
+    }
+
+
+def test_kafka_source_without_attributes():
+    source = KafkaSource(
+        brokers="broker_host:9092",
+        topics="mytopic",
+        group="mygroup",
+        sasl_user="myuser",
+        sasl_pass="mypassword",
+    )
+    function = new_function(kind="remote")
+    source.add_nuclio_trigger(function)
+    attributes = function.spec.config["spec.triggers.kafka"]["attributes"]
+    assert attributes["brokers"] == ["broker_host:9092"]
+    assert attributes["topics"] == ["mytopic"]
+    assert attributes["consumerGroup"] == "mygroup"
+    assert attributes["sasl"] == {
+        "enabled": True,
+        "user": "myuser",
+        "password": "mypassword",
     }
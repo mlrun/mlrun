2e971cc038cc5a288c51191c9a0cc68921ecba27
Saar Cohen <66667568+theSaarco@users.noreply.github.com>
2022-11-14 17:04:26 +0200

    [Artifacts] Add category option to `list_artifact_tags`, fix to return only distinct values (#2570)

--- mlrun/api/db/filedb/db.py
+++ mlrun/api/db/filedb/db.py
@@ -433,7 +433,7 @@ class FileDB(DBInterface):
     def delete_feature_vector(self, session, project, name, tag=None, uid=None):
         raise NotImplementedError()
 
-    def list_artifact_tags(self, session, project):
+    def list_artifact_tags(self, session, project, category):
         return self._transform_run_db_error(self.db.list_artifact_tags, project)
 
     def create_schedule(

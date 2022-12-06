fb43c2c405db7a2079c1b0c2e08b12d48c4c8842
Eyal-Danieli <eyald@iguazio.com>
2022-12-06 15:32:52 +0200

    fetch to dev

--- setup.py
+++ setup.py
@@ -148,6 +148,7 @@ setup(
         "mlrun.api.api.endpoints",
         "mlrun.api.api.endpoints.internal",
         "mlrun.api.crud",
+        "mlrun.api.crud.model_monitoring",
         "mlrun.api.db",
         "mlrun.api.db.filedb",
         "mlrun.api.db.sqldb",

3c38b29cde1a0893f0dc251b122b6f40af64004c
Eyal-Danieli <eyald@iguazio.com>
2022-12-06 16:12:41 +0200

    fetch to dev

--- tests/system/model_monitoring/test_model_monitoring.py
+++ tests/system/model_monitoring/test_model_monitoring.py
@@ -51,7 +51,7 @@ from tests.system.base import TestMLRunSystem
 @TestMLRunSystem.skip_test_if_env_not_configured
 @pytest.mark.enterprise
 class TestModelMonitoringAPI(TestMLRunSystem):
-    project_name = "model-monitor-sys-test9"
+    project_name = "model-monitor-sys-test4"
 
     def test_clear_endpoint(self):
         """Validates the process of create and delete a basic model endpoint"""

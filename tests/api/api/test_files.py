eec443db727ce8decda8935c856b377e8b177ac7
Saar Cohen <saarc@iguazio.com>
2022-12-11 13:55:47 +0200

    add tests

--- tests/api/api/test_files.py
+++ tests/api/api/test_files.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+import unittest.mock
 from http import HTTPStatus
 
 import pytest
@@ -19,10 +21,13 @@ from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
 # fixtures for test, aren't used directly so we need to ignore the lint here
+import mlrun
 from tests.common_fixtures import (  # noqa: F401
     patch_file_forbidden,
     patch_file_not_found,
 )
+
+from mlrun.api.api.utils import get_secrets
 
 
 @pytest.mark.usefixtures("patch_file_forbidden")
@@ -44,3 +49,52 @@ def validate_files_status_code(client: TestClient, status_code: int):
 
     resp = client.get("filestat?schema=v3io&path=mybucket/files.txt")
     assert resp.status_code == status_code
+
+
+class DatastoreObjectMock:
+    def get(self, size, offset):
+        return "dummy body"
+
+    def listdir(self):
+        return ["file1", "file2", "dir1/file3"]
+
+
+@pytest.fixture
+def files_mock():
+    old_object = mlrun.store_manager.object
+    mlrun.store_manager.object = unittest.mock.Mock(return_value=DatastoreObjectMock())
+
+    yield mlrun.store_manager.object
+
+    mlrun.store_manager.object = old_object
+
+
+def test_files(db: Session, client: TestClient, files_mock, k8s_secrets_mock) -> None:
+    path = "s3://somebucket/some/path/file"
+    project = "proj1"
+
+    env_secrets = {"V3IO_ACCESS_KEY": None}
+    project_secrets = {"secret1": "value1", "secret2": "value2"}
+    full_secrets = project_secrets.copy()
+    full_secrets.update(env_secrets)
+    k8s_secrets_mock.store_project_secrets(project, project_secrets)
+
+    resp = client.get(f"files?path={path}")
+    assert resp
+    files_mock.assert_called_once_with(url=path, secrets=env_secrets)
+    files_mock.reset_mock()
+
+    resp = client.get(f"projects/{project}/files?path={path}")
+    assert resp
+    files_mock.assert_called_once_with(url=path, secrets=full_secrets)
+    files_mock.reset_mock()
+
+    resp = client.get(f"projects/wrong-project/files?path={path}")
+    assert resp
+    files_mock.assert_called_once_with(url=path, secrets=env_secrets)
+    files_mock.reset_mock()
+
+    resp = client.get(f"projects/wrong-project/files?path={path}&use-secrets=false")
+    assert resp
+    files_mock.assert_called_once_with(url=path, secrets=env_secrets)
+    files_mock.reset_mock()

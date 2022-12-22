7f680aa3a29ce7db7908ab53e48efb8f487b122a
AlonMaor14 <maoralo@post.bgu.ac.il>
2022-12-22 18:03:40 +0200

    test req

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -130,6 +130,7 @@ def test_requirement_specifiers_convention():
         "protobuf": {">=3.20.2, <4"},
         "pandas": {"~=1.2, <1.5.0"},
         "importlib_metadata": {">=3.6"},
+        "scikit-learn": {"~=1.0, <1.2"},
     }
 
     for (

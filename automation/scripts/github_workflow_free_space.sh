b951946f7685f32fa763ed12346d77bb80fba921
Liran BG <liranbg@gmail.com>
2022-12-26 20:27:14 +0200

    single script

--- automation/scripts/github_workflow_free_space.sh
+++ automation/scripts/github_workflow_free_space.sh
@@ -22,16 +22,31 @@ print_free_space() {
 print_free_space
 
 # clean unneeded os packages and misc
-sudo apt-get remove --yes '^dotnet-.*' 'php.*' azure-cli google-cloud-sdk google-chrome-stable firefox powershell
+sudo apt-get remove -y '^ghc-8.*'
+sudo apt-get remove -y '^dotnet-.*'
+sudo apt-get remove -y '^llvm-.*'
+sudo apt-get remove -y 'php.*'
+sudo apt-get remove -y \
+  azure-cli \
+  google-cloud-sdk \
+  google-chrome-stable \
+  firefox \
+  powershell \
+  monodoc-http \
+  mono-devel
+
 sudo apt-get autoremove --yes
 sudo apt clean
 
-# cleanup unneeded share dirs ~25GB
+# cleanup unneeded share dirs ~30GB
 sudo rm --recursive --force \
     /usr/local/lib/android \
     /usr/share/dotnet \
-    /usr/share/dotnet \
-    /usr/share/swift
+    /usr/share/miniconda \
+    /usr/share/swift \
+    /opt/ghc \
+    /usr/local/share/boost \
+    "$AGENT_TOOLSDIRECTORY"
 
 # clean unneeded docker images
 docker system prune --all --force

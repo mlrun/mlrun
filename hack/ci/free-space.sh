64e8d70552872a119cbb0011d70fafd96b195557
AlonMaor14 <maoralo@post.bgu.ac.il>
2022-12-26 12:59:36 +0200

    clear more space 2 ?

--- hack/ci/free-space.sh
+++ hack/ci/free-space.sh
@@ -31,7 +31,6 @@ sudo rm --recursive --force \
     /usr/local/lib/android \
     /usr/share/dotnet \
     /usr/share/miniconda \
-    /usr/share/dotnet \
     /usr/share/swift \
     /opt/ghc \
     "/usr/local/share/boost" \
@@ -42,3 +41,24 @@ docker system prune --all --force
 
 # post cleanup
 print_free_space
+
+echo "=============================================================================="
+echo "Freeing up disk space on CI system"
+echo "=============================================================================="
+
+echo "Listing 100 largest packages"
+dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
+df -h
+echo "Removing large packages"
+sudo apt-get remove -y '^ghc-8.*'
+sudo apt-get remove -y '^dotnet-.*'
+sudo apt-get remove -y '^llvm-.*'
+sudo apt-get remove -y 'php.*'
+sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
+sudo apt-get autoremove -y
+sudo apt-get clean
+df -h
+echo "Removing large directories"
+# deleting 15GB
+rm -rf /usr/share/dotnet/
+df -h

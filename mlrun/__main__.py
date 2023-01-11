bbbd8ed18e4fe4d5ea6461d8cc009e6eccc6a09e
GIladShapira94 <giladsh@iguazio.com>
2023-01-11 18:40:20 +0200

    "Added information wht to use --save-secrets"

--- mlrun/__main__.py
+++ mlrun/__main__.py
@@ -1052,6 +1052,9 @@ def project(
     if secrets:
         secrets = line2keylist(secrets, "kind", "source")
         secret_store = SecretsStore.from_list(secrets)
+        # Allow the user to run a workflow with secrets on runtime, without using or storing k8s secrets.
+        # if the user wants to run a schedule workflow or to use those secrets in other run he will need to save
+        # those secrets in k8s and use the --save-secret flag
         proj._secrets = secret_store
         if save_secrets:
             proj.set_secrets(secret_store._secrets)

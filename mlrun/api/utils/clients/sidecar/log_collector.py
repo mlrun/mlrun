707137b3417fd302b13839749d738694176a7f25
tankilevitch <tomtankilevitch@gmail.com>
2023-01-12 15:42:15 +0200

    change type hinting in start_logs

--- mlrun/api/utils/clients/sidecar/log_collector.py
+++ mlrun/api/utils/clients/sidecar/log_collector.py
@@ -22,7 +22,7 @@ class LogCollectorClient(BaseGRPCClient):
     name = "log_collector"
     stub_class = mlrun.api.proto.log_collector_pb2_grpc.LogCollectorStub
 
-    async def start_logs(self, run_id: str, selector: dict) -> bool:
+    async def start_logs(self, run_id: str, selector: str) -> bool:
         request = mlrun.api.proto.log_collector_pb2.StartLogRequest(
             runId=run_id, selector=selector
         )

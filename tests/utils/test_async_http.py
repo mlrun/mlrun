d6126eb79e02ec98b7dcd15da21f1f00eab2d3e1
Alon Maor <48641682+AlonMaor14@users.noreply.github.com>
2023-01-14 22:15:39 +0200

    [Utils][Async Client] Filter `None` header values (#2912)

--- tests/utils/test_async_http.py
+++ tests/utils/test_async_http.py
@@ -17,9 +17,10 @@ import http.server
 import typing
 from unittest import mock
 
+import aiohttp.client_exceptions
 import aioresponses
 import pytest
-from aiohttp import ClientConnectorError, ServerDisconnectedError
+from aiohttp import BaseConnector, ClientConnectorError, ServerDisconnectedError
 
 from mlrun.utils.async_http import AsyncClientWithRetry
 
@@ -144,3 +145,17 @@ async def test_retry_method_status_codes(
         assert len(list(aiohttp_mock.requests.values())[0]) == len(
             status_codes
         ), "Wrong number of retries"
+
+
+@pytest.mark.asyncio
+async def test_headers_serialization(async_client: AsyncClientWithRetry):
+    """
+    Header keys/values type must be str to be serializable
+    This tests ensures we drop headers with 'None' values
+    """
+    BaseConnector.connect = mock.AsyncMock()
+    with pytest.raises(aiohttp.client_exceptions.ClientOSError) as e:
+        await async_client.get("http://nothinghere", headers={"x": None, "y": "z"})
+
+    # assert the client tried to write the headers to the socket which means they were serialized successfully
+    assert "Cannot write to closing transport" in str(e.value)

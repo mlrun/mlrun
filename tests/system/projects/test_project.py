e3a88f804797d4e06b0c1301091d2a9e39948105
yonishelach <yonatanshelach@gmail.com>
2023-01-17 12:33:28 +0200

    removed ennessacary tests

--- tests/system/projects/test_project.py
+++ tests/system/projects/test_project.py
@@ -17,7 +17,6 @@ import pathlib
 import re
 import shutil
 import sys
-import time
 from sys import executable
 
 import pytest
@@ -553,100 +552,6 @@ class TestProject(TestMLRunSystem):
             != -1
         )
 
-    def test_submit_workflow_endpoint(self):
-        project_name = "submit-wf"
-        project_dir = f"{projects_dir}/{project_name}"
-
-        self.custom_project_names_to_delete.append(project_name)
-        shutil.rmtree(project_dir, ignore_errors=True)
-
-        mlrun.load_project(
-            project_dir, "git://github.com/mlrun/project-demo.git", name=project_name
-        )
-
-        for engine, workflow_name in [
-            ("local", "newflow"),
-            ("kfp", "main"),
-        ]:
-            # Submitting workflow:
-            workflow_response = self._run_db.submit_workflow(
-                project=project_name,
-                name=workflow_name,
-                workflow_spec={"spec": {"engine": engine}},
-            )
-            assert all(
-                [
-                    hasattr(workflow_response, attr)
-                    for attr in ["project", "name", "status", "run_id", "schedule"]
-                ]
-            )
-
-            # waiting for the workflow to end:
-            workflow_id = None
-            num_tries = 10
-            while workflow_id:
-                get_workflow_response = self._run_db.get_workflow_id(
-                    project=project_name,
-                    run_id=workflow_response.run_id,
-                    json={"name": workflow_name},
-                )
-                assert all(
-                    [
-                        hasattr(get_workflow_response, attr)
-                        for attr in ["workflow_id", "status"]
-                    ]
-                )
-                workflow_id = workflow_response.run_id
-                time.sleep(3)
-                if not num_tries:
-                    break
-                num_tries -= 1
-
-    def test_submit_workflow_endpoint_with_scheduling(self):
-        project_name = "submit-wf-schedule"
-        project_dir = f"{projects_dir}/{project_name}"
-        workflow_name = "main"
-        schedule_name = f"workflow-runner-{workflow_name}"
-        try:
-            self.custom_project_names_to_delete.append(project_name)
-            shutil.rmtree(project_dir, ignore_errors=True)
-            # Loading a project with workflows:
-            project = mlrun.load_project(
-                project_dir,
-                "git://github.com/mlrun/project-demo.git",
-                name=project_name,
-            )
-            project.save()
-
-            # Submitting workflow:
-            workflow_response = self._run_db.submit_workflow(
-                project=project_name,
-                name=workflow_name,
-                workflow_spec={"spec": {"schedule": "*/10 * * * *"}},
-            )
-
-            # Checking scheduled workflow submitted as expected:
-            assert all(
-                [
-                    hasattr(workflow_response, attr)
-                    for attr in ["project", "name", "status", "run_id", "schedule"]
-                ]
-            )
-
-            assert workflow_response.schedule == "scheduled"
-            get_schedule_response = self._run_db.list_schedules(project=project_name)
-
-            schedules = get_schedule_response.schedules
-            assert schedules and len(schedules) == 1
-            schedule = schedules[0]
-            print(schedule)
-            assert schedule.name == schedule_name
-            assert schedule.scheduled_object["task"]["status"]["state"] == "created"
-
-        finally:
-            # Deleting submitted schedule:
-            self._run_db.delete_schedule(project=project_name, name=schedule_name)
-
     def test_build_and_run(self):
         # test that build creates a proper image and run will use the updated function (with the built image)
         name = "buildandrun"
@@ -738,7 +643,7 @@ class TestProject(TestMLRunSystem):
                 dirty=True,
             )
 
-        # overriding workflow from cli:
+        # overwriting workflow from cli:
         args = [
             project_dir,
             "-n",

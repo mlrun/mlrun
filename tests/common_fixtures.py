e7d61cbe3a19e5e49daf8d2f6c8766f0eb0ecf90
TomerShor <tomer.shor@gmail.com>
2023-01-22 18:36:54 +0200

    test fixes

--- tests/common_fixtures.py
+++ tests/common_fixtures.py
@@ -95,6 +95,7 @@ def config_test_base():
     mlrun.runtimes.utils.cached_mpijob_crd_version = None
     mlrun.runtimes.utils.cached_nuclio_version = None
 
+    # TODO: update this to "sidecar" once the default mode is changed
     mlrun.config.config.log_collector.mode = "legacy"
 
 
--- tests/system/logs/test_logs.py
+++ tests/system/logs/test_logs.py
@@ -20,6 +20,12 @@ import tests.system.base
 
 @tests.system.base.TestMLRunSystem.skip_test_if_env_not_configured
 class TestLogCollector(tests.system.base.TestMLRunSystem):
+    custom_project_names_to_delete = []
+
+    def custom_teardown(self):
+        for name in self.custom_project_names_to_delete:
+            self._delete_test_project(name)
+
     def test_log_collector(self):
         self._logger.debug("Testing log collector")
 
@@ -28,7 +34,12 @@ class TestLogCollector(tests.system.base.TestMLRunSystem):
         # we set the function lifecycle, which is equivalent to the number of log lines the function will print
         life_cycle_seconds = 100
 
-        proj = mlrun.get_or_create_project(self.project_name, self.results_path)
+        project_name = "test-log-collector"
+        self.custom_project_names_to_delete.append(project_name)
+        proj = mlrun.new_project(
+            project_name,
+            str(self.assets_path),
+        )
         function = mlrun.code_to_function(
             name="function-with-logs",
             kind="job",
@@ -43,12 +54,12 @@ class TestLogCollector(tests.system.base.TestMLRunSystem):
         time.sleep(10)
 
         # call get logs with the run id
-        logs = mlrun.get_run_db().get_log(run.metadata.uid, project=proj.name)
+        state, logs = mlrun.get_run_db().get_log(run.metadata.uid, project=proj.name)
 
         # verify run state is not unknown
-        assert run.status.state != "unknown"
+        assert state != "unknown"
 
         # verify the logs are not empty
-        assert logs[2]
+        assert logs
 
         self._logger.debug("Finished log collector test")

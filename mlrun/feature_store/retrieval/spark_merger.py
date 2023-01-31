b267067e6242430dcf6854c5180be29539324ce1
davesh0812 <davesh0812@gmail.com>
2023-01-31 16:48:10 +0200

    init

--- mlrun/feature_store/retrieval/spark_merger.py
+++ mlrun/feature_store/retrieval/spark_merger.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import re
+
 import mlrun
 from mlrun.datastore.targets import get_offline_target
 
@@ -52,11 +54,28 @@ class SparkFeatureMerger(BaseMerger):
 
         feature_sets = []
         dfs = []
+        keys = (
+            []
+        )  # the struct of key is [[[],[]], ..] So that each record indicates which way the corresponding
+        # featureset is connected to the previous one, and within each record the left keys are indicated in index 0
+        # and the right keys in index 1, this keys will be the keys that will be used in this join
+        all_columns = []
 
-        for name, columns in feature_set_fields.items():
+        fs_link_list = self._create_linked_relation_list(
+            feature_set_objects, feature_set_fields
+        )
+
+        for node in fs_link_list:
+            name = node.name
             feature_set = feature_set_objects[name]
             feature_sets.append(feature_set)
+            columns = feature_set_fields[name]
             column_names = [name for name, alias in columns]
+
+            for col in node.data["save_cols"]:
+                if col not in column_names:
+                    self._append_drop_column(col)
+            column_names += node.data["save_cols"]
 
             if feature_set.spec.passthrough:
                 if not feature_set.spec.source:
@@ -101,35 +120,78 @@ class SparkFeatureMerger(BaseMerger):
             df = source.to_spark_df(
                 self.spark, named_view=self.named_view, time_field=timestamp_key
             )
+            df = df.select(*column_names)
 
-            if timestamp_key and timestamp_key not in column_names:
-                columns.append((timestamp_key, None))
-            for entity in feature_set.spec.entities.keys():
-                if entity not in column_names:
-                    columns.append((entity, None))
+            column_names += node.data["save_index"]
+            node.data["save_cols"] += node.data["save_index"]
+            entity_timestamp_column_list = (
+                [entity_timestamp_column]
+                if entity_timestamp_column
+                else feature_set.spec.timestamp_key
+            )
+            if entity_timestamp_column_list:
+                column_names += entity_timestamp_column_list
+                node.data["save_cols"] += entity_timestamp_column_list
+            # rename columns to be unique for each feature set
+            rename_col_dict = {
+                col: f"{col}_{name}"
+                for col in column_names
+                if col not in node.data["save_cols"]
+            }
 
             # select requested columns and rename with alias where needed
-            df = df.select([col(name).alias(alias or name) for name, alias in columns])
+            df = df.select(
+                [
+                    col(name).alias(rename_col_dict[name] or name)
+                    for name in column_names
+                ]
+            )
             dfs.append(df)
             del df
+
+            keys.append([node.data["left_keys"], node.data["right_keys"]])
+
+            # update alias according to the unique column name
+            new_columns = []
+            for col, alias in columns:
+                if col in rename_col_dict and alias:
+                    new_columns.append((rename_col_dict[col], alias))
+                elif col in rename_col_dict and not alias:
+                    new_columns.append((rename_col_dict[col], col))
+                else:
+                    new_columns.append((col, alias))
+            all_columns.append(new_columns)
+            self._update_alias(
+                dictionary={name: alias for name, alias in new_columns if alias}
+            )
 
         # convert pandas entity_rows to spark DF if needed
         if entity_rows is not None and not hasattr(entity_rows, "rdd"):
             entity_rows = self.spark.createDataFrame(entity_rows)
 
         # join the feature data frames
-        self.merge(entity_rows, entity_timestamp_column, feature_sets, dfs)
-
-        # filter joined data frame by the query param
-        if query:
-            self._result_df = self._result_df.filter(query)
+        self.merge(
+            entity_df=entity_rows,
+            entity_timestamp_column=entity_timestamp_column,
+            featuresets=feature_sets,
+            featureset_dfs=dfs,
+            keys=keys,
+            all_columns=all_columns,
+        )
 
         self._result_df = self._result_df.drop(*self._drop_columns)
+
+        self._result_df = self._result_df.select(
+            [col(name).alias(alias or name) for name, alias in self._alias]
+        )
 
         if self.vector.status.label_column:
             self._result_df = self._result_df.dropna(
                 subset=[self.vector.status.label_column]
             )
+        # filter joined data frame by the query param
+        if query:
+            self._result_df = self._result_df.filter(query)
 
         self._write_to_target()
         return OfflineVectorResponse(self)
@@ -240,8 +302,17 @@ class SparkFeatureMerger(BaseMerger):
                 be prefixed with featureset_df name.
 
         """
-        indexes = list(featureset.spec.entities.keys())
-        merged_df = entity_df.join(featureset_df, on=indexes)
+        fs_name = featureset.metadata.name
+        merged_df = entity_df.join(
+            featureset_df,
+            how=self._join_type,
+            left_on=left_keys,
+            right_on=right_keys,
+            suffixes=("", f"_{fs_name}_"),
+        )
+        for col in merged_df.columns:
+            if re.findall(f"_{fs_name}_$", col):
+                self._append_drop_column(col)
         return merged_df
 
     def get_df(self, to_pandas=True):

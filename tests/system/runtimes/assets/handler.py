c83d23ae488250e7e633cb0718ed22fe6a9dbfe4
AlonMaor14 <maoralo@post.bgu.ac.il>
2023-02-08 11:05:01 +0200

    [Execution] Fix updating set labels and annotations

--- tests/system/runtimes/assets/handler.py
+++ tests/system/runtimes/assets/handler.py
@@ -29,3 +29,8 @@ def my_func(context, p1: int = 1, p2="a-string"):
     context.log_result("accuracy", p1 * 2)
     context.log_result("loss", p1 * 3)
     context.set_label("framework", "sklearn")
+
+
+def set_labels_handler(context):
+    context.set_label("label1", "value1")
+    context.set_annotation("annotation1", "value2")

7c4ea52925d171a0f727eb347f53082a2986f019
davesh0812 <davesh0812@gmail.com>
2023-02-12 15:42:34 +0200

    lint

--- mlrun/feature_store/api.py
+++ mlrun/feature_store/api.py
@@ -842,7 +842,7 @@ def _ingest_with_spark(
         if featureset.spec.graph and featureset.spec.graph.steps:
             df = run_spark_graph(df, featureset, namespace, spark)
         if isinstance(df, Response) and df.status_code == 400:
-            mlrun.errors.raise_for_status_code(df.status_code, df.body.split(': ')[1])
+            mlrun.errors.raise_for_status_code(df.status_code, df.body.split(": ")[1])
         _infer_from_static_df(df, featureset, options=infer_options)
 
         key_columns = list(featureset.spec.entities.keys())

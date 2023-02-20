56fcc2223d5c35ecce851534aefbfd9fceba2d8f
AlonMaor14 <maoralo@post.bgu.ac.il>
2023-02-20 16:08:13 +0200

    init

--- mlrun/runtimes/sparkjob/abstract.py
+++ mlrun/runtimes/sparkjob/abstract.py
@@ -830,9 +830,7 @@ with ctx:
 
 class SparkRuntimeHandler(BaseRuntimeHandler):
     kind = "spark"
-    class_modes = {
-        RuntimeClassMode.run: "spark",
-    }
+    class_modes = {RuntimeClassMode.run: "spark", RuntimeClassMode.build: "build"}
 
     def _resolve_crd_object_status_info(
         self, db: DBInterface, db_session: Session, crd_object

fef77e966848198324e45e068ba99ebfe501f815
Yoni Shelach <92271540+yonishelach@users.noreply.github.com>
2023-02-26 13:55:12 +0200

    Update mlrun/frameworks/_ml_common/pkl_model_server.py
    Co-authored-by: guy1992l <83535508+guy1992l@users.noreply.github.com>

--- mlrun/frameworks/_ml_common/pkl_model_server.py
+++ mlrun/frameworks/_ml_common/pkl_model_server.py
@@ -37,17 +37,14 @@ class PickleModelServer(V2ModelServer):
         Infer the inputs through the model using MLRun's PyTorch interface and return its output. The inferred data will
         be read from the "inputs" key of the request.
 
-        examples for inputs::
-
-            # dict option:
-            inputs = [{"x": [1, 2], "y": [3, 5.5]}]
-            # list option:
-            inputs = [[1, 2], [3, 5.5]]
-
         :param request: The request of the model. The input to the model will be read from the "inputs" key.
                         The input value can be either a dictionary with the feature names
                         (only one can be sent so only the first one will take into consideration)
-                        or a list with feature values.
+                        or a list with feature values. For example, a batch size of 2 for data of two features 'x' and 'y'
+                        can be given:
+                        
+                        * As a dictionary: `{"inputs": [{"x": [1, 2], "y": [3, 5.5]}]}`
+                        * As a list: `{"inputs": [[1, 2], [3, 5.5]]}`
         :return: The model's prediction on the given input.
         """
         inputs = request["inputs"]

d37677394d10127e2978476305df522d6e7cdf3b
Gal Topper <galt@iguazio.com>
2023-02-28 17:14:55 +0800

    Fix test

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -132,6 +132,7 @@ def test_requirement_specifiers_convention():
         "ipython": {">=7.0, <9.0"},
         "importlib_metadata": {">=3.6"},
         "gitpython": {"~=3.1, >= 3.1.30"},
+        "pyopenssl": {">=23"},
         # plotly artifact body in 5.12.0 may contain chars that are not encodable in 'latin-1' encoding
         # so, it cannot be logged as artifact (raised UnicodeEncode error - ML-3255)
         "plotly": {"~=5.4, <5.12.0"},

cf7aa0c1d7ff9c65ebf77e6e700613f9fae8cd83
Alon Maor <48641682+alonmr@users.noreply.github.com>
2023-03-01 22:35:58 +0200

    [Execution] Update run params, inputs and outputs (#3198)

--- mlrun/execution.py
+++ mlrun/execution.py
@@ -976,6 +976,9 @@ class MLClientCtx(object):
         struct = {
             "metadata.labels": self._labels,
             "metadata.annotations": self._annotations,
+            "spec.parameters": self._parameters,
+            "spec.outputs": self._outputs,
+            "spec.inputs": {k: v.artifact_url for k, v in self._inputs.items()},
             "status.results": self._results,
             "status.start_time": to_date_str(self._start_time),
             "status.last_update": to_date_str(self._last_update),

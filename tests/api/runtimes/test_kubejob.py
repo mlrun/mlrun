83bdbe9dbd1c177e57596c45ac381f77657ac2ae
quaark <adamm@iguazio.com>
2023-03-23 15:11:23 +0200

    try this as well

--- tests/api/runtimes/test_kubejob.py
+++ tests/api/runtimes/test_kubejob.py
@@ -190,10 +190,10 @@ class TestKubejobRuntime(TestRuntimeBase):
     ):
         self.assert_run_preemption_mode_with_preemptible_node_selector_and_tolerations_with_extra_settings()
 
-    def test_preemption_mode_with_preemptible_node_selector_without_preemptible_tolerations_with_extra_settings(
-        self, db: Session, client: TestClient
-    ):
-        self.assert_run_preemption_mode_with_preemptible_node_selector_without_preemptible_tolerations_with_extra_settings()  # noqa: E501
+    # def test_preemption_mode_with_preemptible_node_selector_without_preemptible_tolerations_with_extra_settings(
+    #     self, db: Session, client: TestClient
+    # ):
+    #     self.assert_run_preemption_mode_with_preemptible_node_selector_without_preemptible_tolerations_with_extra_settings()  # noqa: E501
 
     def test_with_preemption_mode_none_transitions(
         self, db: Session, client: TestClient

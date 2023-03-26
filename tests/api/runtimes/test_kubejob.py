051c3cccefe0b1637f9a4f947c3564a4ba0b3745
quaark <adamm@iguazio.com>
2023-03-26 12:17:55 +0300

    bring tests back

--- tests/api/runtimes/test_kubejob.py
+++ tests/api/runtimes/test_kubejob.py
@@ -190,10 +190,10 @@ class TestKubejobRuntime(TestRuntimeBase):
     ):
         self.assert_run_preemption_mode_with_preemptible_node_selector_and_tolerations_with_extra_settings()
 
-    # def test_preemption_mode_with_preemptible_node_selector_without_preemptible_tolerations_with_extra_settings(
-    #     self, db: Session, client: TestClient
-    # ):
-    #     self.assert_run_preemption_mode_with_preemptible_node_selector_without_preemptible_tolerations_with_extra_settings()  # noqa: E501
+    def test_preemption_mode_with_preemptible_node_selector_without_preemptible_tolerations_with_extra_settings(
+        self, db: Session, client: TestClient
+    ):
+        self.assert_run_preemption_mode_with_preemptible_node_selector_without_preemptible_tolerations_with_extra_settings()  # noqa: E501
 
     def test_with_preemption_mode_none_transitions(
         self, db: Session, client: TestClient
@@ -258,13 +258,13 @@ class TestKubejobRuntime(TestRuntimeBase):
         with pytest.raises(mlrun.errors.MLRunInvalidArgumentError):
             runtime.with_priority_class(medium_priority_class_name)
 
-    # def test_set_annotation(self, db: Session, client: TestClient):
-    #     runtime = self._generate_runtime()
-    #     runtime.with_annotations({"annotation-key": "annotation-value"})
-    #     self.execute_function(runtime)
-    #
-    #     pod = self._get_pod_creation_args()
-    #     assert pod.metadata.annotations.get("annotation-key") == "annotation-value"
+    def test_set_annotation(self, db: Session, client: TestClient):
+        runtime = self._generate_runtime()
+        runtime.with_annotations({"annotation-key": "annotation-value"})
+        self.execute_function(runtime)
+
+        pod = self._get_pod_creation_args()
+        assert pod.metadata.annotations.get("annotation-key") == "annotation-value"
 
     def test_run_with_security_context(self, db: Session, client: TestClient):
         runtime = self._generate_runtime()

78c47a04d545333a9ff8abb43eb22e262ae7c5ac
AlonMaor14 <maoralo@post.bgu.ac.il>
2023-03-28 17:31:25 +0300

    minor

--- mlrun/api/launcher/server.py
+++ mlrun/api/launcher/server.py
@@ -11,10 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import mlrun.launcher.abstract
+from mlrun.launcher.abstract import AbstractLauncher
 
 
-class ServerSideLauncher(mlrun.launcher.abstract.AbstractLauncher):
+class ServerSideLauncher(AbstractLauncher):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 

1ef75bb8d087a089b4a9deb1f92e66ccf722864b
tomermamia <tomerm@iguazio.com>
2023-03-28 13:47:47 +0300

    change error messages

--- tests/system/feature_store/test_spark_engine.py
+++ tests/system/feature_store/test_spark_engine.py
@@ -1456,7 +1456,7 @@ class TestFeatureStoreSparkEngine(TestMLRunSystem):
         assert not df["bad_mapped"].isna().any()
         assert not df["bad_mapped"].isnull().any()
         assert not (df["bad_mapped"] == 17).any()
-        # There are no occurrences of -1 in the "bad" field of the original DataFrame.
+        # Note that there are no occurrences of -1 in the "bad" field of the original DataFrame.
         assert (df["bad_mapped"] == -1).sum() == (original_df["bad"] == 17).sum()
 
     def test_mapvalues_with_mixed_types(self):

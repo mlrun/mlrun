f97cb168fefa0447c7b14f1b6511b9d8deb41220
AlonMaor14 <maoralo@post.bgu.ac.il>
2023-03-28 17:28:00 +0300

    minor

--- mlrun/launcher/client/factory.py
+++ mlrun/launcher/client/factory.py
@@ -11,7 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from ..abstract import AbstractLauncher
+from mlrun.launcher.abstract import AbstractLauncher
+
 from .local import ClientLocalLauncher
 from .remote import ClientRemoteLauncher
 

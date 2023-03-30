b94b21723edef61b9a4745a001760eb10b3d237a
AlonMaor14 <maoralo@post.bgu.ac.il>
2023-03-30 20:24:54 +0300

    lint

--- mlrun/launcher/__init__.py
+++ mlrun/launcher/__init__.py
@@ -11,7 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .base import _BaseLauncher
-from .factory import LauncherFactory
-from .local import LocalLauncher
-from .remote import RemoteLauncher
--- mlrun/launcher/factory.py
+++ mlrun/launcher/factory.py
@@ -11,8 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from mlrun.launcher import LocalLauncher, RemoteLauncher
 from mlrun.launcher.base import _BaseLauncher
+from mlrun.launcher.local import LocalLauncher
+from mlrun.launcher.remote import RemoteLauncher
 
 
 class LauncherFactory(object):

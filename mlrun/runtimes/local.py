7869b09b9c0e584909d22b183a2befe3dc1e9f4e
Gal Topper <galt@iguazio.com>
2023-04-02 16:49:47 +0800

    Use StringIO for efficiency

--- mlrun/runtimes/local.py
+++ mlrun/runtimes/local.py
@@ -14,6 +14,7 @@
 
 import importlib.util as imputil
 import inspect
+import io
 import json
 import os
 import socket
@@ -372,24 +373,24 @@ def run_exec(cmd, args, env=None, cwd=None):
             nextline = process.stderr.readline()
             if not nextline and process.poll() is not None:
                 break
-            stderr[0] += nextline
+            stderr.write(nextline)
 
     # ML-3710. We must read stderr in a separate thread to drain the stderr pipe so that the spawned process won't
     # hang if it tries to write more to stderr than the buffer size (default of approx 8kb).
-    stderr = [""]
-    stderr_consumer_thread = threading.Thread(target=read_stderr, args=[stderr])
-    stderr_consumer_thread.start()
+    with io.StringIO() as stderr:
+        stderr_consumer_thread = threading.Thread(target=read_stderr, args=[stderr])
+        stderr_consumer_thread.start()
 
-    while True:
-        nextline = process.stdout.readline()
-        if not nextline and process.poll() is not None:
-            break
-        print(nextline, end="")
-        sys.stdout.flush()
-        out += nextline
+        while True:
+            nextline = process.stdout.readline()
+            if not nextline and process.poll() is not None:
+                break
+            print(nextline, end="")
+            sys.stdout.flush()
+            out += nextline
 
-    stderr_consumer_thread.join()
-    err = stderr[0]
+        stderr_consumer_thread.join()
+        err = stderr.getvalue()
     return out, err
 
 

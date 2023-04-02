7a4d2b88f3e938634a17a3fe41a148c096dc5ce1
Gal Topper <galt@iguazio.com>
2023-04-02 20:56:16 +0800

    Test PR

--- mlrun/serving/server.py
+++ mlrun/serving/server.py
@@ -306,29 +306,29 @@ def v2_serving_init(context, namespace=None):
     if not data:
         raise MLRunInvalidArgumentError("failed to find spec env var")
     spec = json.loads(data)
-    context.logger.info("Initializing server from spec")
+    # context.logger.info("Initializing server from spec")
     server = GraphServer.from_dict(spec)
     if config.log_level.lower() == "debug":
         server.verbose = True
     if hasattr(context, "trigger"):
         server.http_trigger = getattr(context.trigger, "kind", "http") == "http"
-    context.logger.info_with(
-        "Setting current function",
-        current_functiton=os.environ.get("SERVING_CURRENT_FUNCTION", ""),
-    )
+    # context.logger.info_with(
+    #     "Setting current function",
+    #     current_functiton=os.environ.get("SERVING_CURRENT_FUNCTION", ""),
+    # )
     server.set_current_function(os.environ.get("SERVING_CURRENT_FUNCTION", ""))
-    context.logger.info_with(
-        "Initializing states", namespace=namespace or get_caller_globals()
-    )
+    # context.logger.info_with(
+    #     "Initializing states", namespace=namespace or get_caller_globals()
+    # )
     server.init_states(context, namespace or get_caller_globals())
-    context.logger.info("Initializing graph steps")
+    # context.logger.info("Initializing graph steps")
     serving_handler = server.init_object(namespace or get_caller_globals())
     # set the handler hook to point to our handler
     setattr(context, "mlrun_handler", serving_handler)
     setattr(context, "_server", server)
-    context.logger.info_with("Serving was initialized", verbose=server.verbose)
-    if server.verbose:
-        context.logger.info(server.to_yaml())
+    # context.logger.info_with("Serving was initialized", verbose=server.verbose)
+    # if server.verbose:
+    #     context.logger.info(server.to_yaml())
 
 
 def v2_serving_handler(context, event, get_body=False):

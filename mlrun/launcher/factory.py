7513b42d5341e3d4dae86311a636a581189d0389
tankilevitch <tomtankilevitch@gmail.com>
2023-04-17 13:24:00 +0300

    lint

--- mlrun/launcher/factory.py
+++ mlrun/launcher/factory.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import mlrun.errors
-from mlrun.launcher import ClientLocalLauncher, ClientRemoteLauncher, BaseLauncher
+from mlrun.launcher import BaseLauncher, ClientLocalLauncher, ClientRemoteLauncher
 
 
 class LauncherFactory(object):

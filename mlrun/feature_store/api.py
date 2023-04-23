d67f487a9ffd6d4b158054f9a4abb3c90a33c4ed
davesh0812 <85231462+davesh0812@users.noreply.github.com>
2023-04-23 21:22:00 +0300

    [Feature Set] Fix ingest to raise error when user did not provide targets and called set_targets with_default=False (#3420)
    (cherry picked from commit d8adcfd26555ad9eab507f87d13a78f32d124bf7)

--- mlrun/feature_store/api.py
+++ mlrun/feature_store/api.py
@@ -410,6 +410,15 @@ def ingest(
         raise mlrun.errors.MLRunInvalidArgumentError(
             "feature set and source must be specified"
         )
+    if (
+        not mlrun_context
+        and not targets
+        and not (featureset.spec.targets or featureset.spec.with_default_targets)
+    ):
+        raise mlrun.errors.MLRunInvalidArgumentError(
+            f"No targets provided to feature set {featureset.metadata.name} ingest, aborting.\n"
+            "(preview can be used as an alternative to local ingest when targets are not needed)"
+        )
 
     if featureset is not None:
         featureset.validate_steps(namespace=namespace)

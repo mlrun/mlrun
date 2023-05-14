e7accd5d7a3e0af820d4a9a389b0113d80557d36
Liran BG <liranb@iguazio.com>
2023-05-14 22:03:28 +0300

    fix

--- mlrun/launcher/base.py
+++ mlrun/launcher/base.py
@@ -276,11 +276,12 @@ class BaseLauncher(abc.ABC):
                         or mlrun.pipeline_context.workflow_artifact_path
                     )
 
-                if not run.spec.output_path:
+                # get_db might be None when no rundb is set on runtime
+                if not run.spec.output_path and runtime._get_db():
                     try:
                         # not passing or loading the DB before the enrichment on purpose, because we want to enrich the
                         # spec first as get_db() depends on it
-                        project = self.db.get_project(run.metadata.project)
+                        project = runtime._get_db().get_project(run.metadata.project)
                         # this is mainly for tests, so we won't need to mock get_project for so many tests
                         # in normal use cases if no project is found we will get an error
                         if project:

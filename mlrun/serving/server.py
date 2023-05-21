0ffcc2c8001e73c257f03f714adb745756656892
Gal Topper <galt@iguazio.com>
2023-05-21 20:01:18 +0800

    ML-1325

--- mlrun/serving/server.py
+++ mlrun/serving/server.py
@@ -467,6 +467,7 @@ class GraphContext:
             self.logger = nuclio_context.logger
             self.Response = nuclio_context.Response
             self.worker_id = nuclio_context.worker_id
+            self.platform = nuclio_context.platform
         elif not logger:
             self.logger = mlrun.utils.helpers.logger
 

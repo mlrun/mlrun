8698ebdcef3b96062cb2dafef5a32ae6736fb94b
Tomer Mamia <tomerm@iguazio.com>
2023-05-28 14:43:48 +0300

    add comment

--- mlrun/datastore/dbfs_store.py
+++ mlrun/datastore/dbfs_store.py
@@ -105,7 +105,7 @@ class DBFSStore(DataStore):
         files = self._filesystem.glob(remote_path)
         key_length = len(key)
         #  Get only the files and directories under key path, without the key path itself.
-        # for example for /test_mlrun_dbfs_objects/test.txt the function will return ['test.txt']
+        # for example for /test_mlrun_dbfs_objects/test.txt the function will return ['test.txt'].
         files = [
             file.split("/", 1)[1][key_length:]
             for file in files

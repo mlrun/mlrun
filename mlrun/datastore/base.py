52eab92a135580ebcdb0a317705e898f4730aa10
David_Schneider <davesh0812@gmail.com>
2023-05-30 12:59:12 +0300

    .

--- mlrun/datastore/base.py
+++ mlrun/datastore/base.py
@@ -420,7 +420,7 @@ class DataItem:
         :param df_module: optional, py module used to create the DataFrame (e.g. pd, dd, cudf, ..)
         :param format:    file format, if not specified it will be deducted from the suffix
         """
-        kwargs["time_column"] = kwargs.get("time_column") or kwargs.pop("time_field")
+        kwargs["time_column"] = kwargs.get("time_column") or kwargs.pop("time_field", None)
         df = self._store.as_df(
             self._url,
             self._path,

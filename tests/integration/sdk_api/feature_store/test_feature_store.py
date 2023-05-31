920debcc8a88ac92961005fd42d159de0c9550eb
Gal Topper <galt@iguazio.com>
2023-05-31 15:59:50 +0800

    [Feature Store] Raise a clear error when no features are found on retrieval
    [ML-3900](https://jira.iguazeng.com/browse/ML-3900)
    
    Before this change, the error raised was
    ```
    AttributeError: 'RootFlowStep' object has no attribute 'respond'
    ```
    
    After this change, the error raised will be
    ```
    MLRunRuntimeError: No features found for feature vector <vector name>
    ```

--- tests/integration/sdk_api/feature_store/test_feature_store.py
+++ tests/integration/sdk_api/feature_store/test_feature_store.py
@@ -12,11 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import pandas as pd
 import pytest
 
 import mlrun
 import mlrun.feature_store as fstore
 import tests.integration.sdk_api.base
+from mlrun.data_types import InferOptions
 from mlrun.datastore import StreamSource
 from mlrun.features import Entity
 
@@ -39,3 +41,44 @@ class TestFeatureStore(tests.integration.sdk_api.base.TestMLRunIntegration):
                 featureset=fset,
                 source=v3io_source,
             )
+
+    def test_get_online_features_after_ingest_without_inference(self):
+        feature_set = fstore.FeatureSet(
+            "my-fset",
+            entities=[
+                fstore.Entity("fn0"),
+                fstore.Entity(
+                    "fn1",
+                    value_type=mlrun.data_types.data_types.ValueType.STRING,
+                ),
+            ],
+        )
+        # feature_set.set_targets(
+        #     targets=[mlrun.datastore.NoSqlTarget()], with_defaults=False
+        # )
+        # feature_set.save()
+
+        df = pd.DataFrame(
+            {
+                "fn0": [1, 2, 3, 4],
+                "fn1": [1, 2, 3, 4],
+                "fn2": [1, 1, 1, 1],
+                "fn3": [2, 2, 2, 2],
+            }
+        )
+
+        fstore.ingest(feature_set, df, infer_options=InferOptions.Null)
+
+        features = ["my-fset.*"]
+        vector = fstore.FeatureVector("my-vector", features)
+        vector.save()
+
+        sv = fstore.get_online_feature_service(
+            f"store://feature-vectors/{self.project_name}/my-vector:latest"
+        )
+        try:
+            with pytest.raises(mlrun.errors.MLRunRuntimeError) as err:
+                sv.get([{"fn0": "1", "fn1": "1"}])
+        finally:
+            sv.close()
+        assert err.value == "No features found for feature vector 'my-vector'"

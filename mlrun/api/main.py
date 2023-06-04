02b6272ba98b4151a09eb602c277be0e473730db
TomerShor <tomer.shor@gmail.com>
2023-06-04 13:14:36 +0300

    non-aborted states

--- mlrun/api/main.py
+++ mlrun/api/main.py
@@ -272,13 +272,17 @@ async def _initiate_logs_collection(start_logs_limit: asyncio.Semaphore):
     """
     db_session = await fastapi.concurrency.run_in_threadpool(create_session)
     try:
-        # list all the runs in the system which we didn't request logs collection for yet, and are not in terminal state
+        # we don't want initiate logs collection for aborted runs
+        run_states = mlrun.runtimes.constants.RunStates.all()
+        run_states.remove(mlrun.runtimes.constants.RunStates.aborted)
+
+        # list all the runs in the system which we didn't request logs collection for yet
         runs = await fastapi.concurrency.run_in_threadpool(
             get_db().list_distinct_runs_uids,
             db_session,
             requested_logs_modes=[False],
             only_uids=False,
-            states=mlrun.runtimes.constants.RunStates.non_terminal_states(),
+            states=run_states,
         )
         if runs:
             logger.debug(

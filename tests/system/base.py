3f0b319f2ed6493080c42dd64ea2568de20fbc70
Liran BG <liranb@iguazio.com>
2023-06-20 08:57:07 +0300

    initial

--- tests/system/base.py
+++ tests/system/base.py
@@ -293,7 +293,7 @@ class TestMLRunSystem:
         self._logger.debug("Verifying run outputs", spec=run_outputs)
         assert run_outputs["chart"].startswith(str(output_path))
         assert run_outputs["mydf"] == f"store://artifacts/{project}/{name}_mydf:{uid}"
-        assert run_outputs["model"] == f"store://artifacts/{project}/{name}model:{uid}"
+        assert run_outputs["model"] == f"store://artifacts/{project}/{name}_model:{uid}"
         assert (
             run_outputs["html_result"]
             == f"store://artifacts/{project}/{name}_html_result:{uid}"

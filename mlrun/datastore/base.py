mlrun/mlrun

commit de2de400d124c8f721f00fdf311dc8b55558eb80

[Feature Store] Fix time filter issues in `get_offline_features` (#3637)

mlrun/datastore/base.py
@@ -28,6 +28,8 @@ import mlrun.errors
 from mlrun.errors import err_to_str
 from mlrun.utils import is_ipython, logger
 
+from .utils import filter_df_start_end_time, select_columns_from_df
+
 verify_ssl = False
 if not verify_ssl:
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -151,9 +153,20 @@ class DataStore:
         df_module = df_module or pd
         parsed_url = urllib.parse.urlparse(url)
         filepath = parsed_url.path
+        is_csv, is_json, drop_time_column = False, False, False
         if filepath.endswith(".csv") or format == "csv":
+            is_csv = True
+            drop_time_column = False
             if columns:
+                if (
+                    time_column
+                    and (start_time or end_time)
+                    and time_column not in columns
+                ):
+                    columns.append(time_column)
+                    drop_time_column = True
                 kwargs["usecols"] = columns
+
             reader = df_module.read_csv
             filesystem = self.get_filesystem()
             if filesystem:
@@ -216,6 +229,7 @@ class DataStore:
                 return df_module.read_parquet(*args, **kwargs)
 
         elif filepath.endswith(".json") or format == "json":
+            is_json = True
             reader = df_module.read_json
 
         else:
@@ -227,7 +241,7 @@ class DataStore:
                 storage_options = self.get_storage_options()
                 if storage_options:
                     kwargs["storage_options"] = storage_options
-                return reader(url, **kwargs)
+                df = reader(url, **kwargs)
             else:
 
                 file = url
@@ -237,12 +251,26 @@ class DataStore:
                     # support the storage_options parameter.
                     file = file_system.open(url)
 
-                return reader(file, **kwargs)
+                df = reader(file, **kwargs)
+        else:
+            temp_file = tempfile.NamedTemporaryFile(delete=False)
+            self.download(self._join(subpath), temp_file.name)
+            df = reader(temp_file.name, **kwargs)
+            remove(temp_file.name)
 
-        temp_file = tempfile.NamedTemporaryFile(delete=False)
-        self.download(self._join(subpath), temp_file.name)
-        df = reader(temp_file.name, **kwargs)
-        remove(temp_file.name)
+        if is_json or is_csv:
+            # for parquet file the time filtering is executed in `reader`
+            df = filter_df_start_end_time(
+                df,
+                time_column=time_column,
+                start_time=start_time,
+                end_time=end_time,
+            )
+            if drop_time_column:
+                df.drop(columns=[time_column], inplace=True)
+        if is_json:
+            # for csv and parquet files the columns select is executed in `reader`.
+            df = select_columns_from_df(df, columns=columns)
         return df
 
     def to_dict(self):
@@ -418,22 +446,33 @@ class DataItem:
         columns=None,
         df_module=None,
         format="",
+        time_column=None,
+        start_time=None,
+        end_time=None,
         **kwargs,
     ):
         """return a dataframe object (generated from the dataitem).
 
-        :param columns:   optional, list of columns to select
-        :param df_module: optional, py module used to create the DataFrame (e.g. pd, dd, cudf, ..)
-        :param format:    file format, if not specified it will be deducted from the suffix
+        :param columns:     optional, list of columns to select
+        :param df_module:   optional, py module used to create the DataFrame (e.g. pd, dd, cudf, ..)
+        :param format:      file format, if not specified it will be deducted from the suffix
+        :param start_time:  filters out data before this time
+        :param end_time:    filters out data after this time
+        :param time_column: Store timestamp_key will be used if None.
+                            The results will be filtered by this column and start_time & end_time.
         """
-        return self._store.as_df(
+        df = self._store.as_df(
             self._url,
             self._path,
             columns=columns,
             df_module=df_module,
             format=format,
+            time_column=time_column,
+            start_time=start_time,
+            end_time=end_time,
             **kwargs,
         )
+        return df
 
     def show(self, format=None):
         """show the data object content in Jupyter

mlrun/feature_store/retrieval/base.py
@@ -21,7 +21,7 @@ from mlrun.datastore.targets import CSVTarget, ParquetTarget
 from mlrun.feature_store.feature_set import FeatureSet
 from mlrun.feature_store.feature_vector import Feature
 
-from ...utils import logger
+from ...utils import logger, str_to_timestamp
 from ..feature_vector import OfflineVectorResponse
 
 
@@ -73,6 +73,7 @@ class BaseMerger(abc.ABC):
         drop_columns=None,
         start_time=None,
         end_time=None,
+        timestamp_for_filtering=None,
         with_indexes=None,
         update_stats=None,
         query=None,
@@ -84,9 +85,6 @@ class BaseMerger(abc.ABC):
         self._drop_columns = drop_columns or self._drop_columns
         if self.vector.spec.with_indexes or with_indexes:
             self._drop_indexes = False
-
-        if entity_timestamp_column and self._drop_indexes:
-            self._append_drop_column(entity_timestamp_column)
 
         # retrieve the feature set objects/fields needed for the vector
         feature_set_objects, feature_set_fields = self.vector.parse_features(
@@ -102,10 +100,13 @@ class BaseMerger(abc.ABC):
             self.vector.save()
 
         for feature_set in feature_set_objects.values():
-            if not entity_timestamp_column and self._drop_indexes:
+            if self._drop_indexes:
                 self._append_drop_column(feature_set.spec.timestamp_key)
             for key in feature_set.spec.entities.keys():
                 self._append_index(key)
+
+        start_time = str_to_timestamp(start_time)
+        end_time = str_to_timestamp(end_time)
 
         return self._generate_vector(
             entity_rows,
@@ -114,6 +115,7 @@ class BaseMerger(abc.ABC):
             feature_set_fields=feature_set_fields,
             start_time=start_time,
             end_time=end_time,
+            timestamp_for_filtering=timestamp_for_filtering,
             query=query,
             order_by=order_by,
         )
@@ -166,6 +168,7 @@ class BaseMerger(abc.ABC):
         feature_set_fields,
         start_time=None,
         end_time=None,
+        timestamp_for_filtering=None,
         query=None,
         order_by=None,
     ):
@@ -183,6 +186,7 @@ class BaseMerger(abc.ABC):
             feature_set_objects, feature_set_fields
         )
 
+        filtered = False
         for node in fs_link_list:
             name = node.name
             feature_set = feature_set_objects[name]
@@ -196,24 +200,42 @@ class BaseMerger(abc.ABC):
                     self._append_drop_column(column)
                     column_names.append(column)
 
+            if isinstance(timestamp_for_filtering, dict):
+                time_column = timestamp_for_filtering.get(
+                    name, feature_set.spec.timestamp_key
+                )
+            elif isinstance(timestamp_for_filtering, str):
+                time_column = timestamp_for_filtering
+            else:
+                time_column = feature_set.spec.timestamp_key
+
+            if time_column != feature_set.spec.timestamp_key and time_column not in [
+                feature.name for feature in feature_set.spec.features
+            ]:
+                raise mlrun.errors.MLRunInvalidArgumentError(
+                    f"Feature set `{name}` "
+                    f"does not have a column named `{time_column}` to filter on."
+                )
+
+            if (start_time or end_time) and time_column:
+                filtered = True
+
             df = self._get_engine_df(
                 feature_set,
                 name,
                 column_names,
-                start_time,
-                end_time,
-                entity_timestamp_column,
+                start_time if time_column else None,
+                end_time if time_column else None,
+                time_column,
             )
 
             column_names += node.data["save_index"]
             node.data["save_cols"] += node.data["save_index"]
+            fs_entities_and_timestamp = list(feature_set.spec.entities.keys())
             if feature_set.spec.timestamp_key:
-                entity_timestamp_column_list = [feature_set.spec.timestamp_key]
-                column_names += entity_timestamp_column_list
-                node.data["save_cols"] += entity_timestamp_column_list
-                if not entity_timestamp_column:
-                    # if not entity_timestamp_column the firs `FeatureSet` will define it
-                    entity_timestamp_column = feature_set.spec.timestamp_key
+                column_names.append(feature_set.spec.timestamp_key)
+                node.data["save_cols"].append(feature_set.spec.timestamp_key)
+                fs_entities_and_timestamp.append(feature_set.spec.timestamp_key)
 
             # rename columns to be unique for each feature set and select if needed
             rename_col_dict = {
@@ -221,9 +243,10 @@ class BaseMerger(abc.ABC):
                 for column in column_names
                 if column not in node.data["save_cols"]
             }
-            fs_entities = list(feature_set.spec.entities.keys())
             df_temp = self._rename_columns_and_select(
-                df, rename_col_dict, columns=list(set(column_names + fs_entities))
+                df,
+                rename_col_dict,
+                columns=list(set(column_names + fs_entities_and_timestamp)),
             )
 
             if df_temp is not None:
@@ -238,7 +261,7 @@ class BaseMerger(abc.ABC):
             # update alias according to the unique column name
             new_columns = []
             if not self._drop_indexes:
-                new_columns.extend([(ind, ind) for ind in fs_entities])
+                new_columns.extend([(ind, ind) for ind in fs_entities_and_timestamp])
             for column, alias in columns:
                 if column in rename_col_dict:
                     new_columns.append((rename_col_dict[column], alias or column))
@@ -246,6 +269,12 @@ class BaseMerger(abc.ABC):
                     new_columns.append((column, alias))
             self._update_alias(dictionary={name: alias for name, alias in new_columns})
 
+        # None of the feature sets was filtered as required
+        if not filtered and (start_time or end_time):
+            raise mlrun.errors.MLRunRuntimeError(
+                "start_time and end_time can only be provided in conjunction with "
+                "a timestamp column, or when the at least one feature_set has a timestamp key"
+            )
         # convert pandas entity_rows to spark DF if needed
         if (
             entity_rows is not None
@@ -255,20 +284,18 @@ class BaseMerger(abc.ABC):
             entity_rows = self.spark.createDataFrame(entity_rows)
 
         # join the feature data frames
-        self.merge(
+        result_timestamp = self.merge(
             entity_df=entity_rows,
-            entity_timestamp_column=entity_timestamp_column,
+            entity_timestamp_column=entity_timestamp_column if entity_rows else None,
             featuresets=feature_sets,
             featureset_dfs=dfs,
             keys=keys,
         )
 
         all_columns = None
-        if not self._drop_indexes and entity_timestamp_column:
-            if entity_timestamp_column not in self._alias.values():
-                self._update_alias(
-                    key=entity_timestamp_column, val=entity_timestamp_column
-                )
+        if not self._drop_indexes and result_timestamp:
+            if result_timestamp not in self._alias.values():
+                self._update_alias(key=result_timestamp, val=result_timestamp)
             all_columns = list(self._alias.keys())
 
         df_temp = self._rename_columns_and_select(
@@ -341,7 +368,7 @@ class BaseMerger(abc.ABC):
             keys[0][0] = keys[0][1] = list(featuresets[0].spec.entities.keys())
 
         for featureset, featureset_df, lr_key in zip(featuresets, featureset_dfs, keys):
-            if featureset.spec.timestamp_key:
+            if featureset.spec.timestamp_key and entity_timestamp_column:
                 merge_func = self._asof_join
             else:
                 merge_func = self._join
@@ -354,6 +381,9 @@ class BaseMerger(abc.ABC):
                 lr_key[0],
                 lr_key[1],
             )
+            entity_timestamp_column = (
+                entity_timestamp_column or featureset.spec.timestamp_key
+            )
 
             # unpersist as required by the implementation (e.g. spark) and delete references
             # to dataframe to allow for GC to free up the memory (local, dask)
@@ -361,6 +391,7 @@ class BaseMerger(abc.ABC):
             del featureset_df
 
         self._result_df = merged_df
+        return entity_timestamp_column
 
     @abc.abstractmethod
     def _asof_join(
@@ -646,7 +677,7 @@ class BaseMerger(abc.ABC):
         column_names: typing.List[str] = None,
         start_time: typing.Union[str, datetime] = None,
         end_time: typing.Union[str, datetime] = None,
-        entity_timestamp_column: str = None,
+        time_column: typing.Optional[str] = None,
     ):
         """
         Return the feature_set data frame according to the args
@@ -656,7 +687,7 @@ class BaseMerger(abc.ABC):
         :param column_names:            list of columns to select (if not all)
         :param start_time:              filter by start time
         :param end_time:                filter by end time
-        :param entity_timestamp_column: specify the time column name in the file
+        :param time_column:             specify the time column name to filter on
 
         :return: Data frame of the current engine
         """

mlrun/datastore/inmem.py
@@ -80,5 +80,8 @@ class InMemoryStore(DataStore):
             reader = df_module.read_json
         else:
             raise mlrun.errors.MLRunInvalidArgumentError(f"file type unhandled {url}")
+        # InMemoryStore store do not filter on time
+        for field in ["time_column", "start_time", "end_time"]:
+            kwargs.pop(field, None)
 
         return reader(item, **kwargs)

commit 6fa90e9394292f30b9f0da2bdc3a05d15667f454

[Datastore] Fix time zone error
[ML-4025](https://jira.iguazeng.com/browse/ML-4025)

Fixes PyArrow error
```
Cannot compare timestamp with timezone to timestamp without timezone, got: timestamp[us, tz=UTC] and timestamp[us]
```
similar to the solution for [ML-3566](https://jira.iguazeng.com/browse/ML-3566).

This fixes a regression in v1.4.0-rc13 that was introduced by #3637, which propagated time filtering from `preview()`.

mlrun/datastore/base.py
@@ -11,17 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import sys
 import tempfile
 import urllib.parse
 from base64 import b64encode
 from os import path, remove
-from typing import Union
+from typing import Optional, Union
 
 import dask.dataframe as dd
 import fsspec
 import orjson
 import pandas as pd
+import pyarrow
+import pytz
 import requests
 import urllib3
 
@@ -87,7 +88,7 @@ class DataStore:
     def uri_to_ipython(endpoint, subpath):
         return ""
 
-    def get_filesystem(self, silent=True):
+    def get_filesystem(self, silent=True) -> Optional[fsspec.AbstractFileSystem]:
         """return fsspec file system object, if supported"""
         return None
 
@@ -151,6 +152,59 @@ class DataStore:
     def upload(self, key, src_path):
         pass
 
+    @staticmethod
+    def _parquet_reader(df_module, url, file_system, time_column, start_time, end_time):
+        from storey.utils import find_filters, find_partitions
+
+        def set_filters(
+            partitions_time_attributes, start_time_inner, end_time_inner, kwargs
+        ):
+            filters = []
+            find_filters(
+                partitions_time_attributes,
+                start_time_inner,
+                end_time_inner,
+                filters,
+                time_column,
+            )
+            kwargs["filters"] = filters
+
+        def reader(*args, **kwargs):
+            if start_time or end_time:
+                if time_column is None:
+                    raise mlrun.errors.MLRunInvalidArgumentError(
+                        "When providing start_time or end_time, must provide time_column"
+                    )
+
+                partitions_time_attributes = find_partitions(url, file_system)
+                set_filters(partitions_time_attributes, kwargs)
+                try:
+                    return df_module.read_parquet(*args, **kwargs)
+                except pyarrow.lib.ArrowInvalid as ex:
+                    if not str(ex).startswith(
+                        "Cannot compare timestamp with timezone to timestamp without timezone"
+                    ):
+                        raise ex
+
+                    if start_time.tzinfo:
+                        start_time_inner = start_time.replace(tzinfo=None)
+                        end_time_inner = end_time.replace(tzinfo=None)
+                    else:
+                        start_time_inner = start_time.replace(tzinfo=pytz.utc)
+                        end_time_inner = end_time.replace(tzinfo=pytz.utc)
+
+                    set_filters(
+                        partitions_time_attributes,
+                        start_time_inner,
+                        end_time_inner,
+                        kwargs,
+                    )
+                    return df_module.read_parquet(*args, **kwargs)
+            else:
+                return df_module.read_parquet(*args, **kwargs)
+
+        return reader
+
     def as_df(
         self,
         url,
@@ -166,6 +220,7 @@ class DataStore:
         df_module = df_module or pd
         file_url = self._sanitize_url(url)
         is_csv, is_json, drop_time_column = False, False, False
+        file_system = self.get_filesystem()
         if file_url.endswith(".csv") or format == "csv":
             is_csv = True
             drop_time_column = False
@@ -180,13 +235,12 @@ class DataStore:
                 kwargs["usecols"] = columns
 
             reader = df_module.read_csv
-            filesystem = self.get_filesystem()
-            if filesystem:
-                if filesystem.isdir(file_url):
+            if file_system:
+                if file_system.isdir(file_url):
 
                     def reader(*args, **kwargs):
                         base_path = args[0]
-                        file_entries = filesystem.listdir(base_path)
+                        file_entries = file_system.listdir(base_path)
                         filenames = []
                         for file_entry in file_entries:
                             if (
@@ -212,33 +266,9 @@ class DataStore:
             if columns:
                 kwargs["columns"] = columns
 
-            def reader(*args, **kwargs):
-                if start_time or end_time:
-                    if sys.version_info < (3, 7):
-                        raise ValueError(
-                            f"feature not supported for python version {sys.version_info}"
-                        )
-
-                    if time_column is None:
-                        raise mlrun.errors.MLRunInvalidArgumentError(
-                            "When providing start_time or end_time, must provide time_column"
-                        )
-
-                    from storey.utils import find_filters, find_partitions
-
-                    filters = []
-                    partitions_time_attributes = find_partitions(url, file_system)
-
-                    find_filters(
-                        partitions_time_attributes,
-                        start_time,
-                        end_time,
-                        filters,
-                        time_column,
-                    )
-                    kwargs["filters"] = filters
-
-                return df_module.read_parquet(*args, **kwargs)
+            reader = self._parquet_reader(
+                df_module, url, file_system, time_column, start_time, end_time
+            )
 
         elif file_url.endswith(".json") or format == "json":
             is_json = True
@@ -247,7 +277,6 @@ class DataStore:
         else:
             raise Exception(f"file type unhandled {url}")
 
-        file_system = self.get_filesystem()
         if file_system:
             if self.supports_isdir() and file_system.isdir(file_url) or df_module == dd:
                 storage_options = self.get_storage_options()
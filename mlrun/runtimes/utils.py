f436b7f974aa413959df9ddcd59f9f8a3ce582d1
alonmr <alonnmaor@gmail.com>
2023-06-25 15:15:20 +0300

    [Utils] Fix missing logs on failure to add git metadata

--- mlrun/runtimes/utils.py
+++ mlrun/runtimes/utils.py
@@ -208,8 +208,13 @@ def add_code_metadata(path=""):
         ]
         if len(remotes) > 0:
             return f"{remotes[0]}#{repo.head.commit.hexsha}"
-    except (GitCommandNotFound, InvalidGitRepositoryError, NoSuchPathError, ValueError):
-        pass
+    except (
+        GitCommandNotFound,
+        InvalidGitRepositoryError,
+        NoSuchPathError,
+        ValueError,
+    ) as exc:
+        logger.warning("Failed to add git metadata, ignore if path is not part of a git repo", error=err_to_str(exc), path=path)
     return None
 
 

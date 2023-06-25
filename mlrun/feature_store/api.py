46eed2267ec2d9d7e925a38eefe426673730baab
davesh0812 <85231462+davesh0812@users.noreply.github.com>
2023-06-25 11:23:31 +0300

    Update mlrun/feature_store/api.py
    Co-authored-by: Saar Cohen <66667568+theSaarco@users.noreply.github.com>

--- mlrun/feature_store/api.py
+++ mlrun/feature_store/api.py
@@ -257,7 +257,7 @@ def get_online_feature_service(
 
             Example with imputing::
 
-                svc = get_online_feature_service(vector_uri, ['id'], impute_policy={"*": "$mean", "amount": 0))
+                svc = get_online_feature_service(vector_uri, entity_keys=['id'], impute_policy={"*": "$mean", "amount": 0))
                 try:
                     resp = svc.get([{"id": "C123487"}])
                 except Exception as e:

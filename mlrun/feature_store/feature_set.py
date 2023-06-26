d59380a5dad0e09e3ffaefac053a8cfdc6853708
David_Schneider <davesh0812@gmail.com>
2023-06-26 13:26:40 +0300

    fix bug

--- mlrun/feature_store/feature_set.py
+++ mlrun/feature_store/feature_set.py
@@ -944,8 +944,8 @@ class FeatureSet(ModelObj):
                 )
             df = self.spec.source.to_dataframe(
                 columns=columns,
-                start_time=start_time,
-                end_time=end_time,
+                start_time=start_time or datetime.min,
+                end_time=end_time or datetime.max,
                 time_field=time_column,
                 **kwargs,
             )

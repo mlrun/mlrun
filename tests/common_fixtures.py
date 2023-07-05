de47771097e6f0d5d5ab706a3fd811fa9ee7921b
Yaelg <yaelg@iguazio.com>
2023-07-05 17:53:19 +0300

    fix

--- tests/common_fixtures.py
+++ tests/common_fixtures.py
@@ -180,7 +180,7 @@ def chdir_to_test_location(request):
 
     """
     original_working_dir = os.getcwd()
-    test_file_path = os.path.abspath(inspect.getfile(request.function))
+    test_file_path = os.path.dirname(inspect.getfile(request.function))
     os.chdir(os.path.dirname(test_file_path))
 
     yield

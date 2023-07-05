99a954e35476852a26afb0add566b49b2bb750ea
Gal Topper <gal.topper@gmail.com>
2023-07-06 02:22:48 +0800

    [Tests] Fix `test_get_offline_features_with_spark_engine` (#3882)

--- tests/system/feature_store/test_spark_engine.py
+++ tests/system/feature_store/test_spark_engine.py
@@ -44,7 +44,6 @@ from mlrun.feature_store.steps import (
     OneHotEncoder,
 )
 from mlrun.features import Entity
-from mlrun.model import DataTarget
 from tests.system.base import TestMLRunSystem
 from tests.system.feature_store.data_sample import stocks
 from tests.system.feature_store.expected_stats import expected_stats
@@ -1397,20 +1396,18 @@ class TestFeatureStoreSparkEngine(TestMLRunSystem):
             passthrough=passthrough,
         )
         source = ParquetSource("myparquet", path=self.get_pq_source_path())
-        self.set_targets(measurements)
+        if not passthrough:
+            self.set_targets(measurements)
         fstore.ingest(
             measurements,
             source,
             spark_context=self.spark_service,
             run_config=fstore.RunConfig(local=self.run_local),
         )
-        if not self.run_local:
+        if passthrough:
+            assert len(measurements.status.targets) == 0
+        elif not self.run_local:
             assert measurements.status.targets[0].run_id is not None
-
-        # assert that online target exist (nosql) and offline target does not (parquet)
-        if passthrough and not self.run_local:
-            assert len(measurements.status.targets) == 1
-            assert isinstance(measurements.status.targets["nosql"], DataTarget)
 
         fv_name = "measurements-fv"
         features = [

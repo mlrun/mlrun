ff4269124c86114bfd493a564913a9f3f1cf92d9
Tomer Mamia <tomerm@iguazio.com>
2023-07-09 13:17:04 +0300

    remove str() in uuid

--- tests/integration/test_dbfs_store/test_dbfs_store.py
+++ tests/integration/test_dbfs_store/test_dbfs_store.py
@@ -86,7 +86,7 @@ class TestDBFSStore:
             self.workspace.dbfs.delete(path, recursive=True)
 
     def _get_data_item(self, secrets={}):
-        object_path = f"{self.test_root_dir}/file_{str(uuid.uuid4())}.txt"
+        object_path = f"{self.test_root_dir}/file_{uuid.uuid4()}.txt"
         object_url = f"{self._dbfs_url}{object_path}"
         return mlrun.run.get_dataitem(object_url, secrets=secrets), object_url
 

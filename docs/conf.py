b1a1270cd5f90d0912a52ae9dfe6eb45dceeac29
jillnogold <jillg@iguazio.com>
2023-07-10 09:01:59 +0300

    fix conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -144,8 +144,8 @@ myst_substitutions = {
     "ceversion": "v1.2.1",
     "releasedocumentation": "docs.mlrun.org/en/v1.4.0/index.html",
     "errorhandling": "Use the graph `error_handler` if you want an error from the graph " +
-        "or a step to be fed into a specific state (catcher). See the full description " +
-        "in [Error handling](../serving/pipelines-error-handling.html).",
+                     "or a step to be fed into a specific state (catcher). See the full description " +
+                     "in [Error handling](../serving/pipelines-error-handling.html).",
 }
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.

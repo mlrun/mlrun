2c0434f671ca4b5f404ff6b9dab91265701b11a4
jillnogold <jillg@iguazio.com>
2023-07-10 11:01:29 +0300

    remove trailing whitespace

--- docs/conf.py
+++ docs/conf.py
@@ -142,7 +142,7 @@ myst_all_links_external = True
 myst_substitutions = {
     "version": "version",
     "ceversion": "v1.2.1",
-    "releasedocumentation": "docs.mlrun.org/en/v1.4.0/index.html",    
+    "releasedocumentation": "docs.mlrun.org/en/v1.4.0/index.html",
 }
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.

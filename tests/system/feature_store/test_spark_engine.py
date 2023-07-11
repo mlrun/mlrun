d8203a1a7ef91d82de5f140222a7159af5f5a25c
Gal Topper <gal.topper@gmail.com>
2023-07-11 17:58:56 +0800

    [Spark] Fix masking of errors in spark graph (#3919)

--- tests/system/feature_store/test_spark_engine.py
+++ tests/system/feature_store/test_spark_engine.py
@@ -1659,8 +1659,13 @@ class TestFeatureStoreSparkEngine(TestMLRunSystem):
         )
         source = ParquetSource("myparquet", path=self.get_pq_source_path())
         targets = [CSVTarget(name="csv", path=csv_path_spark)]
+        error_type = (
+            mlrun.errors.MLRunBadRequestError
+            if self.run_local
+            else mlrun.runtimes.utils.RunError
+        )
         with pytest.raises(
-            mlrun.runtimes.utils.RunError,
+            error_type,
             match="^MapValues - mapping that changes column type must change all values accordingly,"
             " which is not the case for column 'hr_is_error'$",
         ):

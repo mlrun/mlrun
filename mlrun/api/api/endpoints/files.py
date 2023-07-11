09fa73065945332b996273f63e75481c407196e8
Saar Cohen <saarc@iguazio.com>
2023-07-11 10:52:45 +0300

    reorder

--- mlrun/api/api/endpoints/files.py
+++ mlrun/api/api/endpoints/files.py
@@ -197,6 +197,12 @@ def _get_filestat(
 
 
 async def _verify_and_get_project_secrets(project, auth_info):
+    # If running on Docker or locally, we cannot retrieve project secrets, so skip.
+    if not mlrun.api.utils.singletons.k8s.get_k8s_helper(
+        silent=True
+    ).is_running_inside_kubernetes_cluster():
+        return {}
+
     await mlrun.api.utils.auth.verifier.AuthVerifier().query_project_resource_permissions(
         mlrun.common.schemas.AuthorizationResourceTypes.secret,
         project,
@@ -204,11 +210,6 @@ async def _verify_and_get_project_secrets(project, auth_info):
         mlrun.common.schemas.AuthorizationAction.read,
         auth_info,
     )
-    # If running on Docker or locally, we cannot retrieve project secrets, so skip.
-    if not mlrun.api.utils.singletons.k8s.get_k8s_helper(
-        silent=True
-    ).is_running_inside_kubernetes_cluster():
-        return {}
 
     secrets_data = await run_in_threadpool(
         mlrun.api.crud.Secrets().list_project_secrets,

701544fe6976855394aa46dd67aab6193bca90ce
Adam <adamm@iguazio.com>
2023-07-18 09:03:12 +0300

    [Projects] Fix Project Sync Fails on Malformed Project (#3945)
    * fix sync
    
    * cr
    
    ---------
    
    Co-authored-by: quaark <a.melnick@icloud.com>

--- mlrun/api/utils/projects/follower.py
+++ mlrun/api/utils/projects/follower.py
@@ -346,11 +346,18 @@ class Member(
                         "Found project in the DB that is not in leader. Removing",
                         name=project_to_remove,
                     )
-                    mlrun.api.crud.Projects().delete_project(
-                        db_session,
-                        project_to_remove,
-                        mlrun.common.schemas.DeletionStrategy.cascading,
-                    )
+                    try:
+                        mlrun.api.crud.Projects().delete_project(
+                            db_session,
+                            project_to_remove,
+                            mlrun.common.schemas.DeletionStrategy.cascading,
+                        )
+                    except Exception as exc:
+                        logger.warning(
+                            "Failed to delete project from DB, continuing...",
+                            name=project_to_remove,
+                            exc=err_to_str(exc),
+                        )
             if latest_updated_at:
 
                 # sanity and defensive programming - if the leader returned a latest_updated_at that is older

3647895b0a5992125dd6f260a0e6dc523f5f63bb
yonishelach <yonatanshelach@gmail.com>
2023-07-20 16:34:42 +0300

    revert marketplace -> hub auth

--- mlrun/common/schemas/auth.py
+++ mlrun/common/schemas/auth.py
@@ -87,8 +87,9 @@ class AuthorizationResourceTypes(mlrun.common.types.StrEnum):
             AuthorizationResourceTypes.model_endpoint: "/projects/{project_name}/model-endpoints/{resource_name}",
             AuthorizationResourceTypes.pipeline: "/projects/{project_name}/pipelines/{resource_name}",
             # Hub sources are not project-scoped, and auth is globally on the sources endpoint.
-            # Updated - This should be supported in (>=3.5.4).
-            AuthorizationResourceTypes.hub_source: "/hub/sources",
+            # TODO - this was reverted to /marketplace since MLRun needs to be able to run with old igz versions. Once
+            #  we only have support for igz versions that support /hub (>=3.5.4), change this to "/hub/sources".
+            AuthorizationResourceTypes.hub_source: "/marketplace/sources",
             # workflow define how to run a pipeline and can be considered as the specification of a pipeline.
             AuthorizationResourceTypes.workflow: "/projects/{project_name}/workflows/{resource_name}",
         }[self].format(project_name=project_name, resource_name=resource_name)

62784d6b8ce05217b0f6f17e5133dc20ecf61108
Jonathan Daniel <jonathan_daniel@mckinsey.com>
2023-07-24 15:11:53 +0300

    Test the add minutes offset

--- tests/api/crud/model_monitoring/test_helpers.py
+++ tests/api/crud/model_monitoring/test_helpers.py
@@ -1,11 +1,37 @@
+import typing
+
 import pytest
 
-from mlrun.api.crud.model_monitoring.deployment import Minutes, Seconds, seconds2minutes
+from mlrun.api.crud.model_monitoring.helpers import (
+    Minutes,
+    Seconds,
+    _add_minutes_offset,
+    seconds2minutes,
+)
 
 
 @pytest.mark.parametrize(
     ("seconds", "expected_minutes"),
     [(0, 0), (1, 1), (60, 1), (21, 1), (365, 7)],
 )
-def test_minutes2seconds(seconds, expected_minutes):
+def test_minutes2seconds(seconds: Seconds, expected_minutes: Minutes) -> None:
     assert seconds2minutes(seconds) == expected_minutes
+
+
+@pytest.mark.parametrize(
+    ("minute", "offset", "expected_result"),
+    [
+        (0, 0, 0),
+        ("0", 0, 0),
+        ("*", 22, "*"),
+        ("*/4", 2, "*/4"),
+        ("0", 20, 20),
+        ("40", 30, 10),
+    ],
+)
+def test_add_minutes_offset(
+    minute: typing.Optional[typing.Union[int, str]],
+    offset: Minutes,
+    expected_result: typing.Optional[typing.Union[int, str]],
+) -> None:
+    assert _add_minutes_offset(minute, offset) == expected_result

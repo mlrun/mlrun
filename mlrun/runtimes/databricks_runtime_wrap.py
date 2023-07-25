e93f1b2fda396601c74e7fd8b7ad7860d3f0eb47
Tomer Mamia <tomerm@iguazio.com>
2023-07-25 16:14:59 +0300

    lint 3

--- mlrun/runtimes/databricks_runtime_wrap.py
+++ mlrun/runtimes/databricks_runtime_wrap.py
@@ -85,8 +85,10 @@ handler_arguments = json.loads(handler_arguments)
 
     now = datetime.datetime.now()
     formatted_date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
-    script_path_on_dbfs = f"/home/{workspace.current_user.me().user_name}/mlrun_databricks_run" \
-                          f"time/sample_{formatted_date_time}_{uuid.uuid4()}.py"
+    script_path_on_dbfs = (
+        f"/home/{workspace.current_user.me().user_name}/mlrun_databricks_run"
+        f"time/sample_{formatted_date_time}_{uuid.uuid4()}.py"
+    )
 
     if is_local_code:
         upload_file(

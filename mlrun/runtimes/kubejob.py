2b6965d0a453b9362b32f3f8deec5afc928c8bb2
Tomer Mamia <tomerm@iguazio.com>
2023-07-25 16:07:53 +0300

    remove print 6

--- mlrun/runtimes/kubejob.py
+++ mlrun/runtimes/kubejob.py
@@ -430,7 +430,6 @@ class DatabricksRuntime(KubejobRuntime):
     def _pre_run(self, runspec: RunObject, execution):
         runspec.spec.parameters['internal_handler'] = runspec.spec.handler
         runspec.spec.handler = 'run_mlrun_databricks_job'
-        print(f'pre run handler: {runspec.spec.handler}')
 
     def run(
             self,
@@ -458,10 +457,6 @@ class DatabricksRuntime(KubejobRuntime):
             notifications: Optional[List[mlrun.model.Notification]] = None,
             returns: Optional[List[Union[str, Dict[str, str]]]] = None,
     ) -> RunObject:
-        if isinstance(runspec, RunObject):
-            print(f"runspec: {runspec.to_dict()}")
-        else:
-            print(f"runspec: {runspec}")
         if not project:
             current_project = mlrun.get_current_project(silent=True)
             if current_project:

6d9d383a319a51126c55c0817267a15df26ad9e0
Eyal-Danieli <eyald@iguazio.com>
2023-07-31 13:49:48 +0300

    fix lint

--- mlrun/model_monitoring/api.py
+++ mlrun/model_monitoring/api.py
@@ -209,12 +209,7 @@ def _generate_model_endpoint(
                 "Please provide either a function name or a valid MLRun context"
             )
         # Get the function hash from the provided context
-        (
-            _,
-            _,
-            _,
-            function_name,
-        ) = mlrun.common.helpers.parse_versioned_object_uri(
+        (_, _, _, function_name,) = mlrun.common.helpers.parse_versioned_object_uri(
             context.to_dict()["spec"]["function"]
         )
 

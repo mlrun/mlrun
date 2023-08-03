84d36521238fbfc049f61db406ec420f343ff1ff
alonmr <alonnmaor@gmail.com>
2023-08-03 11:53:17 +0300

    lint

--- tests/package/test_context_handler.py
+++ tests/package/test_context_handler.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import sys
-import unittest.mock
 from types import FunctionType
 
 import pytest

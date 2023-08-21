b510f56c748b6c7159efedd1ee437d325cfcae98
Gal Topper <gal.topper@gmail.com>
2023-08-21 21:45:50 +0800

    [Requirements] Bump storey to 1.5.4 (#4129)

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -94,7 +94,7 @@ def test_requirement_specifiers_convention():
         # See comment near requirement for why we're limiting to patch changes only for all of these
         "kfp": {"~=1.8.0, <1.8.14"},
         "aiobotocore": {">=2.4.2,<2.6"},
-        "storey": {"~=1.5.3"},
+        "storey": {"~=1.5.4"},
         "nuclio-sdk": {">=0.3.0"},
         "bokeh": {"~=2.4, >=2.4.2"},
         # protobuf is limited just for docs

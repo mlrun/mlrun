3f5fbf07f5fecea711a7f3d1be9e18c77f17f241
Jonathan Daniel <jonathan_daniel@mckinsey.com>
2023-09-04 16:48:10 +0300

    Add the writer logic
    No CE version yet, only EE

--- mlrun/model_monitoring/writer.py
+++ mlrun/model_monitoring/writer.py
@@ -11,27 +11,107 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-import mlrun.common.schemas
+
+import os
+from typing import Any, NewType, Tuple
+
+import pandas as pd
+from v3io_frames.client import ClientBase
+from v3io_frames.errors import Error as V3IOFramesError
+
+import mlrun.common.model_monitoring
+import mlrun.model_monitoring
+import mlrun.utils.v3io_clients
+from mlrun.common.schemas.model_monitoring import FileTargetKind
+from mlrun.common.schemas.model_monitoring.constants import WriterEvent
 from mlrun.serving.utils import StepToDict
+from mlrun.utils import logger
+
+_V3IO_ACCESS_KEY_NAME = "V3IO_ACCESS_KEY"
+
+RawEvent = dict[str, Any]
+AppResultEvent = NewType("AppResultEvent", RawEvent)
 
 
 class ModelMonitoringWriter(StepToDict):
-    """DEMO WRITER TODO"""
+    """
+    Write monitoring app events to V3IO KV storage
+    """
 
     kind = "monitoring_application_stream_pusher"
 
     def __init__(self, project: str) -> None:
         self.project = project
+        self._kv_db = self._get_kv_db()
 
-    def do(self, event):
+        self._v3io_access_key = os.getenv(_V3IO_ACCESS_KEY_NAME)
+        self._tsdb_path, self._frames_client = self._get_v3io_frames_client()
 
-        print(
-            f"endpoint_uid ={event[mlrun.common.schemas.model_monitoring.constants.WriterEvent.ENDPOINT_ID]}, \n"
-            f"app_name = {event[mlrun.common.schemas.model_monitoring.constants.WriterEvent.APPLICATION_NAME]}, \n"
-            f"schedule_time = {event[mlrun.common.schemas.model_monitoring.constants.WriterEvent.SCHEDULE_TIME]}, \n"
-            f"result_name ={event[mlrun.common.schemas.model_monitoring.constants.WriterEvent.RESULT_NAME]}, \n"
-            f"result_value ={event[mlrun.common.schemas.model_monitoring.constants.WriterEvent.RESULT_VALUE]}, \n"
-            f"my_name = {self.project}."
+    def _get_kv_db(self) -> mlrun.model_monitoring.stores.ModelEndpointStore:
+        return mlrun.model_monitoring.get_model_endpoint_store(project=self.project)
+
+    def _get_v3io_frames_client(self) -> Tuple[str, ClientBase]:
+        tsdb_path = mlrun.mlconf.get_model_monitoring_file_target_path(
+            project=self.project,
+            kind=FileTargetKind.EVENTS,
         )
-        return event
+        (
+            _,
+            tsdb_container,
+            _,
+        ) = mlrun.common.model_monitoring.helpers.parse_model_endpoint_store_prefix(
+            tsdb_path
+        )
+        return tsdb_path, mlrun.utils.v3io_clients.get_frames_client(
+            address=mlrun.mlconf.v3io_framesd,
+            container=tsdb_container,
+            token=self._v3io_access_key,
+        )
+
+    def _update_kv_db(self, event: AppResultEvent) -> None:
+        event = AppResultEvent(event.copy())
+        endpoint_id = event.pop(WriterEvent.ENDPOINT_ID)
+        self._kv_db.update_model_endpoint(endpoint_id=endpoint_id, attributes=event)
+
+    def _update_tsdb(self, event: AppResultEvent) -> None:
+        try:
+            self._frames_client.write(
+                backend="tsdb",
+                table=self._tsdb_path,
+                dfs=pd.DataFrame.from_records([event]),
+                index_cols=[
+                    WriterEvent.SCHEDULE_TIME,
+                    WriterEvent.ENDPOINT_ID,
+                    WriterEvent.APPLICATION_NAME,
+                ],
+            )
+        except V3IOFramesError as err:
+            logger.warn(
+                "Could not write drift measures to TSDB",
+                err=err,
+                tsdb_path=self._tsdb_path,
+                endpoint=event[WriterEvent.ENDPOINT_ID],
+            )
+
+    @staticmethod
+    def _reconstruct_event(event: RawEvent) -> AppResultEvent:
+        return AppResultEvent(
+            {
+                key: event[key]
+                for key in (
+                    WriterEvent.ENDPOINT_ID,
+                    WriterEvent.SCHEDULE_TIME,
+                    WriterEvent.APPLICATION_NAME,
+                    WriterEvent.RESULT_NAME,
+                    WriterEvent.RESULT_KIND,
+                    WriterEvent.RESULT_VALUE,
+                    WriterEvent.RESULT_STATUS,
+                    WriterEvent.RESULT_EXTRA_DATA,
+                )
+            }
+        )
+
+    def do(self, event: RawEvent) -> None:
+        event = self._reconstruct_event(event)
+        self._update_tsdb(event)
+        self._update_kv_db(event)

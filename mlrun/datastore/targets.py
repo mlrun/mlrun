bb6e7af2b3d2f718cfee41e0cd55bb1c1a38e2e9
David_Schneider <davesh0812@gmail.com>
2023-09-21 15:54:29 +0300

    lint

--- mlrun/datastore/targets.py
+++ mlrun/datastore/targets.py
@@ -484,7 +484,9 @@ class BaseStoreTarget(DataTargetBase):
         if hasattr(df, "rdd"):
             options = self.get_spark_options(key_column, timestamp_key)
             options.update(kwargs)
-            df_to_write = self.prepare_spark_df(df.select("*"), key_column, timestamp_key, options)
+            df_to_write = self.prepare_spark_df(
+                df.select("*"), key_column, timestamp_key, options
+            )
             df_to_write.write.mode("overwrite").save(**options)
         elif hasattr(df, "dask"):
             dask_options = self.get_dask_options()

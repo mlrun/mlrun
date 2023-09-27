dc81afa0d70a97f7b09720cd56af772e707d057b
alonmr <alonnmaor@gmail.com>
2023-09-27 17:19:58 +0300

    fix another import

--- server/api/migrations_mysql/env.py
+++ server/api/migrations_mysql/env.py
@@ -15,10 +15,10 @@
 from logging.config import fileConfig
 
 from alembic import context
-from db.sqldb import models
 from sqlalchemy import engine_from_config, pool
 
 from mlrun import mlconf
+from server.api.db.sqldb import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.

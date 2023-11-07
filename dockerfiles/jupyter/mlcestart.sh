9568646306717c5f9128105fd382fea0e9ca0c11
Eliyahu Noach <eliyahunoach77@gmail.com>
2023-11-07 14:26:10 +0200

    add jupyter_data_persistent support in mlrun ce

---
+++ dockerfiles/jupyter/mlcestart.sh
@@ -0,0 +1,16 @@
+file_path="${HOME}/.intdata"
+
+if [ -f "$file_path" ]; then
+    echo "base data exists."
+    # Perform actions when the file exists
+else
+    echo "base data does not exist."
+    echo "1" > $file_path
+    echo "base data exists."
+    cd / ; tar -xvf /tmp/basehome.tar
+    # Perform actions when the file does not exist
+fi
+
+cd ${HOME}
+
+start-notebook.sh  --ip=0.0.0.0 --port=8888 --NotebookApp.token="" --NotebookApp.password="" --NotebookApp.default_url=/lab

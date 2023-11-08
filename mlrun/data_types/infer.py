mlrun/mlrun

commit d645f872d3e5014189fe5b2423b8492966208877

[Data types] Fix warnings on `ingest` when pandas>=2 (#4525)
* [Data types] Fix warnings on `ingest` when pandas>=2

[ML-4853](https://jira.iguazeng.com/browse/ML-4853)

By raising pyarrow upper bound and replacing `is_numeric` with `is_numeric_dtype`.

* Update comment

---------

Co-authored-by: Gal Topper <galt@iguazio.com>

mlrun/data_types/infer.py
@@ -52,7 +52,7 @@ def infer_schema_from_df(
             entities[name] = {"name": name, "value_type": value_type}
 
     # remove index column if no name provided
-    if not df.index.name and df.index.is_numeric():
+    if not df.index.name and pd.api.types.is_numeric_dtype(df):
         df = df.reset_index().drop("index", axis=1)
 
     schema = pyarrow.Schema.from_pandas(df)

tests/test_requirements.py
@@ -98,7 +98,7 @@ def test_requirement_specifiers_convention():
         # See comment near requirement for why we're limiting to patch changes only for all of these
         "kfp": {"~=1.8.0, <1.8.14"},
         "aiobotocore": {">=2.4.2,<2.6"},
-        "storey": {"~=1.6.2"},
+        "storey": {"~=1.6.3"},
         "nuclio-sdk": {">=0.3.0"},
         "bokeh": {"~=2.4, >=2.4.2"},
         # protobuf is limited just for docs
@@ -127,7 +127,7 @@ def test_requirement_specifiers_convention():
         "numpy": {">=1.16.5, <1.23.0"},
         "boto3": {">=1.24.59,<1.27"},
         "dask-ml": {"~=1.4,<1.9.0"},
-        "pyarrow": {">=10.0, <12"},
+        "pyarrow": {">=10.0, <13"},
         "nbclassic": {">=0.2.8"},
         "pandas": {">=1.2, <3"},
         "gitpython": {"~=3.1, >= 3.1.30"},

commit b5bafd3d914468d7efd3fb1d5e80a8065522e814

[Data types] Fix schema inference of df with a numeric index
Fix issue introduced in #4525.

[ML-4936](https://jira.iguazeng.com/browse/ML-4936)
[ML-4939](https://jira.iguazeng.com/browse/ML-4939)

mlrun/data_types/infer.py
@@ -52,7 +52,7 @@ def infer_schema_from_df(
             entities[name] = {"name": name, "value_type": value_type}
 
     # remove index column if no name provided
-    if not df.index.name and pd.api.types.is_numeric_dtype(df):
+    if not df.index.name and pd.api.types.is_numeric_dtype(df.index):
         df = df.reset_index().drop("index", axis=1)
 
     schema = pyarrow.Schema.from_pandas(df)
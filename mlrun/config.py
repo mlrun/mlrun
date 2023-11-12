cbeef862b466384b09036167c2eafd54b59c4419
Gal Topper <gal.topper@gmail.com>
2023-11-12 20:03:21 +0800

    [Feature Store] Add default path for `CSVTarget`
    [ML-4910](https://jira.iguazeng.com/browse/ML-4910)

--- mlrun/config.py
+++ mlrun/config.py
@@ -504,6 +504,7 @@ default_config = {
             "nosql": "v3io:///projects/{project}/FeatureStore/{name}/{kind}",
             # "authority" is optional and generalizes [userinfo "@"] host [":" port]
             "redisnosql": "redis://{authority}/projects/{project}/FeatureStore/{name}/{kind}",
+            "csv": "v3io:///projects/{project}/FeatureStore/{name}/{kind}",
         },
         "default_targets": "parquet,nosql",
         "default_job_image": "mlrun/mlrun",

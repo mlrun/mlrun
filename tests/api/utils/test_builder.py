91b458e2fd45b38f3207723d273c2b30310bec48
Alon Maor <48641682+alonmr@users.noreply.github.com>
2023-11-27 17:16:52 +0200

    [Builder] Add mlrun requirement when installing PyPs on mlrun base image (#4623)

--- tests/api/utils/test_builder.py
+++ tests/api/utils/test_builder.py
@@ -480,6 +480,7 @@ def test_build_runtime_ecr_with_ec2_iam_policy(monkeypatch):
     server.api.utils.builder.build_runtime(
         mlrun.common.schemas.AuthInfo(),
         function,
+        force_build=True,
     )
     pod_spec = _create_pod_mock_pod_spec()
     assert {"name": "AWS_SDK_LOAD_CONFIG", "value": "true", "value_from": None} in [
@@ -543,6 +544,7 @@ def test_build_runtime_resolve_ecr_registry(monkeypatch):
         server.api.utils.builder.build_runtime(
             mlrun.common.schemas.AuthInfo(),
             function,
+            force_build=True,
         )
         pod_spec = _create_pod_mock_pod_spec()
         for init_container in pod_spec.init_containers:
@@ -715,6 +717,7 @@ def test_kaniko_pod_spec_default_service_account_enrichment(monkeypatch):
     server.api.utils.builder.build_runtime(
         mlrun.common.schemas.AuthInfo(),
         function,
+        force_build=True,
     )
     pod_spec = _create_pod_mock_pod_spec()
     assert pod_spec.service_account == service_account
@@ -739,6 +742,7 @@ def test_kaniko_pod_spec_user_service_account_enrichment(monkeypatch):
     server.api.utils.builder.build_runtime(
         mlrun.common.schemas.AuthInfo(),
         function,
+        force_build=True,
     )
     pod_spec = _create_pod_mock_pod_spec()
     assert pod_spec.service_account == service_account
@@ -931,6 +935,198 @@ def test_resolve_build_requirements(
     assert commands == expected_commands
     assert requirements_list == expected_requirements_list
     assert requirements_path == expected_requirements_path
+
+
+@pytest.mark.parametrize(
+    "mlrun_version_specifier, base_image, client_version, expected_mlrun_version",
+    [
+        (
+            None,
+            "mlrun/mlrun",
+            "1.4.0",
+            "==1.4.0",
+        ),
+        (
+            None,
+            "registry:80/quay.io/mlrun/mlrun:1.6.0-rc7",
+            "1.6.0-rc7",
+            "==1.6.0-rc7",
+        ),
+        (
+            None,
+            "",
+            "1.6.0-rc7",
+            "==1.6.0-rc7",
+        ),
+        (
+            None,
+            "mlrun/mlrun:1.5.2",
+            "1.4.0",
+            "==1.5.2",
+        ),
+        (
+            None,
+            "mlrun/mlrun:1.5.2-rc10",
+            None,
+            "==1.5.2-rc10",
+        ),
+        (
+            None,
+            "mlrun/ml-base:1.5.1",
+            "1.4.0",
+            "==1.5.1",
+        ),
+        (
+            None,
+            "somewhere/mlrun/ml-base:1.5.1",
+            "1.4.0",
+            "==1.5.1",
+        ),
+        (
+            None,
+            "not-an-mlrun/image:1.5.1",
+            "1.4.0",
+            "==1.4.0",
+        ),
+        (
+            "mlrun[complete]==1.6.0",
+            "not-an-mlrun/image:1.5.1",
+            "1.4.0",
+            "",
+        ),
+        (
+            None,
+            "mlrun/mlrun:unstable",
+            "1.4.0",
+            None,
+        ),
+        (
+            None,
+            "mlrun/mlrun",
+            "0.0.0+unstable",
+            None,
+        ),
+        (
+            None,
+            "mlrun/mlrun:1.4.0-rc5-feature",
+            "1.4.0",
+            None,
+        ),
+        (
+            "mlrun~=1.5.0",
+            "mlrun/mlrun:1.6.0",
+            "1.4.0",
+            "",
+        ),
+        (
+            "mlrun==1.5.0",
+            "mlrun/mlrun:not-semver",
+            "1.4.0",
+            "",
+        ),
+        (
+            "mlrun==1.5.0",
+            "mlrun/mlrun",
+            "1.4.0",
+            "",
+        ),
+        (
+            "mlrun==1.5.0",
+            "mlrun/mlrun",
+            None,
+            "",
+        ),
+        (
+            "mlrun==1.5.0",
+            "",
+            "1.4.0",
+            "",
+        ),
+        (
+            "mlrun==1.5.0",
+            "mlrun/mlrun",
+            "unstable",
+            "",
+        ),
+        (
+            "mlrun==1.5.0",
+            "mlrun/mlrun:unstable",
+            "unstable",
+            "",
+        ),
+    ],
+)
+def test_mlrun_base_image_with_requirements(
+    monkeypatch,
+    mlrun_version_specifier,
+    base_image,
+    client_version,
+    expected_mlrun_version,
+):
+    docker_registry = "default.docker.registry/default-repository"
+    config.httpdb.builder.docker_registry = docker_registry
+    _patch_k8s_helper(monkeypatch)
+
+    with unittest.mock.patch(
+        "server.api.utils.builder.make_kaniko_pod", new=unittest.mock.MagicMock()
+    ):
+        function = mlrun.new_function(
+            "some-function",
+            "some-project",
+            "some-tag",
+            kind="job",
+            requirements=["some-package"],
+        )
+        function.spec.build.base_image = base_image
+
+        server.api.utils.builder.build_runtime(
+            mlrun.common.schemas.AuthInfo(),
+            function,
+            client_version=client_version,
+            mlrun_version_specifier=mlrun_version_specifier,
+        )
+
+        requirements = server.api.utils.builder.make_kaniko_pod.call_args[1][
+            "requirements"
+        ]
+        if expected_mlrun_version is None:
+            assert requirements == [
+                "some-package",
+            ]
+        elif mlrun_version_specifier:
+            assert requirements == [
+                mlrun_version_specifier,
+                "some-package",
+            ]
+        else:
+            assert requirements == [
+                f"mlrun[complete]{expected_mlrun_version}",
+                "some-package",
+            ]
+
+
+def test_mlrun_base_image_no_requirements():
+    with unittest.mock.patch(
+        "server.api.utils.builder.build_image", new=unittest.mock.MagicMock()
+    ):
+        function = mlrun.new_function(
+            "some-function",
+            "some-project",
+            "some-tag",
+            kind="job",
+            source="some-source.zip",
+        )
+        function.spec.build.base_image = "mlrun/mlrun:1.6.0"
+
+        server.api.utils.builder.build_runtime(
+            mlrun.common.schemas.AuthInfo(),
+            function,
+        )
+
+        requirements = server.api.utils.builder.build_image.call_args[1]["requirements"]
+        with_mlrun = server.api.utils.builder.build_image.call_args[1]["with_mlrun"]
+        assert requirements == []
+        assert with_mlrun is False
 
 
 def _get_target_image_from_create_pod_mock():

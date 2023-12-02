7b98b41ec1c9b458b3e158f6dc4ada526b1babb8
alonmr <alonnmaor@gmail.com>
2023-12-02 11:55:15 +0200

    maybe now

--- tests/api/crud/test_runs.py
+++ tests/api/crud/test_runs.py
@@ -22,56 +22,58 @@ import mlrun.errors
 import server.api.crud
 import server.api.runtime_handlers
 import server.api.utils.singletons.k8s
+import tests.api.conftest
 
 
-def test_delete_runs_with_resources(db: sqlalchemy.orm.Session):
-    project = "project-name"
-    server.api.crud.Runs().store_run(
-        db,
-        {
-            "metadata": {
-                "name": "run-name",
-                "labels": {
-                    "kind": "job",
+class TestRuns(tests.api.conftest.MockedK8sHelper):
+    def test_delete_runs_with_resources(self, db: sqlalchemy.orm.Session):
+        project = "project-name"
+        server.api.crud.Runs().store_run(
+            db,
+            {
+                "metadata": {
+                    "name": "run-name",
+                    "labels": {
+                        "kind": "job",
+                    },
                 },
             },
-        },
-        "uid",
-        project=project,
-    )
-    run = server.api.crud.Runs().get_run(db, "uid", 0, project)
-    assert run["metadata"]["name"] == "run-name"
+            "uid",
+            project=project,
+        )
+        run = server.api.crud.Runs().get_run(db, "uid", 0, project)
+        assert run["metadata"]["name"] == "run-name"
 
-    k8s_helper = server.api.utils.singletons.k8s.get_k8s_helper()
-    with unittest.mock.patch.object(k8s_helper, "v1api"), unittest.mock.patch.object(
-        k8s_helper.v1api, "delete_namespaced_pod"
-    ) as delete_namespaced_pod_mock, unittest.mock.patch.object(
-        k8s_helper.v1api,
-        "list_namespaced_pod",
-        side_effect=[
-            k8s_client.V1PodList(
-                items=[
-                    k8s_client.V1Pod(
-                        metadata=k8s_client.V1ObjectMeta(
-                            name="pod-name",
-                            labels={
-                                "mlrun/class": "job",
-                                "mlrun/project": project,
-                                "mlrun/uid": "uid",
-                            },
-                        ),
-                        status=k8s_client.V1PodStatus(phase="Running"),
-                    )
-                ]
-            ),
-            # 2nd time for waiting for pod to be deleted
-            k8s_client.V1PodList(items=[]),
-        ],
-    ), unittest.mock.patch.object(
-        server.api.runtime_handlers.BaseRuntimeHandler, "_ensure_run_logs_collected"
-    ):
-        server.api.crud.Runs().delete_run(db, "uid", 0, project)
-        delete_namespaced_pod_mock.assert_called_once()
+        k8s_helper = server.api.utils.singletons.k8s.get_k8s_helper()
+        with unittest.mock.patch.object(
+            k8s_helper.v1api, "delete_namespaced_pod"
+        ) as delete_namespaced_pod_mock, unittest.mock.patch.object(
+            k8s_helper.v1api,
+            "list_namespaced_pod",
+            side_effect=[
+                k8s_client.V1PodList(
+                    items=[
+                        k8s_client.V1Pod(
+                            metadata=k8s_client.V1ObjectMeta(
+                                name="pod-name",
+                                labels={
+                                    "mlrun/class": "job",
+                                    "mlrun/project": project,
+                                    "mlrun/uid": "uid",
+                                },
+                            ),
+                            status=k8s_client.V1PodStatus(phase="Running"),
+                        )
+                    ]
+                ),
+                # 2nd time for waiting for pod to be deleted
+                k8s_client.V1PodList(items=[]),
+            ],
+        ), unittest.mock.patch.object(
+            server.api.runtime_handlers.BaseRuntimeHandler, "_ensure_run_logs_collected"
+        ):
+            server.api.crud.Runs().delete_run(db, "uid", 0, project)
+            delete_namespaced_pod_mock.assert_called_once()
 
-    with pytest.raises(mlrun.errors.MLRunNotFoundError):
-        server.api.crud.Runs().get_run(db, "uid", 0, project)
+        with pytest.raises(mlrun.errors.MLRunNotFoundError):
+            server.api.crud.Runs().get_run(db, "uid", 0, project)

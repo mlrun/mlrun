6031ca54602811a8ae7e9ffa610d88710bbe695a
Peng Wei <pengwei715@gmail.com>
2023-12-04 09:25:31 -0800

    adding both LLM as judge and the bleu and rouge sore to the metrcs

--- mlrun/model_monitoring/genai/metrics_base.py
+++ mlrun/model_monitoring/genai/metrics_base.py
@@ -21,7 +21,10 @@ from typing import Union, List, Optional, Dict, Any
 """
 @misc{zheng2023judging,
       title={Judging LLM-as-a-judge with MT-Bench and Chatbot Arena},
-      author={Lianmin Zheng and Wei-Lin Chiang and Ying Sheng and Siyuan Zhuang and Zhanghao Wu and Yonghao Zhuang and Zi Lin and Zhuohan Li and Dacheng Li and Eric. P Xing and Hao Zhang and Joseph E. Gonzalez and Ion Stoica},
+      author={Lianmin Zheng and Wei-Lin Chiang and Ying Sheng and Siyuan Zhuang 
+      and Zhanghao Wu and Yonghao Zhuang and Zi Lin and Zhuohan Li 
+      and Dacheng Li and Eric. P Xing and Hao Zhang and Joseph E. Gonzalez 
+      and Ion Stoica},
       year={2023},
       eprint={2306.05685},
       archivePrefix={arXiv},
@@ -58,7 +61,7 @@ class LlmMetric(object):
         self, name: str, model_config: Dict[str, Any], data_config: Dict[str, Any]
     ):
         """
-        Base class for LLM as a judge metrics
+        Base class for LLM as a judge metrics.
         These metrics are used for more open-ended question for the model
         and the algorithm is based on the paper https://arxiv.org/pdf/2306.05685.pdf
         """

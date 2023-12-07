f3096e6f805fe325c9d23d5140173d4884ab0484
Tomer Mamia <tomerm@iguazio.com>
2023-12-07 15:14:58 +0200

    rename var

--- mlrun/runtimes/databricks_job/databricks_runtime.py
+++ mlrun/runtimes/databricks_job/databricks_runtime.py
@@ -186,8 +186,8 @@ mlrun_logger.setLevel(logging.DEBUG)
 
 mlrun_console_handler = logging.StreamHandler()
 mlrun_console_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-mlrun_console_handler.setFormatter(formatter)
+mlrun_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+mlrun_console_handler.setFormatter(mlrun_formatter)
 mlrun_logger.addHandler(mlrun_console_handler)
 """
 

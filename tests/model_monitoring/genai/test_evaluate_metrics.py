e69c14dd9a351cc1b2017eb8aff66e64049be0c8
Peng Wei <pengwei715@gmail.com>
2023-12-11 10:44:44 -0800

    added the simple test cases for these metrics

--- tests/model_monitoring/genai/test_evaluate_metrics.py
+++ tests/model_monitoring/genai/test_evaluate_metrics.py
@@ -1,35 +1,39 @@
 import pytest
 from mlrun.model_monitoring.genai.metrics_base import LLMEvaluateMetric
 
+
 def test_bleu_metric():
     metric = LLMBaseMetric(name="bleu")
-    predictions = ["the cat is on the mat"] 
+    predictions = ["the cat is on the mat"]
     references = ["the cat is playing on the mat"]
     score = metric.compute_over_data(predictions, references)
     assert score > 0
     assert score <= 1
+
 
 def test_rouge_metric():
     metric = LLMBaseMetric(name="rouge")
     predictions = ["cat on the mat"]
     references = ["the cat is playing on the blue mat"]
     score = metric.compute_over_data(predictions, references)
-    assert score > 0 
+    assert score > 0
     assert score <= 1
-    
+
+
 def test_invalid_metric():
     with pytest.raises(ImportError):
         LLMBaseMetric(name="invalid")
-        
+
+
 def test_bleu_wrong_inputs():
     metric = LLMBaseMetric(name="bleu")
-    
+
     # Wrong types
     with pytest.raises(TypeError):
         metric.compute_over_data("text", 5)
-        
+
     # Different cardinality
     with pytest.raises(ValueError):
         predictions = ["hi"]
-        references = ["1", "2"] 
+        references = ["1", "2"]
         metric.compute_over_data(predictions, references)
---
+++ tests/model_monitoring/genai/test_llm_judge_metrics.py
@@ -0,0 +1,65 @@
+import pytest
+from mlrun.model_monitoring.genai.metrics_base import (
+    LLMJudgeSingleGrading,
+    LLMJudgePairwiseGrading,
+    LLMJudgeReferenceGrading,
+)
+
+JUDGE_MODEL = "TheBloke/Mistral-7B-OpenOrca-GPTQ"
+JUDGE_CONFIG = {"max_length": 300}
+
+
+@pytest.fixture
+def single_grading():
+    prompt_template = SINGLE_GRADE_PROMPT
+    prompt_config = {"name": "accuracy"}
+    return LLMJudgeSingleGrading(
+        model_judge=JUDGE_MODEL,
+        model_judge_config=JUDGE_CONFIG,
+        prompt_template=prompt_template,
+        prompt_config=prompt_config,
+    )
+
+
+def test_single_grading_score(single_grading):
+    q = "What is 2 + 2?"
+    a = "2 + 2 equals 4"
+    result = single_grading.compute_over_one_data(q, a)
+    assert 0 <= result["score"] <= 10
+
+
+def test_pairwise_grading_scores():
+    prompt_template = PAIR_GRADE_PROMPT
+    prompt_config = {}
+
+    metric = LLMJudgePairwiseGrading(
+        model_judge=JUDGE_MODEL,
+        prompt_template=prompt_template,
+        prompt_config=prompt_config,
+    )
+
+    q = "What is the capital of France?"
+    a1 = "The capital of France is Paris"
+    a2 = "France's capital city is Lyon"
+
+    result = metric.compute_over_one_data(q, a1, a2)
+    scores = metric.extract_score_and_explanation(result["response"])
+    assert 0 <= scores["score_of_assistant_a"] <= 10
+    assert 0 <= scores["score_of_assistant_b"] <= 10
+
+
+def test_reference_grading_scores():
+    metric = LLMJudgeReferenceGrading(
+        model_judge=JUDGE_MODEL, prompt_template=REF_GRADE_PROMPT, prompt_config={}
+    )
+
+    q = "Who wrote Hamlet?"
+    a1 = "Hamlet was written by Charles Dickens"
+    a2 = "William Shakespeare wrote the play Hamlet"
+    ref = "The author of the play Hamlet is William Shakespeare"
+
+    result = metric.compute_over_one_data(q, a1, a2, ref)
+    scores = metric.extract_score_and_explanation(result["response"])
+
+    assert 0 <= scores["score_of_assistant_a"] <= 10
+    assert 0 <= scores["score_of_assistant_b"] <= 10

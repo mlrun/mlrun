a15424107ade6635082c0bb0852f31e1858d65b5
Peng Wei <pengwei715@gmail.com>
2023-12-11 17:37:45 -0800

    should run

--- tests/model_monitoring/genai/test_llm_judge_metrics.py
+++ tests/model_monitoring/genai/test_llm_judge_metrics.py
@@ -102,7 +102,7 @@ def test_pairwise_grading_scores(prompt_fixture):
     a2 = "France's capital city is Lyon"
 
     result = metric.compute_over_one_data(q, a1)
-    scores = metric.extract_score_and_explanation(result["response"])
+    scores = metric.extract_score_explanation(result["response"])
     assert 0 <= scores["score_of_assistant_a"] <= 5
     assert 0 <= scores["score_of_assistant_b"] <= 5
 
@@ -130,7 +130,7 @@ def test_reference_grading_scores(prompt_fixture):
     ref = "The author of the play Hamlet is William Shakespeare"
 
     result = metric.compute_over_one_data(q, a1, ref)
-    scores = metric.extract_score_and_explanation(result["response"])
+    scores = metric.extract_score_explanation(result["response"])
 
     assert 0 <= scores["score_of_assistant_a"] <= 5
     assert 0 <= scores["score_of_assistant_b"] <= 5

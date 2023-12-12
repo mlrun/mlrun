a03dc4c6a3dcfae655e2445ef2bf7e5868ee3557
Peng Wei <pengwei715@gmail.com>
2023-12-12 13:14:03 -0800

    should pass the tests

--- tests/model_monitoring/genai/test_llm_judge_metrics.py
+++ tests/model_monitoring/genai/test_llm_judge_metrics.py
@@ -94,9 +94,9 @@ def test_pairwise_grading_scores(prompt_fixture):
     metric = LLMJudgePairwiseGrading(
         name="accuracy_metrics",
         model_judge=JUDGE_MODEL,
+        tokenizer_judge_config=TOKENIZER_JUDGE_CONFIG,
         model_judge_config=JUDGE_CONFIG,
         model_judge_infer_config=JUDGE_INFER_CONFIG,
-        tokenizer_judge_config=TOKENIZER_JUDGE_CONFIG,
         model_bench_mark=BENCHMARK_MODEL,
         model_bench_mark_config=BENCHMARK_CONFIG,
         model_bench_mark_infer_config=BENCHMARK_INFER_CONFIG,
@@ -120,9 +120,9 @@ def test_reference_grading_scores(prompt_fixture):
     metric = LLMJudgeReferenceGrading(
         name="accuracy_metrics",
         model_judge=JUDGE_MODEL,
+        tokenizer_judge_config=TOKENIZER_JUDGE_CONFIG,
         model_judge_config=JUDGE_CONFIG,
         model_judge_infer_config=JUDGE_INFER_CONFIG,
-        tokenizer_judge_config=TOKENIZER_JUDGE_CONFIG,
         model_bench_mark=BENCHMARK_MODEL,
         model_bench_mark_config=BENCHMARK_CONFIG,
         model_bench_mark_infer_config=BENCHMARK_INFER_CONFIG,

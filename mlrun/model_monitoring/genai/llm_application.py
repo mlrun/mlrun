7493e79473bbebfa0105a848fabb03e7a970af0a
Peng Wei <pengwei715@gmail.com>
2023-12-13 15:45:17 -0800

    need the logic to plot the radar plot

--- mlrun/model_monitoring/genai/llm_application.py
+++ mlrun/model_monitoring/genai/llm_application.py
@@ -24,6 +24,8 @@
 
 
 import pandas as pd
+from abc import ABC, abstractmethod
+from statistics import mean, median
 from typing import List, Optional, Union, Dict
 from mlrun.artifacts import Artifact
 from mlrun.datastore import DataItem
@@ -32,7 +34,69 @@ from mlrun.utils import logger
 from mlrun.model_monitoring.genai.metrics import LLMEvaluateMetric, LLMJudgeBaseMetric
 
 
-class LLMMonitoringApp(ModelObj):
+class MonitoringAppResult:
+    def __init__(
+        self,
+        app_name: str,
+        tsdb_record: Dict[str, float],
+        drift_result: Tuple[DriftStatus, float, DriftKind] = None,
+    ):
+        self.app_name = app_name
+        self.tsdb_record = tsdb_record.update({"record_type": "drift_measures"})
+        self.drift_result = drift_result
+
+
+class DriftStatus(Enum):
+    """
+    Enum for the drift status values.
+    """
+
+    NO_DRIFT = "NO_DRIFT"
+    DRIFT_DETECTED = "DRIFT_DETECTED"
+    POSSIBLE_DRIFT = "POSSIBLE_DRIFT"
+    NONE = "NONE"
+
+
+class DriftKind(Enum):
+    """
+    Enum for the drift kind.
+    """
+
+    DATA_DRIFT = "DATA_DRIFT"
+    CONCEPT_DRIFT = "CONCEPT_DRIFT"
+    MODEL_PERFORMANCE_DRIFT = "MODEL_PERFORMANCE_DRIFT"
+    SYSTEM_PERFORMANCE_DRIFT = "SYSTEM_PERFORMANCE_DRIFT"
+
+
+# A type for representing a drift result, a tuple of the status and the drift mean:
+DriftResultType = Tuple[DriftStatus, float, DriftKind]
+
+
+def aggregate(agg_type):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            values = func(*args, **kwargs)
+
+            if not isinstance(values, list) or not all(
+                isinstance(x, (int, float)) for x in values
+            ):
+                raise TypeError("Function must return a list of numbers")
+
+            if agg_type == "mean":
+                agg_value = mean(values)
+            elif agg_type == "median":
+                agg_value = median(values)
+            else:
+                raise ValueError("Invalid aggregation type")
+
+            return agg_value
+
+        return wrapper
+
+    return decorator
+
+
+class LLMMonitoringApp(ModelObj, ABC):
     kind = "LLM_monitoring_app"
 
     def __init__(
@@ -42,7 +106,10 @@ class LLMMonitoringApp(ModelObj):
         possible_drift_threshold: float = None,
         obvious_drift_threshold: float = None,
     ):
-        pass
+        self.name = name
+        self.metrics = ObjectList.from_list(metrics)
+        self.possible_drift_threshold = possible_drift_threshold
+        self.obvious_drift_threshold = obvious_drift_threshold
 
     def do(
         self,
@@ -64,17 +131,43 @@ class LLMMonitoringApp(ModelObj):
         self._metrics = ObjectList.from_list(metrics)
 
     def compute_metrics_over_data(
-        self, train_df: pd.DataFrame, sample_df: pd.DataFrame, metrics_names: List[str]
-    ) -> Dict[str, float]:
+        self, sample_df: pd.DataFrame, train_df: pd.DataFrame = None
+    ) -> Dict[str, Any]:
         """
         Calculate metrics values - helper for the user .
         """
+        for metric in self.metrics:
+            res[metric.name] = metric.compute_over_data(sample_df, train_df)
 
-        pass
+    @aggregate("mean")
+    def compute_one_metric_over_data(
+        self,
+        metric: Union[LLMEvaluateMetric, LLMJudgeBaseMetric],
+        sample_df: pd.DataFrame,
+        train_df: pd.DataFrame = None,
+    ) -> List[Union[int, float]]:
+        """
+        Calculate one metric value - helper for the user .
+        """
+        sample_questions = sample_df["question"].tolist()
+        sample_answers = sample_df["answer"].tolist()
+        res = []
+        for i in range(len(sample_questions)):
+            res.append(
+                metric.compute_one_metric_over_data(
+                    sample_questions[i], sample_answers[i], train_df
+                )
+            )
+        return res
 
-    def radar_chart(self, metrics_names: List[str], metrics_values: List[float]):
+    def build_radar_chart(self, metrics_res: Dict[str, Union[float, int]]):
         """
         Create a radar chart for the metrics values.
         """
 
         pass
+
+    def build_report(self, metrics_names: List[str], metrics_values: List[float]):
+        """
+        Create a report for the metrics values.
+        """
--- tests/model_monitoring/genai/test_llm_application.py
+++ tests/model_monitoring/genai/test_llm_application.py
@@ -13,52 +13,6 @@
 # limitations under the License.
 #
 import pytest
-from unittest.mock import MagicMock, patch
 import pandas as pd
 from mlrun.model_monitoring.llm_application import LLMMonitoringApp
 from mlrun.model_monitoring.metrics import LLMEvaluateMetric, LLMJudgeBaseMetric
-
-
-@pytest.fixture
-def mock_metrics():
-    return [MagicMock(spec=LLMEvaluateMetric), MagicMock(spec=LLMJudgeBaseMetric)]
-
-
-@pytest.fixture
-def mock_dataframe():
-    return pd.DataFrame({"column1": [1, 2, 3], "column2": [4, 5, 6]})
-
-
-@pytest.fixture
-def llm_monitoring_app(mock_metrics):
-    return LLMMonitoringApp(name="TestApp", metrics=mock_metrics)
-
-
-def test_initialization(mock_metrics):
-    app = LLMMonitoringApp(name="TestApp", metrics=mock_metrics)
-    assert app.name == "TestApp"
-    assert app.metrics == mock_metrics
-
-
-def test_metrics_getter_setter(llm_monitoring_app, mock_metrics):
-    new_metrics = [MagicMock(spec=LLMEvaluateMetric)]
-    llm_monitoring_app.metrics = new_metrics
-    assert llm_monitoring_app.metrics == new_metrics
-
-
-def test_compute_metrics_over_data(llm_monitoring_app, mock_dataframe):
-    # Assuming compute_metrics_over_data returns a dictionary of metric results
-    metrics_names = ["metric1", "metric2"]
-    results = llm_monitoring_app.compute_metrics_over_data(
-        mock_dataframe, mock_dataframe, metrics_names
-    )
-    assert isinstance(results, dict)
-    # Add more assertions here based on the expected behavior of compute_metrics_over_data
-
-
-def test_radar_chart(llm_monitoring_app):
-    # Assuming radar_chart creates a chart and returns some form of result or output
-    metrics_names = ["metric1", "metric2"]
-    metrics_values = [0.5, 0.75]
-    chart = llm_monitoring_app.radar_chart(metrics_names, metrics_values)
-    # Add assertions here based on the expected behavior of radar_chart

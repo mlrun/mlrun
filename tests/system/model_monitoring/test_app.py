0d4ef3a1d21e520c1c6f95baf0e4b4d0f9514b0d
Jonathan Daniel <36337649+jond01@users.noreply.github.com>
2023-12-14 15:28:34 +0200

    [Model Monitoring] Fix the option to return a list of results (#4735)

--- tests/system/model_monitoring/test_app.py
+++ tests/system/model_monitoring/test_app.py
@@ -136,7 +136,7 @@ class TestMonitoringAppFlow(TestMLRunSystem, _V3IORecordsChecker):
             _AppData(
                 class_=CustomEvidentlyMonitoringApp,
                 rel_path="assets/custom_evidently_app.py",
-                requirements=["evidently~=0.4.7"],
+                requirements=["evidently==0.4.7"],
                 kwargs={
                     "evidently_workspace_path": cls.evidently_workspace_path,
                     "evidently_project_id": cls.evidently_project_id,
@@ -166,7 +166,7 @@ class TestMonitoringAppFlow(TestMLRunSystem, _V3IORecordsChecker):
                     requirements=app_data.requirements,
                     **app_data.kwargs,
                 )
-                executor.submit(self.project.deploy_function, fn)
+                executor.submit(fn.deploy)
 
     def _log_model(self) -> None:
         dataset = load_iris()

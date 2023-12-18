1bfdb23d1ad398b156e681dfaf756ca64fed7472
alonmr <alonnmaor@gmail.com>
2023-12-18 16:03:34 +0200

    [Tracker] Dont commit run if no trackers

--- mlrun/track/tracker_manager.py
+++ mlrun/track/tracker_manager.py
@@ -87,6 +87,9 @@ class TrackerManager(metaclass=Singleton):
         """
         # Check if the context received is a dict to initialize it as an `MLClientCtx` object:
         is_context_dict = isinstance(context, dict)
+        if not self._trackers:
+            return context.to_dict() if is_context_dict else context
+
         if is_context_dict:
             context = MLClientCtx.from_dict(
                 context, include_status=True, store_run=False

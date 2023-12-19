d8ff70bae6fb29aa2e46d7e0a315277c913613db
alonmr <alonnmaor@gmail.com>
2023-12-19 19:27:21 +0200

    :(

--- mlrun/track/tracker_manager.py
+++ mlrun/track/tracker_manager.py
@@ -85,10 +85,10 @@ class TrackerManager(metaclass=Singleton):
 
         :return: The context updated with the trackers products.
         """
-        # Check if the context received is a dict to initialize it as an `MLClientCtx` object:
         if not self._trackers:
             return context
 
+        # Check if the context received is a dict to initialize it as an `MLClientCtx` object:
         is_context_dict = isinstance(context, dict)
         if is_context_dict:
             context = MLClientCtx.from_dict(

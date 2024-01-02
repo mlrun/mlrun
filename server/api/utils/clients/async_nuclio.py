ab5cb79ca1fe72b337909352f44f6ce50fca2c7c
rokatyy <ekaterina_molchanova@mckinsey.com>
2024-01-02 12:21:53 +0000

    replaced POST with PUT method to create api_gateway

--- server/api/utils/clients/async_nuclio.py
+++ server/api/utils/clients/async_nuclio.py
@@ -68,7 +68,7 @@ class Client:
         )
 
         return await self._send_request_to_api(
-            method="POST",
+            method="PUT",
             url=self._nuclio_dashboard_url,
             path=NUCLIO_API_GATEWAYS_ENDPOINT,
             headers=headers,

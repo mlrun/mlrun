dd1a86d8d01cbf9d01c52f6ea6ac40cd38884805
Jonathan Daniel <jonathan_daniel@mckinsey.com>
2024-01-08 17:14:45 +0200

    Local testing
    Both app tests passed:
    ```
    time MLRUN_SYSTEM_TESTS_CLEAN_RESOURCES=false pytest tests/system/model_monitoring/test_app.py --disable-warnings --capture=no
    ```
    Output:
    ```
    ====================================== 2 passed, 58 warnings in 667.49s (0:11:07) ======================================
    MLRUN_SYSTEM_TESTS_CLEAN_RESOURCES=false pytest  --disable-warnings   6.96s user 5.60s system 1% cpu 11:08.43 total
    ```

--- tests/system/model_monitoring/test_app.py
+++ tests/system/model_monitoring/test_app.py
@@ -126,9 +126,9 @@ class _V3IORecordsChecker:
 @TestMLRunSystem.skip_test_if_env_not_configured
 @pytest.mark.enterprise
 class TestMonitoringAppFlow(TestMLRunSystem, _V3IORecordsChecker):
-    project_name = "test-monitoring-app-flow"
+    project_name = "test-monitoring-5407-1"
     # Set image to "<repo>/mlrun:<tag>" for local testing
-    image: typing.Optional[str] = None
+    image: typing.Optional[str] = "jonathandaniel503/mlrun:ml-5407-1"
 
     @classmethod
     def custom_setup_class(cls) -> None:
@@ -266,10 +266,10 @@ class TestMonitoringAppFlow(TestMLRunSystem, _V3IORecordsChecker):
 @TestMLRunSystem.skip_test_if_env_not_configured
 @pytest.mark.enterprise
 class TestRecordResults(TestMLRunSystem, _V3IORecordsChecker):
-    project_name = "test-monitoring-record-results"
+    project_name = "test-monitoring-rec-5407-1"
     name_prefix = "infer-monitoring"
     # Set image to "<repo>/mlrun:<tag>" for local testing
-    image: typing.Optional[str] = None
+    image: typing.Optional[str] = "jonathandaniel503/mlrun:ml-5407-1"
 
     @classmethod
     def custom_setup_class(cls) -> None:

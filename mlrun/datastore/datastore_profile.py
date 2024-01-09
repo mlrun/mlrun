c5f131c8fb411879becd65eb7dc636aece3ae935
alxtkr77 <3098237+alxtkr77@users.noreply.github.com>
2024-01-09 17:04:39 +0200

    [Spark] Add support for GCS datastore profiles (#4859)

--- mlrun/datastore/datastore_profile.py
+++ mlrun/datastore/datastore_profile.py
@@ -224,7 +224,13 @@ class DatastoreProfileGCS(DatastoreProfile):
     type: str = pydantic.Field("gcs")
     _private_attributes = ("gcp_credentials",)
     credentials_path: typing.Optional[str] = None  # path to file.
-    gcp_credentials: typing.Optional[str] = None
+    gcp_credentials: typing.Optional[typing.Union[str, typing.Dict]] = None
+
+    @pydantic.validator("gcp_credentials", pre=True, always=True)
+    def convert_dict_to_json(cls, v):
+        if isinstance(v, dict):
+            return json.dumps(v)
+        return v
 
     def url(self, subpath) -> str:
         if subpath.startswith("/"):

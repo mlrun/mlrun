2f8b4998fc07d4ecb7ed333956ac0ef7bc0d57c0
alonmr <alonnmaor@gmail.com>
2024-01-10 17:34:44 +0200

    [Build] Warn ECR and S3 permissions conflict

--- mlrun/db/httpdb.py
+++ mlrun/db/httpdb.py
@@ -1310,7 +1310,7 @@ class HTTPRunDB(RunDBInterface):
 
     def remote_builder(
         self,
-        func,
+        func: BaseRuntime,
         with_mlrun,
         mlrun_version_specifier=None,
         skip_deployed=False,
@@ -1330,6 +1330,20 @@ class HTTPRunDB(RunDBInterface):
         :param builder_env:   Kaniko builder pod env vars dict (for config/credentials)
         :param force_build:   Force building the image, even when no changes were made
         """
+        is_s3_source = func.spec.build.source and func.spec.build.source.startswith(
+            "s3://"
+        )
+        is_ecr_image = mlrun.utils.is_ecr_url(config.httpdb.builder.docker_registry)
+        if not func.spec.build.load_source_on_run and is_s3_source and is_ecr_image:
+            logger.warning(
+                "Building a function image to ECR and loading an S3 source to the image may require conflicting access "
+                "keys. Only the permissions attached to the platform's configured secret will take affect "
+                "(see mlrun.mlconf..httpdb.builder.docker_registry_secret). "
+                "It is recommended to pull the source at runtime instead",
+                source=func.spec.build.source,
+                load_source_on_run=func.spec.build.load_source_on_run,
+                default_docker_registry=config.httpdb.builder.docker_registry,
+            )
 
         try:
             req = {

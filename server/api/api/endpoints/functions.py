486a5a888cfa2d9fe6cc6519db94b285b6fe9916
alonmr <alonnmaor@gmail.com>
2024-01-17 16:00:22 +0200

    [SDK] Remove deprecated fields

--- server/api/api/endpoints/functions.py
+++ server/api/api/endpoints/functions.py
@@ -63,12 +63,6 @@ from server.api.utils.singletons.scheduler import get_scheduler
 router = APIRouter()
 
 
-@router.post(
-    "/func/{project}/{name}",
-    deprecated=True,
-    description="/func/{project}/{name} is deprecated in 1.4.0 and will be removed in 1.6.0, "
-    "use /projects/{project}/functions/{name} instead",
-)
 @router.post("/projects/{project}/functions/{name}")
 async def store_function(
     request: Request,
@@ -116,12 +110,6 @@ async def store_function(
     }
 
 
-@router.get(
-    "/func/{project}/{name}",
-    deprecated=True,
-    description="/func/{project}/{name} is deprecated in 1.4.0 and will be removed in 1.6.0, "
-    "use /projects/{project}/functions/{name} instead",
-)
 @router.get("/projects/{project}/functions/{name}")
 async def get_function(
     project: str,
@@ -203,12 +191,6 @@ async def delete_function(
     return Response(status_code=HTTPStatus.NO_CONTENT.value)
 
 
-@router.get(
-    "/funcs",
-    deprecated=True,
-    description="/funcs is deprecated in 1.4.0 and will be removed in 1.6.0, "
-    "use /projects/{project}/functions instead",
-)
 @router.get("/projects/{project}/functions")
 async def list_functions(
     project: str = None,

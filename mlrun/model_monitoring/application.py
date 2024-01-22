a85832c6513696a2e8e5d314c65069a2ad69812b
David_Schneider <davesh0812@gmail.com>
2024-01-22 17:07:10 +0200

    raise MLRunInvalidArgumentError

--- mlrun/model_monitoring/application.py
+++ mlrun/model_monitoring/application.py
@@ -14,6 +14,7 @@
 
 import dataclasses
 import json
+import re
 from abc import ABC, abstractmethod
 from typing import Any, Optional, Tuple, Union
 
@@ -49,6 +50,13 @@ class ModelMonitoringApplicationResult:
     kind: mm_constant.ResultKindApp
     status: mm_constant.ResultStatusApp
     extra_data: dict = dataclasses.field(default_factory=dict)
+
+    def __post_init__(self):
+        pat = re.compile(r"[a-zA-Z_][a-zA-Z0-9_]*")
+        if not re.fullmatch(pat, self.name):
+            raise mlrun.errors.MLRunInvalidArgumentError(
+                "Attribute name must be of the format [a-zA-Z_][a-zA-Z0-9_]*"
+            )
 
     def to_dict(self):
         """

359cbfe6886e56ac471dc50b3780597773305b53
Alex Toker <alex_toker@mckinsey.com>
2024-01-29 10:04:33 +0200

    [Datastore] Clarify error message

--- mlrun/datastore/sources.py
+++ mlrun/datastore/sources.py
@@ -118,7 +118,10 @@ class BaseSourceDriver(DataSource):
             if named_view:
                 df.createOrReplaceTempView(self.name)
             return self._filter_spark_df(df, time_field, columns)
-        raise NotImplementedError()
+        raise NotImplementedError(
+            f"Conversion of a source of type '{type(self).__name__}' "
+            "to a Spark dataframe is not possible, as this operation is not supported"
+        )
 
     def _filter_spark_df(self, df, time_field=None, columns=None):
         if not (columns or time_field):

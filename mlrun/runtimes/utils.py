9b679cae1356de5511417c50758bf7bfbc85160c
alonmr <alonnmaor@gmail.com>
2024-01-30 16:31:20 +0200

    .

--- mlrun/runtimes/utils.py
+++ mlrun/runtimes/utils.py
@@ -134,13 +134,14 @@ def add_code_metadata(path=""):
             return f"{remotes[0]}#{repo.head.commit.hexsha}"
 
     except (
-        GitCommandNotFound,
         InvalidGitRepositoryError,
         NoSuchPathError,
-        ValueError,
-    ) as exc:
-        logger.debug(
-            "Path not part of a git repo, git metadata will not be added",
+    ):
+        pass
+
+    except (GitCommandNotFound, ValueError) as exc:
+        logger.warning(
+            "Failed to add git metadata",
             path=path,
             error=err_to_str(exc),
         )

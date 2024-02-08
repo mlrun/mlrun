c7964c21230814bb31a60ea1cec73b53a9dcb8f0
TomerShor <tomer.shor@gmail.com>
2024-02-08 16:02:48 +0200

    .

--- tests/api/db/conftest.py
+++ tests/api/db/conftest.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from tempfile import NamedTemporaryFile
 from collections.abc import Generator
+from tempfile import NamedTemporaryFile
 
 import pytest
 

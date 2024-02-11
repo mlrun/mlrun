080e8a621b8ae04c7b32db15c67a1bdf32a9239a
Liran BG <liranbg@users.noreply.github.com>
2024-02-11 10:11:15 +0200

    [Projects] Fix project deletion missing auth_info (#5103)

--- server/api/api/endpoints/projects_v2.py
+++ server/api/api/endpoints/projects_v2.py
@@ -93,7 +93,7 @@ async def delete_project(
     # we need to implement the verify_project_is_empty, since we don't want
     # to spawn a background task for this, only to return a response
     if deletion_strategy.strategy_to_check():
-        server.api.crud.Projects().verify_project_is_empty(db_session, name)
+        server.api.crud.Projects().verify_project_is_empty(db_session, name, auth_info)
         if deletion_strategy == mlrun.common.schemas.DeletionStrategy.check:
             # if the strategy is check, we don't want to delete the project, only to check if it is empty
             return fastapi.Response(status_code=http.HTTPStatus.NO_CONTENT.value)

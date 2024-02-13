65b1146558dc60ee34da056da71790c204043b05
alonmr <alonnmaor@gmail.com>
2024-02-13 12:59:23 +0200

    .

--- tests/runtimes/test_application.py
+++ tests/runtimes/test_application.py
@@ -1,26 +1,26 @@
 import mlrun
 
 
-def test_create_deployment_runtime():
+def test_create_application_runtime():
     fn = mlrun.code_to_function(
-        "deployment-test", kind="deployment", image="mlrun/mlrun"
+        "application-test", kind="application", image="mlrun/mlrun"
     )
-    assert fn.kind == mlrun.runtimes.RuntimeKinds.deployment
+    assert fn.kind == mlrun.runtimes.RuntimeKinds.application
     assert fn.spec.image == "mlrun/mlrun"
-    assert fn.metadata.name == "deployment-test"
+    assert fn.metadata.name == "application-test"
     assert "IyBDb3B5cmlnaHQgMjAyMyBJZ3VhemlvCiM" in fn.spec.build.functionSourceCode
     assert fn.spec.function_handler == "handler:handler"
 
 
-def test_deploy_deployment_runtime(rundb_mock):
+def test_deploy_application_runtime(rundb_mock):
     fn = mlrun.code_to_function(
-        "deployment-test", kind="deployment", image="my/web-app:latest"
+        "application-test", kind="application", image="my/web-app:latest"
     )
     fn.deploy()
     assert fn.spec.config["spec.sidecars"] == [
         {
             "image": "my/web-app:latest",
-            "name": "deployment-test-sidecar",
+            "name": "application-test-sidecar",
             "ports": [{"containerPort": 8080, "name": "http", "protocol": "TCP"}],
         }
     ]

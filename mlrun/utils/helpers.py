cd69d35a9ecaad86f7fc7607e3601f0c57509ce4
Yaelg <yaelg@iguazio.com>
2024-02-26 10:43:50 +0200

    add None to type hints

--- mlrun/utils/helpers.py
+++ mlrun/utils/helpers.py
@@ -271,7 +271,7 @@ def validate_artifact_key_name(
     )
 
 
-def validate_inline_artifact_body_size(body: typing.Union[str, bytes]) -> None:
+def validate_inline_artifact_body_size(body: typing.Union[str, bytes, None]) -> None:
     if body and len(body) > MYSQL_MEDIUMBLOB_SIZE_BYTES:
         raise mlrun.errors.MLRunBadRequestError(
             "The body of the artifact exceeds the maximum allowed size. "

9951466c6bbfe7c44a4e3844062b29ffeb912496
Gal Topper <gal.topper@gmail.com>
2024-02-26 21:07:09 +0800

    [Datastore] Add HDFS Datastore (#5186)
    * [Datastore] Add HDFS Datastore
    
    [ML-5726](https://jira.iguazeng.com/browse/ML-5726)
    
    * Add copyright notice
    
    * Fix None URL
    
    * Fixes
    
    * Revert change
    
    * Remove unnecessary test parameterization
    
    * Refactor `store_path_to_spark()` functionality into `DataStore`
    
    * Fix
    
    * Minor refactoring
    
    * Fix spark S3 system tests

--- mlrun/datastore/utils.py
+++ mlrun/datastore/utils.py
@@ -15,59 +15,12 @@
 import tarfile
 import tempfile
 import typing
-from urllib.parse import parse_qs, urlparse, urlunparse
+from urllib.parse import parse_qs, urlparse
 
 import pandas as pd
 import semver
 
 import mlrun.datastore
-
-
-def store_path_to_spark(path, spark_options=None):
-    schemas = ["redis://", "rediss://", "ds://"]
-    if any(path.startswith(schema) for schema in schemas):
-        url = urlparse(path)
-        if url.path:
-            path = url.path
-    elif path.startswith("gcs://"):
-        path = "gs:" + path[len("gcs:") :]
-    elif path.startswith("v3io:///"):
-        path = "v3io:" + path[len("v3io:/") :]
-    elif path.startswith("az://"):
-        account_key = None
-        path = "wasbs:" + path[len("az:") :]
-        prefix = "spark.hadoop.fs.azure.account.key."
-        if spark_options:
-            for key in spark_options:
-                if key.startswith(prefix):
-                    account_key = key[len(prefix) :]
-                    break
-        if account_key:
-            # transfer "wasb://basket/some/path" to wasb://basket@account_key.blob.core.windows.net/some/path
-            parsed_url = urlparse(path)
-            new_netloc = f"{parsed_url.hostname}@{account_key}"
-            path = urlunparse(
-                (
-                    parsed_url.scheme,
-                    new_netloc,
-                    parsed_url.path,
-                    parsed_url.params,
-                    parsed_url.query,
-                    parsed_url.fragment,
-                )
-            )
-    elif path.startswith("s3://"):
-        if path.startswith("s3:///"):
-            # 's3:///' not supported since mlrun 0.9.0 should use s3:// instead
-            from mlrun.errors import MLRunInvalidArgumentError
-
-            valid_path = "s3:" + path[len("s3:/") :]
-            raise MLRunInvalidArgumentError(
-                f"'s3:///' is not supported, try using 's3://' instead.\nE.g: '{valid_path}'"
-            )
-        else:
-            path = "s3a:" + path[len("s3:") :]
-    return path
 
 
 def parse_kafka_url(url: str, bootstrap_servers: list = None) -> tuple[str, list]:

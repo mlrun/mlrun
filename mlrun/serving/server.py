mlrun/mlrun

commit 0eca39083c36bd1a995acd42b3dfc6262ea9d91f

[Serving] Use `AsyncEmitSource` (#4724)

mlrun/serving/server.py
@@ -188,11 +188,7 @@ class GraphServer(ModelObj):
 
     def init_object(self, namespace):
         self.graph.init_object(self.context, namespace, self.load_mode, reset=True)
-        return (
-            v2_serving_async_handler
-            if config.datastore.async_source_mode == "enabled"
-            else v2_serving_handler
-        )
+        return v2_serving_handler if self.context.is_mock else v2_serving_async_handler
 
     def test(
         self,
@@ -310,7 +306,7 @@ class GraphServer(ModelObj):
 
     def wait_for_completion(self):
         """wait for async operation to complete"""
-        self.graph.wait_for_completion()
+        return self.graph.wait_for_completion()
 
 
 def v2_serving_init(context, namespace=None):
@@ -334,7 +330,14 @@ def v2_serving_init(context, namespace=None):
     context.logger.info_with(
         "Initializing states", namespace=namespace or get_caller_globals()
     )
-    server.init_states(context, namespace or get_caller_globals())
+    kwargs = {}
+    if hasattr(context, "is_mock"):
+        kwargs["is_mock"] = context.is_mock
+    server.init_states(
+        context,
+        namespace or get_caller_globals(),
+        **kwargs,
+    )
     context.logger.info("Initializing graph steps")
     serving_handler = server.init_object(namespace or get_caller_globals())
     # set the handler hook to point to our handler
@@ -351,9 +354,9 @@ def v2_serving_init(context, namespace=None):
             "Setting termination callback to terminate graph on worker shutdown"
         )
 
-        def termination_callback():
+        async def termination_callback():
             context.logger.info("Termination callback called")
-            server.wait_for_completion()
+            await server.wait_for_completion()
             context.logger.info("Termination of async flow is completed")
 
         context.platform.set_termination_callback(termination_callback)
@@ -363,9 +366,9 @@ def v2_serving_init(context, namespace=None):
             "Setting drain callback to terminate and restart the graph on a drain event (such as rebalancing)"
         )
 
-        def drain_callback():
+        async def drain_callback():
             context.logger.info("Drain callback called")
-            server.wait_for_completion()
+            await server.wait_for_completion()
             context.logger.info(
                 "Termination of async flow is completed. Rerunning async flow."
             )

mlrun/config.py
@@ -149,7 +149,6 @@ default_config = {
         "url": "",
     },
     "v3io_framesd": "http://framesd:8080",
-    "datastore": {"async_source_mode": "disabled"},
     # default node selector to be applied to all functions - json string base64 encoded format
     "default_function_node_selector": "e30=",
     # default priority class to be applied to functions running on k8s cluster

mlrun/datastore/sources.py
@@ -27,9 +27,9 @@ from nuclio import KafkaTrigger
 from nuclio.config import split_path
 
 import mlrun
+from mlrun.config import config
 from mlrun.secrets import SecretsStore
 
-from ..config import config
 from ..model import DataSource
 from ..platforms.iguazio import parse_path
 from ..utils import get_class, is_explicit_ack_supported
@@ -812,16 +812,11 @@ class OnlineSource(BaseSourceDriver):
     def to_step(self, key_field=None, time_field=None, context=None):
         import storey
 
-        source_class = (
-            storey.AsyncEmitSource
-            if config.datastore.async_source_mode == "enabled"
-            else storey.SyncEmitSource
-        )
         source_args = self.attributes.get("source_args", {})
         explicit_ack = (
             is_explicit_ack_supported(context) and mlrun.mlconf.is_explicit_ack()
         )
-        src_class = source_class(
+        src_class = storey.AsyncEmitSource(
             context=context,
             key_field=self.key_field or key_field,
             full_event=True,

mlrun/serving/states.py
@@ -14,6 +14,7 @@
 
 __all__ = ["TaskStep", "RouterStep", "RootFlowStep", "ErrorStep"]
 
+import asyncio
 import os
 import pathlib
 import traceback
@@ -1160,19 +1161,19 @@ class FlowStep(BaseStep):
         if self._controller:
             # async flow (using storey)
             event._awaitable_result = None
-            if config.datastore.async_source_mode == "enabled":
+            if self.context.is_mock:
+                resp = self._controller.emit(
+                    event, return_awaitable_result=self._wait_for_result
+                )
+                if self._wait_for_result and resp:
+                    return resp.await_result()
+            else:
                 resp_awaitable = self._controller.emit(
                     event, await_result=self._wait_for_result
                 )
                 if self._wait_for_result:
                     return resp_awaitable
                 return self._await_and_return_id(resp_awaitable, event)
-            else:
-                resp = self._controller.emit(
-                    event, return_awaitable_result=self._wait_for_result
-                )
-                if self._wait_for_result and resp:
-                    return resp.await_result()
             event = copy(event)
             event.body = {"id": event.id}
             return event
@@ -1210,10 +1211,20 @@ class FlowStep(BaseStep):
 
     def wait_for_completion(self):
         """wait for completion of run in async flows"""
+
         if self._controller:
-            if hasattr(self._controller, "terminate"):
-                self._controller.terminate()
-            return self._controller.await_termination()
+            if asyncio.iscoroutinefunction(self._controller.await_termination):
+
+                async def terminate_and_await_termination():
+                    if hasattr(self._controller, "terminate"):
+                        await self._controller.terminate()
+                    return await self._controller.await_termination()
+
+                return terminate_and_await_termination()
+            else:
+                if hasattr(self._controller, "terminate"):
+                    self._controller.terminate()
+                return self._controller.await_termination()
 
     def plot(self, filename=None, format=None, source=None, targets=None, **kw):
         """plot/save graph using graphviz
@@ -1555,10 +1566,14 @@ def _init_async_objects(context, steps):
                 wait_for_result = True
 
     source_args = context.get_param("source_args", {})
-
     explicit_ack = is_explicit_ack_supported(context) and mlrun.mlconf.is_explicit_ack()
 
-    default_source = storey.SyncEmitSource(
+    if context.is_mock:
+        source_class = storey.SyncEmitSource
+    else:
+        source_class = storey.AsyncEmitSource
+
+    default_source = source_class(
         context=context,
         explicit_ack=explicit_ack,
         **source_args,

tests/serving/test_serving.py
@@ -224,6 +224,7 @@ def init_ctx(
             spec["graph"]["class_args"][extra_class_args_names[i]] = extra_class_args[i]
     os.environ["SERVING_SPEC_ENV"] = json.dumps(spec)
     context = context or GraphContext()
+    context.is_mock = True
     nuclio_init_hook(context, globals(), serving_subkind)
     return context
 
@@ -459,6 +460,7 @@ def test_v2_stream_mode():
 def test_v2_raised_err():
     os.environ["SERVING_SPEC_ENV"] = json.dumps(raiser_spec)
     context = GraphContext()
+    context.is_mock = True
     nuclio_init_hook(context, globals(), serving_subkind)
 
     event = MockEvent(testdata, path="/v2/models/m6/infer")
@@ -471,6 +473,7 @@ def test_v2_async_mode():
     # model loading is async
     os.environ["SERVING_SPEC_ENV"] = json.dumps(asyncspec)
     context = GraphContext()
+    context.is_mock = True
     nuclio_init_hook(context, globals(), serving_subkind)
     context.logger.info("model initialized")
 

tests/system/runtimes/test_nuclio.py
@@ -318,7 +318,7 @@ class TestNuclioRuntimeWithKafka(tests.system.base.TestMLRunSystem):
         kafka_producer = kafka.KafkaProducer(bootstrap_servers=self.brokers)
 
         # Test runs
-        yield kafka_consumer, kafka_producer
+        yield kafka_consumer, kafka_producer, kafka_admin_client
 
         # Teardown
         kafka_admin_client.delete_topics([self.topic, self.topic_out])
@@ -380,11 +380,13 @@ class TestNuclioRuntimeWithKafka(tests.system.base.TestMLRunSystem):
         )
         stocks_set.save()
 
+        consumer_group = "my_group"
+
         kafka_source = KafkaSource(
             brokers=self.brokers,
             topics=self.topic_out,
             initial_offset="earliest",
-            group="my_group",
+            group=consumer_group,
         )
 
         func = mlrun.code_to_function(
@@ -408,7 +410,7 @@ class TestNuclioRuntimeWithKafka(tests.system.base.TestMLRunSystem):
         )
         print(stocks_set_endpoint)
 
-        kafka_consumer, kafka_producer = kafka_fixture
+        kafka_consumer, kafka_producer, kafka_admin = kafka_fixture
         self.produce_kafka_helper(kafka_producer, stocks_df[row_divide:])
 
         time.sleep(20)  # wait for ingestion-service parquet to be written
@@ -422,6 +424,14 @@ class TestNuclioRuntimeWithKafka(tests.system.base.TestMLRunSystem):
         # setting check_like=True since the order of the two parquet merge
         # can happen two-ways (based on alphanumeric order)
         pd.testing.assert_frame_equal(actual_df, expected_df, check_like=True)
+
+        consumer_group_offsets = kafka_admin.list_consumer_group_offsets(consumer_group)
+        print(f"consumer_group_offsets={consumer_group_offsets}")
+        sum_of_offsets = 0
+        for topic_partition, offset_and_metadata in consumer_group_offsets.items():
+            if topic_partition.topic == self.topic_out:
+                sum_of_offsets += offset_and_metadata.offset
+        assert sum_of_offsets == 3
 
     @pytest.mark.skipif(
         not brokers, reason="MLRUN_SYSTEM_TESTS_KAFKA_BROKERS not defined"

tests/test_requirements.py
@@ -110,7 +110,7 @@ def test_requirement_specifiers_convention():
     ignored_invalid_map = {
         # See comment near requirement for why we're limiting to patch changes only for all of these
         "aiobotocore": {">=2.5.0,<2.8"},
-        "storey": {"~=1.7.3"},
+        "storey": {"~=1.7.4"},
         "nuclio-sdk": {">=0.5"},
         "bokeh": {"~=2.4, >=2.4.2"},
         # protobuf is limited just for docs

commit b1a47988ded33f3c673441c8a68e118b041ae36b

[Model Monitoring] Fix to use sync handler (#5203)
* [Model Monitoring] Use sync handler

[ML-5801](https://jira.iguazeng.com/browse/ML-5801)

Fixes error introduced in #4724.

* Minor refactoring

* Use sync handler also when kind is "task"

* Simplify

mlrun/serving/server.py
@@ -188,7 +188,6 @@ class GraphServer(ModelObj):
 
     def init_object(self, namespace):
         self.graph.init_object(self.context, namespace, self.load_mode, reset=True)
-        return v2_serving_handler if self.context.is_mock else v2_serving_async_handler
 
     def test(
         self,
@@ -339,9 +338,9 @@ def v2_serving_init(context, namespace=None):
         **kwargs,
     )
     context.logger.info("Initializing graph steps")
-    serving_handler = server.init_object(namespace or get_caller_globals())
+    server.init_object(namespace or get_caller_globals())
     # set the handler hook to point to our handler
-    setattr(context, "mlrun_handler", serving_handler)
+    setattr(context, "mlrun_handler", v2_serving_handler)
     setattr(context, "_server", server)
     context.logger.info_with("Serving was initialized", verbose=server.verbose)
     if server.verbose:
@@ -387,11 +386,6 @@ def v2_serving_handler(context, event, get_body=False):
             event.body = None
 
     return context._server.run(event, context, get_body)
-
-
-async def v2_serving_async_handler(context, event, get_body=False):
-    """hook for nuclio handler()"""
-    return await context._server.run(event, context, get_body)
 
 
 def create_graph_server(
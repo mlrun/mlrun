dc0197a934061f9342d341ff85a0cc30ef45f164
liranbg <liranbg@gmail.com>
2024-03-19 22:40:28 +0200

    fix

--- mlrun/config.py
+++ mlrun/config.py
@@ -325,8 +325,7 @@ default_config = {
                 #
                 # if set to "nil" or "none", nothing would be set
                 "modes": (
-                    "ONLY_FULL_GROUP_BY"
-                    ",STRICT_TRANS_TABLES"
+                    "STRICT_TRANS_TABLES"
                     ",NO_ZERO_IN_DATE"
                     ",NO_ZERO_DATE"
                     ",ERROR_FOR_DIVISION_BY_ZERO"

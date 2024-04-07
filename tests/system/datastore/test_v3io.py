b71106746f40d56784b1e05bb98bec9fcd9d1286
Tomer Mamia <tomerm@iguazio.com>
2024-04-07 14:09:33 +0300

    change comments

--- tests/system/datastore/test_v3io.py
+++ tests/system/datastore/test_v3io.py
@@ -319,7 +319,7 @@ class TestV3ioDataStore(TestMLRunSystem):
         response = upload_data_item.as_df(**reader_args)
         pd.testing.assert_frame_equal(source, response)
 
-        # dd
+        # dask
         source = dd_reader(local_file_path, **reader_args)
         response = upload_data_item.as_df(**reader_args, df_module=dd)
         dd.assert_eq(source, response)
@@ -357,7 +357,7 @@ class TestV3ioDataStore(TestMLRunSystem):
             tested_df = tested_df.sort_values("id").reset_index(drop=True)
         pd.testing.assert_frame_equal(tested_df, expected_df)
 
-        # dd
+        # dask
         dd_df1 = dd_reader(df1_path)
         dd_df2 = dd_reader(df2_path)
         expected_dd_df = dd.concat([dd_df1, dd_df2], axis=0)

100c6961777ee04022659db08cb34bf3eec34622
Gal Topper <gal.topper@gmail.com>
2024-04-16 18:26:28 +0800

    [Serving] Revert #5322
    The original change was a first step in implementing [ML-5136](https://iguazio.atlassian.net/browse/ML-5136), but is no longer necessary because we have decided to direct users to flow topology instead, and ultimately deprecate router topology.

--- tests/system/model_monitoring/test_model_monitoring.py
+++ tests/system/model_monitoring/test_model_monitoring.py
@@ -247,8 +247,7 @@ class TestBasicModelMonitoring(TestMLRunSystem):
     image: Optional[str] = None
 
     @pytest.mark.timeout(270)
-    @pytest.mark.parametrize("engine", ["sync", "async"])
-    def test_basic_model_monitoring(self, engine) -> None:
+    def test_basic_model_monitoring(self) -> None:
         # Main validations:
         # 1 - a single model endpoint is created
         # 2 - stream metrics are recorded as expected under the model endpoint
@@ -271,11 +270,6 @@ class TestBasicModelMonitoring(TestMLRunSystem):
         serving_fn = mlrun.import_function(
             "hub://v2-model-server", project=self.project_name
         ).apply(mlrun.auto_mount())
-
-        serving_fn.set_topology(
-            "router",
-            engine=engine,
-        )
 
         # enable model monitoring
         serving_fn.set_tracking()

aa93a9076568768ce73ff325b0caf747843077ca
David_Schneider <davesh0812@gmail.com>
2024-04-17 16:14:37 +0300

    lint

--- server/api/api/endpoints/nuclio.py
+++ server/api/api/endpoints/nuclio.py
@@ -485,7 +485,8 @@ def _deploy_nuclio_runtime(
                     < semver.Version.parse("1.6.3")
                 ):
                     raise mlrun.errors.MLRunBadRequestError(
-                        f"On deploy of serving-functions which is based on mlrun image ("mlrun/") and with set-tracking enabled, client version must be >= 1.6.3"
+                        "On deploy of serving-functions which is based on mlrun image "
+                        "('mlrun/') and with set-tracking enabled, client version must be >= 1.6.3"
                     )
                 if not monitoring_deployment.is_monitoring_stream_has_the_new_stream_trigger():
                     monitoring_deployment.deploy_model_monitoring_stream_processing(

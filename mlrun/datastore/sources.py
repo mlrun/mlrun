5e68b8098f0fe56bec547f6d73df62948b74c5c7
Tomer Mamia <tomerm@iguazio.com>
2024-05-01 13:07:04 +0300

    docs

--- mlrun/datastore/sources.py
+++ mlrun/datastore/sources.py
@@ -290,9 +290,12 @@ class ParquetSource(BaseSourceDriver):
     :parameter end_time: filters out data after this time
     :parameter attributes: additional parameters to pass to storey.
     :param additional_filters: (list of tuples, optional): List of additional_filters conditions as tuples.
+                               Each tuple should be in the format (column_name, operator, value).
+                               Supported operators: "=", ">=", "<=", ">", "<".
+                               Example: [("Product", "=", "Computer")]
 
-                                For all supported filters, please see:
-                                https://arrow.apache.org/docs/python/generated/pyarrow.parquet.ParquetDataset.html
+                               For all supported filters, please see:
+                               https://arrow.apache.org/docs/python/generated/pyarrow.parquet.ParquetDataset.html
     """
 
     kind = "parquet"

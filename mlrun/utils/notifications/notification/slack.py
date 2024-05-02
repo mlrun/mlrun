f79b8e9eeef9a958310376dcf0c94b73eed9db71
quaark <a.melnick@icloud.com>
2024-05-02 11:36:53 +0300

    better

--- mlrun/utils/notifications/notification/slack.py
+++ mlrun/utils/notifications/notification/slack.py
@@ -119,12 +119,7 @@ class SlackNotification(NotificationBase):
             {"type": "header", "text": {"type": "plain_text", "text": header_text}}
         ]
         if section_text:
-            blocks.append(
-                {
-                    "type": "section",
-                    "text": {"type": "plain_text", "text": section_text},
-                }
-            )
+            blocks.append(self._get_slack_row(section_text))
         return blocks
 
     def _get_alert_fields(

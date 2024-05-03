7ab5e43e7d7fa959657999cc77e46c6fd3eb4bca
roei3000b <40743125+roei3000b@users.noreply.github.com>
2024-05-03 07:08:36 +0300

    [ProjectSummaries] Make `_calculate_pipelines_counters` use pagination (#5496)

--- tests/api/api/test_projects.py
+++ tests/api/api/test_projects.py
@@ -1706,7 +1706,15 @@ def _mock_pipelines(project_name):
     for status, count in status_count_map.items():
         for index in range(count):
             pipelines.append({"status": status, "project": project_name})
+
+    def list_pipelines_return_value(*args, **kwargs):
+        next_page_token = "some-token"
+        if kwargs["page_token"] == "":
+            return (None, next_page_token, pipelines[: len(pipelines) // 2])
+        elif kwargs["page_token"] == next_page_token:
+            return (None, None, pipelines[len(pipelines) // 2 :])
+
     server.api.crud.Pipelines().list_pipelines = unittest.mock.Mock(
-        return_value=(None, None, pipelines)
+        side_effect=list_pipelines_return_value
     )
     return status_count_map[mlrun.run.RunStatuses.running]

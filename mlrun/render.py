ba9a2aa93c2138727ee58e0a2cba23807c4dde0c
alonmr <alonnmaor@gmail.com>
2024-05-12 18:05:45 +0300

    minor fix

--- mlrun/render.py
+++ mlrun/render.py
@@ -408,7 +408,7 @@ def runs_to_html(
     else:
         df["labels"] = df["labels"].apply(dict_html)
         df["inputs"] = df["inputs"].apply(inputs_html)
-        if not df["artifact_uris"].empty:
+        if df["artifact_uris"][0]:
             df["artifact_uris"] = df["artifact_uris"].apply(dict_html)
             df.drop("artifacts", axis=1, inplace=True)
         else:

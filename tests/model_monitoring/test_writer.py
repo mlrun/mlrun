c42c5d2ad7def663f64c0701c43d6ae62b44ca91
Jonathan Daniel <jonathan_daniel@mckinsey.com>
2024-05-12 17:42:02 +0300

    Raise ValueError for correct return type hint

--- tests/model_monitoring/test_writer.py
+++ tests/model_monitoring/test_writer.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import datetime
 import json
 import os
@@ -101,6 +102,8 @@ def event(request: pytest.FixtureRequest) -> _AppResultEvent:
                 ),
             }
         )
+    else:
+        raise ValueError
 
 
 @pytest.fixture

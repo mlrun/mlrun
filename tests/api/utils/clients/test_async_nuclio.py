6524d751d674bd645f8c9e381d666a4e28f96b85
rokatyy <ekaterina_molchanova@mckinsey.com>
2024-05-15 17:53:51 +0100

    Fix test

--- tests/api/utils/clients/test_async_nuclio.py
+++ tests/api/utils/clients/test_async_nuclio.py
@@ -79,7 +79,7 @@ async def test_nuclio_get_api_gateway(
         received_api_gateway.authentication.authentication_mode
         == api_gateway.spec.authentication.authentication_mode
     )
-    assert received_api_gateway.spec.functions == ["test", "test2"]
+    assert received_api_gateway.spec.functions == ["default-test", "default-test2"]
     assert received_api_gateway.spec.canary == [20, 80]
 
 

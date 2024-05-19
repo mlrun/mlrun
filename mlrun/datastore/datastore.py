cfccda21bb66cea0d92203d6a299f98bf1120bff
Tomer Mamia <tomerm@iguazio.com>
2024-05-19 17:56:00 +0300

    change file schema condition

--- mlrun/datastore/datastore.py
+++ mlrun/datastore/datastore.py
@@ -223,6 +223,14 @@ class StoreManager:
             subpath = url[len("memory://") :]
             return in_memory_store, subpath, url
 
+        elif schema == "":
+            # parse_url() will drop the windows drive-letter from the path for url like "c:\a\b".
+            # As a workaround, we set subpath to the url.
+            subpath = url
+
+        elif schema == "file://":
+            subpath = url.replace("file://", "", 1)
+
         if not schema and endpoint:
             if endpoint in self._stores.keys():
                 return self._stores[endpoint], subpath, url
@@ -241,9 +249,6 @@ class StoreManager:
         )
         if not secrets and not mlrun.config.is_running_as_api():
             self._stores[store_key] = store
-        # in file stores in windows path like c:\a\b the drive letter is dropped from the path, so we return the url
-        if store.kind == "file" and isinstance(url, str):
-            subpath = url.replace("file://", "", 1)
         return store, subpath, url
 
     def reset_secrets(self):

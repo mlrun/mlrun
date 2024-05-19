729b9532fc5ac7019aaa4d6bd5c67aa1c18bb614
Alex Toker <alex_toker@mckinsey.com>
2024-05-19 17:15:29 +0300

    repace hasattr() with dir()

--- mlrun/datastore/targets.py
+++ mlrun/datastore/targets.py
@@ -500,7 +500,7 @@ class BaseStoreTarget(DataTargetBase):
         chunk_id=0,
         **kwargs,
     ) -> Optional[int]:
-        if hasattr(df, "rdd"):
+        if "rdd" in dir(df):
             options = self.get_spark_options(key_column, timestamp_key)
             options.update(kwargs)
             df = self.prepare_spark_df(df, key_column, timestamp_key, options)
@@ -540,9 +540,9 @@ class BaseStoreTarget(DataTargetBase):
                 or isinstance(file_system.protocol, (tuple, list))
                 and "file" in file_system.protocol
             ):
-                dir = os.path.dirname(target_path)
-                if dir:
-                    os.makedirs(dir, exist_ok=True)
+                directory = os.path.dirname(target_path)
+                if directory:
+                    os.makedirs(directory, exist_ok=True)
             target_df = df
             partition_cols = None  # single parquet file
             if not target_path.endswith(".parquet") and not target_path.endswith(
@@ -1325,7 +1325,7 @@ class NoSqlBaseTarget(BaseStoreTarget):
     def write_dataframe(
         self, df, key_column=None, timestamp_key=None, chunk_id=0, **kwargs
     ):
-        if hasattr(df, "rdd"):
+        if "rdd" in dir(df):
             options = self.get_spark_options(key_column, timestamp_key)
             options.update(kwargs)
             df = self.prepare_spark_df(df)
@@ -2078,7 +2078,7 @@ class SQLTarget(BaseStoreTarget):
 
         self._create_sql_table()
 
-        if hasattr(df, "rdd"):
+        if "rdd" in dir(df):
             raise ValueError("Spark is not supported")
         else:
             (

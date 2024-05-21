f9ec636f67368239e09114c91cf3e329c6a7cb7a
alonmr <alonnmaor@gmail.com>
2024-05-21 20:20:22 +0300

    .

--- dependencies.py
+++ dependencies.py
@@ -76,7 +76,6 @@ def extra_requirements() -> typing.Dict[str, typing.List[str]]:
             "dask~=2023.9.0",
             "distributed~=2023.9.0",
         ],
-        "alibaba-oss": ["ossfs==2023.12.0", "oss2==2.18.1"],
     }
 
     # see above why we are excluding google-cloud

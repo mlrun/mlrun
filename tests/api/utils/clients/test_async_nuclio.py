b59b09c7e7499eb8f847a7f1dd1ddbf3d6dea8fc
liranbg <liranbg@gmail.com>
2024-05-22 10:41:50 +0300

    zz

--- tests/api/utils/clients/test_async_nuclio.py
+++ tests/api/utils/clients/test_async_nuclio.py
@@ -89,5 +89,5 @@ async def test_nuclio_list_api_gateways(
     assert r == response_body
 
     mock_aioresponse.get(request_url, status=http.HTTPStatus.UNAUTHORIZED)
-    with pytest.raises(aiohttp.client_exceptions.ClientResponseError):
+    with pytest.raises(mlrun.errors.MLRunUnauthorizedError):
         await nuclio_client.list_api_gateways()

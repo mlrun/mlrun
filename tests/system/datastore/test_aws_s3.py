mlrun/mlrun

commit 3f1f13823b6db9cddf80ff0cd7f2082e6f63d894

[Datastore] Fix delete method in filestore and s3 (#5582)

mlrun/datastore/s3.py
@@ -198,6 +198,11 @@ class S3Store(DataStore):
         bucket = self.s3.Bucket(bucket)
         return [obj.key[key_length:] for obj in bucket.objects.filter(Prefix=key)]
 
+    def rm(self, path, recursive=False, maxdepth=None):
+        bucket, key = self.get_bucket_and_key(path)
+        path = f"{bucket}/{key}"
+        self.filesystem.rm(path=path, recursive=recursive, maxdepth=maxdepth)
+
 
 def parse_s3_bucket_and_key(s3_path):
     try:

tests/integration/aws_s3/test_aws_s3.py
@@ -21,6 +21,7 @@ import fsspec
 import pandas as pd
 import pytest
 import yaml
+from botocore.exceptions import ClientError
 
 import mlrun
 import mlrun.errors
@@ -144,6 +145,10 @@ class TestAwsS3:
         upload_data_item.upload(self.test_file)
         response = upload_data_item.get()
         assert response.decode() == self.test_string
+        upload_data_item.delete()
+        with pytest.raises(ClientError) as client_exception:
+            upload_data_item.stat()
+        assert client_exception.value.response["Error"]["Code"] == "404"
 
         # Verify as_df() creates a proper DF. Note that the AWS case as_df() works through the fsspec interface, that's
         # why it's important to test it as well.

mlrun/datastore/datastore.py
@@ -223,6 +223,11 @@ class StoreManager:
             subpath = url[len("memory://") :]
             return in_memory_store, subpath, url
 
+        elif schema in get_local_file_schema():
+            # parse_url() will drop the windows drive-letter from the path for url like "c:\a\b".
+            # As a workaround, we set subpath to the url.
+            subpath = url.replace("file://", "", 1)
+
         if not schema and endpoint:
             if endpoint in self._stores.keys():
                 return self._stores[endpoint], subpath, url
@@ -241,8 +246,7 @@ class StoreManager:
         )
         if not secrets and not mlrun.config.is_running_as_api():
             self._stores[store_key] = store
-        # in file stores in windows path like c:\a\b the drive letter is dropped from the path, so we return the url
-        return store, url if store.kind == "file" else subpath, url
+        return store, subpath, url
 
     def reset_secrets(self):
         self._secrets = {}

tests/datastore/test_filestore.py
@@ -1,0 +1,42 @@
+# Copyright 2023 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os.path
+import tempfile
+
+import pytest
+
+import mlrun
+
+
+class TestFileStore:
+    @pytest.mark.parametrize(
+        "prefix",
+        ["", "file://"],
+    )
+    def test_put_stat_delete(self, prefix):
+        try:
+            with tempfile.NamedTemporaryFile(suffix=".txt", delete=False) as temp_file:
+                object_url = f"{prefix}{temp_file.name}"
+                data_item = mlrun.run.get_dataitem(object_url)
+                test_text = "test string"
+                data_item.put(test_text)
+                assert data_item.stat().size == len(test_text)
+                data_item.delete()
+                with pytest.raises(FileNotFoundError):
+                    data_item.stat()
+                assert not os.path.exists(temp_file.name)
+        finally:
+            if os.path.exists(temp_file.name):
+                os.remove(temp_file.name)

commit 6ff0ebc549b725155dcd8019545cf01b1c6dde18

[Datastore] Fix `S3Store.rm()`
[ML-6587](https://iguazio.atlassian.net/browse/ML-6587)

Following breakage in 1.7.8-rc18, introduced by #5582.

mlrun/datastore/s3.py
@@ -198,11 +198,6 @@ class S3Store(DataStore):
         bucket = self.s3.Bucket(bucket)
         return [obj.key[key_length:] for obj in bucket.objects.filter(Prefix=key)]
 
-    def rm(self, path, recursive=False, maxdepth=None):
-        bucket, key = self.get_bucket_and_key(path)
-        path = f"{bucket}/{key}"
-        self.filesystem.rm(path=path, recursive=recursive, maxdepth=maxdepth)
-
 
 def parse_s3_bucket_and_key(s3_path):
     try:

tests/system/datastore/test_aws_s3.py
@@ -30,6 +30,7 @@ from mlrun.datastore.datastore_profile import (
 )
 from mlrun.datastore.sources import ParquetSource
 from mlrun.datastore.targets import ParquetTarget, get_default_prefix_for_target
+from mlrun.utils import logger
 from tests.system.base import TestMLRunSystem
 
 test_environment = TestMLRunSystem._get_env_from_file()
@@ -125,7 +126,7 @@ class TestAwsS3(TestMLRunSystem):
             "s3", key=self._access_key_id, secret=self._secret_access_key
         )
         param = self.s3[url_type]
-        print(f"Using URL {param['parquet_url']}\n")
+        logger.info(f"Using URL {param['parquet_url']}")
         data = {"Column1": [1, 2, 3], "Column2": ["A", "B", "C"]}
         df = pd.DataFrame(data)
         source_path = param["parquet_url"]
@@ -137,13 +138,13 @@ class TestAwsS3(TestMLRunSystem):
 
         # ingest
         target_path = f"{os.path.dirname(param['parquet_url'])}/target_{uuid.uuid4()}"
-        targets = [ParquetTarget(path=target_path)]
+        target = ParquetTarget(path=target_path)
         fset = fstore.FeatureSet(
             name="test_fs",
             entities=[fstore.Entity("Column1")],
         )
 
-        fset.ingest(source=parquet_source, targets=targets)
+        fset.ingest(source=parquet_source, targets=[target])
         result = ParquetSource(path=target_path).to_dataframe(
             columns=("Column1", "Column2")
         )
@@ -153,12 +154,15 @@ class TestAwsS3(TestMLRunSystem):
             df.sort_index(axis=1), result.sort_index(axis=1), check_like=True
         )
 
+        # Check for ML-6587 regression
+        target.purge()
+
     def test_ingest_ds_default_target(self):
         s3_fs = fsspec.filesystem(
             "s3", key=self._access_key_id, secret=self._secret_access_key
         )
         param = self.s3["ds_with_bucket"]
-        print(f"Using URL {param['parquet_url']}\n")
+        logger.info(f"Using URL {param['parquet_url']}")
         data = {"Column1": [1, 2, 3], "Column2": ["A", "B", "C"]}
         df = pd.DataFrame(data)
         source_path = param["parquet_url"]
@@ -169,20 +173,20 @@ class TestAwsS3(TestMLRunSystem):
 
         parquet_source = ParquetSource(name="test", path=source_path)
 
-        targets = [ParquetTarget(path="ds://s3ds_profile_with_bucket")]
+        target = ParquetTarget(path="ds://s3ds_profile_with_bucket")
         fset = fstore.FeatureSet(
             name="test_fs",
             entities=[fstore.Entity("Column1")],
         )
 
-        fset.ingest(source=parquet_source, targets=targets)
+        fset.ingest(source=parquet_source, targets=[target])
 
         expected_default_ds_data_prefix = get_default_prefix_for_target(
             "dsnosql"
         ).format(
             ds_profile_name="s3ds_profile_with_bucket",
             project=fset.metadata.project,
-            kind=targets[0].kind,
+            kind=target.kind,
             name=fset.metadata.name,
         )
 
@@ -196,3 +200,6 @@ class TestAwsS3(TestMLRunSystem):
         assert_frame_equal(
             df.sort_index(axis=1), result.sort_index(axis=1), check_like=True
         )
+
+        # Check for ML-6587 regression
+        target.purge()
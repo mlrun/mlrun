89da5b43491c12c54df50b9dee6bc40cd27431c6
quaark <a.melnick@icloud.com>
2024-06-04 16:26:13 +0300

    better

--- mlrun/common/formatters/base.py
+++ mlrun/common/formatters/base.py
@@ -19,10 +19,22 @@ import mlrun.errors
 
 
 class ObjectFormat:
+    """
+    MLRun object formatter. Any class that inherits from this class should implement the `format_method` method
+    to specify the formatting method for each format.
+    A `filter_obj_method` utility method is provided to filter the object based on a list of keys.
+    """
+
     full = "full"
 
     @staticmethod
     def format_method(_format: str) -> typing.Optional[typing.Callable]:
+        """
+        Get the formatting method for the provided format.
+        A `None` value signifies a pass-through formatting method.
+        :param _format: The format as a string representation.
+        :return: The formatting method.
+        """
         return {
             ObjectFormat.full: None,
         }[_format]
@@ -34,6 +46,13 @@ class ObjectFormat:
         _format: str,
         exclude_formats: typing.Optional[list[str]] = None,
     ) -> typing.Any:
+        """
+        Format the provided object based on the provided format.
+        :param obj: The object to format.
+        :param _format: The format as a string representation.
+        :param exclude_formats: A list of formats to exclude from the formatting process. If the provided format is in
+                                this list, an invalid format exception will be raised.
+        """
         exclude_formats = exclude_formats or []
         _format = _format or cls.full
         invalid_format_exc = mlrun.errors.MLRunBadRequestError(
@@ -54,10 +73,19 @@ class ObjectFormat:
         return format_method(obj)
 
     @staticmethod
-    def filter_obj_method(_filter: list[list[str]]) -> typing.Callable:
+    def filter_obj_method(_filter: list[str]) -> typing.Callable:
+        """
+        Returns a method that filters the object based on the provided list of keys.
+        The keys should be in a dot-separated format, denoting the path within the dictionary to the desired key.
+        The object maintains its structure, with the filtered keys and their values, while all other keys are removed.
+        :param _filter: The list of keys to filter by.
+        :return: The filtering method.
+        """
+
         def _filter_method(obj: dict) -> dict:
             formatted_obj = {}
-            for key_list in _filter:
+            for key in _filter:
+                key_list = key.split(".")
                 obj_recursive_iterator = obj
                 formatted_obj_recursive_iterator = formatted_obj
                 for idx, key in enumerate(key_list):

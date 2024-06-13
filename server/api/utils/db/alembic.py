f368b7530bfe27c95e3307df7ab7d808e8809a2b
alonmr <alonnmaor@gmail.com>
2024-06-13 20:05:26 +0300

    undo drop index changes

--- server/api/utils/db/alembic.py
+++ server/api/utils/db/alembic.py
@@ -47,13 +47,6 @@ class AlembicUtil(object):
             return True
         return current_revision == self._initial_revision
 
-    def is_artifacts_v2_index_migration_complete(self):
-        # Determine if the artifact v2 index migration was already applied
-        return any(
-            revision in self._revision_history[1:]
-            for revision in ["aa28cdc5bb17", "ebf6f5af763d"]
-        )
-
     def _get_current_revision(self) -> typing.Optional[str]:
         # create separate config in order to catch the stdout
         catch_stdout_config = alembic.config.Config(self._alembic_config_path)

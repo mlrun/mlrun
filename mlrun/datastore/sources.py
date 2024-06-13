65b1649855cdd64e3085fca4a83ef1e291864d80
Alex Toker <alex_toker@mckinsey.com>
2024-06-13 13:25:36 +0300

    [ML-6767] Make all streaming sources to be wildcarded

--- mlrun/datastore/sources.py
+++ mlrun/datastore/sources.py
@@ -73,15 +73,10 @@ def load_spark_dataframe_with_options(
 
         # Spark streaming expects either directory or file path with wildcard. Single file path is not supported
         # So we trick it making it by adding a wildcard to the end of the path: file.parquet -> file.parque[t]
-        if "path" in non_hadoop_spark_options and (
-            non_hadoop_spark_options["path"].endswith(
-                (".txt", ".csv", ".prq", ".parquet", ".pq", ".parq")
-            )
-        ):
-            last_char = non_hadoop_spark_options["path"][-1]
-            non_hadoop_spark_options["path"] = (
-                non_hadoop_spark_options["path"][:-1] + f"[{last_char}]"
-            )
+        last_char = non_hadoop_spark_options["path"][-1]
+        non_hadoop_spark_options["path"] = (
+            non_hadoop_spark_options["path"][:-1] + f"[{last_char}]"
+        )
 
         df = reader.load(**non_hadoop_spark_options)
         schema = df.schema

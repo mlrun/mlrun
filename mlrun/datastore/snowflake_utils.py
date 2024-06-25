d3422cf40d588bf237ec5dec85bc531ee6446dd4
Tomer Mamia <tomerm@iguazio.com>
2024-06-25 14:43:19 +0300

    add print

--- mlrun/datastore/snowflake_utils.py
+++ mlrun/datastore/snowflake_utils.py
@@ -32,6 +32,7 @@ def get_snowflake_password():
 def get_snowflake_spark_options(attributes):
     if not attributes:
         return {}
+    print(f"my attributes: {attributes}")
     return {
         "format": "net.snowflake.spark.snowflake",
         "sfURL": attributes.get("url"),

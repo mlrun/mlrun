f9d693aaac742edd784874d3f949053e9a5881e4
Liran BG <liranbg@users.noreply.github.com>
2024-06-25 08:05:23 +0300

    [Artifacts] Adding configurable limit for list_artifacts (#5839)

--- server/api/api/endpoints/artifacts_v2.py
+++ server/api/api/endpoints/artifacts_v2.py
@@ -153,6 +153,7 @@ async def list_artifacts(
     producer_uri: str = None,
     best_iteration: bool = Query(False, alias="best-iteration"),
     format_: str = Query(mlrun.common.formatters.ArtifactFormat.full, alias="format"),
+    limit: int = Query(None),
     auth_info: mlrun.common.schemas.AuthInfo = Depends(deps.authenticate_request),
     db_session: Session = Depends(deps.get_db_session),
 ):
@@ -176,6 +177,7 @@ async def list_artifacts(
         format_=format_,
         producer_id=tree,
         producer_uri=producer_uri,
+        limit=limit,
     )
 
     artifacts = await server.api.utils.auth.verifier.AuthVerifier().filter_project_resources_by_permissions(
@@ -272,6 +274,7 @@ async def delete_artifacts(
     tag: str = "",
     tree: str = None,
     labels: list[str] = Query([], alias="label"),
+    limit: int = Query(None),
     auth_info: mlrun.common.schemas.AuthInfo = Depends(deps.authenticate_request),
     db_session: Session = Depends(deps.get_db_session),
 ):
@@ -283,6 +286,7 @@ async def delete_artifacts(
         tag,
         labels,
         producer_id=tree,
+        limit=limit,
     )
     await server.api.utils.auth.verifier.AuthVerifier().query_project_resources_permissions(
         mlrun.common.schemas.AuthorizationResourceTypes.artifact,

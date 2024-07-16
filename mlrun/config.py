019481420013489d6037a2e8153b6e7f474494fe
TomerShor <tomer.shor@gmail.com>
2024-07-16 08:56:55 +0300

    use schema constant

--- mlrun/config.py
+++ mlrun/config.py
@@ -247,7 +247,7 @@ default_config = {
         },
         "application": {
             "default_sidecar_internal_port": 8050,
-            "default_authentication_mode": "none",
+            "default_authentication_mode": mlrun.common.schemas.APIGatewayAuthenticationMode.none,
         },
     },
     # TODO: function defaults should be moved to the function spec config above

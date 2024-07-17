20c57701c087c2ea67040c640cd402b2db6d86ba
ZeevRispler <zeev_rispler@mckinsey.com>
2024-07-17 16:24:58 +0300

    [GenAI] Add Langchain model server

---
+++ mlrun/frameworks/langchain/model_server.py
@@ -0,0 +1,152 @@
+# Copyright 2023 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# flake8: noqa  - this is until we take care of the F401 violations with respect to __all__ & sphinx
+
+from typing import Any, Dict, Union
+
+import langchain_community.llms
+from langchain_core.language_models.llms import LLM
+
+import mlrun
+from mlrun.serving.v2_serving import V2ModelServer
+
+
+class LangChainModelServer(V2ModelServer):
+    """
+    LangChain Model serving class, inheriting the V2ModelServer class for being initialized automatically by the model
+    server and be able to run locally as part of a nuclio serverless function, or as part of a real-time pipeline.
+    For compatibility with langchain models, this class supports the following operations:
+    - invoke
+    - batch
+    - ainvoke
+    - abatch
+    in the future we will add streaming support.
+    Notice: In order to use this serving class, please ensure that the langchain_community package is installed.
+    """
+
+    def __init__(
+        self,
+        context: mlrun.MLClientCtx = None,
+        model_class: str = None,
+        llm: Union[str, LLM] = None,
+        init_kwargs: Dict[str, Any] = None,
+        generation_kwargs: Dict[str, Any] = None,
+        name: str = None,
+        model_path: str = None,
+        **kwargs,
+    ):
+        """
+        Initialize a serving class for general llm usage.
+        :param context:           The mlrun context to use.
+        :param model_class:       The class of the model to use.
+        :param llm:               The llm object itself in case of local usage.
+        :param init_kwargs:       The initialization arguments to use while initializing the llm.
+        :param generation_kwargs: The generation arguments to use while generating text.
+        :param name:              The name of this server to be initialized.
+        :param model_path:        Not in use. When adding a model pass any string value
+        """
+        super().__init__(name=name, context=context, model_path=model_path)
+        self.model_class = model_class
+        self.llm = llm
+        self.init_kwargs = init_kwargs or {}
+        self.generation_kwargs = generation_kwargs
+
+    def load(self):
+        """
+        loads the model.
+        """
+        # If the llm is already an LLM object, use it directly
+        if isinstance(self.llm, LLM):
+            self.model = self.llm
+            return
+        # If the llm is a string (or not given, then we take default model), load the llm from langchain.
+        self.model = getattr(langchain_community.llms, self.model_class)(
+            **self.init_kwargs
+        )
+
+    def predict(
+        self, request: Dict[str, Any], generation_kwargs: Dict[str, Any] = None
+    ):
+        """
+        Predict the output of the model.
+        :param request:           The request to the model. The input to the model will be read from the "inputs" key.
+        :param generation_kwargs: The generation arguments to use while generating response.
+        :return:                  The model's prediction on the given input.
+        """
+        inputs = request.get("inputs", [])
+        generation_kwargs = generation_kwargs or self.generation_kwargs
+        return self.model.invoke(input=inputs[0], config=generation_kwargs)
+
+    def op_invoke(
+        self, request: Dict[str, Any], generation_kwargs: Dict[str, Any] = None
+    ):
+        """
+        Invoke the model. (Same as predict)
+        :param request:           The request to the model. The input to the model will be read from the "body.inputs"
+                                  key.
+        :param generation_kwargs: The generation arguments to use while generating response.
+        :return:                  The model's prediction on the given input.
+        """
+        request = request.body
+        inputs = request.get("inputs", [])
+        generation_kwargs = generation_kwargs or self.generation_kwargs
+        return self.model.invoke(input=inputs[0], config=generation_kwargs)
+
+    def op_batch(
+        self, request: Dict[str, Any], generation_kwargs: Dict[str, Any] = None
+    ):
+        """
+        Invoke the model in batch.
+        :param request:           The request to the model. The input to the model will be read from the "body.inputs"
+                                  key.
+        :param generation_kwargs: The generation arguments to use while generating response.
+        :return:                  The model's prediction on the given input.
+        """
+        request = request.body
+        inputs = request.get("inputs", [])
+        generation_kwargs = generation_kwargs or self.generation_kwargs
+        return self.model.batch(inputs=inputs, config=generation_kwargs)
+
+    def op_ainvoke(
+        self, request: Dict[str, Any], generation_kwargs: Dict[str, Any] = None
+    ):
+        """
+        Invoke the model asynchronously.
+        :param request:           The request to the model. The input to the model will be read from the "body.inputs"
+                                  key.
+        :param generation_kwargs: The generation arguments to use while generating response.
+        :return:                  The model's prediction on the given input.
+        """
+        request = request.body
+        inputs = request.get("inputs", [])
+        generation_kwargs = generation_kwargs or self.generation_kwargs
+        response = self.model.ainvoke(input=inputs, config=generation_kwargs)
+        return response
+
+    def op_abatch(
+        self, request: Dict[str, Any], generation_kwargs: Dict[str, Any] = None
+    ):
+        """
+        Invoke the model asynchronously in batches.
+        :param request:           The request to the model. The input to the model will be read from the "body.inputs"
+                                  key.
+        :param generation_kwargs: The generation arguments to use while generating response.
+        :return:                  The model's prediction on the given input.
+        """
+        request = request.body
+        inputs = request.get("inputs", [])
+        generation_kwargs = generation_kwargs or self.generation_kwargs
+        response = self.model.abatch(inputs=inputs, config=generation_kwargs)
+        return response
---
+++ tests/frameworks/langchain/test_model_server.py
@@ -0,0 +1,344 @@
+# Copyright 2023 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import asyncio
+import os
+import subprocess
+from subprocess import PIPE, Popen
+
+import pytest
+
+import mlrun
+
+langchain_model_server_path = "mlrun/frameworks/langchain/model_server.py"
+
+#: if true, delete the model after the test
+_OLLAMA_DELETE_MODEL_POST_TEST = False
+_OLLAMA_MODEL = "qwen:0.5b"
+_OPENAI_MODEL = "gpt-3.5-turbo-instruct"
+_HUGGINGFACE_MODEL = "gpt2"
+
+
+# To run this test, you need to:
+# 1. install ollama on your computer
+# 2. pull the desired model from the ollama repository (for example, in terminal: ollama pull llama3)
+# 3. run ollama with said model in terminal (for example, in terminal: ollama run llama3)
+def ollama_check_skip():
+    """
+    Check if ollama is installed
+    """
+    result = subprocess.run(["ollama", "--help"], stdout=PIPE)
+    return result.returncode != 0
+
+
+@pytest.fixture
+def ollama_fixture():
+    """
+    Do the setup and cleanup for the ollama test
+    :return:
+    """
+    # pull or make sure the model is available
+    subprocess.run(["ollama", "pull", _OLLAMA_MODEL], stdout=PIPE)
+
+    # start the ollama server
+    ollama_serve_process = Popen(["ollama", "serve"], stdout=PIPE)
+
+    yield
+
+    ollama_serve_process.kill()
+    # delete the model after the test if requested
+    global _OLLAMA_DELETE_MODEL_POST_TEST
+    if _OLLAMA_DELETE_MODEL_POST_TEST:
+        subprocess.run(["ollama", "rm", _OLLAMA_MODEL], stdout=PIPE)
+
+
+@pytest.mark.skipif(ollama_check_skip(), reason="Ollama not installed")
+def test_ollama(ollama_fixture):
+    """
+    Test the langchain model server with an ollama model
+    """
+    project = mlrun.get_or_create_project(
+        name="ollama-model-server-example", context="./"
+    )
+    serving_func = project.set_function(
+        func=langchain_model_server_path,
+        name="ollama-langchain-model-server",
+        kind="serving",
+        image="mlrun/mlrun",
+    )
+    serving_func.add_model(
+        "ollama-langchain-model",
+        model_class="Ollama",
+        class_name="LangChainModelServer",
+        llm=_OLLAMA_MODEL,
+        init_kwargs={"model": _OLLAMA_MODEL},
+        model_path=".",
+    )
+    server = serving_func.to_mock_server()
+    predict_result = server.test(
+        "/v2/models/ollama-langchain-model/predict", {"inputs": ["how old are you?"]}
+    )
+    assert predict_result
+    print("ollama predict successful predict_result", predict_result)
+    invoke_result = server.test(
+        "/v2/models/ollama-langchain-model/invoke", {"inputs": ["how old are you?"]}
+    )
+    assert invoke_result
+    print("ollama invoke successful invoke_result", invoke_result)
+    batch_result = server.test(
+        "/v2/models/ollama-langchain-model/batch",
+        {"inputs": ["how old are you?", "how old are you?"]},
+    )
+    assert batch_result
+    print("ollama batch successful batch_result", batch_result)
+
+
+@pytest.mark.skipif(ollama_check_skip(), reason="OpenAI API credentials not set")
+@pytest.mark.asyncio
+async def test_ollama_async(ollama_fixture):
+    """
+    Test the langchain model server with an ollama model asynchronously
+    """
+    project = mlrun.get_or_create_project(
+        name="ollama-model-server-example", context="./"
+    )
+    serving_func = project.set_function(
+        func=langchain_model_server_path,
+        name="ollama-langchain-model-server",
+        kind="serving",
+        image="mlrun/mlrun",
+    )
+    serving_func.add_model(
+        "ollama-langchain-model",
+        model_class="Ollama",
+        class_name="LangChainModelServer",
+        llm=_OLLAMA_MODEL,
+        init_kwargs={"model": _OLLAMA_MODEL},
+        model_path=".",
+    )
+    server = serving_func.to_mock_server()
+    ainvoke_result = await asyncio.gather(
+        server.test(
+            "/v2/models/ollama-langchain-model/ainvoke",
+            {"inputs": ["how old are you?"]},
+        )
+    )
+    ainvoke_result = ainvoke_result[0]
+    assert ainvoke_result
+    print("ollama ainvoke successful ainvoke_result", ainvoke_result)
+    abatch_result = await asyncio.gather(
+        server.test(
+            "/v2/models/ollama-langchain-model/abatch",
+            {"inputs": ["how old are you?", "how old are you?"]},
+        )
+    )
+    abatch_result = abatch_result[0]
+    assert abatch_result
+    print("ollama abatch successful abatch_result", abatch_result)
+
+
+def skip_openai():
+    """
+    Check if the OpenAI API credentials are set
+    """
+    return not os.environ.get("OPENAI_API_KEY") or not os.environ.get("OPENAI_BASE_URL")
+
+
+@pytest.mark.skipif(skip_openai(), reason="OpenAI API credentials not set")
+def test_openai():
+    """
+    Test the langchain model server with an openai model
+    """
+    project = mlrun.get_or_create_project(
+        name="openai-model-server-example", context="./"
+    )
+    serving_func = project.set_function(
+        func=langchain_model_server_path,
+        name="openai-langchain-model-server",
+        kind="serving",
+        image="mlrun/mlrun",
+    )
+    serving_func.add_model(
+        "openai-langchain-model",
+        model_class="OpenAI",
+        class_name="LangChainModelServer",
+        llm="OpenAI",
+        init_kwargs={"model": _OPENAI_MODEL},
+        model_path=".",
+    )
+    server = serving_func.to_mock_server()
+    predict_result = server.test(
+        "/v2/models/openai-langchain-model/predict", {"inputs": ["how old are you?"]}
+    )
+    assert predict_result
+    print("openai predict successful predict_result", predict_result)
+    invoke_result = server.test(
+        "/v2/models/openai-langchain-model/invoke", {"inputs": ["how old are you?"]}
+    )
+    assert invoke_result
+    print("openai invoke successful invoke_result", invoke_result)
+    batch_result = server.test(
+        "/v2/models/openai-langchain-model/batch",
+        {"inputs": ["how old are you?", "how old are you?"]},
+    )
+    assert batch_result
+    print("openai batch successful batch_result", batch_result)
+
+
+@pytest.mark.skipif(skip_openai(), reason="OpenAI API credentials not set")
+@pytest.mark.asyncio
+async def test_openai_async():
+    """
+    Test the langchain model server with an openai model asynchronously
+    """
+    project = mlrun.get_or_create_project(
+        name="openai-model-server-example", context="./"
+    )
+    serving_func = project.set_function(
+        func=langchain_model_server_path,
+        name="openai-langchain-model-server",
+        kind="serving",
+        image="mlrun/mlrun",
+    )
+    serving_func.add_model(
+        "openai-langchain-model",
+        model_class="OpenAI",
+        class_name="LangChainModelServer",
+        llm="OpenAI",
+        init_kwargs={"model": _OPENAI_MODEL},
+        model_path=".",
+    )
+    server = serving_func.to_mock_server()
+    ainvoke_result = await asyncio.gather(
+        server.test(
+            "/v2/models/openai-langchain-model/ainvoke",
+            {"inputs": ["how old are you?"]},
+        )
+    )
+    ainvoke_result = ainvoke_result[0]
+    assert ainvoke_result
+    print("openai ainvoke successful ainvoke_result", ainvoke_result)
+    abatch_result = await asyncio.gather(
+        server.test(
+            "/v2/models/openai-langchain-model/abatch",
+            {"inputs": ["how old are you?", "how old are you?"]},
+        )
+    )
+    abatch_result = abatch_result[0]
+    assert abatch_result
+    print("openai abatch successful abatch_result", abatch_result)
+
+
+def test_huggingface():
+    """
+    Test the langchain model server with a huggingface model
+    """
+    from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+    model_id = _HUGGINGFACE_MODEL
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(model_id)
+    pipe = pipeline(
+        "text-generation", model=model, tokenizer=tokenizer, max_new_tokens=10
+    )
+
+    project = mlrun.get_or_create_project(
+        name="huggingface-model-server-example", context="./"
+    )
+    serving_func = project.set_function(
+        func=langchain_model_server_path,
+        name="huggingface-langchain-model-server",
+        kind="serving",
+        image="mlrun/mlrun",
+    )
+    serving_func.add_model(
+        "huggingface-langchain-model",
+        model_class="HuggingFacePipeline",
+        class_name="LangChainModelServer",
+        llm=_HUGGINGFACE_MODEL,
+        init_kwargs={"pipeline": pipe},
+        model_path=".",
+    )
+    server = serving_func.to_mock_server()
+    predict_result = server.test(
+        "/v2/models/huggingface-langchain-model/predict",
+        {"inputs": ["how old are you?"]},
+    )
+    assert predict_result
+    print("huggingface successful predict predict_result", predict_result)
+    invoke_result = server.test(
+        "/v2/models/huggingface-langchain-model/invoke",
+        {"inputs": ["how old are you?"]},
+    )
+    assert invoke_result
+    print("huggingface successful invoke invoke_result", invoke_result)
+    batch_result = server.test(
+        "/v2/models/huggingface-langchain-model/batch",
+        {"inputs": ["how old are you?", "how old are you?"]},
+    )
+    assert batch_result
+    print("huggingface successful batch batch_result", batch_result)
+
+
+@pytest.mark.asyncio
+async def test_huggingface_async():
+    """
+    Test the langchain model server with a huggingface model asynchronously
+    """
+    from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+    model_id = _HUGGINGFACE_MODEL
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(model_id)
+    pipe = pipeline(
+        "text-generation", model=model, tokenizer=tokenizer, max_new_tokens=10
+    )
+
+    project = mlrun.get_or_create_project(
+        name="huggingface-model-server-example", context="./"
+    )
+    serving_func = project.set_function(
+        func=langchain_model_server_path,
+        name="huggingface-langchain-model-server",
+        kind="serving",
+        image="mlrun/mlrun",
+    )
+    serving_func.add_model(
+        "huggingface-langchain-model",
+        model_class="HuggingFacePipeline",
+        class_name="LangChainModelServer",
+        llm=_HUGGINGFACE_MODEL,
+        init_kwargs={"pipeline": pipe},
+        model_path=".",
+    )
+    server = serving_func.to_mock_server()
+    ainvoke_result = await asyncio.gather(
+        server.test(
+            "/v2/models/huggingface-langchain-model/ainvoke",
+            {"inputs": ["how old are you?"]},
+        )
+    )
+    ainvoke_result = ainvoke_result[0]
+    assert ainvoke_result
+    print("huggingface successful ainvoke ainvoke_result", ainvoke_result)
+
+    abatch_result = await asyncio.gather(
+        server.test(
+            "/v2/models/huggingface-langchain-model/abatch",
+            {"inputs": ["how old are you?", "how old are you?"]},
+        )
+    )
+    abatch_result = abatch_result[0]
+    assert abatch_result
+    print("huggingface successful abatch abatch_result", abatch_result)

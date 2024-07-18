941fb81bdff3d6424ac32d9630c07f74b627a698
Liran BG <liranbg@users.noreply.github.com>
2024-07-18 10:03:45 +0300

    [Requirement] Bump pip (#6008)
    \

--- automation/package_test/test.py
+++ automation/package_test/test.py
@@ -273,7 +273,7 @@ class PackageTester:
             extra=extra,
         )
         self._run_command(
-            "python -m pip install --upgrade pip~=23.2.1",
+            "python -m pip install --upgrade pip~=24.1",
             run_in_venv=True,
         )
 

499c3b89cebfbf9b388f31bd67e4c814614bf561
Yonatan Shelach <92271540+yonishelach@users.noreply.github.com>
2024-07-21 12:29:23 +0300

    [GenAI] Clean API and Client (#6012)

--- mlrun/genai/api.py
+++ mlrun/genai/api.py
@@ -59,8 +59,15 @@ async def get_auth_user(
         return AuthInfo(username="yhaviv@gmail.com", token=token)
 
 
-@router.post("/collections/{collection}/{path}/{loader}/ingest")
-async def ingest(collection, path, loader, metadata, version, from_file):
+@router.post("/collections/{collection}/{loader}/ingest")
+async def ingest(
+    collection: str,
+    loader: str,
+    path: str,
+    metadata: dict = None,
+    version: str = None,
+    from_file: bool = False,
+):
     """Ingest documents into the vector database"""
     data_loader = get_data_loader(config, client=client, collection_name=collection)
     if from_file:
@@ -75,6 +82,7 @@ async def ingest(collection, path, loader, metadata, version, from_file):
     else:
         loader_obj = get_loader_obj(path, loader_type=loader)
         data_loader.load(loader_obj, metadata=metadata, version=version)
+    return {"status": "ok"}
 
 
 @router.post("/pipeline/{name}/run")
@@ -96,75 +104,3 @@ async def run_pipeline(
     resp = app_server.run_pipeline(name, event)
     print(f"resp: {resp}")
     return resp
-
-
-@router.get("/users")
-async def list_users(user, email):
-    """List users"""
-    data = client.list_users(email, user, output_mode="short")
-    return data
-
-
-@router.get("/collections")
-async def list_collections(owner, metadata):
-    """List document collections"""
-    data = client.list_collections(owner, metadata, output_mode="short").with_raise()
-    return data
-
-
-@router.put("/collection/{name}")
-async def update_collection(name, owner, description, category, labels):
-    """Create or update a document collection"""
-    labels = fill_params(labels)
-
-    # check if the collection exists, if it does, update it, otherwise create it
-    collection_exists = client.get_collection(name)
-    if collection_exists:
-        client.update_collection(
-            name=name,
-            description=description,
-            category=category,
-            labels=labels,
-        )
-    else:
-        client.create_collection(
-            name=name,
-            description=description,
-            owner_name=owner,
-            category=category,
-            labels=labels,
-        )
-
-
-@router.get("/session/{session_id}")
-async def get_session(session_id):
-    """Get a chat session"""
-    data = client.get_session(session_id)
-    return data
-
-
-@router.get("/sessions/")
-def list_sessions(username, mode, last=None, created: str = None):
-    """List chat sessions"""
-    data = client.list_sessions(
-        username=username,
-        last=last,
-        output_mode=mode,
-        created_after=created,
-    )
-    return data
-
-
-def fill_params(params, params_dict=None):
-    params_dict = params_dict or {}
-    for param in params:
-        i = param.find("=")
-        if i == -1:
-            continue
-        key, value = param[:i].strip(), param[i + 1 :].strip()
-        if key is None:
-            raise ValueError(f"cannot find param key in line ({param})")
-        params_dict[key] = value
-    if not params_dict:
-        return None
-    return params_dict

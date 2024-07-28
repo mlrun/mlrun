dbc45b674d9380044a737ca90189562adbe4f259
Gal Topper <gal.topper@gmail.com>
2024-07-28 15:18:12 +0800

    Refactor to avoid importing server from mlrun

--- mlrun/config.py
+++ mlrun/config.py
@@ -1238,18 +1238,10 @@ class Config:
         return storage_options
 
     def is_explicit_ack_enabled(self) -> bool:
-        version = None
-        if is_running_as_api():
-            import server.api.utils.runtimes.nuclio
-
-            version = server.api.utils.runtimes.nuclio.resolve_nuclio_version()
-
-        if not version:
-            version = self.nuclio_version
-
         return self.httpdb.nuclio.explicit_ack == "enabled" and (
-            not version
-            or semver.VersionInfo.parse(version) >= semver.VersionInfo.parse("1.12.10")
+            not self.nuclio_version
+            or semver.VersionInfo.parse(self.nuclio_version)
+            >= semver.VersionInfo.parse("1.12.10")
         )
 
 

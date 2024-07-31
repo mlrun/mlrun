32cdf6dae139726f7bd6bc0ef5f61e70e83c1f62
Tomer Mamia <tomerm@iguazio.com>
2024-07-31 16:27:39 +0300

    add comment

--- mlrun/feature_store/api.py
+++ mlrun/feature_store/api.py
@@ -1097,6 +1097,8 @@ def _ingest_with_spark(
                 df_to_write, key_columns, timestamp_key, spark_options
             )
             write_format = spark_options.pop("format", None)
+            # We can get to this point if the column exists in different letter cases,
+            # so PySpark will be able to read it, but we still have to raise an exception for it.
             check_special_columns_exists(
                 spark_df=df_to_write,
                 entities=featureset.spec.entities,

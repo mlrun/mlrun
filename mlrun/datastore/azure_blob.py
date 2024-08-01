35ff86e507c675bb9919d5e16d0afa9fd08a3296
Tomer Mamia <tomerm@iguazio.com>
2024-08-01 15:08:38 +0300

    max_concurrency=2,instead of 1568

--- mlrun/datastore/azure_blob.py
+++ mlrun/datastore/azure_blob.py
@@ -49,7 +49,7 @@ class AzureBlobStore(DataStore):
         self._filesystem = makeDatastoreSchemaSanitizer(
             filesystem_class,
             using_bucket=self.using_bucket,
-            max_concurrency=5,
+            max_concurrency=2,
             **self.get_storage_options(),
         )
         return self._filesystem

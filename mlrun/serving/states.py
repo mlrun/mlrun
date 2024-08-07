mlrun/mlrun

commit 4306e356e72d2ab198504a84fbfb2ec01bb028c3

[Serving] Avoid setting termination and drain callbacks in router topology (#6044)

mlrun/serving/states.py
@@ -388,6 +388,9 @@ class BaseStep(ModelObj):
         """
         raise NotImplementedError("set_flow() can only be called on a FlowStep")
 
+    def supports_termination(self):
+        return False
+
 
 class TaskStep(BaseStep):
     """task execution step, runs a class or handler"""
@@ -1385,6 +1388,9 @@ class FlowStep(BaseStep):
                 step = step.to(next_step)
 
         return step
+
+    def supports_termination(self):
+        return self.engine == "async"
 
 
 class RootFlowStep(FlowStep):

mlrun/serving/server.py
@@ -344,9 +344,14 @@ def v2_serving_init(context, namespace=None):
     if server.verbose:
         context.logger.info(server.to_yaml())
 
-    if hasattr(context, "platform") and hasattr(
-        context.platform, "set_termination_callback"
-    ):
+    _set_callbacks(server, context)
+
+
+def _set_callbacks(server, context):
+    if not server.graph.supports_termination() or not hasattr(context, "platform"):
+        return
+
+    if hasattr(context.platform, "set_termination_callback"):
         context.logger.info(
             "Setting termination callback to terminate graph on worker shutdown"
         )
@@ -358,7 +363,7 @@ def v2_serving_init(context, namespace=None):
 
         context.platform.set_termination_callback(termination_callback)
 
-    if hasattr(context, "platform") and hasattr(context.platform, "set_drain_callback"):
+    if hasattr(context.platform, "set_drain_callback"):
         context.logger.info(
             "Setting drain callback to terminate and restart the graph on a drain event (such as rebalancing)"
         )

commit b78c7b7b9a5998229baae1055a1d9a379503371e

[Serving] Fix setting of drain/termination callbacks
Broken in #6044.

[ML-7304](https://iguazio.atlassian.net/browse/ML-7304)

mlrun/serving/states.py
@@ -1391,7 +1391,7 @@ class FlowStep(BaseStep):
         return step
 
     def supports_termination(self):
-        return self.engine == "async"
+        return self.engine != "sync"
 
 
 class RootFlowStep(FlowStep):
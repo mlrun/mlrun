6031268135a19e6e7afe19921cf08b8ef4137cc0
Tomer Mamia <tomerm@iguazio.com>
2024-08-12 11:17:13 +0300

    change workers and chunk size ratio

--- mlrun/datastore/google_cloud_storage.py
+++ mlrun/datastore/google_cloud_storage.py
@@ -24,8 +24,8 @@ from .base import DataStore, FileStats, makeDatastoreSchemaSanitizer
 
 # Google storage objects will be represented with the following URL: gcs://<bucket name>/<path> or gs://...
 
-WORKERS = 8
-CHUNK_SIZE = 32 * 1024 * 1024
+WORKERS = 25
+CHUNK_SIZE = 10 * 1024 * 1024
 
 
 class GoogleCloudStorageStore(DataStore):

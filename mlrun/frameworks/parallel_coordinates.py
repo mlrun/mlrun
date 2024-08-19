d47a017bf17378ee67b9149c1fafcee70eb75f25
Jonathan Daniel <jonathan_daniel@mckinsey.com>
2024-08-19 15:22:32 +0300

    Update deprecated IPython imports

--- mlrun/frameworks/parallel_coordinates.py
+++ mlrun/frameworks/parallel_coordinates.py
@@ -18,8 +18,7 @@ from typing import Union
 
 import numpy as np
 import pandas as pd
-from IPython.core.display import HTML
-from IPython.display import display
+from IPython.display import HTML, display
 from pandas.api.types import is_numeric_dtype, is_string_dtype
 
 import mlrun

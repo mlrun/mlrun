9fa77b8aefbb45e6062ae8952ae4aea9755c90fe
Eyal Danieli <eyald@iguazio.com>
2024-08-25 17:10:13 +0300

    [Project delete] on failure to delete `kafka` stream or `tdengine` TSDB, do not fail the project deletion  (#6194)

--- server/api/crud/model_monitoring/deployment.py
+++ server/api/crud/model_monitoring/deployment.py
@@ -892,11 +892,19 @@ class MonitoringDeployment:
             background_task_name=background_task_name,
         )
         if delete_app_stream_resources:
-            MonitoringDeployment._delete_model_monitoring_stream_resources(
-                project=project,
-                function_names=[function_name],
-                access_key=access_key,
-            )
+            try:
+                MonitoringDeployment._delete_model_monitoring_stream_resources(
+                    project=project,
+                    function_names=[function_name],
+                    access_key=access_key,
+                )
+            except mlrun.errors.MLRunStreamConnectionFailure as e:
+                logger.warning(
+                    "Failed to delete stream resources, you may need to delete them manually",
+                    project_name=project,
+                    function=function_name,
+                    error=mlrun.errors.err_to_str(e),
+                )
 
     @staticmethod
     def _delete_model_monitoring_stream_resources(
@@ -966,7 +974,7 @@ class MonitoringDeployment:
                     logger.debug("Deleted v3io stream", stream_path=stream_path)
                 except v3io.dataplane.response.HttpResponseError as e:
                     logger.warning(
-                        "Can't delete v3io stream",
+                        "Failed to delete v3io stream",
                         stream_path=stream_path,
                         error=mlrun.errors.err_to_str(e),
                     )
@@ -993,7 +1001,7 @@ class MonitoringDeployment:
                 logger.debug("Deleted kafka topics", topics=topics)
             except kafka.errors.TopicAuthorizationFailedError as e:
                 logger.warning(
-                    "Can't delete kafka topics",
+                    "Failed to delete kafka topics",
                     topics=topics,
                     error=mlrun.errors.err_to_str(e),
                 )
@@ -1002,6 +1010,11 @@ class MonitoringDeployment:
                     "Kafka model monitoring topics not found, probably not created",
                     topics=topics,
                     error=mlrun.errors.err_to_str(e),
+                )
+            except kafka.errors.NoBrokersAvailable as e:
+                # Raise an error that will be caught by the caller and skip the deletion of the stream
+                raise mlrun.errors.MLRunStreamConnectionFailure(
+                    f"Failed to delete kafka topics {topics}, no brokers available, {mlrun.errors.err_to_str(e)}"
                 )
         else:
             logger.warning(

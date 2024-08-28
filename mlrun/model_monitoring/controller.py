15e5dc7aa92544b6db2dcd71c57b65c3716ea14a
David_Schneider <davesh0812@gmail.com>
2024-08-28 15:47:48 +0300

    .

--- mlrun/model_monitoring/controller.py
+++ mlrun/model_monitoring/controller.py
@@ -334,9 +334,10 @@ class MonitoringApplicationController:
                 logger.info("No model endpoints found", project=self.project)
                 return
             monitoring_functions = self.project_obj.list_model_monitoring_functions()
-            applications_names = list(
-                {app.metadata.name for app in monitoring_functions}
-            )
+            if monitoring_functions:
+                applications_names = list(
+                    {app.metadata.name for app in monitoring_functions}
+                )
             # if monitoring_functions: - TODO : ML-7700
             #   Gets only application in ready state
             #   applications_names = list(

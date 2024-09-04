b8c668c7253b0404aed7bed122315fd5b6c9c7bf
rokatyy <ekaterina_molchanova@mckinsey.com>
2024-09-04 17:01:30 +0100

    Alon CR

--- mlrun/runtimes/nuclio/application/application.py
+++ mlrun/runtimes/nuclio/application/application.py
@@ -436,7 +436,7 @@ class ApplicationRuntime(RemoteRuntime):
         """
         Create the application API gateway. Once the application is deployed, the API gateway can be created.
         An application without an API gateway is not accessible.
-        :param name:                    The name of the API gateway, defaults to <function-name>-<function-tag>
+        :param name:                    The name of the API gateway
         :param path:                    Optional path of the API gateway, default value is "/".
             Note that given path should be supported by deployed application
         :param direct_port_access:      Set True to allow direct port access to the application sidecar

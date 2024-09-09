3fce3ae79848d26d7636a51d8f285bbeb2b4bcdb
Alon Maor <48641682+alonmr@users.noreply.github.com>
2024-09-09 20:37:39 +0300

    [Notifications] Add kinds docstring (#6291)

--- mlrun/model.py
+++ mlrun/model.py
@@ -681,7 +681,8 @@ class ImageBuilder(ModelObj):
 class Notification(ModelObj):
     """Notification object
 
-    :param kind: notification implementation kind - slack, webhook, etc.
+    :param kind: notification implementation kind - slack, webhook, etc. See
+        :py:class:`mlrun.common.schemas.notification.NotificationKind`
     :param name: for logging and identification
     :param message: message content in the notification
     :param severity: severity to display in the notification

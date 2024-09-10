8d5d7fe8ac6e316f7debf5bdd3b92f652f6888d3
David_Schneider <davesh0812@gmail.com>
2024-09-10 17:29:42 +0300

    .

--- tests/system/model_monitoring/test_app.py
+++ tests/system/model_monitoring/test_app.py
@@ -384,9 +384,9 @@ class _V3IORecordsChecker:
 @pytest.mark.enterprise
 @pytest.mark.model_monitoring
 class TestMonitoringAppFlow(TestMLRunSystem, _V3IORecordsChecker):
-    project_name = "test-app-flow-v68"
+    project_name = "test-app-flow-v2"
     # Set image to "<repo>/mlrun:<tag>" for local testing
-    image: typing.Optional[str] = "quay.io/davesh0812/mlrun:1.7.0"
+    image: typing.Optional[str] = None
     error_count = 10
 
     @classmethod

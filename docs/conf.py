059e9f8dc546ed8287b0affd959316c1c097ba93
Jonathan Daniel <jonathan_daniel@mckinsey.com>
2024-09-17 18:18:05 +0300

    Update `exclude_patterns`
    https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-exclude_patterns

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,10 @@ language = "en"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
+exclude_patterns = [
+    "_build",
+    "CONTRIBUTING.md",
+]
 
 source_suffix = {
     ".rst": "restructuredtext",

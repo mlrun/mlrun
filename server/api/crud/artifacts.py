400973b9570e92ca2f55600038ffd237d3b5a653
Liran BG <liranbg@users.noreply.github.com>
2024-09-19 10:17:33 +0300

    Revert "[Artifacts] Limit the artifact body size when logging artifacts" (#6364)

--- server/api/crud/artifacts.py
+++ server/api/crud/artifacts.py
@@ -282,8 +282,8 @@ class Artifacts(
                         err=err_to_str(err),
                     )
         if "spec" in artifact and "inline" in artifact["spec"]:
-            mlrun.utils.helpers.validate_artifact_body_size(
-                artifact["spec"]["inline"], is_inline=True
+            mlrun.utils.helpers.validate_inline_artifact_body_size(
+                artifact["spec"]["inline"]
             )
 
     def _delete_artifact_data(

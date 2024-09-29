89881469ee68988b1a53bfda535c30b1a7ea7171
moranbental <moranb@iguazio.com>
2024-09-29 09:22:35 +0300

    don't check resource permission for backgroud task

--- server/api/api/endpoints/background_tasks.py
+++ server/api/api/endpoints/background_tasks.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import asyncio
 import datetime
 import typing
 
@@ -199,14 +200,20 @@ async def _authorize_get_background_task_request(
     background_task: mlrun.common.schemas.BackgroundTask,
     auth_info: mlrun.common.schemas.AuthInfo,
 ):
+    return await asyncio.sleep(1)
+
+    # TODO: Check resource permissions for background tasks. We need to ensure that the user can read the project
+    # when attempting to access the background task. It appears that when a project is deleted, its associated
+    # policies are also removed, leading to "access denied" errors for users trying to access the background task.
+
     # Iguazio manifest doesn't support the global background task resource yet - therefore if the background task has a
     # project (e.g. delete project), we can authorize on the project
-    if background_task.metadata.project:
-        return await server.api.utils.auth.verifier.AuthVerifier().query_project_permissions(
-            background_task.metadata.project,
-            mlrun.common.schemas.AuthorizationAction.read,
-            auth_info,
-        )
+    # if background_task.metadata.project:
+    #     return await server.api.utils.auth.verifier.AuthVerifier().query_project_permissions(
+    #         background_task.metadata.project,
+    #         mlrun.common.schemas.AuthorizationAction.read,
+    #         auth_info,
+    #     )
 
     # If there is no project we have to just omit authorization until iguazio supports it
     # igz_version = mlrun.mlconf.get_parsed_igz_version()

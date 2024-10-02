e9417c8f7574c6bd9b9d84e77c37d252e4a48450
Eyal Danieli <eyald@iguazio.com>
2024-10-02 17:03:48 +0300

    [Model Monitoring] Fix TDEngine client version and add logs to delete resources [1.7.x] (#6467)

--- dependencies.py
+++ dependencies.py
@@ -76,7 +76,7 @@ def extra_requirements() -> dict[str, list[str]]:
             "distributed~=2023.12.1",
         ],
         "alibaba-oss": ["ossfs==2023.12.0", "oss2==2.18.1"],
-        "tdengine": ["taos-ws-py~=0.3.3"],
+        "tdengine": ["taos-ws-py==0.3.2"],
         "snowflake": ["snowflake-connector-python~=3.7"],
     }
 

3c0788f35dfe843daea5de4c015e17613bb06400
Jonathan Daniel <36337649+jond01@users.noreply.github.com>
2024-10-08 17:52:23 +0300

    [Model Monitoring] Revert TDEngine retry changes
    Cherry-pick #6498, reverts #6481.

--- mlrun/model_monitoring/db/tsdb/tdengine/tdengine_connector.py
+++ mlrun/model_monitoring/db/tsdb/tdengine/tdengine_connector.py
@@ -57,22 +57,6 @@ class TDEngineConnector(TSDBConnector):
             self._connection = self._create_connection()
         return self._connection
 
-    def with_retry_on_closed_connection(self, fn, **kwargs):
-        try:
-            return fn(self.connection, **kwargs)
-        except (taosws.QueryError, taosws.FetchError) as err:
-            logger.warn(f"TDEngine error: {err}")
-            if "Internal error:" in str(err):
-                logger.info("Retrying TDEngine query with a new connection")
-                try:
-                    self._connection.close()
-                except Exception:
-                    pass
-                self._connection = None
-                return fn(self.connection, **kwargs)
-            else:
-                raise err
-
     def _create_connection(self) -> taosws.Connection:
         """Establish a connection to the TSDB server."""
         logger.debug("Creating a new connection to TDEngine", project=self.project)
@@ -109,9 +93,7 @@ class TDEngineConnector(TSDBConnector):
         """Create TDEngine supertables."""
         for table in self.tables:
             create_table_query = self.tables[table]._create_super_table_query()
-            self.with_retry_on_closed_connection(
-                lambda conn: conn.execute(create_table_query)
-            )
+            self.connection.execute(create_table_query)
 
     def write_application_event(
         self,
@@ -157,14 +139,10 @@ class TDEngineConnector(TSDBConnector):
         )
 
         create_table_sql = table._create_subtable_sql(subtable=table_name, values=event)
-        self.with_retry_on_closed_connection(
-            lambda conn: conn.execute(create_table_sql)
-        )
+        self.connection.execute(create_table_sql)
 
-        insert_statement = self.with_retry_on_closed_connection(
-            lambda conn: table._insert_subtable_stmt(
-                conn, subtable=table_name, values=event
-            )
+        insert_statement = table._insert_subtable_stmt(
+            self.connection, subtable=table_name, values=event
         )
         insert_statement.add_batch()
         insert_statement.execute()
@@ -232,9 +210,7 @@ class TDEngineConnector(TSDBConnector):
             get_subtable_names_query = self.tables[table]._get_subtables_query(
                 values={mm_schemas.EventFieldType.PROJECT: self.project}
             )
-            subtables = self.with_retry_on_closed_connection(
-                lambda conn: conn.query(get_subtable_names_query)
-            )
+            subtables = self.connection.query(get_subtable_names_query)
             for subtable in subtables:
                 drop_query = self.tables[table]._drop_subtable_query(
                     subtable=subtable[0]
@@ -313,9 +289,7 @@ class TDEngineConnector(TSDBConnector):
         )
         logger.debug("Querying TDEngine", query=full_query)
         try:
-            query_result = self.with_retry_on_closed_connection(
-                lambda conn: conn.query(full_query)
-            )
+            query_result = self.connection.query(full_query)
         except taosws.QueryError as e:
             raise mlrun.errors.MLRunInvalidArgumentError(
                 f"Failed to query table {table} in database {self.database}, {str(e)}"

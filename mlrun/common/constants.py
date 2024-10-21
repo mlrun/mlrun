8d6bed52002a7526f2a465eb32b38a21c04d051d
alonmr <alonnmaor@gmail.com>
2024-10-21 11:47:27 +0200

    minor

--- mlrun/common/constants.py
+++ mlrun/common/constants.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 import mlrun.common.types
 
 IMAGE_NAME_ENRICH_REGISTRY_PREFIX = "."  # prefix for image name to enrich with registry

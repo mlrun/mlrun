c2769da69983c500ca47e313bc77013347b96656
roei3000b <roei3000b@gmail.com>
2024-10-29 11:06:36 +0200

    [ProjectSummaries] Fixing schedule count logic for jobs

--- server/api/db/sqldb/db.py
+++ server/api/db/sqldb/db.py
@@ -3004,7 +3004,7 @@ class SQLDB(DBInterface):
             project_name, schedule_name, kind = result
             if kind == mlrun_constants.MLRunInternalLabels.workflow:
                 project_to_schedule_pending_workflows_count[project_name] += 1
-            elif kind == mlrun.common.schemas.ScheduleKinds.job:
+            else:
                 project_to_schedule_pending_jobs_count[project_name] += 1
 
         return (

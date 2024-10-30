d031a323966fa91160b101bcd916b4f715a2e5f7
alonmr <alonnmaor@gmail.com>
2024-10-30 14:26:33 +0200

    .

--- server/py/framework/api/deps.py
+++
@@ -1,7 +0,0 @@
-from dependency_injector import containers, providers
-from services.api.api import deps
-
-
-class DepsContainer(containers.DeclarativeContainer):
-    db_session = providers.Callable(deps.get_db_session)
-    authenticate_request = providers.Callable(deps.authenticate_request)

a980b48f09ba435790c4b7b4ccf6c774e5ce555b
rokatyy <ekaterina_molchanova@mckinsey.com>
2024-11-01 11:05:23 +0000

    linting

--- tests/api/crud/test_alert_activations.py
+++ tests/api/crud/test_alert_activations.py
@@ -55,7 +55,10 @@ def test_get_partition_info_for_datetime(
 ):
     # Get actual values from the function
     partition_name, partition_value, partition_expression = (
-        server.api.crud.alert_activation.AlertActivation().get_partition_info_for_datetime(partition_interval, partition_datetime)
+        server.api.crud.alert_activation.AlertActivation().get_partition_info_for_datetime(
+            partition_interval,
+            partition_datetime,
+        )
     )
 
     # Assertions

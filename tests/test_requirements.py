d3390a6523769b6dbf49c1f52a6c7e501ea28dd0
liranbg <liranbg@gmail.com>
2024-11-06 12:40:33 +0200

    initial

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -127,7 +127,6 @@ def test_requirement_specifiers_convention():
         "databricks-sdk": {"~=0.13.0"},
         "distributed": {"~=2023.12.1"},
         "dask": {"~=2023.12.1"},
-        "nbclassic": {">=0.2.8"},
         "gitpython": {"~=3.1, >=3.1.41"},
         "pydantic": {"~=1.10, >=1.10.8"},
         "jinja2": {"~=3.1, >=3.1.3"},

d69e5ffdb247adf4422b176f2560766eb4f37b17
Katerina Molchanova <35141662+rokatyy@users.noreply.github.com>
2024-11-23 10:12:50 +0000

    Update partitioner.py

--- server/py/services/api/utils/db/partitioner.py
+++ server/py/services/api/utils/db/partitioner.py
@@ -18,7 +18,7 @@ from datetime import datetime, timedelta
 from sqlalchemy.orm import Session
 
 import mlrun.common.schemas.partition
-import mlrun.config
+import mlrun.mlconf
 
 import framework.db.sqldb.db
 import framework.utils.singletons.db

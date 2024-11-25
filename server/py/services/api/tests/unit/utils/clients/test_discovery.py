2f830413c0720c55f961712461000964dde23366
alonmr <alonnmaor@gmail.com>
2024-11-25 17:54:28 +0200

    fix discovery tests

--- server/py/services/api/tests/unit/utils/clients/test_discovery.py
+++ server/py/services/api/tests/unit/utils/clients/test_discovery.py
@@ -41,15 +41,14 @@ def test_discovery_register_api_hydra():
         == discovery._resolve_service_url("api-chief")
         == "http://mlrun-api-chief.default.svc.cluster.local:8080"
     )
-    assert re.compile("alert_templates.*") in service_instance.method_routes["put"]
 
 
 def test_star_notation_translation():
-    mlconf.services.hydra.services = "*"
+    mlconf.services.hydra.services = ""
     star_pattern = "projects/.+/alerts.*"
     discovery = framework.utils.clients.discovery.Client()
 
-    chief_routes = discovery._service_routes("api-chief")
+    chief_routes = discovery._service_routes("alerts")
     for methods, pattern in chief_routes:
         if pattern == star_pattern:
             assert methods == ["*"]
@@ -57,7 +56,7 @@ def test_star_notation_translation():
     else:
         assert False, f"pattern {star_pattern} not found in chief routes"
 
-    service_instance = discovery.get_service("api-chief")
+    service_instance = discovery.get_service("alerts")
     route_regex = re.compile(star_pattern)
     assert route_regex in service_instance.method_routes["put"]
     assert route_regex in service_instance.method_routes["post"]
@@ -70,7 +69,7 @@ def test_find_service():
     mlconf.services.hydra.services = "*"
     discovery = framework.utils.clients.discovery.Client()
     service_instance = discovery.resolve_service_by_request(method, path)
-    assert service_instance.name == "api-chief"
+    assert service_instance is None
 
     mlconf.services.hydra.services = ""
     discovery.initialize()

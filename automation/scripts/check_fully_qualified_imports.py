f0db64b5510ab590d0dd28ede1377914b7aea1eb
adamdelman <flyn@flyn.cc>
2024-11-26 17:09:51 +0200

    more fixes

--- automation/scripts/check_fully_qualified_imports.py
+++ automation/scripts/check_fully_qualified_imports.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python3
+# Copyright 2023 Iguazio
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import pathlib
 import re
 import sys
 
@@ -6,7 +21,7 @@ TARGET_MODULES = {"mlrun", "mlrun_pipelines"}
 DISALLOWED_PATTERN = re.compile(r"^\s*from\s+(mlrun|mlrun_pipelines)\s+import")
 
 
-def check_disallowed_imports(file_path: str):
+def check_disallowed_imports(file_path: pathlib.Path) -> list[str]:
     """
     Check for disallowed imports in a file and return a list of violations.
     """
@@ -14,8 +29,10 @@ def check_disallowed_imports(file_path: str):
     with open(file_path) as file:
         for line_num, line in enumerate(file, start=1):
             if DISALLOWED_PATTERN.match(line.strip()):
-                violations.append(f"{file_path}:{line_num}: Disallowed import '{line.strip()}'."
-                                  f" Use 'import {line.split()[1]}' instead.")
+                violations.append(
+                    f"{file_path}:{line_num}: Disallowed import '{line.strip()}'."
+                    f" Use 'import {line.split()[1]}' instead."
+                )
     return violations
 
 

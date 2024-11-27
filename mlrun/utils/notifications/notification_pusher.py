5ccc4088e0f1c0c25aa374941bbcff554a80444d
roei3000b <roei3000b@gmail.com>
2024-11-27 18:27:03 +0200

    [Notifications] Make notification pusher more resistant

--- mlrun/utils/notifications/notification_pusher.py
+++ mlrun/utils/notifications/notification_pusher.py
@@ -118,21 +118,39 @@ class NotificationPusher(_NotificationPusherBase):
         ] = []
 
         for run in self._runs:
-            if isinstance(run, dict):
-                run = mlrun.model.RunObject.from_dict(run)
+            try:
+                if isinstance(run, dict):
+                    run = mlrun.model.RunObject.from_dict(run)
 
-            for notification in run.spec.notifications:
-                try:
-                    notification.status = run.status.notifications.get(
-                        notification.name
-                    ).get("status", mlrun.common.schemas.NotificationStatus.PENDING)
-                except (AttributeError, KeyError):
-                    notification.status = (
-                        mlrun.common.schemas.NotificationStatus.PENDING
-                    )
+                for notification in run.spec.notifications:
+                    try:
+                        try:
+                            notification.status = run.status.notifications.get(
+                                notification.name
+                            ).get(
+                                "status",
+                                mlrun.common.schemas.NotificationStatus.PENDING,
+                            )
+                        except (AttributeError, KeyError):
+                            notification.status = (
+                                mlrun.common.schemas.NotificationStatus.PENDING
+                            )
 
-                if self._should_notify(run, notification):
-                    self._load_notification(run, notification)
+                        if self._should_notify(run, notification):
+                            self._load_notification(run, notification)
+                    except Exception as exc:
+                        logger.warning(
+                            "Failed to load notification",
+                            run_uid=run.metadata.uid,
+                            notification=notification,
+                            error=mlrun.errors.err_to_str(exc),
+                        )
+            except Exception as exc:
+                logger.warning(
+                    "Failed to load notification from run",
+                    run_uid=run.metadata.uid,
+                    error=mlrun.errors.err_to_str(exc),
+                )
 
     def push(self):
         """

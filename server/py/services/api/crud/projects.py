076813a55e2f32643fe3c973e2cda07b16c87349
rokatyy <ekaterina_molchanova@mckinsey.com>
2024-12-03 17:17:57 +0000

    comment

--- server/py/services/api/crud/projects.py
+++ server/py/services/api/crud/projects.py
@@ -353,7 +353,7 @@ class Projects(
             action=action,
         )
         # Filter the original list based on allowed names
-        # we need to return full project object (not only project names)
+        # we need to return list of project objects (not project names)
         return [
             project
             for project in projects_output.projects

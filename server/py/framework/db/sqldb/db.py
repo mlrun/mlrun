e9b9e072656b79693b7ae7d6828121007768a947
rokatyy <ekaterina_molchanova@mckinsey.com>
2024-12-16 10:50:50 +0000

    lint

--- server/py/framework/db/sqldb/db.py
+++ server/py/framework/db/sqldb/db.py
@@ -2076,7 +2076,9 @@ class SQLDB(DBInterface):
             struct = function.struct
             for key, val in updates.items():
                 update_in(struct, key, val)
-            function.kind = struct.pop("kind", None) if not function.kind else function.kind
+            function.kind = (
+                struct.pop("kind", None) if not function.kind else function.kind
+            )
             function.struct = struct
             self._upsert(session, [function])
 

8a9c21bfb0c7a6ed539b72d603ced3deeabd2773
Yacouby <yacouby@iguazio.com>
2024-12-24 09:47:04 +0200

    [Client-spec] Add system id

--- mlrun/db/httpdb.py
+++ mlrun/db/httpdb.py
@@ -580,6 +580,7 @@ class HTTPRunDB(RunDBInterface):
                 or config.feature_store.default_targets
             )
             config.alerts.mode = server_cfg.get("alerts_mode") or config.alerts.mode
+            config.system_id = server_cfg.get("system_id")
 
         except Exception as exc:
             logger.warning(

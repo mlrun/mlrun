05218c3441757046170d24146d903ed152b4adae
roei3000b <roei3000b@gmail.com>
2025-01-12 13:31:30 +0200

    Fix CR

--- mlrun/utils/notifications/notification_pusher.py
+++ mlrun/utils/notifications/notification_pusher.py
@@ -414,14 +414,14 @@ class NotificationPusher(_NotificationPusherBase):
     ):
         # Skip update the notification state if the following conditions are met:
         # 1. the run is not in a terminal state
-        # 2. the notification is not only for running state
+        # 2. the when contains only one state (which is the current state)
         # Skip updating because currently each notification has only one row in the db, even if it has multiple when.
         # This means that if the notification is updated to sent for running state for example, it will not send for
         # The terminal state
         # TODO: Change this behavior after implementing ML-8723
         if (
             run_state not in runtimes_constants.RunStates.terminal_states()
-            and notification.when != [runtimes_constants.RunStates.running]
+            and len(notification.when) > 1
         ):
             logger.debug(
                 "Skip updating notification status - run not in terminal state",

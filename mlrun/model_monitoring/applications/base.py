7f57bd0f1fbfe47446a3e66f5314c02bf9155863
royischoss <royishoss@gmail.com>
2025-02-04 12:11:08 +0200

    debug

--- mlrun/model_monitoring/applications/base.py
+++ mlrun/model_monitoring/applications/base.py
@@ -135,6 +135,7 @@ class ModelMonitoringApplicationBase(MonitoringApplicationToDict, ABC):
                         (endpoint.metadata.name, endpoint.metadata.uid)
                         for endpoint in endpoints_list
                     ]
+                    context.logger.info("endpoints", endpoints=endpoints, list_endpoints=endpoints_list)
                 else:
                     raise mlrun.errors.MLRunNotFoundError(
                         f"Did not find any model endpoint named ' {endpoints}'"

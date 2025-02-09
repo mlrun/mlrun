b759adae52cf33a192c919cddc191717358d4b51
Gal Topper <gal.topper@gmail.com>
2025-02-09 19:04:11 +0800

    Tolerate empty lines in stderr

--- tests/test_import_mlrun.py
+++ tests/test_import_mlrun.py
@@ -25,7 +25,7 @@ def test_import_mlrun():
     stderr_lines = out.stderr.decode("utf-8").strip().split("\n")
     unexpected_stdout_errors = [line for line in stdout_lines if "[error]" in line]
     unexpected_stderr_errors = [
-        line for line in stderr_lines if line not in acceptable_stderr_errors
+        line for line in stderr_lines if line and line not in acceptable_stderr_errors
     ]
     assert unexpected_stdout_errors == [], "`import mlrun` wrote unexpected error logs"
     assert (

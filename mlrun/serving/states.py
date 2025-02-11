eed3a7dc0e197edf1b0b154f2331a15e6e1eff22
jillnogold <jillg@iguazio.com>
2025-02-11 18:05:51 +0200

    fix docstring format

--- mlrun/serving/states.py
+++ mlrun/serving/states.py
@@ -356,15 +356,22 @@ class BaseStep(ModelObj):
                             event: {"x": 5} , result_path="y" means the output of the step will be written
                             to event["y"] resulting in {"x": 5, "y": <result>}
         :param model_endpoint_creation_strategy: Strategy for creating or updating the model endpoint:
-            * **overwrite**:
-            1. If model endpoints with the same name exist, delete the `latest` one.
-            2. Create a new model endpoint entry and set it as `latest`.
-            * **inplace** (default):
-            1. If model endpoints with the same name exist, update the `latest` entry.
-            2. Otherwise, create a new entry.
-            * **archive**:
-            1. If model endpoints with the same name exist, preserve them.
-            2. Create a new model endpoint with the same name and set it to `latest`.
+            
+                            * **overwrite**:
+                            
+                            1. If model endpoints with the same name exist, delete the `latest` one.
+                            2. Create a new model endpoint entry and set it as `latest`.
+                            
+                            * **inplace** (default):
+                            
+                            1. If model endpoints with the same name exist, update the `latest` entry.
+                            2. Otherwise, create a new entry.
+                            
+                            * **archive**:
+                                                        
+                            1. If model endpoints with the same name exist, preserve them.
+                            2. Create a new model endpoint with the same name and set it to `latest`.
+            
         :param class_args:  class init arguments
         """
         if hasattr(self, "steps"):
@@ -803,16 +810,22 @@ class RouterStep(TaskStep):
         :param handler:    class handler to invoke on run/event
         :param function:   function this step should run in
         :param creation_strategy: Strategy for creating or updating the model endpoint:
-            * **overwrite**:
-            1. If model endpoints with the same name exist, delete the `latest` one.
-            2. Create a new model endpoint entry and set it as `latest`.
-            * **inplace** (default):
-            1. If model endpoints with the same name exist, update the `latest` entry.
-            2. Otherwise, create a new entry.
-            * **archive**:
-            1. If model endpoints with the same name exist, preserve them.
-            2. Create a new model endpoint with the same name and set it to `latest`.
 
+                           * **overwrite**:
+                           
+                           1. If model endpoints with the same name exist, delete the `latest` one.
+                           2. Create a new model endpoint entry and set it as `latest`.
+
+                           * **inplace** (default):
+                           
+                           1. If model endpoints with the same name exist, update the `latest` entry.
+                           2. Otherwise, create a new entry.
+
+                           * **archive**:
+                           
+                           1. If model endpoints with the same name exist, preserve them.
+                           2. Create a new model endpoint with the same name and set it to `latest`.
+                            
         """
         if len(self.routes.keys()) >= MAX_MODELS_PER_ROUTER and key not in self.routes:
             raise mlrun.errors.MLRunModelLimitExceededError(
@@ -1188,15 +1201,22 @@ class FlowStep(BaseStep):
                             event: {"x": 5} , result_path="y" means the output of the step will be written
                             to event["y"] resulting in {"x": 5, "y": <result>}
         :param model_endpoint_creation_strategy: Strategy for creating or updating the model endpoint:
-            * **overwrite**:
-            1. If model endpoints with the same name exist, delete the `latest` one.
-            2. Create a new model endpoint entry and set it as `latest`.
-            * **inplace** (default):
-            1. If model endpoints with the same name exist, update the `latest` entry.
-            2. Otherwise, create a new entry.
-            * **archive**:
-            1. If model endpoints with the same name exist, preserve them.
-            2. Create a new model endpoint with the same name and set it to `latest`.
+        
+                            * **overwrite**:
+                            
+                            1. If model endpoints with the same name exist, delete the `latest` one.
+                            2. Create a new model endpoint entry and set it as `latest`.
+                            
+                            * **inplace** (default):
+                            
+                            1. If model endpoints with the same name exist, update the `latest` entry.
+                            2. Otherwise, create a new entry.
+                            
+                            * **archive**:
+                            
+                            1. If model endpoints with the same name exist, preserve them.
+                            2. Create a new model endpoint with the same name and set it to `latest`.
+            
         :param class_args:  class init arguments
         """
 

22d52ea7e50d2f080e2f2bf405f4c7b419884394
alonmr <alonnmaor@gmail.com>
2025-02-19 14:27:17 +0200

    minor

--- mlrun/common/schemas/auth.py
+++ mlrun/common/schemas/auth.py
@@ -114,7 +114,7 @@ class AuthorizationVerificationInput(pydantic.v1.BaseModel):
 
 
 class AuthInfo(pydantic.v1.BaseModel):
-    # Metadata
+    # Keep request headers for inter-service communication
     request_headers: typing.Optional[dict[str, str]] = None
     # Basic + Iguazio auth
     username: typing.Optional[str] = None

870e7e50ca78845fe4898e1c203aa19bc3e43608
liranbg <liranbg@gmail.com>
2025-02-23 19:51:44 +0200

    that too

--- tests/integration/sdk_api/artifacts/test_artifacts.py
+++ tests/integration/sdk_api/artifacts/test_artifacts.py
@@ -207,9 +207,9 @@ class TestArtifacts(tests.integration.sdk_api.base.TestMLRunIntegration):
         assert artifacts[1].tag is None
 
         # Assert attempting to retrieve an artifact with a URI missing the UID raises the expected error
-        uri_without_uid = artifacts[0].uri.split("^")[0]
+        uri_without_uid = artifacts[1].uri.split("^")[0]
         with pytest.raises(mlrun.errors.MLRunNotFoundError):
             project.get_store_resource(uri_without_uid)
 
         # Ensure we can retrieve the untagged artifact by its URI
-        assert project.get_store_resource(artifacts[0].uri)
+        assert project.get_store_resource(artifacts[1].uri)

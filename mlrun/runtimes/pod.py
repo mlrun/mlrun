c044cfb847ae62867da547d1840679ee008631ea
Yacouby <yacouby@iguazio.com>
2025-02-25 15:54:26 +0200

    [Runtimes] fix code duplication in build watch logic

--- mlrun/runtimes/pod.py
+++ mlrun/runtimes/pod.py
@@ -1578,15 +1578,12 @@ class KubeResource(BaseRuntime):
                 f"Started building image: {data.get('data', {}).get('spec', {}).get('build', {}).get('image')}"
             )
         if watch and not ready:
-            state = self._build_watch(
+            self.status.state = self._build_watch(
                 watch=watch,
                 show_on_failure=show_on_failure,
             )
-            ready = state == "ready"
-            self.status.state = state
-
-        if watch and not ready:
-            raise mlrun.errors.MLRunRuntimeError("Deploy failed")
+            if self.status.state != "ready":
+                raise mlrun.errors.MLRunRuntimeError("Deploy failed")
         return ready
 
     def _build_watch(

ba1816d8a90d62af57e107affc77b2e743b5db5a
Yacouby <yacouby@iguazio.com>
2025-02-25 16:33:53 +0200

    .

--- mlrun/runtimes/pod.py
+++ mlrun/runtimes/pod.py
@@ -1578,11 +1578,11 @@ class KubeResource(BaseRuntime):
                 f"Started building image: {data.get('data', {}).get('spec', {}).get('build', {}).get('image')}"
             )
         if watch and not ready:
-            state = self._build_watch(
+            self.status.state = self._build_watch(
                 watch=watch,
                 show_on_failure=show_on_failure,
             )
-            ready = state == "ready"
+            ready = self.status.state == "ready"
             if not ready:
                 raise mlrun.errors.MLRunRuntimeError("Deploy failed")
         return ready

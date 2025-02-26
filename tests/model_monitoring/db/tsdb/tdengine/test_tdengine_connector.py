6fca49fef07c1e4e6743433cb0acf335e81a982a
Eyal-Danieli <eyald@iguazio.com>
2025-02-26 17:53:28 +0200

    add test

--- tests/model_monitoring/db/tsdb/tdengine/test_tdengine_connector.py
+++ tests/model_monitoring/db/tsdb/tdengine/test_tdengine_connector.py
@@ -31,7 +31,6 @@ from mlrun.model_monitoring.db.tsdb.tdengine import TDEngineConnector
 project = "test-tdengine-connector"
 connection_string = os.getenv("MLRUN_MODEL_ENDPOINT_MONITORING__TSDB_CONNECTION")
 database = "test_tdengine_connector_" + uuid.uuid4().hex
-connection_string = "taosws://root:taosdata@localhost:6041"
 
 
 def drop_database(connection: taosws.Connection) -> None:

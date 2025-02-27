d79f80e87ef2a81fc8690a75f0ec5fe081bdc3c0
adamdelman <flyn@flyn.cc>
2025-02-27 15:06:43 +0200

    fix tests

--- mlrun/utils/async_http.py
+++ mlrun/utils/async_http.py
@@ -27,7 +27,7 @@ from mlrun.config import config
 from mlrun.errors import err_to_str
 from mlrun.errors import raise_for_status as ml_raise_for_status
 
-from .helpers import logger as mlrun_logger
+from mlrun.utils.helpers import logger as mlrun_logger
 
 DEFAULT_BLACKLISTED_METHODS = [
     "POST",

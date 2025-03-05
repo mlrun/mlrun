4e0cdaa43a76a061412d9409fbd77f5d75356852
alxtkr77 <alext@iguazio.com>
2025-03-05 06:45:16 -0500

    [ModelMonitoring] Support multiple workers for mm applications

--- mlrun/config.py
+++ mlrun/config.py
@@ -567,18 +567,18 @@ default_config = {
         },
         "application_stream_args": {
             "v3io": {
-                "shard_count": 1,
+                "shard_count": 8,
                 "retention_period_hours": 24,
-                "num_workers": 1,
-                "min_replicas": 1,
-                "max_replicas": 1,
+                "num_workers": 4,
+                "min_replicas": 8,
+                "max_replicas": 8,
             },
             "kafka": {
-                "partition_count": 1,
+                "partition_count": 16,
                 "replication_factor": 1,
-                "num_workers": 1,
+                "num_workers": 4,
                 "min_replicas": 1,
-                "max_replicas": 1,
+                "max_replicas": 8,
             },
         },
         "writer_stream_args": {

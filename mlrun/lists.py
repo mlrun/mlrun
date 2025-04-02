ae0578350d7d2e13ec7a04b7712c9d4666ae9170
moranbental <moranb@iguazio.com>
2025-04-02 15:39:50 +0300

    fix

--- mlrun/lists.py
+++ mlrun/lists.py
@@ -106,7 +106,7 @@ class RunList(list):
         rows = self.to_rows(extend_iterations=extend_iterations)
         df = pd.DataFrame(rows[1:], columns=rows[0])  # .set_index('iter')
         for time_column in ["start", "end"]:
-            # Using the format to handle cases where there are no milliseconds in the timestamp
+            # Using the format to handle cases where the timestamp has milliseconds and where it does not
             df[time_column] = pd.to_datetime(df[time_column], format="ISO8601")
 
         if flat:

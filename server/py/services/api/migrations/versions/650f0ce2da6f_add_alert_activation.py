62c573d413feb17b7e9f76b635401861de2cd9f5
Gal Topper <gal.topper@gmail.com>
2025-04-06 16:07:54 +0700

    [All] Standardize copyright message end

--- server/py/services/api/migrations/versions/650f0ce2da6f_add_alert_activation.py
+++ server/py/services/api/migrations/versions/650f0ce2da6f_add_alert_activation.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
 """add alert activation
 
 Revision ID: 650f0ce2da6f
--- tests/run/assets/hyper_func.py
+++ tests/run/assets/hyper_func.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-
 
 def hyper_func(context, p1, p2, p3):
     print(f"p2={p2}, p3={p3}")

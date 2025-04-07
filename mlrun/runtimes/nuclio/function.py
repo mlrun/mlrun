cea89f913a2597a8b92714654eb29f32d9f1a7e8
Yacouby <yacouby@iguazio.com>
2025-04-07 09:50:45 +0300

    use deepmerge

--- mlrun/runtimes/nuclio/function.py
+++ mlrun/runtimes/nuclio/function.py
@@ -27,6 +27,7 @@ import requests
 import semver
 from aiohttp.client import ClientSession
 from kubernetes import client
+from mergedeep import merge
 from nuclio.deploy import find_dashboard_url, get_deploy_status
 from nuclio.triggers import V3IOStreamTrigger
 
@@ -651,7 +652,7 @@ class RemoteRuntime(KubeResource):
         logger.info("Starting remote function deploy")
         data = db.deploy_nuclio_function(func=self, builder_env=builder_env)
         self.status = data["data"].get("status")
-        self.spec = data["data"].get("spec")
+        self.spec = merge(self.spec.to_dict(), data["data"].get("spec"))
 
         self._update_credentials_from_remote_build(data["data"])
 

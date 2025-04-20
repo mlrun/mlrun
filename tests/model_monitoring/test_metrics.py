15b8d2d9342dbc4ce132b23ccd649e575ccf760e
Gal Topper <gal.topper@gmail.com>
2025-04-20 14:44:52 +0700

    [Tests] Disable hypothesis health check to fix failures in CI

--- tests/model_monitoring/test_metrics.py
+++ tests/model_monitoring/test_metrics.py
@@ -17,7 +17,7 @@ from typing import Union
 
 import numpy as np
 import pytest
-from hypothesis import given
+from hypothesis import HealthCheck, given, settings
 from hypothesis import strategies as st
 from hypothesis.extra.numpy import arrays
 
@@ -144,6 +144,7 @@ def two_distributions_strategy(draw: st.DrawFn) -> tuple[np.ndarray, np.ndarray]
 class TestMetricProperties:
     @staticmethod
     @given(distrib=distribution_strategy())
+    @settings(suppress_health_check=(HealthCheck.too_slow,))
     def test_same_distrib_gives_zero_distance(
         metric_class: type[HistogramDistanceMetric], distrib: np.ndarray
     ) -> None:
@@ -157,6 +158,7 @@ class TestMetricProperties:
 
     @staticmethod
     @given(two_distributions=two_distributions_strategy())
+    @settings(suppress_health_check=(HealthCheck.too_slow,))
     def test_symmetric(
         metric_class: type[HistogramDistanceMetric],
         two_distributions: tuple[np.ndarray, np.ndarray],

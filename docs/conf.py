a3265de4e1d7d5f9144e5bb415ecc9a09ccca4f5
jillnogold <jillg@iguazio.com>
2025-04-22 11:18:58 +0300

    fmt

--- docs/conf.py
+++ docs/conf.py
@@ -212,7 +212,6 @@ linkcheck_ignore = [
     "https://docs.databricks.com/aws/en/reference/jobs-2.0-api"
     # can be removed after v1.8.0 is released:
     "https://docs.mlrun.org/en/stable/api/mlrun.datastore/index.html#mlrun.datastore.datastore_profile.TDEngineDatastoreProfile",
-
     # Mckinsey restricted
     "https://ollama.com/download",
     "https://ollama.com/library/llama3",

8bd6c079d7ffabff37eef2d51d3c7b801cea4325
adamdelman <flyn@flyn.cc>
2025-04-23 15:34:49 +0300

    fix

--- server/py/services/api/migrations/env.py
+++ server/py/services/api/migrations/env.py
@@ -87,58 +87,59 @@ def run_migrations_online():
         )
 
     with connectable.connect() as connection:
-        # This query retrieves information about database connections that have acquired
-        # locks on objects in the 'mlrun' schema,
-        # excluding the current connection and the 'alembic_version' table.
-        # This is order to find and kill connections that might be blocking the migration.
-        connection_ids = connection.execute(
-            sqlalchemy.sql.text(
-                """SELECT
-                t.PROCESSLIST_ID,
-                t.PROCESSLIST_USER,
-                t.PROCESSLIST_HOST,
-                GROUP_CONCAT(DISTINCT ml.OBJECT_NAME ORDER BY ml.OBJECT_NAME SEPARATOR ', ') AS locked_objects
-            FROM
-                performance_schema.metadata_locks AS ml
-            INNER JOIN
-                performance_schema.threads AS t
-                ON ml.OWNER_THREAD_ID = t.THREAD_ID
-            WHERE
-                t.PROCESSLIST_ID <> CONNECTION_ID()
-                AND ml.OBJECT_SCHEMA = 'mlrun'
-                AND ml.OBJECT_NAME != 'alembic_version'
-                AND ml.LOCK_STATUS = 'GRANTED'
-            GROUP BY
-                t.PROCESSLIST_ID,
-                t.PROCESSLIST_USER,
-                t.PROCESSLIST_HOST
-            ORDER BY
-                t.PROCESSLIST_ID;
-            """
-            )
-        ).fetchall()
-        for connection_id, user, host, locked_objects in connection_ids:
-            logger.warning(
-                "Killing DB connection with acquired lock.",
-                connection_id=connection_id,
-                user=user,
-                host=host,
-                locked_objects=locked_objects,
-                db="mlrun",
-            )
-            try:
-                connection.execute(sqlalchemy.sql.text(f"KILL {connection_id};"))
-            except sqlalchemy.exc.OperationalError as exc:
-                if "Unknown thread id" in str(exc):
-                    logger.warning(
-                        "DB connection already closed.",
-                        connection_id=connection_id,
-                        user=user,
-                        host=host,
-                        db="mlrun",
-                    )
-                else:
-                    raise exc
+        if connection.dialect.name == "mysql":
+            # This query retrieves information about database connections that have acquired
+            # locks on objects in the 'mlrun' schema,
+            # excluding the current connection and the 'alembic_version' table.
+            # This is order to find and kill connections that might be blocking the migration.
+            connection_ids = connection.execute(
+                sqlalchemy.sql.text(
+                    """SELECT
+                    t.PROCESSLIST_ID,
+                    t.PROCESSLIST_USER,
+                    t.PROCESSLIST_HOST,
+                    GROUP_CONCAT(DISTINCT ml.OBJECT_NAME ORDER BY ml.OBJECT_NAME SEPARATOR ', ') AS locked_objects
+                FROM
+                    performance_schema.metadata_locks AS ml
+                INNER JOIN
+                    performance_schema.threads AS t
+                    ON ml.OWNER_THREAD_ID = t.THREAD_ID
+                WHERE
+                    t.PROCESSLIST_ID <> CONNECTION_ID()
+                    AND ml.OBJECT_SCHEMA = 'mlrun'
+                    AND ml.OBJECT_NAME != 'alembic_version'
+                    AND ml.LOCK_STATUS = 'GRANTED'
+                GROUP BY
+                    t.PROCESSLIST_ID,
+                    t.PROCESSLIST_USER,
+                    t.PROCESSLIST_HOST
+                ORDER BY
+                    t.PROCESSLIST_ID;
+                """
+                )
+            ).fetchall()
+            for connection_id, user, host, locked_objects in connection_ids:
+                logger.warning(
+                    "Killing DB connection with acquired lock.",
+                    connection_id=connection_id,
+                    user=user,
+                    host=host,
+                    locked_objects=locked_objects,
+                    db="mlrun",
+                )
+                try:
+                    connection.execute(sqlalchemy.sql.text(f"KILL {connection_id};"))
+                except sqlalchemy.exc.OperationalError as exc:
+                    if "Unknown thread id" in str(exc):
+                        logger.warning(
+                            "DB connection already closed.",
+                            connection_id=connection_id,
+                            user=user,
+                            host=host,
+                            db="mlrun",
+                        )
+                    else:
+                        raise exc
 
         context.configure(connection=connection, target_metadata=target_metadata)
 

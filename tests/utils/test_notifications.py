a6dd1896931c8db698a9314219327ee7640cec0e
adamdelman <flyn@flyn.cc>
2025-04-24 15:55:06 +0300

    add tests

--- tests/utils/test_notifications.py
+++ tests/utils/test_notifications.py
@@ -998,11 +998,11 @@ def test_notification_validation_defaults(monkeypatch):
         "name": "",
     }
 
-    for field, expected_value in notification_fields.items():
-        value = getattr(notification, field)
+    for notification_field, expected_value in notification_fields.items():
+        value = getattr(notification, notification_field)
         assert (
             value == expected_value
-        ), f"{field} field value is {value}, expected {expected_value}"
+        ), f"{notification_field} field value is {value}, expected {expected_value}"
 
 
 @pytest.mark.parametrize(

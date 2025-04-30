b400873405d41be197a28b13f00e8a88e54f0ca4
Gal Topper <gal.topper@gmail.com>
2025-04-30 13:28:38 +0300

    [Model Monitoring] Remove taoswrap dependency
    Port its basic functionality into mlrun.
    
    [ML-9900](https://iguazio.atlassian.net/browse/ML-9900)

--- mlrun/config.py
+++ mlrun/config.py
@@ -635,12 +635,6 @@ default_config = {
         "offline_storage_path": "model-endpoints/{kind}",
         "parquet_batching_max_events": 10_000,
         "parquet_batching_timeout_secs": timedelta(minutes=1).total_seconds(),
-        "tdengine": {
-            "run_directly": True,
-            # timeout and retry are ignored when run_directly is set to True
-            "timeout": 10,
-            "retries": 1,
-        },
     },
     "secret_stores": {
         # Use only in testing scenarios (such as integration tests) to avoid using k8s for secrets (will use in-memory

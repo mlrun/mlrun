2dee93fc214448654c211d49a43d4eb995e2efab
royischoss <royishoss@gmail.com>
2025-05-13 16:09:46 +0300

    small fix

--- mlrun/runtimes/nuclio/serving.py
+++ mlrun/runtimes/nuclio/serving.py
@@ -642,7 +642,7 @@ class ServingRuntime(RemoteRuntime):
         for step in self.spec.graph.steps.values():
             if isinstance(step, mlrun.serving.states.ModelRunnerStep):
                 error_step = step.to(
-                    class_name="mlrun.serving.states.ModelRunnerErrorRaise",
+                    class_name="mlrun.serving.states.ModelRunnerErrorRaiser",
                     name=f"{step.name}_error_raise",
                     raise_exception = step._raise_exception,
                     models_names = list(step.class_args["models"].keys()),

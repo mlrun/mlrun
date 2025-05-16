23084874499c7c57269c6af38af6d7194cbe8d24
GiladShapira94 <giladsh@iguazio.com>
2025-05-16 15:14:59 +0000

    fix

--- docs/tutorials/src/data-prep.py
+++ docs/tutorials/src/data-prep.py
@@ -5,7 +5,7 @@ from sklearn.datasets import load_breast_cancer
 
 def breast_cancer_generator():
     """
-    A function that generates the breast cancer dataset
+    A function which generates the breast cancer dataset
     """
     breast_cancer = load_breast_cancer()
     breast_cancer_dataset = pd.DataFrame(

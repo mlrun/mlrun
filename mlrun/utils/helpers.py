a1ee550ee7558bcf14425feb522981931dde1d6e
liranbg <liranbg@gmail.com>
2025-05-22 16:38:10 +0300

    60 seconds is too long waiting for when we send start notification

--- mlrun/utils/helpers.py
+++ mlrun/utils/helpers.py
@@ -2219,9 +2219,9 @@ class Workflow:
     ) -> typing.Optional[mlrun_pipelines.models.PipelineManifest]:
         kfp_client = mlrun_pipelines.utils.get_client(mlrun.mlconf.kfp_url)
 
-        # arbitrary timeout of 60 seconds, the workflow should be done by now, however sometimes kfp takes a few
+        # arbitrary timeout of 30 seconds, the workflow should be done by now, however sometimes kfp takes a few
         # seconds to update the workflow status
-        kfp_run = kfp_client.wait_for_run_completion(workflow_id, 60)
+        kfp_run = kfp_client.wait_for_run_completion(workflow_id, 30)
         if not kfp_run:
             return None
 

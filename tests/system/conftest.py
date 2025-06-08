bac5e4f936a2ba2239d1ac6880c7394f39800b7a
liranbg <liranbg@gmail.com>
2025-06-08 16:21:55 +0300

    zz

--- tests/system/conftest.py
+++ tests/system/conftest.py
@@ -64,10 +64,12 @@ def post_report_session_finish_to_slack(
     reporter: TerminalReporter = session.config.pluginmanager.get_plugin(
         "terminalreporter"
     )
+
+    # pytest < 8.4
     if hasattr(reporter, "_sessionstarttime"):
         test_duration = time.time() - reporter._sessionstarttime
     else:
-        test_duration = "N/A"
+        test_duration = reporter._session_start.elapsed()
     mlrun_version = os.getenv("MLRUN_VERSION", "")
     mlrun_current_branch = os.getenv("MLRUN_SYSTEM_TESTS_BRANCH", "")
     mlrun_system_tests_component = os.getenv("MLRUN_SYSTEM_TESTS_COMPONENT", "")

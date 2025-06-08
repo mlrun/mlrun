f7574dc76354ad9978f3fb26b1cc147fcb7778ff
alonmr <alonnmaor@gmail.com>
2025-06-08 16:19:18 +0300

    dm

--- tests/system/projects/test_project.py
+++ tests/system/projects/test_project.py
@@ -23,8 +23,8 @@ from sys import executable
 import igz_mgmt
 import pandas as pd
 import pytest
+from kfp import dsl
 
-# from kfp import dsl
 import mlrun
 import mlrun.common.runtimes.constants
 import mlrun.common.schemas
@@ -52,7 +52,7 @@ def exec_project(args):
 
 
 # pipeline for inline test (run pipeline from handler)
-# @dsl.pipeline(name="test pipeline", description="test")
+@dsl.pipeline(name="test pipeline", description="test")
 def pipe_test():
     # train the model using a library (hub://) function and the generated data
     funcs["auto-trainer"].as_step(

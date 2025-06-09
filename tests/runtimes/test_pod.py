5d997ff3843b2387f1de1573ec293a8c4fafce4a
moranbental <moranb@iguazio.com>
2025-06-09 16:52:26 +0300

    fix tests

--- tests/runtimes/test_pod.py
+++ tests/runtimes/test_pod.py
@@ -256,9 +256,8 @@ def test_build_config_with_multiple_commands():
     fn.build_config(commands=["pip install pandas", "pip install numpy"])
     assert len(fn.spec.build.commands) == 2
 
-    # By default, overwrite=True, which means the existing parameters are replaced with the new ones
-    fn.build_config(commands=["pip install pandas"])
-    assert len(fn.spec.build.commands) == 1
+    fn.build_config(commands=["pip install pandas"], overwrite=False)
+    assert len(fn.spec.build.commands) == 2
 
 
 def test_build_config_preserve_order():

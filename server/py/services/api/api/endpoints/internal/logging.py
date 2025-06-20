ce8b23ec33abfea95bdcdf450d907f83b5cb3753
adamdelman <flyn@flyn.cc>
2025-06-20 20:51:54 +0300

    fix

--- server/py/services/api/api/endpoints/internal/logging.py
+++ server/py/services/api/api/endpoints/internal/logging.py
@@ -1,4 +1,17 @@
-# ---- logging endpoint -------------------------------------------------
+# Copyright 2025 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import logging
 import os
 
@@ -6,8 +19,8 @@ import fastapi
 from fastapi import Request
 from fastapi.openapi.models import Response
 
-from mlrun.common.schemas.internal.logging import LogLevelMapping
-from mlrun.errors import MLRunBadRequestError
+import mlrun.common.schemas.internal.logging
+import mlrun.errors
 
 import services.discovery.service
 
@@ -18,12 +31,12 @@ _discovery_service = services.discovery.service.K8sServiceDiscovery(
 
 
 def _apply_log_level_locally(
-    cfg: LogLevelMapping,
+    cfg: mlrun.common.schemas.internal.logging.LogLevelMapping,
 ) -> None:
     for domain, level in cfg.domain_to_levels.items():
         num = getattr(logging, level.upper(), None)
         if num is None:
-            raise MLRunBadRequestError(f"Invalid log level '{level}'")
+            raise mlrun.errors.MLRunBadRequestError(f"Invalid log level '{level}'")
         if cfg.recursive:
             for name, logger in logging.root.manager.loggerDict.items():
                 if isinstance(logger, logging.Logger) and (
@@ -36,7 +49,7 @@ def _apply_log_level_locally(
 
 @router.post("")
 async def set_log_levels(
-    cfg: LogLevelMapping,
+    cfg: mlrun.common.schemas.internal.logging.LogLevelMapping,
     request: Request,
 ):
     _apply_log_level_locally(cfg)
@@ -52,10 +65,10 @@ async def set_log_levels(
 
 @router.get(
     "",
-    response_model=LogLevelMapping,
+    response_model=mlrun.common.schemas.internal.logging.LogLevelMapping,
 )
 async def get_log_levels():
-    return LogLevelMapping(
+    return mlrun.common.schemas.internal.logging.LogLevelMapping(
         domain_to_levels={
             n: logging.getLevelName(lg.getEffectiveLevel())
             for n, lg in logging.root.manager.loggerDict.items()

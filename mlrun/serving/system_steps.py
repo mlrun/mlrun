98afca43e2d0daeac1be31ccc496d0303c199de2
royischoss <royishoss@gmail.com>
2025-07-07 12:53:31 +0300

    remove comment

--- mlrun/serving/system_steps.py
+++ mlrun/serving/system_steps.py
@@ -261,11 +261,8 @@ class BackgroundTaskStatus(storey.MapClass):
         super().__init__(**kwargs)
 
     def do(self, event):
-        # if (self.context and self.context.is_mock) or self.context is None:
-        #     return event
         if self.server is None:
             return None
-
         if (
             self._background_task_state
             == mlrun.common.schemas.BackgroundTaskState.running

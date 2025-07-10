3a22adc6647d66faf0995077d4b2691feba21b59
Gal Topper <gal.topper@gmail.com>
2025-07-10 12:29:11 +0700

    Revert "Fix import lint"
    This reverts commit 1da08fca4e18134ea0c3900cf43a640c61a5f27b.

--- server/py/framework/api/utils.py
+++ server/py/framework/api/utils.py
@@ -45,6 +45,9 @@ from mlrun.run import import_function, new_function
 from mlrun.runtimes import RuntimeKinds
 from mlrun.runtimes.utils import enrich_function_from_dict
 from mlrun.utils import get_in, logger
+from server.py.services.api.utils.model_monitoring import (
+    start_model_endpoint_creation_background_task,
+)
 
 import framework.constants
 import framework.db.session
@@ -204,60 +207,6 @@ def _generate_function_and_task_from_submit_run_body(db_session: Session, data):
     apply_enrichment_and_validation_on_task(task)
 
     return function, task
-
-
-async def start_model_endpoint_creation_background_task(
-    project: str,
-    name: str,
-    background_tasks: fastapi.BackgroundTasks,
-    function: dict,
-    db_session: sqlalchemy.orm.Session,
-):
-    # avoid circular import
-    import services.api.crud.model_monitoring.deployment as mm_deployment
-
-    returned_background_tasks = mlrun.common.schemas.BackgroundTaskList(
-        background_tasks=[]
-    )
-    kind = function.get("kind")
-    if (
-        kind == RuntimeKinds.serving
-        or kind == RuntimeKinds.job
-        and function["spec"].get("serving_spec")
-    ):
-        monitoring_deployment = mm_deployment.MonitoringDeployment(project=project)
-        (
-            model_endpoints_instructions,
-            function,
-        ) = await monitoring_deployment._create_model_endpoints_instructions(
-            db_session=db_session,
-            function=function,
-            function_name=name,
-            project=project,
-        )
-        logger.info(
-            "Creating Background Task for model endpoints creation",
-            project=project,
-            function=name,
-        )
-        returned_background_task = await run_in_threadpool(
-            monitoring_deployment._create_model_endpoint_background_task,
-            db_session=db_session,
-            background_tasks=background_tasks,
-            project_name=project,
-            function_name=name,
-            function_tag=function.get("metadata", {}).get("tag") or "latest",
-            model_endpoints_instructions=model_endpoints_instructions,
-        )
-        returned_background_tasks.background_tasks.append(returned_background_task)
-
-    model_endpoint_creation_task_name = (
-        returned_background_tasks.background_tasks[0].metadata.name
-        if returned_background_tasks.background_tasks
-        else None
-    )
-
-    return model_endpoint_creation_task_name, returned_background_tasks
 
 
 async def submit_run(

f6bcb03944d1dbdc42606b9353f004e4cff2f21d
Tomer Mamia <tomerm@iguazio.com>
2025-07-10 11:31:11 +0300

    start working on hf

---
+++ mlrun/datastore/model_provider/hugging_face_provider.py
@@ -0,0 +1,187 @@
+# Copyright 2025 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from collections.abc import Awaitable
+from typing import Callable, Optional, TypeVar, Union
+
+import mlrun
+from mlrun.datastore.model_provider.model_provider import ModelProvider
+
+T = TypeVar("T")
+
+
+class HuggingFaceProvider(ModelProvider):
+    """
+    OpenAIProvider is a wrapper around the OpenAI SDK that provides an interface
+    for interacting with OpenAI's generative AI services.
+
+    It supports both synchronous and asynchronous operations, allowing flexible
+    integration into various workflows.
+
+    This class extends the ModelProvider base class and implements OpenAI-specific
+    functionality, including client initialization, model invocation, and custom
+    operations tailored to the OpenAI API.
+    """
+
+    support_async = True
+
+    def __init__(
+        self,
+        parent,
+        schema,
+        name,
+        endpoint="",
+        secrets: Optional[dict] = None,
+        default_invoke_kwargs: Optional[dict] = None,
+    ):
+        endpoint = endpoint or mlrun.mlconf.model_providers.openai_default_model
+        if schema != "huggingface":
+            raise mlrun.errors.MLRunInvalidArgumentError(
+                "OpenAIProvider supports only 'openai' as the provider kind."
+            )
+        super().__init__(
+            parent=parent,
+            kind=schema,
+            name=name,
+            endpoint=endpoint,
+            secrets=secrets,
+            default_invoke_kwargs=default_invoke_kwargs,
+        )
+        self.options = self.get_client_options()
+        self.load_client()
+
+    @classmethod
+    def parse_endpoint_and_path(cls, endpoint, subpath) -> (str, str):
+        if endpoint and subpath:
+            endpoint = endpoint + subpath
+            #  in openai there is no usage of subpath variable. if the model contains "/", it is part of the model name.
+            subpath = ""
+        return endpoint, subpath
+
+    @property
+    def model(self):
+        return self.endpoint
+
+    def load_client(self) -> None:
+        try:
+            from openai import OpenAI, AsyncOpenAI  # noqa
+
+            self._client = OpenAI(**self.options)
+            self._default_operation = self.client.chat.completions.create
+
+            self._async_client = AsyncOpenAI(**self.options)
+            self._default_async_operation = self.async_client.chat.completions.create
+        except ImportError as exc:
+            raise ImportError("openai package is not installed") from exc
+
+    def get_client_options(self):
+        res = dict(
+            api_key=self._get_secret_or_env("OPENAI_API_KEY"),
+            organization=self._get_secret_or_env("OPENAI_ORG_ID"),
+            project=self._get_secret_or_env("OPENAI_PROJECT_ID"),
+            base_url=self._get_secret_or_env("OPENAI_BASE_URL"),
+            timeout=self._get_secret_or_env("OPENAI_TIMEOUT"),
+            max_retries=self._get_secret_or_env("OPENAI_MAX_RETRIES"),
+        )
+        return self._sanitize_options(res)
+
+    def custom_invoke(
+        self, operation: Optional[Callable[..., T]] = None, **invoke_kwargs
+    ) -> Optional[T]:
+        """
+        OpenAI-specific implementation of `ModelProvider.custom_invoke`.
+
+        Invokes an OpenAI model operation using the sync client. For full details, see
+        `ModelProvider.custom_invoke`.
+
+        Example:
+            ```python
+            result = openai_model_provider.invoke(
+                openai_model_provider.client.images.generate,
+                prompt="A futuristic cityscape at sunset",
+                n=1,
+                size="1024x1024",
+            )
+            ```
+        :param operation: A callable representing the model operation (e.g., a client method).
+        :param invoke_kwargs: Keyword arguments to pass to the operation.
+        :return: The full response returned by the operation.
+
+        """
+        invoke_kwargs = self.get_invoke_kwargs(invoke_kwargs)
+        if operation:
+            return operation(**invoke_kwargs, model=self.model)
+        else:
+            return self._default_operation(**invoke_kwargs, model=self.model)
+
+    async def async_custom_invoke(
+        self,
+        operation: Optional[Callable[..., Awaitable[T]]] = None,
+        **invoke_kwargs,
+    ) -> Optional[T]:
+        """
+        OpenAI-specific implementation of `ModelProvider.async_custom_invoke`.
+
+        Invokes an OpenAI model operation using the async client. For full details, see
+        `ModelProvider.async_custom_invoke`.
+
+        Example:
+            ```python
+            result = openai_model_provider.invoke(
+                openai_model_provider.async_client.images.generate,
+                prompt="A futuristic cityscape at sunset",
+                n=1,
+                size="1024x1024",
+            )
+            ```
+        :param operation: An async callable representing the model operation (e.g., an async_client method).
+        :param invoke_kwargs: Keyword arguments to pass to the operation.
+        :return: The full response returned by the awaited operation.
+
+        """
+        invoke_kwargs = self.get_invoke_kwargs(invoke_kwargs)
+        if operation:
+            return await operation(**invoke_kwargs, model=self.model)
+        else:
+            return await self._default_async_operation(
+                **invoke_kwargs, model=self.model
+            )
+
+    def invoke(
+        self,
+        messages: Optional[list[dict]] = None,
+        as_str: bool = False,
+        **invoke_kwargs,
+    ) -> Optional[Union[str, T]]:
+        invoke_kwargs = self.get_invoke_kwargs(invoke_kwargs)
+        response = self._default_operation(
+            model=self.endpoint, messages=messages, **invoke_kwargs
+        )
+        if as_str:
+            return response.choices[0].message.content
+        return response
+
+    async def async_invoke(
+        self,
+        messages: Optional[list[dict]] = None,
+        as_str: bool = False,
+        **invoke_kwargs,
+    ) -> str:
+        invoke_kwargs = self.get_invoke_kwargs(invoke_kwargs)
+        response = await self._default_async_operation(
+            model=self.endpoint, messages=messages, **invoke_kwargs
+        )
+        if as_str:
+            return response.choices[0].message.content
+        return response

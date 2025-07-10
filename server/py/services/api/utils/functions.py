75fd6ba638c4a4473bfcffc29020ccb1c5eed6ad
Gal Topper <gal.topper@gmail.com>
2025-07-10 14:57:40 +0700

    Partial fix

--- server/py/services/api/utils/functions.py
+++ server/py/services/api/utils/functions.py
@@ -16,6 +16,8 @@ import os
 import traceback
 from http import HTTPStatus
 
+import fastapi
+
 import mlrun.common.schemas
 from mlrun.errors import err_to_str
 from mlrun.run import new_function
@@ -31,6 +33,7 @@ from services.api.utils.builder import build_runtime
 def build_function(
     db_session,
     auth_info: mlrun.common.schemas.AuthInfo,
+    background_tasks: fastapi.BackgroundTasks,
     function,
     with_mlrun=True,
     skip_deployed=False,

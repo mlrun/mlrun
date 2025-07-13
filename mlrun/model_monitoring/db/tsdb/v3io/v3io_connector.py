bcec2c50a0a2e1c127ed530507124482fb6f328d
danielperezz <daniel06715@gmail.com>
2025-07-13 16:01:16 +0300

    remove redundant post-query rounding

--- mlrun/model_monitoring/db/tsdb/v3io/v3io_connector.py
+++ mlrun/model_monitoring/db/tsdb/v3io/v3io_connector.py
@@ -1306,5 +1306,4 @@ class V3IOTSDBConnector(TSDBConnector):
         if df.empty:
             return mm_schemas.ModelEndpointDriftValues(values=[])
         df = df[df[f"max({mm_schemas.ResultData.RESULT_STATUS})"] >= 1]
-        df["_wstart"] = df.index.floor("1h")
         return self._df_to_drift_data(df)

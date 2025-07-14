0d14c8f7bac424cd594428b303e6d5e464a3a416
danielperezz <daniel06715@gmail.com>
2025-07-14 10:27:57 +0300

    fix the set creation

--- server/py/services/api/crud/model_monitoring/model_endpoints.py
+++ server/py/services/api/crud/model_monitoring/model_endpoints.py
@@ -1207,7 +1207,8 @@ class ModelEndpoints:
                 df=df, type=type, project=project
             )
         elif metrics_format == mm_constants.GetEventsFormat.INTERSECTION:
-            if set(df["endpoint_id"].unique().tolist()) != set(endpoint_id):
+            endpoint_id_set = set(endpoint_id) if isinstance(endpoint_id, list) else {endpoint_id}
+            if set(df["endpoint_id"].unique().tolist()) != endpoint_id_set:
                 logger.info("some endpoints does not have metrics, intersection is empty")
                 return { mlrun.common.schemas.model_monitoring.INTERSECT_DICT_KEYS[type]: []}
             return tsdb_connector.df_to_events_intersection_dict(

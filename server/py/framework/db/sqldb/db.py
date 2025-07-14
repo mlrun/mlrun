47ffec3ce952419c71b471492a8fec381c9b37ab
David_Schneider <davesh0812@gmail.com>
2025-07-14 13:25:30 +0300

    lint

--- server/py/framework/db/sqldb/db.py
+++ server/py/framework/db/sqldb/db.py
@@ -1456,7 +1456,7 @@ class SQLDB(DBInterface):
             session=session,
             main_table=ArtifactV2,
             project=project,
-            additional_filter=(ArtifactV2.parent is not None),
+            additional_filter=(ArtifactV2.paren != None),  # noqa: E711
         )
         self._delete_multi_objects(
             session=session,

03bbfae2c75045f24b87ed6f53591ca47ccd5544
royischoss <royishoss@gmail.com>
2025-07-20 10:21:35 +0300

    small fix

--- mlrun/serving/states.py
+++ mlrun/serving/states.py
@@ -2385,7 +2385,12 @@ class FlowStep(BaseStep):
         if not step.before and not any(
             [step.name in other_step.after for other_step in self._steps.values()]
         ):
-            if any([step_in_graph.responder for step_in_graph in self._steps.values()]):
+            if any(
+                [
+                    getattr(step_in_graph, "responder", False)
+                    for step_in_graph in self._steps.values()
+                ]
+            ):
                 step.responder = True
             return
 

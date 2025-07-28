ee1f51318d81c845f424fe0ff56eebb0eeca1a14
adamdelman <flyn@flyn.cc>
2025-07-28 10:01:05 +0300

    fix

--- server/py/services/api/tests/conftest.py
+++ server/py/services/api/tests/conftest.py
@@ -11,4 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import tests.conftest  # noqa: F401
+
+pytest_plugins = [
+    "tests.common_fixtures",
+    "tests.conftest",
+]

7cc6229ed8b7acc80845748d6113a872bcd66a27
rokatyy <ekaterina_molchanova@mckinsey.com>
2025-07-29 15:56:40 +0100

    [Application] Set multiple ports to runtime spec

--- mlrun/utils/helpers.py
+++ mlrun/utils/helpers.py
@@ -2422,3 +2422,7 @@ def get_data_from_path(
     if isinstance(output_data, (int, float)):
         output_data = [output_data]
     return output_data
+
+def is_valid_port(port):
+    if port < 0 or port > 65535:
+        raise ValueError("Port must be in the range 0-65535")

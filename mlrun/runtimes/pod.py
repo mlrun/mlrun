5949261c21c783062a169203dbb57aaba3963d40
elbamit <elbamit@hotmail.com>
2025-08-18 15:40:11 +0300

    (BONUS) - Added foo as part of the KubeResourceSpec dict fields so that it can be added to a function from a runtime dict

--- mlrun/runtimes/pod.py
+++ mlrun/runtimes/pod.py
@@ -107,6 +107,7 @@ class KubeResourceSpec(FunctionSpec):
         "track_models",
         "parameters",
         "graph",
+        "foo",
     ]
     _default_fields_to_strip = FunctionSpec._default_fields_to_strip + [
         "volumes",

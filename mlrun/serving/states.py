6d3266f986b0bc70152c3b6d114f1644fda1ff0c
royischoss <royishoss@gmail.com>
2025-08-20 15:12:26 +0300

    small fix function plot

--- mlrun/serving/states.py
+++ mlrun/serving/states.py
@@ -3002,7 +3002,7 @@ def _add_graphviz_model_runner(graph, step, source=None):
         graph.node("_start", source.name, shape=source.shape, style="filled")
         graph.edge("_start", step.fullname)
 
-    is_monitored = step._extract_root_step().track_models
+    is_monitored = graph.track_models if isinstance(graph, RootFlowStep) else False
     m_cell = '<FONT POINT-SIZE="9">🄼</FONT>' if is_monitored else ""
 
     number_of_models = len(

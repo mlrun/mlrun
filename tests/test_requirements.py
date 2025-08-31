a4037200b4f19abc083338ccff6c6d13b1a24c60
adamdelman <flyn@flyn.cc>
2025-08-31 19:05:28 +0300

    fix

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -442,6 +442,9 @@ def test_scikit_learn_requirements_are_aligned() -> None:
         "dockerfiles/test/locked-requirements.txt",  # lock file
         "dockerfiles/test-system/locked-requirements.txt",  # lock file
         "dockerfiles/mlrun-kfp/locked-requirements.txt",  # lock file
+        "dockerfiles/mlrun-kfp/locked-requirements_3.9.txt",  # lock file
+        "dockerfiles/mlrun-kfp/locked-requirements_3.11.txt",  # lock file
+
     ]
     pathspec = [f":!{file}" for file in ignored_files]
 

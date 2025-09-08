f928c50facb395a1e1eff4a9ce4bd237eac1c08b
elbamit <elbamit@hotmail.com>
2025-09-08 16:37:35 +0300

    Raise an error when no project or active project is set

--- mlrun/run.py
+++ mlrun/run.py
@@ -222,7 +222,7 @@ def get_or_create_ctx(
     :param spec:     dictionary holding run spec
     :param with_env: look for context in environment vars, default True
     :param rundb:    path/url to the metadata and artifact database
-    :param project:  project to initiate the context in (by default `mlrun.mlconf.active_project`)
+    :param project:  project to initiate the context in (by default `mlrun.mlconf.active_project`). If not set, an active project must exist.
     :param upload_artifacts:  when using local context (not as part of a job/run), upload artifacts to the
                               system default artifact path location
     :return: execution context
@@ -276,6 +276,9 @@ def get_or_create_ctx(
 
     if newspec and not isinstance(newspec, dict):
         newspec = json.loads(newspec)
+
+    if not newspec.get("metadata", {}).get("project") and not project and not mlconf.active_project:
+        raise mlrun.errors.MLRunMissingProjectError("No 'project' parameter specified, and no active project available.")
 
     if not newspec:
         newspec = {}

9747631bdd4417545c46a166d0096330f420d9df
elbamit <elbamit@hotmail.com>
2025-09-10 14:53:05 +0300

    Test error message based on project/active project.

--- tests/test_execution.py
+++ tests/test_execution.py
@@ -285,6 +285,51 @@ def test_set_state_pending_retry(rundb_mock, max_retries, retry_count, expected_
     assert context._state == expected_state, "task state was not set correctly"
 
 
+@pytest.mark.parametrize(
+    "active_project, project_name, should_raise, expected_project",
+    [
+        (
+            "",
+            None,
+            True,
+            None,
+        ),  # No project and no active project -> should raise error
+        (
+            "myproject",
+            None,
+            False,
+            "myproject",
+        ),  # active_project is set -> should not raise error
+        (
+            "",
+            "otherproject",
+            False,
+            "otherproject",
+        ),  # project argument is set -> should not raise error
+        (
+            "myproject",
+            "otherproject",
+            False,
+            "otherproject",
+        ),  # active_project and project argument are set -> should not raise error and use project argument
+    ],
+)
+def test_get_or_create_ctx_project_handling(
+    monkeypatch, active_project, project_name, should_raise, expected_project
+):
+    monkeypatch.setattr(mlrun.mlconf, "active_project", active_project)
+    if should_raise:
+        with pytest.raises(mlrun.errors.MLRunMissingProjectError):
+            mlrun.get_or_create_ctx("test")
+    else:
+        if project_name:
+            ctx = mlrun.get_or_create_ctx("test", project=project_name)
+            assert ctx._project == expected_project
+        else:
+            ctx = mlrun.get_or_create_ctx("test")
+            assert ctx._project == expected_project
+
+
 def _generate_run_dict():
     return {
         "metadata": {

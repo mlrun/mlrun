7286179ac5b3481cb23be263322466c196c39abc
Eyal-Danieli <eyald@iguazio.com>
2025-10-05 17:59:50 +0300

    add test passing a list

--- tests/system/model_monitoring/test_model_monitoring.py
+++ tests/system/model_monitoring/test_model_monitoring.py
@@ -311,6 +311,11 @@ class TestModelEndpointsOperations(TestMLRunSystemModelMonitoring):
         ).endpoints
         assert len(batch_eps) == number_of_batch_eps
 
+
+        real_time_and_batch = self.project.list_model_endpoints(
+            modes=[EndpointMode.REAL_TIME.value, EndpointMode.BATCH.value]).endpoints
+        assert len(real_time_and_batch) == number_of_real_time_eps + number_of_batch_eps
+
     def test_labels(self):
         db = mlrun.get_run_db()
         endpoint_name = "testing-endpoint"

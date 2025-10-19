5af95128cdfe901fd55bdb023db129f9263ccb0d
Alex Toker <alex_toker@mckinsey.com>
2025-10-19 07:51:27 +0000

    Test

--- mlrun/model_monitoring/db/tsdb/timescaledb/timescaledb_stream.py
+++ mlrun/model_monitoring/db/tsdb/timescaledb/timescaledb_stream.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 import mlrun
 import mlrun.common.schemas.model_monitoring as mm_schemas
 import mlrun.model_monitoring.db.tsdb.timescaledb.timescaledb_schema as timescaledb_schema

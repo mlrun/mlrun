02beeb1ff355cf3e4e5e66e1136a527be587dfd5
danielperezz <daniel06715@gmail.com>
2025-10-27 19:54:10 +0200

    lint

--- docs/tutorials/src/my_model.py
+++ docs/tutorials/src/my_model.py
@@ -1,18 +1,23 @@
-
-from mlrun.serving import Model
-from cloudpickle import load
 import numpy as np
 import pandas as pd
+from cloudpickle import load
+
+from mlrun.serving import Model
+
 
 class MyModel(Model):
-    def __init__(self, *args, artifact_uri: str = None, raise_exception = False, **kwargs):
-        super().__init__(*args, artifact_uri=artifact_uri, raise_exception=raise_exception, **kwargs)
-    
+    def __init__(
+        self, *args, artifact_uri: str = None, raise_exception=False, **kwargs
+    ):
+        super().__init__(
+            *args, artifact_uri=artifact_uri, raise_exception=raise_exception, **kwargs
+        )
+
     def load(self):
         """load and initialize the model and/or other elements"""
         model_file, _ = self.get_local_model_path(".pkl")
         self.model = load(open(model_file, "rb"))
-    
+
     def predict(self, data: dict, **kwargs) -> dict:
         """Generate model predictions from sample."""
         df = pd.DataFrame([data])

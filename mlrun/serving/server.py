48ab9a61ad6d18c9527fa46d387306a5b3ab070a
Daniel Perez <100069700+danielperezz@users.noreply.github.com>
2025-10-27 18:42:54 +0200

    remove redundant "/" from the check
    Co-authored-by: Saar Cohen <66667568+theSaarco@users.noreply.github.com>

--- mlrun/serving/server.py
+++ mlrun/serving/server.py
@@ -773,7 +773,7 @@ async def async_execute_graph(
     num_of_meps_in_the_graph = len(server.graph.model_endpoints_names)
     artifact_path = None
     if (
-        "/{{run.uid}}" not in context.artifact_path
+        "{{run.uid}}" not in context.artifact_path
     ):  # TODO: delete when IG-22841 is resolved
         artifact_path = "+/{{run.uid}}"  # will be concatenated to the context's path in extend_artifact_path
     if num_of_meps_in_the_graph <= 1:

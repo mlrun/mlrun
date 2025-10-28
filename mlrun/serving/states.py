2d8a2eef9d964a142558d81c89f77bce4f83b99a
jillnogold <88145832+jillnogold@users.noreply.github.com>
2025-10-28 17:49:11 -0400

    Update mlrun/serving/states.py
    Co-authored-by: tomer-mamia <125267619+tomerm-iguazio@users.noreply.github.com>

--- mlrun/serving/states.py
+++ mlrun/serving/states.py
@@ -2881,7 +2881,7 @@ class RootFlowStep(FlowStep):
                 The runnable is expected to be pre-initialized and reused across events, enabling efficient use of
                 memory and hardware accelerators.
 
-            * **naive**: To run in the main event loop. This is appropriate only for trivial computation and/or fileß
+            * **naive**: To run in the main event loop. This is appropriate only for trivial computation and/or file
                 I/O. It means that the runnable will not actually be run in parallel to anything else.
 
             :param model_artifact:      model artifact or mlrun model artifact uri

901b20c5589a103a4defb51a46b2bbab43740dee
royischoss <royishoss@gmail.com>
2025-11-03 20:31:56 +0200

    docs fix 4

--- mlrun/serving/states.py
+++ mlrun/serving/states.py
@@ -2113,9 +2113,10 @@ class ModelRunnerStep(MonitoredStep):
         """
         Configure the resource limits for the shared models in the graph.
         :param max_processes: Maximum number of processes to spawn (excluding dedicated processes).
-                             Defaults to the number of CPUs or 16 if undetectable.
+                              defaults to the number of CPUs or 16 if undetectable.
         :param max_threads: Maximum number of threads to spawn. Defaults to 32.
         :param pool_factor: Multiplier to scale the number of process/thread workers per runnable. Defaults to 1.
+
         """
         self.max_processes = max_processes
         self.max_threads = max_threads

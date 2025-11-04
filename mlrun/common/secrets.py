dce573fd4049c90a3fe7ab8c173fe8b19df98718
Yaelg <yaelg@iguazio.com>
2025-11-04 13:39:36 +0200

    lint

--- mlrun/common/secrets.py
+++ mlrun/common/secrets.py
@@ -11,8 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import typing
 import re
+import typing
 from abc import ABC, abstractmethod
 
 import mlrun.common.schemas

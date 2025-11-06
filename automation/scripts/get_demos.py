97c4311f03c2f50dc13e8bef6c2a0f7c40e90f74
daniels290813 <daniels@iguazio.com>
2025-11-06 10:13:42 +0200

    updating to mlrun/mlrun demos list

--- automation/scripts/get_demos.py
+++ automation/scripts/get_demos.py
@@ -263,7 +263,7 @@ def get_demos(mlrun_version):
     #     with Path(CONFIG_PATH).open("r", encoding="utf-8") as f:
     #         config = json.load(f)
 
-    config_url = "https://raw.githubusercontent.com/mlrun/demos/refs/heads/1.7.x/demos_config.json"
+    config_url = "https://raw.githubusercontent.com/mlrun/mlrun/refs/heads/development/automation/scripts/demos_config.json"
     try:
         response = requests.get(config_url, timeout=10)
         response.raise_for_status()  # raise HTTPError if not 200

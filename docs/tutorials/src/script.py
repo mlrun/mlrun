01c71c982b9a1d66ac16f76d7a5eafa316554cd7
jillnogold <88145832+jillnogold@users.noreply.github.com>
2025-11-17 17:09:55 +0200

    Update docs/tutorials/src/script.py
    Co-authored-by: Amit Elbaz <66309521+elbamit@users.noreply.github.com>

--- docs/tutorials/src/script.py
+++ docs/tutorials/src/script.py
@@ -11,7 +11,7 @@ from mlrun.frameworks.lgbm import apply_mlrun
 
 # [MLRun] Create a project and get MLRun's context:
 project = mlrun.get_or_create_project("my-project")
-mlrun.get_or_create_ctx("my-context", project="my-project")
+context = mlrun.get_or_create_ctx("my-context", project="my-project")
 
 # [MLRun] Reading train data from context instead of local file:
 train_df = context.get_input("train_set", "./train.csv").as_df()

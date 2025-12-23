23109fc3832d733e9ff7b6dbba9a8bf880832909
Omer_Mimon <omermaimon2@gmail.com>
2025-12-23 10:17:44 +0200

    removed mlflow from the complete extra

--- dependencies.py
+++ dependencies.py
@@ -78,6 +78,7 @@ def extra_requirements() -> dict[str, list[str]]:
         ],
         "alibaba-oss": ["ossfs==2025.5.0", "oss2==2.18.4"],
         "tdengine": ["taos-ws-py==0.3.2"],
+        "timescaledb": ["psycopg[binary,pool]~=3.2"],
         "snowflake": ["snowflake-connector-python~=3.7"],
     }
 
@@ -88,6 +89,7 @@ def extra_requirements() -> dict[str, list[str]]:
         {
             "dev-postgres": ["pytest-mock-resources[postgres]~=2.12"],
             "kfp18": ["mlrun_pipelines_kfp_v1_8[kfp]~=0.5.8"],
+            "mlflow": ["mlflow~=3.0.0"],
             # TODO uncomment when KFP 1.8 support is removed
             # "kfp2": ["mlrun_pipelines_kfp_v2[kfp]>=0.5.0 ; python_version >= '3.11'"],
             "api": api_deps,

04870875394c522097ee2b883f6c87825bd1f3b6
TomerShor <tomer.shor@gmail.com>
2026-02-01 20:44:53 +0200

    Liran CR

--- dependencies.py
+++ dependencies.py
@@ -75,6 +75,7 @@ def extra_requirements() -> dict[str, list[str]]:
         "alibaba-oss": ["ossfs==2025.5.0", "oss2==2.18.4"],
         "timescaledb": ["psycopg[binary,pool]~=3.2"],
         "snowflake": ["snowflake-connector-python~=3.7"],
+        "ig4": ["iguazio~=0.0.1"],
     }
 
     api_deps = list(

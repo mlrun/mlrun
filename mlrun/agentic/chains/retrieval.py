e5271b282552d7440dd35cb69e4d90f11c168e43
tomerbv <tomerbv@gmail.com>
2026-02-05 16:04:15 +0200

    fix import

--- mlrun/agentic/chains/retrieval.py
+++ mlrun/agentic/chains/retrieval.py
@@ -15,7 +15,7 @@
 from typing import Dict, List, Optional
 
 from langchain_core.callbacks import BaseCallbackHandler
-from langchain.chains import RetrievalQAWithSourcesChain
+from langchain_community.chains import RetrievalQAWithSourcesChain
 from langchain_core.prompts import PromptTemplate
 from langchain_core.documents import Document
 

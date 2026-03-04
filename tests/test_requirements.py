06f5d1a0e9f5255c83542b81009a0eebff768f34
Tomer_Mamia <tomerm@iguazio.com>
2026-03-04 13:06:34 +0200

    bump storey to 1.11.20

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -128,7 +128,7 @@ def test_requirement_specifiers_convention():
 
     ignored_invalid_map = {
         # See comment near requirement for why we're limiting to patch changes only for all of these
-        "storey": {"~=1.11.18"},
+        "storey": {"~=1.11.20"},
         "pydantic": {">=1.10.15", ">=1,<2"},
         "nuclio-sdk": {">=0.5"},
         "scipy": {"~=1.16.3"},

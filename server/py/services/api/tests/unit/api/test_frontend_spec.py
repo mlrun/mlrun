3b050138c919dff419df090fdad4a467f77bbfa8
Emilio Nunes <emilio_cafe_nunes@external.mckinsey.com>
2026-04-23 11:35:59 +0200

    added unit test for preemptible_nodes enabled conditions

--- server/py/services/api/tests/unit/api/test_frontend_spec.py
+++ server/py/services/api/tests/unit/api/test_frontend_spec.py
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import base64
 import http
+import json
 import unittest.mock
 
 import deepdiff
@@ -215,6 +217,48 @@ def test_get_frontend_spec_nuclio_streams(
         assert response.status_code == http.HTTPStatus.OK.value
         assert frontend_spec.feature_flags.nuclio_streams == test_case.get(
             "expected_feature_flag"
+        )
+
+
+def test_get_frontend_spec_preemption_nodes(
+    db: sqlalchemy.orm.Session, client: fastapi.testclient.TestClient
+) -> None:
+    empty_node_selector = base64.b64encode(json.dumps({}).encode("utf-8"))
+    empty_tolerations = base64.b64encode(json.dumps([]).encode("utf-8"))
+    non_empty_node_selector = base64.b64encode(
+        json.dumps({"node-lifecycle": "preemptible"}).encode("utf-8")
+    )
+    non_empty_tolerations = base64.b64encode(
+        json.dumps(
+            [{"key": "preemptible", "operator": "Exists", "effect": "NoSchedule"}]
+        ).encode("utf-8")
+    )
+    for test_case in [
+        {
+            "node_selector": empty_node_selector,
+            "tolerations": empty_tolerations,
+            "expected_feature_flag": mlrun.common.schemas.PreemptionNodesFeatureFlag.disabled,
+        },
+        {
+            "node_selector": non_empty_node_selector,
+            "tolerations": empty_tolerations,
+            "expected_feature_flag": mlrun.common.schemas.PreemptionNodesFeatureFlag.enabled,
+        },
+        {
+            "node_selector": empty_node_selector,
+            "tolerations": non_empty_tolerations,
+            "expected_feature_flag": mlrun.common.schemas.PreemptionNodesFeatureFlag.enabled,
+        },
+    ]:
+        mlrun.mlconf.preemptible_nodes.node_selector = test_case["node_selector"]
+        mlrun.mlconf.preemptible_nodes.tolerations = test_case["tolerations"]
+
+        response = client.get("frontend-spec")
+        assert response.status_code == http.HTTPStatus.OK.value
+        frontend_spec = mlrun.common.schemas.FrontendSpec(**response.json())
+        assert (
+            frontend_spec.feature_flags.preemption_nodes
+            == test_case["expected_feature_flag"]
         )
 
 

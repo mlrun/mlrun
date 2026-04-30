597da51ddd9fdaed8e56770999e5a4b2a661014b
Yaelg <yaelg@iguazio.com>
2026-04-30 17:20:43 +0300

    fix

--- server/py/services/api/migrations/versions/8712e85b0281_index_runs_project_iteration_start_time.py
+++ server/py/services/api/migrations/versions/8712e85b0281_index_runs_project_iteration_start_time.py
@@ -37,7 +37,7 @@ def upgrade():
     inspector = sa.inspect(op.get_bind())
     existing = {idx["name"] for idx in inspector.get_indexes(_table_name)}
     if _index_name in existing:
-        op.drop_index(_index_name, table_name=_table_name)
+        return
     op.create_index(
         _index_name,
         _table_name,

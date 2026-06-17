e24bf115f4db02da696844ce134b874e31f8053b
Maitray Mukeshkumar Modi <maitraymukeshkumarmodi@gmail.com>
2026-06-16 21:13:51 -0700

    WIP: repro attempt for #7191 S3 ListObjectsV2 issue

--- test_repro.py
+++ test_repro.py
@@ -6,7 +6,6 @@ os.environ["S3_ENDPOINT_URL"] = "http://localhost:9000"
 import pandas as pd
 import mlrun
 
-# write a small dataframe up to MinIO as a csv, mimicking the tutorial's artifact
 df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
 target_path = "s3://mlrun/test-dataset.csv"
 df.to_csv(target_path, storage_options={
@@ -16,7 +15,6 @@ df.to_csv(target_path, storage_options={
 })
 print("Uploaded.")
 
-# now try to read it back the way MLRun's packager would
 item = mlrun.get_dataitem(target_path)
 result_df = item.as_df()
 print(result_df)
